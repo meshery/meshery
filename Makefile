beaacaeaf32112ea50374c59f52197898eda0eee
Girish Ranganathan <girish.rranganathan@gmail.com>
2019-01-21 16:07:51 -0500

    - created an interface for meshes to implement. - organized templates in a separate folder - change istio playground to meshery
    Signed-off: raghavaggarwal99<ragha170103048@iitg.ac.in>

--- Makefile
+++ Makefile
@@ -1,5 +1,15 @@
+fortio_port := 9080
+
 fortio:
-	docker run -p $(fortio_port):8080 -p 8079:8079 -d fortio/fortio server
+	docker run --name fortio -p $(fortio_port):8080 -p 8079:8079 -d fortio/fortio server
 
 docker:
-	docker build -t layer5/meshery-istio .
+	docker build -t layer5/meshery .
+
+docker-run:
+	docker run -d \
+	-e TWITTER_APP_HOST="http://auth.layer5.ga" \
+	-e EVENT=istioPlay01 \
+	-e FORTIO_URL="http://localhost:$(fortio_port)/fortio/" \
+	-e PRODUCT_PAGE_URL="http://localhost:8080/" \
+	layer5/meshery
---
+++ handlers/vars.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"encoding/base64"
+	"html/template"
+	"os"
+
+	"github.com/dghubble/sessions"
+	"github.com/layer5io/meshery/meshes"
+)
+
+const (
+	sessionUserKey       = "twitterID"
+	sessionUserName      = "twitterUserName"
+	sessionTwitterToken  = "token"
+	sessionTwitterSecret = "secret"
+	cookieSuffix         = "_referrer"
+)
+
+var (
+	sessionName   = os.Getenv("EVENT") + "_pl"
+	sessionSecret = base64.StdEncoding.EncodeToString([]byte(sessionName))
+	sessionStore  = sessions.NewCookieStore([]byte(sessionSecret), nil)
+	cookieName    = os.Getenv("EVENT") + cookieSuffix + "_pl"
+
+	dashTempl       = template.Must(template.ParseFiles("../public/dashboard.html"))
+	getAOTokenTempl = template.Must(template.ParseFiles("../public/get-ao-token.html"))
+)
+
+type ServerConfig struct {
+	MeshClient meshes.MeshClient
+
+	CallbackURL string
+}
