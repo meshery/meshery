beed73bf51d556ba1233fd2361e240d5d3f9ca88
Girish Ranganathan <girish.rranganathan@gmail.com>
2019-01-30 14:37:15 -0500

    adding favicon

--- handlers/server.go
+++ handlers/server.go
@@ -8,6 +8,9 @@ import (
 // New returns a new ServeMux with app routes.
 func New(ctx context.Context, config *ServerConfig) *http.ServeMux {
 	mux := http.NewServeMux()
+	mux.Handle("/favicon.ico", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "../public/static/img/meshery-logo.png")
+	}))
 	mux.Handle("/play/static/", http.StripPrefix("/play/static/", http.FileServer(http.Dir("../public/static/"))))
 	mux.Handle("/play/dashboard", authMiddleware(http.HandlerFunc(dashboardHandler(ctx, config.MeshClient))))
 	mux.HandleFunc("/play/", indexHandler)
