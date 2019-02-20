f71e56a051fb942dd9464c80b0bf1f8a098de5b2
Girish Ranganathan <girish.rranganathan@gmail.com>
2019-02-20 15:18:46 -0500

    initial commit with next js and the new theme
    Signed-off: raghavaggarwal99<ragha170103048@iitg.ac.in>

---
+++ ui/pages/post.js
@@ -0,0 +1,22 @@
+import Layout from '../components/layout.js'
+import fetch from 'isomorphic-unfetch'
+
+const Post = (props) => (
+    <Layout>
+       <h1>{props.show.name}</h1>
+       <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+       <img src={props.show.image.medium}/>
+    </Layout>
+)
+
+Post.getInitialProps = async function (context) {
+  const { id } = context.query
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+  const show = await res.json()
+
+  console.log(`Fetched show: ${show.name}`)
+
+  return { show }
+}
+
+export default Post
