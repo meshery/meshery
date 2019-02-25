ad8828a8770b0f73ffb3888de12f465f151d34d0
Girish Ranganathan <girish.rranganathan@gmail.com>
2019-02-25 08:51:10 -0500

    handling redirection based on environment
    Signed-off: raghavaggarwal99<ragha170103048@iitg.ac.in>

--- ui/lib/data-fetch.js
+++ ui/lib/data-fetch.js
@@ -4,8 +4,11 @@ const dataFetch = (url, options = {}, successFn, errorFn) => {
   fetch(url, options)
     .then(res => {
       if (res.status === 401 || res.redirected){
-        window.location = "/login"; // for local dev thru node server
-        // window.location.reload(); // for use with Go server
+        if (window.location.host.endsWith('3000')){
+          window.location = "/login"; // for local dev thru node server
+        } else {
+          window.location.reload(); // for use with Go server
+        }
       }
       if (res.ok) {
         return res.json();
