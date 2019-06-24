f3edcfab5c8b68794594ea4c622f3340a982d8b4
Girish Ranganathan <girish.rranganathan@gmail.com>
2019-06-24 14:11:38 -0400

    trying a different theme
    Signed-off: raghavaggarwal99<ragha170103048@iitg.ac.in>

--- ui/pages/_app.js
+++ ui/pages/_app.js
@@ -25,7 +25,7 @@ import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 import 'codemirror/addon/lint/lint.css';
 
-import 'billboard.js/dist/billboard.min.css';
+import 'billboard.js/dist/theme/insight.min.css';
 import { blueGrey, grey } from '@material-ui/core/colors';
 import MesheryProgressBar from '../components/MesheryProgressBar';
 
