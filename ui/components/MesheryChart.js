325d7cc0787a2f9f0d6565ee23655a71042d3e13
Girish Ranganathan <girish.rranganathan@gmail.com>
2019-06-24 22:47:58 -0400

    handling the empty chart case
    Signed-off: raghavaggarwal99<ragha170103048@iitg.ac.in>

--- ui/components/MesheryChart.js
+++ ui/components/MesheryChart.js
@@ -25,190 +25,199 @@ class MesheryChart extends React.Component {
 
   singleChart = (data) => {
       if (typeof data === 'undefined' || typeof data.StartTime === 'undefined'){
-          data = {
-            DurationHistogram: {
-                Count: 0,
-                Data: [
-                    {
-                        Start: new Date(),
-                    }
-                ],
-                Max: 0,
-                Min: 0,
-                Avg: 0,
-                Percentiles: []
-            },
-            StartTime: new Date(),
-            URL: '',
-            Labels: '',
-            RetCodes: {}
-        };
+        return {};
+        //   data = {
+        //     DurationHistogram: {
+        //         Count: 0,
+        //         Data: [
+        //             {
+        //                 Start: new Date(),
+        //             }
+        //         ],
+        //         Max: 0,
+        //         Min: 0,
+        //         Avg: 0,
+        //         Percentiles: []
+        //     },
+        //     StartTime: new Date(),
+        //     URL: '',
+        //     Labels: '',
+        //     RetCodes: {}
+        // };
       }
       return makeChart(fortioResultToJsChartData(data));
   }
 
   processChartData(chartData) {
     const self = this;
-    if(chartData && chartData.data && chartData.options){
-      const xAxes = [];
-      const yAxes = [];
-      const colors = [];
-      const types = {};
-      const axes = {};
-      const axis = {};
-      const yAxisTracker = {};
-      const xAxisTracker = {};
+    if(self.chartRef && self.chartRef !== null) {
+      if(chartData && chartData.data && chartData.options){
+        const xAxes = [];
+        const yAxes = [];
+        const colors = [];
+        const types = {};
+        const axes = {};
+        const axis = {};
+        const yAxisTracker = {};
+        const xAxisTracker = {};
 
-      if(chartData.data && chartData.data.datasets){
-        chartData.data.datasets.forEach((ds, ind) => {
-          // xAxis.push('x');
-          const yAxis = [ds.label];
-          const xAxis = [`x${ind+1}`];
-          xAxisTracker[ds.label] = `x${ind+1}`;
-          yAxisTracker[ds.yAxisID] = `y${ind>0?ind+1:''}`;
-          axes[ds.label] = `y${ind>0?ind+1:''}`;
+        if(chartData.data && chartData.data.datasets){
+          chartData.data.datasets.forEach((ds, ind) => {
+            // xAxis.push('x');
+            const yAxis = [ds.label];
+            const xAxis = [`x${ind+1}`];
+            xAxisTracker[ds.label] = `x${ind+1}`;
+            yAxisTracker[ds.yAxisID] = `y${ind>0?ind+1:''}`;
+            axes[ds.label] = `y${ind>0?ind+1:''}`;
 
-          ds.data.forEach((d) => {
-            // if(ind === 0){
-              xAxis.push(d.x);
-            // }
-            yAxis.push(d.y);
-          });
-          yAxes.push(yAxis);
-          xAxes.push(xAxis);
-          if(ds.cubicInterpolationMode) {
-            // types[ds.label] = "spline";
-          } else {
-            types[ds.label] = "area-step";
-          }
-          colors[ds.label] = ds.borderColor; // not sure which is better border or background
-        });
-      }
-
-      if(chartData.options.scales.xAxes){
-        chartData.options.scales.xAxes.forEach((ya) => {
-          axis['x'] = {
-            show: true,
-            label: {
-              text: ya.scaleLabel.labelString,
-              position: 'outer-middle',
-            },
-          };
-        });
-      }
-      if(chartData.options.scales.yAxes){
-        chartData.options.scales.yAxes.forEach(ya => {
-          axis[yAxisTracker[ya.id]] = {
-            show: true,
-            label: {
-              text: ya.scaleLabel.labelString,
-              position: 'outer-middle',
-            },
-          }
-        });
-      }
-
-      // chartConfig.data.colors[chartConfig.data.columns[1][0]] = panel.sparkline.lineColor;
-      // chartConfig.color = {
-        //     pattern: [
-        //       panel.sparkline.fillColor,
-        //     ]
-        //   };
-
-      if (self.chartRef && self.chartRef !== null){
-        const chartConfig = {
-          // oninit: function(args){
-          //   console.log(JSON.stringify(args));
-          // },
-          // title: {
-          //   text: chartData.options.title.text.join('\n'),
-          // },
-          bindto: self.chartRef,
-          data: {
-            // x: 'x',
-            xs: xAxisTracker,
-            // xFormat: self.bbTimeFormat,
-            columns: [...xAxes, ...yAxes],
-            colors,
-            axes,
-            types,
-            // groups,
-            // type: 'area',
-          },
-          axis,
-          // zoom: {
-          //   enabled: {
-          //     type: "drag"
-          //   },
-          //   onzoomend: self.updateDateRange(),
-          // },
-          // grid: grid,
-          legend: {
-            show: true,
-          },
-          // point: {
-          //   show: false
-          // },
-          point: {
-            r: 0,
-            focus: {
-              expand: {
-                r: 5
-              }
+            ds.data.forEach((d) => {
+              // if(ind === 0){
+                xAxis.push(d.x);
+              // }
+              yAxis.push(d.y);
+            });
+            yAxes.push(yAxis);
+            xAxes.push(xAxis);
+            if(ds.cubicInterpolationMode) {
+              // types[ds.label] = "spline";
+            } else {
+              types[ds.label] = "area-step";
             }
+            colors[ds.label] = ds.borderColor; // not sure which is better border or background
+          });
+        }
+
+        if(chartData.options.scales.xAxes){
+          chartData.options.scales.xAxes.forEach((ya) => {
+            axis['x'] = {
+              show: true,
+              label: {
+                text: ya.scaleLabel.labelString,
+                position: 'outer-middle',
+              },
+            };
+          });
+        }
+        if(chartData.options.scales.yAxes){
+          chartData.options.scales.yAxes.forEach(ya => {
+            axis[yAxisTracker[ya.id]] = {
+              show: true,
+              label: {
+                text: ya.scaleLabel.labelString,
+                position: 'outer-middle',
+              },
+            }
+          });
+        }
+
+        // chartConfig.data.colors[chartConfig.data.columns[1][0]] = panel.sparkline.lineColor;
+        // chartConfig.color = {
+          //     pattern: [
+          //       panel.sparkline.fillColor,
+          //     ]
+          //   };
+
+          const chartConfig = {
+            // oninit: function(args){
+            //   console.log(JSON.stringify(args));
+            // },
+            // title: {
+            //   text: chartData.options.title.text.join('\n'),
+            // },
+            bindto: self.chartRef,
+            data: {
+              // x: 'x',
+              xs: xAxisTracker,
+              // xFormat: self.bbTimeFormat,
+              columns: [...xAxes, ...yAxes],
+              colors,
+              axes,
+              types,
+              // groups,
+              // type: 'area',
+            },
+            axis,
+            // zoom: {
+            //   enabled: {
+            //     type: "drag"
+            //   },
+            //   onzoomend: self.updateDateRange(),
+            // },
+            // grid: grid,
+            legend: {
+              show: true,
+            },
+            // point: {
+            //   show: false
+            // },
+            point: {
+              r: 0,
+              focus: {
+                expand: {
+                  r: 5
+                }
+              }
+            },
+            tooltip: {
+              show: true,
+              // linked: !inDialog,
+              // format: {
+              //   title: function(x) {
+              //     // return d3.timeFormat(self.bbTimeFormat)(x);
+              //     return moment(x).format(self.timeFormat);
+              //   }
+              // }
+            },
+            // area: {
+            //   linearGradient: true
+            // },
+          };
+
+          // if(self.panelType === 'sparkline' && panel.sparkline && panel.sparkline.lineColor && panel.sparkline.fillColor){
+          //   // cd.datasets[datasetInd].borderColor = panel.sparkline.lineColor;
+          //   // cd.datasets[datasetInd].backgroundColor = panel.sparkline.fillColor;
+            
+          //   const dataLength = chartConfig.data.columns && chartConfig.data.columns.length > 1? chartConfig.data.columns[1].length:0; // 0 is for x axis
+          //   if(dataLength > 0){
+          //     if(typeof chartConfig.data.colors === 'undefined'){
+          //       chartConfig.data.colors = {};
+          //     }
+          //     chartConfig.data.colors[chartConfig.data.columns[1][0]] = panel.sparkline.lineColor;
+
+          //     if(dataLength > 1){
+          //       let content = '';
+          //       if(panel.format.toLowerCase().startsWith('percent')){
+          //         const mulFactor = panel.format.toLowerCase() === 'percentunit'?100:1;
+          //         if(!isNaN(chartConfig.data.columns[1][dataLength-1])){
+          //           const tk = (chartConfig.data.columns[1][dataLength-1] * mulFactor).toFixed(2);
+          //           content = `${tk}%`;
+          //         }
+          //       } else {
+          //         content = `${chartConfig.data.columns[1][dataLength-1]} ${panel.format}`;
+          //       }
+          //       chartConfig.title = {
+          //         text: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + content, // for sparkline, we want to print the value as title
+          //       };
+          //     }
+          //   }
+            
+          //   chartConfig.color = {
+          //     pattern: [
+          //       panel.sparkline.fillColor,
+          //     ]
+          //   };
+
+          // }
+          self.titleRef.innerText = chartData.options.title.text.join('\n');
+          self.chart = bb.bb.generate(chartConfig);
+      } else {
+        self.chart = bb.bb.generate({
+          data: {
+            columns: [
+            ]
           },
-          tooltip: {
-            show: true,
-            // linked: !inDialog,
-            // format: {
-            //   title: function(x) {
-            //     // return d3.timeFormat(self.bbTimeFormat)(x);
-            //     return moment(x).format(self.timeFormat);
-            //   }
-            // }
-          },
-          // area: {
-          //   linearGradient: true
-          // },
-        };
-
-        // if(self.panelType === 'sparkline' && panel.sparkline && panel.sparkline.lineColor && panel.sparkline.fillColor){
-        //   // cd.datasets[datasetInd].borderColor = panel.sparkline.lineColor;
-        //   // cd.datasets[datasetInd].backgroundColor = panel.sparkline.fillColor;
-          
-        //   const dataLength = chartConfig.data.columns && chartConfig.data.columns.length > 1? chartConfig.data.columns[1].length:0; // 0 is for x axis
-        //   if(dataLength > 0){
-        //     if(typeof chartConfig.data.colors === 'undefined'){
-        //       chartConfig.data.colors = {};
-        //     }
-        //     chartConfig.data.colors[chartConfig.data.columns[1][0]] = panel.sparkline.lineColor;
-
-        //     if(dataLength > 1){
-        //       let content = '';
-        //       if(panel.format.toLowerCase().startsWith('percent')){
-        //         const mulFactor = panel.format.toLowerCase() === 'percentunit'?100:1;
-        //         if(!isNaN(chartConfig.data.columns[1][dataLength-1])){
-        //           const tk = (chartConfig.data.columns[1][dataLength-1] * mulFactor).toFixed(2);
-        //           content = `${tk}%`;
-        //         }
-        //       } else {
-        //         content = `${chartConfig.data.columns[1][dataLength-1]} ${panel.format}`;
-        //       }
-        //       chartConfig.title = {
-        //         text: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + content, // for sparkline, we want to print the value as title
-        //       };
-        //     }
-        //   }
-          
-        //   chartConfig.color = {
-        //     pattern: [
-        //       panel.sparkline.fillColor,
-        //     ]
-        //   };
-
-        // }
-        self.titleRef.innerText = chartData.options.title.text.join('\n');
-        self.chart = bb.bb.generate(chartConfig);
+          bindto: self.chartRef,
+        });
       }
     }
   }
@@ -248,7 +257,7 @@ class MesheryChart extends React.Component {
       }
       if(chartData.data && chartData.data.labels){
         chartData.data.labels.forEach(l => {
-          categories.push(l.join('\n'));
+          categories.push(l.join(' '));
         });
       }
 
@@ -327,11 +336,12 @@ class MesheryChart extends React.Component {
             //   // anchor: 'bottom-left',
             //   // x: 125,
             //   // y: -200,
-            //   anchor: 'top-left',
-            //   x: 125,
-            //   y: -20,
+            //   anchor: 'top-right',
+            //   // x: 125,
+            //   y: -5,
             //   step: 1,
             // },
+            position: 'right',
           },
           // point: {
           //   show: false
