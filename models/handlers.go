099862bdbfa5f4f95735b414dc918ed2d9cd7619
Girish Ranganathan <girish.rranganathan@gmail.com>
2019-07-03 14:27:37 -0400

    WIP commit - working on persisting grafana and prometheus boards on the server

--- models/handlers.go
+++ models/handlers.go
@@ -30,12 +30,14 @@ type HandlerInterface interface {
 	GrafanaBoardsHandler(w http.ResponseWriter, req *http.Request)
 	GrafanaQueryHandler(w http.ResponseWriter, req *http.Request)
 	GrafanaQueryRangeHandler(w http.ResponseWriter, req *http.Request)
+	SaveSelectedGrafanaBoardsHandler(w http.ResponseWriter, req *http.Request)
 
 	PrometheusConfigHandler(w http.ResponseWriter, req *http.Request)
 	GrafanaBoardImportForPrometheusHandler(w http.ResponseWriter, req *http.Request)
 	PrometheusQueryHandler(w http.ResponseWriter, req *http.Request)
 	PrometheusQueryRangeHandler(w http.ResponseWriter, req *http.Request)
 	PrometheusStaticBoardHandler(w http.ResponseWriter, req *http.Request)
+	SaveSelectedPrometheusBoardsHandler(w http.ResponseWriter, req *http.Request)
 
 	SessionSyncHandler(w http.ResponseWriter, req *http.Request)
 }
