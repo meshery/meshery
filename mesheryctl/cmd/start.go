027f187ebb8a7ac3fa9dc0bbe20e261c3fff05d3
Girish Ranganathan <girish.rranganathan@gmail.com>
2019-08-30 11:29:53 -0400

    #91 update mesheryctl for better UX
    Signed-off-by: anandrkskd <anandrkskd@gmail.com>
    
    Signed-off: raghavaggarwal99<ragha170103048@iitg.ac.in>

--- mesheryctl/cmd/start.go
+++ mesheryctl/cmd/start.go
@@ -14,12 +14,14 @@
 package cmd
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -43,6 +45,9 @@ var startCmd = &cobra.Command{
 
 		fmt.Println("Starting Meshery...")
 		start := exec.Command("docker-compose", "-f", dockerComposeFile, "up", "-d")
+		time.Sleep(2 * time.Second)
+		//testing purpose
+		//exec.Command("docker", "kill", "meshery_meshery_1")
 		start.Stdout = &out
 		start.Stderr = &stderr
 
@@ -50,7 +55,58 @@ var startCmd = &cobra.Command{
 			fmt.Println(stderr.String())
 			return
 		}
+		// Name of all running container
+		cmda := exec.Command("docker", "ps", "--format", "{{.Names}}")
+		outa, err := cmda.Output()
+		s := strings.Split(string(outa), "\n")
+		fmt.Println(s)
+		if err != nil {
+			log.Fatalf("cmd.Run() failed with %s\n", err)
+		}
+		check(s, 0)
+	},
+}
 
+func check(s []string, atempt int) {
+	count := 0
+	for _, x := range s {
+		if strings.TrimRight(x, " ") == "meshery_meshery_1" {
+			count++
+		}
+	}
+	if count < 1 && atempt < 1 {
+		//checks for meshery_meshery_1 container running or not if not then restarting meshery
+		fmt.Println("service cannot be started, attempting to restart the service!")
+		exec.Command("mesheryctl", "stop")
+		exec.Command("mesheryctl", "start")
+		atempt++
+		check(s, atempt)
+	} else if atempt >= 1 {
+		//showing logs if restart doesn't work
+		fmt.Println("service cannot be started. Showing meshery logs")
+		//\
+		fmt.Println("Starting Meshery logging . . .")
+		cmd_log := exec.Command("docker-compose", "-f", dockerComposeFile, "logs", "-f")
+		cmdReader, err := cmd_log.StdoutPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+		scanner := bufio.NewScanner(cmdReader)
+		go func() {
+			for scanner.Scan() {
+				fmt.Println(scanner.Text())
+			}
+		}()
+		if err := cmd_log.Start(); err != nil {
+			log.Fatal(err)
+		}
+		if err := cmd_log.Wait(); err != nil {
+			log.Fatal(err)
+		}
+		//
+
+	} else {
+		// If meshery_meshery_1 container runs fine then open web browser
 		fmt.Println("Opening Meshery in your broswer. If Meshery does not open, please point your browser to http://localhost:9081 to access Meshery.")
 		ostype, err := exec.Command("uname", "-s").Output()
 		if err != nil {
@@ -65,12 +121,10 @@ var startCmd = &cobra.Command{
 			// Meshery running on Linux host
 			exec.Command("xdg-open", url).Start()
 		} else {
-
 			// Asssume Meshery running on MacOS host
 			exec.Command("open", url).Start()
 		}
-
-	},
+	}
 }
 
 func init() {
