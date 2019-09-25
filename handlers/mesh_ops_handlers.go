f5b6cd683ae9024ccc787adea65f45ebc3fafc86
Ashis Kumar Singh <49238464+ashiscs@users.noreply.github.com>
2019-09-25 20:55:38 +0530

    updated

--- handlers/mesh_ops_handlers.go
+++ handlers/mesh_ops_handlers.go
@@ -287,7 +287,7 @@ func (h *Handler) MeshOpsHandler(w http.ResponseWriter, req *http.Request) {
 	}
 
 	_, err = mClient.MClient.ApplyOperation(req.Context(), &meshes.ApplyRuleRequest{
-		OperationId: operationId.String(),
+		OperationID: operationId.String(),
 		OpName:      opName,
 		Username:    user.UserID,
 		Namespace:   namespace,
