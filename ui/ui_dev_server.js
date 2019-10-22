a31f59f4fec423e933d9f175ff59281e9d2d5163
Lee Calcote <leecalcote@gmail.com>
2019-10-22 17:38:21 -0500

    snackbar message improvements
    Signed-off-by: Lee Calcote <leecalcote@gmail.com>
    Signed-off-by: ashiscs <ashissingh640@gmail.com>
    
    Signed-off: raghavaggarwal99<ragha170103048@iitg.ac.in>

--- ui/ui_dev_server.js
+++ ui/ui_dev_server.js
@@ -18,7 +18,11 @@ proxy.on('error', function (err, req, res) {
     'Content-Type': 'text/plain'
   });
 
+<<<<<<< HEAD
   res.end('Something went wrong');
+=======
+  res.end('Unexpected issue.');
+>>>>>>> snackbar message improvements
 });
 
 
