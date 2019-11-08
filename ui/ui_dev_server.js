510146445d26344e2145ebaf336db4b88eb7df86
Girish Ranganathan <girish.rranganathan@gmail.com>
2019-11-07 20:24:54 -0500

    fixed a merge issue
    Signed-off-by: Girish Ranganathan <girish.rranganathan@gmail.com>

--- ui/ui_dev_server.js
+++ ui/ui_dev_server.js
@@ -17,16 +17,7 @@ proxy.on('error', function (err, req, res) {
   res.writeHead(500, {
     'Content-Type': 'text/plain'
   });
-
-
-
-  res.end('Something went wrong');
-
   res.end('Unexpected issue.');
-
-
-  res.end('Unexpected issue.');
-
 });
 
 
