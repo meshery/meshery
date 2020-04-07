446bd01e24f5eb3dd6bf3e7ee0f666aa44f8b32a
ashiscs <ashissingh640@gmail.com>
2020-04-07 05:37:56 +0530

    updated
    Signed-off-by: ashiscs <ashissingh640@gmail.com>

--- mesheryctl/cmd/root.go
+++ mesheryctl/cmd/root.go
@@ -72,7 +72,9 @@ var rootCmd = &cobra.Command{
 		if b {
 			versionCmd.Run(nil, nil)
 		}
-		
+		if len(args) == 0 {
+			log.Print(cmdDetails)
+		}
 	},
 }
 
@@ -105,7 +107,6 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	rootCmd.Flags().BoolP("version", "v", false, "Version flag")
-	rootCmd.Flags().BoolP("version", "version", false, "Version flag")
 }
 
 // initConfig reads in config file and ENV variables if set.
