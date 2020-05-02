94a7bbea4d6c7a91cc3030ea9236bcd0ce3d31e2
kushthedude <kushthedude@gmail.com>
2020-05-02 13:59:25 +0530

    feat: server improvements for provider-ui
    Signed-off-by: kushthedude <kushthedude@gmail.com>

--- provider-ui/ui_dev_server.js
+++ provider-ui/ui_dev_server.js
@@ -1,14 +1,11 @@
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
-// const pathMatch = require('path-match')
 
 const port = parseInt(process.env.PORT, 10) || 3001;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-// const route = pathMatch()
-// const match = route('/blog/:id')
 const httpProxy = require('http-proxy');
 
 const proxy = httpProxy.createProxyServer({});
@@ -23,7 +20,10 @@ proxy.on('error', (err, req, res) => {
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const { pathname } = parse(req.url, true);
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
     if (pathname.startsWith('/api') || pathname.startsWith('/logout') || pathname.startsWith('/login')) {
       proxy.web(req, res, { target: 'http://localhost:9081' });
     } else {
@@ -32,14 +32,6 @@ app.prepare().then(() => {
       }
       handle(req, res);
     }
-    // const params = match(pathname)
-    // if (params === false) {
-    //   handle(req, res)
-    //   return
-    // }
-    // // assigning `query` into the params means that we still
-    // // get the query string passed to our application
-    // // i.e. /blog/foo?show-comments=true
     // app.render(req, res, '/blog', Object.assign(params, query))
   }).listen(port, (err) => {
     if (err) throw err;
