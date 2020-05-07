b8dfe0fbc9a9d863a8d4fe3e510f2d79db5d003d
kushthedude <kushthedude@gmail.com>
2020-05-07 09:58:44 +0530

    feat: Addition of performance defaults in settings
    Signed-off-by: kushthedude <kushthedude@gmail.com>

---
+++ ui/components/MesherySettingsPerformanceComponent.js
@@ -0,0 +1,301 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import {
+  NoSsr, Tooltip, IconButton, CircularProgress, FormControl, RadioGroup, FormControlLabel, Radio,
+} from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import { withSnackbar } from 'notistack';
+import { connect } from 'react-redux';
+import CloseIcon from '@material-ui/icons/Close';
+import LoadTestTimerDialog from './load-test-timer-dialog';
+
+
+const loadGenerators = [
+  'fortio',
+  'wrk2',
+];
+
+const styles = (theme) => ({
+  root: {
+    padding: theme.spacing(10),
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  centerTimer: {
+    width: '100%',
+  },
+});
+
+class MesherySettingsPerformanceComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    const {
+    qps, c, t,
+    } = props;
+
+    this.state = {
+      qps,
+      c,
+      t,
+      loadGenerator: 'fortio',
+
+      timerDialogOpen: false,
+      blockRunTest: false,
+      tError: false,
+    };
+  }
+
+  handleChange = (name) => (event) => {
+    if (name === 't' && (event.target.value.toLowerCase().endsWith('h')
+      || event.target.value.toLowerCase().endsWith('m') || event.target.value.toLowerCase().endsWith('s'))) {
+      this.setState({ tError: false });
+    }
+    this.setState({ [name]: event.target.value });
+  };
+
+  handleSubmit = () => {
+    const {
+      t
+    } = this.state;
+
+    let err = false; 
+    let tNum = 0;
+    try {
+      tNum = parseInt(t.substring(0, t.length - 1));
+    } catch (ex) {
+      err = true;
+    }
+
+    if (t === '' || !(t.toLowerCase().endsWith('h')
+      || t.toLowerCase().endsWith('m') || t.toLowerCase().endsWith('s')) || err || tNum <= 0) {
+      this.setState({ tError: true });
+      return;
+    }
+
+    this.submitLoadTest();
+  }
+
+  submitLoadTest = () => {
+    const {
+qps, c, t, loadGenerator,
+    } = this.state;
+
+
+
+    const t1 = t.substring(0, t.length - 1);
+    const dur = t.substring(t.length - 1, t.length).toLowerCase();
+
+    const data = {
+
+      qps,
+      c,
+      t: t1,
+      dur,
+      loadGenerator,
+    };
+    const params = Object.keys(data).map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`).join('&');
+    this.startEventStream(`/api/load-test?${params}`);
+    this.setState({ blockRunTest: true }); // to block the button
+  }
+
+  handleSuccess() {
+    const self = this;
+
+      self.closeEventStream();
+      self.setState({ blockRunTest: false, timerDialogOpen: false });
+    }
+  
+
+  handleEvents() {
+    const self = this;
+    let track = 0;
+    return (e) => {
+      const data = JSON.parse(e.data);
+      switch (data.status) {
+        case 'info':
+          self.props.enqueueSnackbar(data.message, {
+            variant: 'info',
+            autoHideDuration: 1000,
+            action: (key) => (
+              <IconButton
+                key="close"
+                aria-label="Close"
+                color="inherit"
+                onClick={() => self.props.closeSnackbar(key)}
+              >
+                <CloseIcon />
+              </IconButton>
+            ),
+          });
+          break;
+        case 'error':
+          self.handleError('Load test did not run successfully with msg')(data.message);
+          break;
+        case 'success':
+          self.handleSuccess();
+          break;
+      }
+    };
+  }
+
+  closeEventStream() {
+    if (this.eventStream && this.eventStream.close) {
+      this.eventStream.close();
+      this.eventStream = null;
+    }
+  }
+
+  handleError(msg) {
+    const self = this;
+    return (error) => {
+      self.setState({ blockRunTest: false, timerDialogOpen: false });
+      self.closeEventStream();
+      let finalMsg = msg;
+      if (typeof error === 'string') {
+        finalMsg = `${msg}: ${error}`;
+      }
+      self.props.enqueueSnackbar(finalMsg, {
+        variant: 'error',
+        action: (key) => (
+          <IconButton
+            key="close"
+            aria-label="Close"
+            color="inherit"
+            onClick={() => self.props.closeSnackbar(key)}
+          >
+            <CloseIcon />
+          </IconButton>
+        ),
+        autoHideDuration: 8000,
+      });
+    };
+  }
+
+  handleTimerDialogClose = () => {
+    this.setState({ timerDialogOpen: false });
+  }
+
+  render() {
+    const { classes } = this.props;
+    const {
+      timerDialogOpen, blockRunTest, qps, t, c, loadGenerator,
+      tError,
+    } = this.state;
+
+    return (
+      <NoSsr>
+        <React.Fragment>
+          <div className={classes.root}>
+                        <label><strong>Performance Load Test Defaults</strong></label>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={4}>
+                <TextField
+                  required
+                  id="c"
+                  name="c"
+                  label="Concurrent requests"
+                  type="number"
+                  fullWidth
+                  value={c}
+                  inputProps={{ min: '0', step: '1' }}
+                  margin="normal"
+                  variant="outlined"
+                  onChange={this.handleChange('c')}
+                />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <TextField
+                  required
+                  id="qps"
+                  name="qps"
+                  label="Queries per second"
+                  type="number"
+                  fullWidth
+                  value={qps}
+                  inputProps={{ min: '0', step: '1' }}
+                  margin="normal"
+                  variant="outlined"
+                  onChange={this.handleChange('qps')}
+                />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Tooltip title={"Please use 'h', 'm' or 's' suffix for hour, minute or second respectively."}>
+                  <TextField
+                    required
+                    id="t"
+                    name="t"
+                    label="Duration"
+                    fullWidth
+                    value={t}
+                    error={tError}
+                    margin="normal"
+                    variant="outlined"
+                    onChange={this.handleChange('t')}
+                  />
+                </Tooltip>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <FormControl component="loadGenerator" className={classes.formControl}>
+                  <label><strong>Default Load Generator</strong></label>
+                  <RadioGroup aria-label="loadGenerator" name="loadGenerator" value={loadGenerator} onChange={this.handleChange('loadGenerator')} row>
+                    {loadGenerators.map((lg) => (
+                      <FormControlLabel value={lg} control={<Radio color="primary" />} label={lg} />
+                    ))}
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
+            </Grid>
+            <React.Fragment>
+              <div className={classes.buttons}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  onClick={this.handleSubmit}
+                  className={classes.button}
+                  disabled={blockRunTest}
+                >
+                  {blockRunTest ? <CircularProgress size={30} /> : 'Submit Preferences'}
+                </Button>
+              </div>
+            </React.Fragment>
+
+            <div className={classes.centerTimer}>
+              <LoadTestTimerDialog
+                open={timerDialogOpen}
+                t={t}
+                onClose={this.handleTimerDialogClose}
+                countDownComplete={this.handleTimerDialogClose}
+              />
+            </div>
+
+
+          </div>
+        </React.Fragment>
+      </NoSsr>
+    );
+  }
+}
+
+MesherySettingsPerformanceComponent.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+
+
+
+export default withStyles(styles)(connect(
+)(withSnackbar(MesherySettingsPerformanceComponent)));
