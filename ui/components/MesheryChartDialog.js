647fe04b7f908fcedac9b8cc255f60e76bc4ebf7
Kush Trivedi <kushthedude@gmail.com>
2020-06-03 05:47:33 +0530

    fix: UI Build
    Signed-off-by: Kush Trivedi <kushthedude@gmail.com>

--- ui/components/MesheryChartDialog.js
+++ ui/components/MesheryChartDialog.js
@@ -8,6 +8,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
+const styles = () => ({});
 
 class MesheryChartDialog extends React.Component {
 
