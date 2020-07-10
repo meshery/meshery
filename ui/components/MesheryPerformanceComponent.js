cda95dd1739d8957aaaacf4f6e4a71d50503b6a7
Lee Calcote <leecalcote@gmail.com>
2020-07-10 13:04:44 -0500

    Changed from `2.5h` to `2h`

--- ui/components/MesheryPerformanceComponent.js
+++ ui/components/MesheryPerformanceComponent.js
@@ -79,7 +79,7 @@ const styles = (theme) => ({
   },
 });
 
-const prePopulatedOptions = ['15s', '30s', '1m', '3m', '5m', '10m', '30m', '1h', '2.5h', '5h', '10h', '1d']
+const prePopulatedOptions = ['15s', '30s', '1m', '3m', '5m', '10m', '30m', '1h', '2h', '5h', '10h', '1d']
 
 class MesheryPerformanceComponent extends React.Component {
   constructor(props) {
