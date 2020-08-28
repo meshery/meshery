04bb32fe4d742c77254a2e993422912bda566e84
Anirudh Jain <anirudh.jain@outlook.com>
2020-08-28 21:46:02 +0530

    add boilerplate code for cxtmenu working with react-cytoscape
    Signed-off-by: Anirudh Jain <anirudh.jain@outlook.com>

--- ui/components/visualize/MesheryVisualizeComponent.js
+++ ui/components/visualize/MesheryVisualizeComponent.js
@@ -5,6 +5,10 @@ import GraphStyle from './styles/styleContainer';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import { withStyles } from '@material-ui/core/styles';
+import cytoscape from 'cytoscape';
+import cxtmenu from 'cytoscape-cxtmenu';
+
+cytoscape.use(cxtmenu)
 
 const style = (theme) => ({
   
@@ -31,6 +35,53 @@ const style = (theme) => ({
     background: '#fff',
   }
 });
+
+let cxtMenuSettings = {
+  menuRadius: 100, // the radius of the circular menu in pixels
+  selector: 'node', // elements matching this Cytoscape.js selector will trigger cxtmenus
+  commands: [ // an array of commands to list in the menu or a function that returns the array
+    {
+      fillColor: 'rgba(200, 200, 200, 0.75)', // optional: custom background color for item
+      content: 'cmd1', // html/text content to be displayed in the menu
+      contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+      select: function(ele){ // a function to execute when the command is selected
+        console.log( ele.id() ) // `ele` holds the reference to the active element
+      },
+      enabled: true // whether the command is selectable
+    },
+    {
+      fillColor: 'rgba(200, 200, 200, 0.75)', // optional: custom background color for item
+      content: 'cmd2', // html/text content to be displayed in the menu
+      contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+      select: function(ele){ // a function to execute when the command is selected
+        console.log( ele.id() ) // `ele` holds the reference to the active element
+      },
+      enabled: true // whether the command is selectable
+    },
+    {
+      fillColor: 'rgba(200, 200, 200, 0.75)', // optional: custom background color for item
+      content: 'cmd3', // html/text content to be displayed in the menu
+      contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+      select: function(ele){ // a function to execute when the command is selected
+        console.log( ele.id() ) // `ele` holds the reference to the active element
+      },
+      enabled: true // whether the command is selectable
+    }
+  ], // function( ele ){ return [ /*...*/ ] }, // a function that returns commands or a promise of commands
+  fillColor: 'rgba(0, 0, 0, 0.75)', // the background colour of the menu
+  activeFillColor: 'rgba(1, 105, 217, 0.75)', // the colour used to indicate the selected command
+  activePadding: 20, // additional size in pixels for the active command
+  indicatorSize: 24, // the size in pixels of the pointer to the active command
+  separatorWidth: 3, // the empty spacing in pixels between successive commands
+  spotlightPadding: 4, // extra spacing in pixels between the element and the spotlight
+  minSpotlightRadius: 24, // the minimum radius in pixels of the spotlight
+  maxSpotlightRadius: 38, // the maximum radius in pixels of the spotlight
+  openMenuEvents: 'cxttapstart taphold', // space-separated cytoscape events that will open the menu; only `cxttapstart` and/or `taphold` work here
+  itemColor: 'white', // the colour of text in the command's content
+  itemTextShadowColor: 'transparent', // the text shadow colour of the command's content
+  zIndex: 9999, // the z-index of the ui div
+  atMouse: false // draw menu at mouse position
+};
 
 class MesheryVisualizeComponent extends React.Component {
   
@@ -137,7 +188,13 @@ class MesheryVisualizeComponent extends React.Component {
           },
           classes: ['service']
         },
-
+        {
+          data: {
+            source: 'product_namespace',
+            target: 'ingress_gateway',
+            label: 'traffic'
+          }
+        }
       ],
     };
 
@@ -150,7 +207,10 @@ class MesheryVisualizeComponent extends React.Component {
               style={ {width: '100%', height: '100%'} }
               layout={GraphStyle.getLayout()}
               stylesheet={GraphStyle.getStylesheetContainer()}
-              cy={cy => this.cy = cy}
+              cy={cy => {
+                this.cy = cy
+                this.cy.cxtmenu( cxtMenuSettings );
+              }}
             />
           </div>
           <ButtonGroup className={classes.zoomButton} color="primary" aria-label="outlined primary button group">
