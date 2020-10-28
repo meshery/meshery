dd7c4ababb37aa712c594498789a17de542e70a1
vineethvanga18 <reddy.8@iitj.ac.in>
2020-10-28 20:38:12 +0530

    added charts, actions and logs
    Signed-off-by: vineethvanga18 <reddy.8@iitj.ac.in>

---
+++ ui/components/visualize/logs.js
@@ -0,0 +1,23 @@
+export default {
+  "logs": [
+    "I1027 03:15:44.473392       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/cluster-admin",
+    "I1027 03:15:44.481292       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:discovery",
+    "I1027 03:15:44.488739       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:basic-user",
+    "I1027 03:15:44.495492       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:public-info-viewer",
+    "I1027 03:15:44.502321       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/admin",
+    "I1027 03:15:44.509201       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/edit",
+    "I1027 03:15:44.515795       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/view",
+    "I1027 03:15:44.525227       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:aggregate-to-admin",
+    "I1027 03:15:44.532685       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:aggregate-to-edit",
+    "I1027 03:15:44.539824       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:aggregate-to-view",
+    "I1027 03:15:44.546870       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:heapster",
+    "I1027 03:15:44.554370       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:node",
+    "I1027 03:15:44.561092       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:node-problem-detector",
+    "I1027 03:15:44.567844       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:node-proxier",
+    "I1027 03:15:44.575107       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:kubelet-api-admin",
+    "I1027 03:15:44.581835       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:node-bootstrapper",
+    "I1027 03:15:44.588578       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:auth-delegator",
+    "I1027 03:15:44.595717       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:kube-aggregator",
+    "I1027 03:15:44.602849       1 storage_rbac.go:195] created clusterrole.rbac.authorization.k8s.io/system:kube-controller-manager"
+  ]
+}
