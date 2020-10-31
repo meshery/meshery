6ccb698bc8c171f8f6e5a7d5fb9ef48e0c62cc69
Nitish Malhotra <nitish.malhotra@gmail.com>
2020-10-31 13:05:41 -0400

    Initial setup for system check command
    Signed-off-by: Nitish Malhotra <nitish.malhotra@gmail.com>

---
+++ mesheryctl/internal/cli/root/experimental/system/check.go
@@ -0,0 +1,124 @@
+// Copyright 2020 Layer5, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package system
+
+import (
+	"os/exec"
+
+	"github.com/pkg/errors"
+
+	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var (
+	preflightChecks bool = false
+)
+
+// StopCmd represents the stop command
+var checkCmd = &cobra.Command{
+	Use:   "check",
+	Short: "Run checks on Meshery",
+	Long:  `Run comprehensive checks on Meshery environment, server and all adapters`,
+	Args:  cobra.NoArgs,
+	// XXX (nitishm): Do we need to run this as a pre-run?
+	//PreRunE: func(cmd *cobra.Command, args []string) error {
+	//	return utils.PreReqCheck(cmd.Use)
+	//},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		log.Info("Checking Meshery status...")
+		if err := check(); err != nil {
+			return errors.Wrap(err, utils.SystemError("failed to run Meshery checks"))
+		}
+		return nil
+	},
+}
+
+func check() error {
+	// XXX (nitishm): Do we require meshery to by up and running. Possibly no since the check command
+	// should have the capability to respond with meshery server not running.
+
+	// First check if Meshery is up and running.
+	//if !utils.IsMesheryRunning() {
+	//	log.Info("Meshery is not running. Nothing to check.")
+	//	return nil
+	//}
+
+	// Check docker status:
+	if err := exec.Command("docker", "ps").Run(); err != nil {
+		log.Info("[-] Docker daemon is not running")
+	} else {
+		log.Info("[√] Docker daemon is running")
+	}
+
+	// Check docker-compose status
+	if err := exec.Command("docker-compose", "-v").Run(); err != nil {
+		log.Info("[-] docker-compose is not available")
+	} else {
+		log.Info("[√] docker-compose is available")
+	}
+
+	// TODO:
+	// Check kubernetes status:
+
+	// Kubernetes API
+	// --------------
+	// √ can initialize the client
+	// √ can query the Kubernetes API
+	// √ has required level of privileges
+	//
+	// Kubernetes Version
+	// ------------------
+	// √ is running the minimum Kubernetes API version
+	// √ is running the minimum kubectl version
+
+	if preflightChecks {
+		return nil
+	}
+
+	// TODO:
+	// Check Meshery versions:
+
+	// TODO:
+	// Meshery Version
+	// ---------------
+	// √ server is up-to-date (stable-v0.x.x)
+	// √ cli is up-to-date (stable-v0.x.x)
+
+	// TODO:
+	// Check meshery adapter status
+
+	// TODO:
+	// Meshery Adapters
+	// ---------------
+	// √ meshery-istio adapter is reachable and running
+	// √ meshery-consul adapter is reachable and running
+	// √ meshery-linkerd adapter is reachable and running
+	// .
+	// .
+	// .
+	// √ meshery-nginx-sm adapter is reachable and running
+	// √ meshery-octarine adapter is reachable and running
+
+	return nil
+}
+
+func init() {
+	checkCmd.Flags().BoolVarP(&preflightChecks, "preflight", "",
+		false, "(optional) Checks status of docker and kubernetes only.")
+
+}
---
+++ mesheryctl/internal/cli/root/experimental/system/system.go
@@ -0,0 +1,53 @@
+// Copyright 2020 Layer5, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package system
+
+import (
+	"fmt"
+
+	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+const (
+	url     = "http://localhost:9081"
+	fileURL = "https://raw.githubusercontent.com/layer5io/meshery/master/docker-compose.yaml"
+)
+
+var (
+	availableSubcommands = []*cobra.Command{}
+)
+
+// SystemCmd represents Meshery Lifecycle Management cli commands
+var SystemCmd = &cobra.Command{
+	Use:   "system",
+	Short: "Meshery Lifecycle Management",
+	Long:  `Manage the state and configuration of Meshery server, adapters, and client.`,
+	Args:  cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if ok := utils.IsValidSubcommand(availableSubcommands, args[0]); !ok {
+			return errors.New(utils.SystemError(fmt.Sprintf("invalid command: \"%s\"", args[0])))
+		}
+		return nil
+	},
+}
+
+func init() {
+	availableSubcommands = []*cobra.Command{
+		checkCmd,
+	}
+	SystemCmd.AddCommand(availableSubcommands...)
+}
