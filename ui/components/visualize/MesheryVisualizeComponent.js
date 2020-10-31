fdf2cc17c2b1e2401888f63ada555e1f77c26de8
Anirudh jain <anirudh.jain@outlook.com>
2020-11-01 01:51:52 +0530

    test state dialog with cxtmenu
    Signed-off-by: Anirudh jain <anirudh.jain@outlook.com>

--- ui/components/visualize/MesheryVisualizeComponent.js
+++ ui/components/visualize/MesheryVisualizeComponent.js
@@ -3,6 +3,11 @@ import NoSsr from '@material-ui/core/NoSsr';
 import CytoscapeComponent from 'react-cytoscapejs';
 import GraphStyle from './styles/styleContainer';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import { withStyles } from '@material-ui/core/styles';
 import cytoscape from 'cytoscape';
@@ -116,53 +121,6 @@ const style = (theme) => ({
   }
 });
 
-let cxtMenuSettings = {
-  menuRadius: 100, // the radius of the circular menu in pixels
-  selector: 'node', // elements matching this Cytoscape.js selector will trigger cxtmenus
-  commands: [ // an array of commands to list in the menu or a function that returns the array
-    {
-      fillColor: 'rgba(200, 200, 200, 0.75)', // optional: custom background color for item
-      content: 'cmd1', // html/text content to be displayed in the menu
-      contentStyle: {}, // css key:value pairs to set the command's css in js if you want
-      select: function (ele) { // a function to execute when the command is selected
-        console.log(ele.id()) // `ele` holds the reference to the active element
-      },
-      enabled: true // whether the command is selectable
-    },
-    {
-      fillColor: 'rgba(200, 200, 200, 0.75)', // optional: custom background color for item
-      content: 'cmd2', // html/text content to be displayed in the menu
-      contentStyle: {}, // css key:value pairs to set the command's css in js if you want
-      select: function (ele) { // a function to execute when the command is selected
-        console.log(ele.id()) // `ele` holds the reference to the active element
-      },
-      enabled: true // whether the command is selectable
-    },
-    {
-      fillColor: 'rgba(200, 200, 200, 0.75)', // optional: custom background color for item
-      content: 'cmd3', // html/text content to be displayed in the menu
-      contentStyle: {}, // css key:value pairs to set the command's css in js if you want
-      select: function (ele) { // a function to execute when the command is selected
-        console.log(ele.id()) // `ele` holds the reference to the active element
-      },
-      enabled: true // whether the command is selectable
-    }
-  ], // function( ele ){ return [ /*...*/ ] }, // a function that returns commands or a promise of commands
-  fillColor: 'rgba(0, 0, 0, 0.75)', // the background colour of the menu
-  activeFillColor: 'rgba(1, 105, 217, 0.75)', // the colour used to indicate the selected command
-  activePadding: 20, // additional size in pixels for the active command
-  indicatorSize: 24, // the size in pixels of the pointer to the active command
-  separatorWidth: 3, // the empty spacing in pixels between successive commands
-  spotlightPadding: 4, // extra spacing in pixels between the element and the spotlight
-  minSpotlightRadius: 24, // the minimum radius in pixels of the spotlight
-  maxSpotlightRadius: 38, // the maximum radius in pixels of the spotlight
-  openMenuEvents: 'cxttapstart taphold', // space-separated cytoscape events that will open the menu; only `cxttapstart` and/or `taphold` work here
-  itemColor: 'white', // the colour of text in the command's content
-  itemTextShadowColor: 'transparent', // the text shadow colour of the command's content
-  zIndex: 9999, // the z-index of the ui div
-  atMouse: false // draw menu at mouse position
-};
-
 class MesheryVisualizeComponent extends React.Component {
 
   constructor(props) {
@@ -173,7 +131,8 @@ class MesheryVisualizeComponent extends React.Component {
       open: false,
       data: null,
       tab: 0,
-      logs: []
+      logs: [],
+      showDialog: false
     }
     this.prev = null;
   }
@@ -246,8 +205,78 @@ class MesheryVisualizeComponent extends React.Component {
     //Checkout the docs for JSON format https://js.cytoscape.org/#notation/elements-json
     const elements = elementsJson.elements;
 
+    let cxtMenuSettings = {
+      menuRadius: 100, // the radius of the circular menu in pixels
+      selector: 'node', // elements matching this Cytoscape.js selector will trigger cxtmenus
+      commands: [ // an array of commands to list in the menu or a function that returns the array
+        {
+          fillColor: 'rgba(200, 200, 200, 0.75)', // optional: custom background color for item
+          content: 'perf', // html/text content to be displayed in the menu
+          contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+          select: function () { // a function to execute when the command is selected
+            this.setState({showDialog: true}) // `ele` holds the reference to the active element
+          },
+          enabled: true // whether the command is selectable
+        },
+        {
+          fillColor: 'rgba(200, 200, 200, 0.75)', // optional: custom background color for item
+          content: 'cmd2', // html/text content to be displayed in the menu
+          contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+          select: function (ele) { // a function to execute when the command is selected
+            console.log(ele.id()) // `ele` holds the reference to the active element
+          },
+          enabled: true // whether the command is selectable
+        },
+        {
+          fillColor: 'rgba(200, 200, 200, 0.75)', // optional: custom background color for item
+          content: 'cmd3', // html/text content to be displayed in the menu
+          contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+          select: function (ele) { // a function to execute when the command is selected
+            console.log(ele.id()) // `ele` holds the reference to the active element
+          },
+          enabled: true // whether the command is selectable
+        }
+      ], // function( ele ){ return [ /*...*/ ] }, // a function that returns commands or a promise of commands
+      fillColor: 'rgba(0, 0, 0, 0.75)', // the background colour of the menu
+      activeFillColor: 'rgba(1, 105, 217, 0.75)', // the colour used to indicate the selected command
+      activePadding: 20, // additional size in pixels for the active command
+      indicatorSize: 24, // the size in pixels of the pointer to the active command
+      separatorWidth: 3, // the empty spacing in pixels between successive commands
+      spotlightPadding: 4, // extra spacing in pixels between the element and the spotlight
+      minSpotlightRadius: 24, // the minimum radius in pixels of the spotlight
+      maxSpotlightRadius: 38, // the maximum radius in pixels of the spotlight
+      openMenuEvents: 'cxttapstart taphold', // space-separated cytoscape events that will open the menu; only `cxttapstart` and/or `taphold` work here
+      itemColor: 'white', // the colour of text in the command's content
+      itemTextShadowColor: 'transparent', // the text shadow colour of the command's content
+      zIndex: 9999, // the z-index of the ui div
+      atMouse: false // draw menu at mouse position
+    };
+    
+
     return (
       <NoSsr>
+        <Dialog
+          open={this.state.showDialog}
+          onClose={() => this.setState({showDialog: false})}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Let Google help apps determine location. This means sending anonymous location data to
+              Google, even when no apps are running.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => this.setState({showDialog: false})} color="primary">
+              Disagree
+            </Button>
+            <Button onClick={() => this.setState({showDialog: false})} color="primary" autoFocus>
+              Agree
+            </Button>
+          </DialogActions>
+        </Dialog>
         <MiniDrawer 
           toggle={(tab) => {
             this.toggleMiniDrawer(tab);
