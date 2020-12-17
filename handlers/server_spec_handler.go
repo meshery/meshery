db551cd3a95c2c4326c4d4740e41ff09ae8294be
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2020-12-17 12:05:53 +0530

    revert prompt to classic y/n
    Signed-off-by: Uddeshya Singh <singhuddeshyaofficial@gmail.com>

--- handlers/server_spec_handler.go
+++ handlers/server_spec_handler.go
@@ -4,10 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
 
-	"github.com/manifoldco/promptui"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -69,17 +69,16 @@ func CheckLatestVersion(serverVersion string) (*latest.CheckResponse, error) {
 	// If user is running an outdated release, let them know.
 	if res.Outdated {
 		logrus.Info("\n", serverVersion, " is not the latest Meshery release. Update to v", res.Current, ". Run `mesheryctl system update`")
-		promptLabel := fmt.Sprintf("Would you like to upgrade to v%s now?(y/n)", res.Current)
-
-		prompt := promptui.Select{
-			Label: promptLabel,
-			Items: []string{"y", "n"},
-		}
-		_, result, err := prompt.Run()
+		promptLabel := fmt.Sprintf("Would you like to upgrade to v%s now?(y/n): ", res.Current)
+		fmt.Printf(promptLabel)
+		var result string
+		_, err := fmt.Scan(&result)
 		if err != nil {
 			logrus.Error("Prompt failed %w\n", err)
 		}
-		if result != "n" {
+		result = strings.TrimSpace(result)
+		result = strings.ToLower(result)
+		if result != "n" && result != "no" {
 			err = utils.UpdateMesheryContainers()
 			if err != nil {
 				logrus.Error("Unable to update meshery: %w", err)
