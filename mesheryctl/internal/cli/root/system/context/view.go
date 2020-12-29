6d95806a098ec0ac4205330f99e6d573fb0fe879
Anirudh Jain <anirudh.jain@outlook.com>
2020-12-29 14:45:21 +0000

    Fix issue with context view
    Signed-off-by: Anirudh Jain <anirudh.jain@outlook.com>

--- mesheryctl/internal/cli/root/system/context/view.go
+++ mesheryctl/internal/cli/root/system/context/view.go
@@ -11,7 +11,7 @@ var viewContextCmd = &cobra.Command{
 	Use:   "view",
 	Short: "view current context",
 	Long:  `Display active Meshery context`,
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		err := viper.Unmarshal(&configuration)
 		if err != nil {
