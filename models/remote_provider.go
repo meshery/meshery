d9b59ebf57f2e0a9d64c8253f210419f0a95ff4f
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-12-30 19:00:10 +0530

    fix golanglint and gosec errors
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- models/remote_provider.go
+++ models/remote_provider.go
@@ -150,7 +150,9 @@ func (l *RemoteProvider) SyncPreferences() {
 // GetProviderCapabilities returns all of the provider properties
 func (l *RemoteProvider) GetProviderCapabilities(w http.ResponseWriter, r *http.Request) {
 	encoder := json.NewEncoder(w)
-	encoder.Encode(l.ProviderProperties)
+	if err := encoder.Encode(l.ProviderProperties); err != nil {
+		http.Error(w, "failed to encode provider capabilities", http.StatusInternalServerError)
+	}
 }
 
 // StopSyncPreferences - used to stop sync preferences
