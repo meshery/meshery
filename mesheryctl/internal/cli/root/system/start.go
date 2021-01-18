124e9272659ba821c34cd6ad003b72117e11a70a
Jay Batra <jaybatra73@gmail.com>
2021-01-18 22:12:51 +0530

    Use docker for default and use context instead of configuration
    Signed-off-by: Jay Batra <jaybatra73@gmail.com>

--- mesheryctl/internal/cli/root/system/start.go
+++ mesheryctl/internal/cli/root/system/start.go
@@ -96,159 +96,159 @@ func start() error {
 	if err != nil {
 		return errors.Wrap(err, "error processing config")
 	}
-	if(mctlCfg.Contexts["local"].Platform== "docker") 
-	{
+	ctxPlatform := mctlCfg.GetContextContent().Platform
+	if ctxPlatform == "docker" || ctxPlatform == "" {
 		// Viper instance used for docker compose
-	utils.ViperCompose.SetConfigFile(utils.DockerComposeFile)
-	err = utils.ViperCompose.ReadInConfig()
-	if err != nil {
-		return err
-	}
-
-	compose := &utils.DockerCompose{}
-	err = utils.ViperCompose.Unmarshal(&compose)
-	if err != nil {
-		return err
-	}
-
-	services := compose.Services                                        // Current Services
-	RequestedAdapters := mctlCfg.GetContextContent().Adapters           // Requested Adapters / Services
-	err = ValidateComposeFileForRecreation(services, RequestedAdapters) // Validates docker-compose file and recreates and sync's if required
-	if err != nil {
-		return err
-	}
-
-	err = utils.ViperCompose.Unmarshal(&compose)
-	if err != nil {
-		return err
-	}
-	services = compose.Services // Current Services
-	RequiredService := []string{"meshery", "watchtower"}
-
-	AllowedServices := map[string]utils.Service{}
-	for _, v := range RequestedAdapters {
-		if services[v].Image == "" {
-			log.Fatalf("Invalid adapter specified %s", v)
-		}
-		AllowedServices[v] = services[v]
-	}
-	for _, v := range RequiredService {
-		AllowedServices[v] = services[v]
-	}
-
-	utils.ViperCompose.Set("services", AllowedServices)
-	err = utils.ViperCompose.WriteConfig()
-	if err != nil {
-		return err
-	}
-
-	//fmt.Println("Services", services);
-	//fmt.Println("RequiredAdapters", RequestedAdapters);
-	//fmt.Println("AllowedServices", AllowedServices);
-	//fmt.Println("version", utils.ViperCompose.GetString("version")) // Works here
-
-	//////// FLAGS
-	// Control whether to pull for new Meshery container images
-	if skipUpdateFlag {
-		log.Info("Skipping Meshery update...")
-	} else {
-		err := utils.UpdateMesheryContainers()
+		utils.ViperCompose.SetConfigFile(utils.DockerComposeFile)
+		err = utils.ViperCompose.ReadInConfig()
 		if err != nil {
-			return errors.Wrap(err, utils.SystemError("failed to update meshery containers"))
+			return err
 		}
-	}
 
-	// Reset Meshery config file to default settings
-	if utils.ResetFlag {
-		err := resetMesheryConfig()
+		compose := &utils.DockerCompose{}
+		err = utils.ViperCompose.Unmarshal(&compose)
 		if err != nil {
-			return errors.Wrap(err, utils.SystemError("failed to reset meshery config"))
+			return err
 		}
-	}
 
-	log.Info("Starting Meshery...")
-	start := exec.Command("docker-compose", "-f", utils.DockerComposeFile, "up", "-d")
-	start.Stdout = os.Stdout
-	start.Stderr = os.Stderr
+		services := compose.Services                                        // Current Services
+		RequestedAdapters := mctlCfg.GetContextContent().Adapters           // Requested Adapters / Services
+		err = ValidateComposeFileForRecreation(services, RequestedAdapters) // Validates docker-compose file and recreates and sync's if required
+		if err != nil {
+			return err
+		}
 
-	if err := start.Run(); err != nil {
-		return errors.Wrap(err, utils.SystemError("failed to run meshery server"))
-	}
-	checkFlag := 0 //flag to check
+		err = utils.ViperCompose.Unmarshal(&compose)
+		if err != nil {
+			return err
+		}
+		services = compose.Services // Current Services
+		RequiredService := []string{"meshery", "watchtower"}
 
-	//connection to docker-client
-	cli, err := client.NewEnvClient()
-	if err != nil {
-		return errors.Wrap(err, utils.SystemError("failed to create new env client"))
-	}
-
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-	if err != nil {
-		return errors.Wrap(err, utils.SystemError("failed to fetch the list of containers"))
-	}
-
-	//check for container meshery_meshery_1 running status
-	for _, container := range containers {
-		if container.Names[0] == "/meshery_meshery_1" {
-			log.Info("Opening Meshery in your browser. If Meshery does not open, please point your browser to http://localhost:9081 to access Meshery.")
-
-			//check for os of host machine
-			if runtime.GOOS == "windows" {
-				// Meshery running on Windows host
-				err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-				if err != nil {
-					return errors.Wrap(err, utils.SystemError("failed to exec command"))
-				}
-			} else if runtime.GOOS == "linux" {
-				// Meshery running on Linux host
-				_, err = exec.LookPath("xdg-open")
-				if err != nil {
-					break
-				}
-				err = exec.Command("xdg-open", url).Start()
-				if err != nil {
-					return errors.Wrap(err, utils.SystemError("failed to exec command"))
-				}
-			} else {
-				// Assume Meshery running on MacOS host
-				err = exec.Command("open", url).Start()
-				if err != nil {
-					return errors.Wrap(err, utils.SystemError("failed to exec command"))
-				}
+		AllowedServices := map[string]utils.Service{}
+		for _, v := range RequestedAdapters {
+			if services[v].Image == "" {
+				log.Fatalf("Invalid adapter specified %s", v)
 			}
+			AllowedServices[v] = services[v]
+		}
+		for _, v := range RequiredService {
+			AllowedServices[v] = services[v]
+		}
 
-			//check flag to check successful deployment
-			checkFlag = 0
-			break
+		utils.ViperCompose.Set("services", AllowedServices)
+		err = utils.ViperCompose.WriteConfig()
+		if err != nil {
+			return err
+		}
+
+		//fmt.Println("Services", services);
+		//fmt.Println("RequiredAdapters", RequestedAdapters);
+		//fmt.Println("AllowedServices", AllowedServices);
+		//fmt.Println("version", utils.ViperCompose.GetString("version")) // Works here
+
+		//////// FLAGS
+		// Control whether to pull for new Meshery container images
+		if skipUpdateFlag {
+			log.Info("Skipping Meshery update...")
 		} else {
-			checkFlag = 1
+			err := utils.UpdateMesheryContainers()
+			if err != nil {
+				return errors.Wrap(err, utils.SystemError("failed to update meshery containers"))
+			}
+		}
+
+		// Reset Meshery config file to default settings
+		if utils.ResetFlag {
+			err := resetMesheryConfig()
+			if err != nil {
+				return errors.Wrap(err, utils.SystemError("failed to reset meshery config"))
+			}
+		}
+
+		log.Info("Starting Meshery...")
+		start := exec.Command("docker-compose", "-f", utils.DockerComposeFile, "up", "-d")
+		start.Stdout = os.Stdout
+		start.Stderr = os.Stderr
+
+		if err := start.Run(); err != nil {
+			return errors.Wrap(err, utils.SystemError("failed to run meshery server"))
+		}
+		checkFlag := 0 //flag to check
+
+		//connection to docker-client
+		cli, err := client.NewEnvClient()
+		if err != nil {
+			return errors.Wrap(err, utils.SystemError("failed to create new env client"))
+		}
+
+		containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+		if err != nil {
+			return errors.Wrap(err, utils.SystemError("failed to fetch the list of containers"))
+		}
+
+		//check for container meshery_meshery_1 running status
+		for _, container := range containers {
+			if container.Names[0] == "/meshery_meshery_1" {
+				log.Info("Opening Meshery in your browser. If Meshery does not open, please point your browser to http://localhost:9081 to access Meshery.")
+
+				//check for os of host machine
+				if runtime.GOOS == "windows" {
+					// Meshery running on Windows host
+					err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+					if err != nil {
+						return errors.Wrap(err, utils.SystemError("failed to exec command"))
+					}
+				} else if runtime.GOOS == "linux" {
+					// Meshery running on Linux host
+					_, err = exec.LookPath("xdg-open")
+					if err != nil {
+						break
+					}
+					err = exec.Command("xdg-open", url).Start()
+					if err != nil {
+						return errors.Wrap(err, utils.SystemError("failed to exec command"))
+					}
+				} else {
+					// Assume Meshery running on MacOS host
+					err = exec.Command("open", url).Start()
+					if err != nil {
+						return errors.Wrap(err, utils.SystemError("failed to exec command"))
+					}
+				}
+
+				//check flag to check successful deployment
+				checkFlag = 0
+				break
+			} else {
+				checkFlag = 1
+			}
+		}
+
+		//if meshery_meshery_1 failed to start showing logs
+		//code for logs
+		if checkFlag == 1 {
+			log.Info("Starting Meshery logging . . .")
+			cmdlog := exec.Command("docker-compose", "-f", utils.DockerComposeFile, "logs", "-f")
+			cmdReader, err := cmdlog.StdoutPipe()
+			if err != nil {
+				return errors.Wrap(err, utils.SystemError("failed to create stdout pipe"))
+			}
+			scanner := bufio.NewScanner(cmdReader)
+			go func() {
+				for scanner.Scan() {
+					log.Println(scanner.Text())
+				}
+			}()
+			if err := cmdlog.Start(); err != nil {
+				return errors.Wrap(err, utils.SystemError("failed to start logging"))
+			}
+			if err := cmdlog.Wait(); err != nil {
+				return errors.Wrap(err, utils.SystemError("failed to wait for command to execute"))
+			}
 		}
 	}
 
-	//if meshery_meshery_1 failed to start showing logs
-	//code for logs
-	if checkFlag == 1 {
-		log.Info("Starting Meshery logging . . .")
-		cmdlog := exec.Command("docker-compose", "-f", utils.DockerComposeFile, "logs", "-f")
-		cmdReader, err := cmdlog.StdoutPipe()
-		if err != nil {
-			return errors.Wrap(err, utils.SystemError("failed to create stdout pipe"))
-		}
-		scanner := bufio.NewScanner(cmdReader)
-		go func() {
-			for scanner.Scan() {
-				log.Println(scanner.Text())
-			}
-		}()
-		if err := cmdlog.Start(); err != nil {
-			return errors.Wrap(err, utils.SystemError("failed to start logging"))
-		}
-		if err := cmdlog.Wait(); err != nil {
-			return errors.Wrap(err, utils.SystemError("failed to wait for command to execute"))
-		}
-	}
-	}
-	
 	return nil
 }
 
