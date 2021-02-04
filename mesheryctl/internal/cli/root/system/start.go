3964419271b82c76f5ef5125938d44f522e10864
navendu-pottekkat <navendupottekkat@gmail.com>
2021-02-04 17:55:38 +0530

    fix indent
    Signed-off-by: navendu-pottekkat <navendupottekkat@gmail.com>

--- mesheryctl/internal/cli/root/system/start.go
+++ mesheryctl/internal/cli/root/system/start.go
@@ -266,25 +266,25 @@ func start() error {
 	case "kubernetes":
 
 		nginx := `apiVersion: apps/v1
-		kind: Deployment
-		metadata:
-		  name: nginx-deployment
-		spec:
-		  selector:
-			matchLabels:
-			  app: nginx
-		  replicas: 2 # tells deployment to run 2 pods matching the template
-		  template:
-			metadata:
-			  labels:
-				app: nginx
-			spec:
-			  containers:
-			  - name: nginx
-				image: nginx:1.14.2
-				ports:
-				- containerPort: 80
-		`
+kind: Deployment
+metadata:
+name: nginx-deployment
+spec:
+selector:
+	matchLabels:
+	app: nginx
+replicas: 2 # tells deployment to run 2 pods matching the template
+template:
+	metadata:
+	labels:
+		app: nginx
+	spec:
+	containers:
+	- name: nginx
+		image: nginx:1.14.2
+		ports:
+		- containerPort: 80
+`
 
 		config, err := meshkitkube.DetectKubeConfig()
 
