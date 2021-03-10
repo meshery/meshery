112eda44c394c681d2094f276fc9529acf8f548f
Piyush Singariya <piyushsingariya@gmail.com>
2021-03-10 19:16:06 +0530

    trailing spaces removed
    Signed-off-by: Piyush Singariya <piyushsingariya@gmail.com>

--- ui/cypress/integration/integration/settings_spec.js
+++ ui/cypress/integration/integration/settings_spec.js
@@ -1,16 +1,16 @@
 describe("Settings", () => {
     describe("Service Meshes", () => {
-      before(() => {  
+      before(() => {
         cy.selectProviderNone()
 
         cy.visit("/settings");
-  
+
         cy.get('[data-cy="tabServiceMeshes"]').click();
       });
       it("select, submit, and confirm", () => {
         cy.get(".MuiInputBase-input").type("mesherylocal.layer5.io:10000{enter}");
-  
-        cy.get("[data-cy=btnSubmitMeshAdapter]").click();   
+
+        cy.get("[data-cy=btnSubmitMeshAdapter]").click();
 
         cy.get("[data-cy=adapterSuccessSnackbar]").should('exist')
       });
