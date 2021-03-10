3599cdb442e9018eb1db39ce476dc0afe5dc7069
Piyush Singariya <piyushsingariya@gmail.com>
2021-03-10 19:30:49 +0530

    backend response added trial-1
    Signed-off-by: Piyush Singariya <piyushsingariya@gmail.com>

--- ui/cypress/integration/integration/settings_spec.js
+++ ui/cypress/integration/integration/settings_spec.js
@@ -4,14 +4,14 @@ describe("Settings", () => {
         cy.selectProviderNone()
 
         cy.visit("/settings");
-
         cy.get('[data-cy="tabServiceMeshes"]').click();
       });
       it("select, submit, and confirm", () => {
+        cy.intercept('POST', '/api/mesh/manage', { fixture: 'stats.json' }).as('submitMeshAdapter')
+
         cy.get(".MuiInputBase-input").type("mesherylocal.layer5.io:10000{enter}");
-
         cy.get("[data-cy=btnSubmitMeshAdapter]").click();
-
+        cy.get('@submitMeshAdapter')
         cy.get("[data-cy=adapterSuccessSnackbar]").should('exist')
       });
     });
