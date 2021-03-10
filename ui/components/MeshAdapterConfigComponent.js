579b81ad40a11057cda077df3a9c2761f6d1ebc6
vineethvanga18 <reddy.8@iitj.ac.in>
2021-03-10 21:33:28 +0530

    remove console log
    Signed-off-by: vineethvanga18 <reddy.8@iitj.ac.in>

--- ui/components/MeshAdapterConfigComponent.js
+++ ui/components/MeshAdapterConfigComponent.js
@@ -341,7 +341,6 @@ class MeshAdapterConfigComponent extends React.Component {
 
           <Grid container spacing={1} alignItems="flex-end">
             <Grid item xs={12}>
-              {console.log(availableAdapters)}
               <ReactSelectWrapper
                 onChange={this.handleMeshLocURLChange}
                 options={availableAdapters}
