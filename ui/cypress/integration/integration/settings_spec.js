d5b33e1d7c2b99ec6dbe94579179a42acf5a73c8
Piyush Singariya <piyushsingariya@gmail.com>
2021-03-11 14:28:30 +0530

    trial stage -2
    Signed-off-by: Piyush Singariya <piyushsingariya@gmail.com>

--- ui/cypress/integration/integration/settings_spec.js
+++ ui/cypress/integration/integration/settings_spec.js
@@ -9,11 +9,13 @@ describe("Settings", () => {
       });
 
       it("select, submit, and confirm", () => {
-        cy.intercept('POST', '/api/mesh/manage', { fixture: 'stats.json' }).as('submitMeshAdapter')
+        cy.intercept('POST', '/api/mesh/manage', { fixture: 'stats.json' }).as('postMeshManage')
 
-        cy.get(".MuiInputBase-input").type("mesherylocal.layer5.io:10000{enter}");
+        cy.get(".MuiInputBase-input")
+          .type("mesherylocal.layer5.io:10000{enter}");
+
         cy.get("[data-cy=btnSubmitMeshAdapter]").click();
-        cy.wait('@submitMeshAdapter')
+        cy.wait('@postMeshManage')
         cy.get("[data-cy=adapterSuccessSnackbar]").should('exist')
       });
     });
