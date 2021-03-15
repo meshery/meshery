584659dafbb493314c06d540b73349d4cef2dbe3
Piyush Singariya <piyushsingariya@gmail.com>
2021-03-15 10:00:26 +0530

    added check to compare latest and the current version of mesheryctl
    Signed-off-by: Piyush Singariya <piyushsingariya@gmail.com>

--- mesheryctl/internal/cli/root/root.go
+++ mesheryctl/internal/cli/root/root.go
@@ -69,7 +69,9 @@ var RootCmd = &cobra.Command{
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		latest, err := utils.GetLatestStableReleaseTag()
 		if err == nil && latest != version {
-			log.Printf("A new release of mesheryctl is available: %s → %s", version, latest)
+			if latest != version {
+				log.Printf("A new release of mesheryctl is available: %s → %s", version, latest)
+			}
 			log.Printf("https://github.com/layer5io/meshery/releases/tag/%s", latest)
 			log.Print("Check https://docs.meshery.io/guides/upgrade#upgrading-meshery-cli for instructions on how to update mesheryctl\n")
 		}
