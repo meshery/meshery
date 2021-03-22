89abd07c3c87e9491a261c5dc5fcf5c862dbfcd3
metonymic-smokey <ahuja.aditi@gmail.com>
2021-03-22 23:28:02 +0530

    added version check
    Signed-off-by: metonymic-smokey <ahuja.aditi@gmail.com>

--- mesheryctl/internal/cli/root/config/config.go
+++ mesheryctl/internal/cli/root/config/config.go
@@ -1,10 +1,11 @@
 package config
 
 import (
-	log "github.com/sirupsen/logrus"
-
+	"fmt"
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"regexp"
 )
 
 // Version unmarshals the json response from the server's version api
@@ -53,13 +54,42 @@ func (mc *MesheryCtlConfig) CheckIfCurrentContextIsValid() (Context, error) {
 	if mc.CurrentContext == "" {
 		return Context{}, errors.New("current context not set")
 	}
+
 	if ctx, exists := mc.Contexts[mc.CurrentContext]; exists {
 		if ctx.Version == "" {
 			ctx.Version = "latest"
 		}
+
+		version, err := CheckIfCurrentVersionIsValid(ctx.Version)
+		if err != nil || version == "" {
+			return Context{}, errors.New("invalid version " + ctx.Version + " specified")
+		}
+		ctx.Version = version
+		fmt.Println("The version for this context is: ", ctx.Version)
+
 		return ctx, nil
 	}
+
 	return Context{}, errors.New("current context:" + mc.CurrentContext + " does not exist")
+}
+
+// CheckIfCurrentVersionIsValid checks if current version is valid, return context as a string
+func CheckIfCurrentVersionIsValid(version string) (string, error) {
+
+	if version == "" || version == "latest" {
+		return "latest", nil
+	}
+
+	matched, err := regexp.MatchString(`v[0-9]+.[0-9]+.[0-9]+[-a-z0-9]*`, version)
+	if err != nil || !matched {
+		return "", errors.New("Invalid version " + version + " specified")
+	}
+
+	if matched {
+		return version, nil
+	}
+
+	return "", errors.New("Invalid version " + version + " specified")
 }
 
 // GetBaseMesheryURL returns the base meshery server URL
