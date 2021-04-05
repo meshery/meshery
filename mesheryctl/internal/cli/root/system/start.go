9e2cc764c6cad88563d73df329edd21ba3f1243e
Piyush Singariya <piyushsingariya@gmail.com>
2021-04-05 21:17:55 +0530

    commented-out adapters part for future reference
    Signed-off-by: Piyush Singariya <piyushsingariya@gmail.com>

--- mesheryctl/internal/cli/root/system/start.go
+++ mesheryctl/internal/cli/root/system/start.go
@@ -292,22 +292,23 @@ func start() error {
 			return err
 		}
 
+		// Commented for future references
 		// loop through the required adapters as specified in the config.yaml file
-		for _, adapter := range RequestedAdapters {
-			// for each adapter, there is a meshery-adapterName-deployment.yaml and meshery-adapterName-service.yaml
-			// manifest file. See- https://github.com/layer5io/meshery/tree/master/install/deployment_yamls/k8s
-			adapterFile := filepath.Join(manifestFiles, adapter+"-deployment.yaml")
+		// for _, adapter := range RequestedAdapters {
+		// 	// for each adapter, there is a meshery-adapterName-deployment.yaml and meshery-adapterName-service.yaml
+		// 	// manifest file. See- https://github.com/layer5io/meshery/tree/master/install/deployment_yamls/k8s
+		// 	adapterFile := filepath.Join(manifestFiles, adapter+"-deployment.yaml")
 
-			if !utils.IsAdapterValid(manifests, adapter+"-deployment.yaml") {
-				return fmt.Errorf("invalid adapter %s specified. Please check %s/config.yaml file", adapter, utils.MesheryFolder)
-			}
+		// 	if !utils.IsAdapterValid(manifests, adapter+"-deployment.yaml") {
+		// 		return fmt.Errorf("invalid adapter %s specified. Please check %s/config.yaml file", adapter, utils.MesheryFolder)
+		// 	}
 
-			// now replace version in deployment.yaml files
-			err = utils.ChangeManifestVersion(adapter+"-deployment.yaml", version, adapterFile)
-			if err != nil {
-				return err
-			}
-		}
+		// 	// now replace version in deployment.yaml files
+		// 	err = utils.ChangeManifestVersion(adapter+"-deployment.yaml", version, adapterFile)
+		// 	if err != nil {
+		// 		return err
+		// 	}
+		// }
 
 		// downloaded required files successfully now apply the manifest files
 		log.Info("Starting Meshery...")
