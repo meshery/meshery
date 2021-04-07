8fa92ad0573a22e86365ab2435c2580a82117b5e
s1ntaxe770r <cyberdev01@protonmail.com>
2021-04-07 07:34:33 +0100

    adds api response model

--- models/meshery_patterns_api_response.go
+++ models/meshery_patterns_api_response.go
@@ -1,6 +1,7 @@
 package models
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 type PatternsAPIResponse struct {
 	Page       uint             `json:"page"`
 	PageSize   uint             `json:"page_size"`
@@ -9,10 +10,14 @@ type PatternsAPIResponse struct {
 =======
 type PatternsApiResponse struct {
 <<<<<<< HEAD
+=======
+type PatternsApiResponse struct {
+>>>>>>> 0753cdeb... adds api response model
 	Page       uint              `json:"page"`
 	PageSize   uint              `json:"page_size"`
 	TotalCount uint              `json:"total_size"`
 	Patterns   *[]MesheryPattern `json:"patterns"`
+<<<<<<< HEAD
 >>>>>>> 87573286... adds api response model
 =======
 	Page       uint             `json:"page"`
@@ -20,4 +25,6 @@ type PatternsApiResponse struct {
 	TotalCount uint             `json:"total_size"`
 	Patterns   []MesheryPattern `json:"patterns"`
 >>>>>>> 4c2ebc10... adds struct representation for patterns api response
+=======
+>>>>>>> 0753cdeb... adds api response model
 }
