2c41dd5c98a615ac9d7a7dbeec0279c3bd2b8d3c
s1ntaxe770r <cyberdev01@protonmail.com>
2021-04-15 23:06:34 +0100

    wip list command

--- mesheryctl/internal/cli/root/pattern/list.go
+++ mesheryctl/internal/cli/root/pattern/list.go
@@ -2,7 +2,9 @@ package pattern
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/layer5io/meshery/mesheryctl/internal/cli/root/config"
 	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
@@ -13,34 +15,62 @@ import (
 )
 
 var (
-	token string
+	token   string
+	allflag bool
 )
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "list pattern files",
-	Long:  "List available pattern files",
-	Args:  cobra.MinimumNArgs(1),
+	Long:  "Display list of all available pattern files",
+	Args:  cobra.MinimumNArgs(0),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		mctlCfg, err := config.GetMesheryCtl(viper.GetViper())
 		if err != nil {
 			return errors.Wrap(err, "error processing config")
 		}
-		var Patterns models.PatternsApiResponse
+		var response models.PatternsApiResponse
 
 		client := &http.Client{}
-		req, err := http.NewRequest("GET", mctlCfg.GetBaseMesheryURL()+"/api/experimental/patternfile/", nil)
+		req, err := http.NewRequest("GET", mctlCfg.GetBaseMesheryURL()+"/api/experimental/patternfile", nil)
 		if err != nil {
 			return err
 		}
-		err = utils.AddAuthDetails(req, "./auth.json")
+		err = utils.AddAuthDetails(req, token)
 		if err != nil {
 			return err
 		}
-		client.Do(req)
-		err = json.NewDecoder(req.Body).Decode(&Patterns)
+		res, err := client.Do(req)
 		if err != nil {
 			return err
 		}
-
+		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+		json.Unmarshal(body, &response)
+		if allflag == true {
+			for _, v := range response.Patterns {
+				headers := []string{"PATTERN ID", "NAME", "USER ID", "CREATED", "UPDATED"}
+				data := [][]string{
+					{v.ID.String(), v.Name, v.ID.String(), v.CreatedAt.Format(time.RFC3339Nano), v.UpdatedAt.Format(time.RFC3339Nano)},
+				}
+				utils.PrintToTable(headers, data)
+			}
+			return nil
+		}
+		for _, v := range response.Patterns {
+			headers := []string{"NAME", "USER ID", "CREATED", "UPDATED"}
+			data := [][]string{
+				{v.Name, v.ID.String(), v.CreatedAt.Format(time.RFC3339Nano), v.UpdatedAt.Format(time.RFC3339Nano)},
+			}
+			utils.PrintToTable(headers, data)
+		}
+		return nil
 	},
+}
+
+func init() {
+	listCmd.Flags().BoolVarP(&allflag, "all", "a", false, "Display full length user and pattern file identifiers")
+	listCmd.MarkFlagRequired("token")
 }
