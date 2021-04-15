38aa31c3acadbc50db827fc18534298849ec2968
s1ntaxe770r <cyberdev01@protonmail.com>
2021-04-15 23:05:27 +0100

    adds struct representation for patterns api response

--- models/meshery_patterns_api_response.go
+++ models/meshery_patterns_api_response.go
@@ -8,9 +8,16 @@ type PatternsAPIResponse struct {
 	Patterns   []MesheryPattern `json:"patterns"`
 =======
 type PatternsApiResponse struct {
+<<<<<<< HEAD
 	Page       uint              `json:"page"`
 	PageSize   uint              `json:"page_size"`
 	TotalCount uint              `json:"total_size"`
 	Patterns   *[]MesheryPattern `json:"patterns"`
 >>>>>>> 87573286... adds api response model
+=======
+	Page       uint             `json:"page"`
+	PageSize   uint             `json:"page_size"`
+	TotalCount uint             `json:"total_size"`
+	Patterns   []MesheryPattern `json:"patterns"`
+>>>>>>> 4c2ebc10... adds struct representation for patterns api response
 }
