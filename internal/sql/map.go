34fe37b3198c4c21ad13ccb05feaff4da896d285
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-04-18 16:48:11 +0530

    create support for map type in gorm
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

---
+++ internal/sql/map.go
@@ -0,0 +1,72 @@
+package sql
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+
+	"errors"
+)
+
+// Map type is an alias for map[string]interface{}
+//
+// It implements native SQL driver interfaces and hence can
+// be used for SQL json or jsonb types as a drop in replacement
+// of golang native maps
+type Map map[string]interface{}
+
+// Interface implements the nulls.nullable interface.
+func (m Map) Interface() interface{} {
+	return map[string]interface{}(m)
+}
+
+// Scan implements the sql.Scanner interface.
+// It allows to read the map from the database value.
+func (m *Map) Scan(src interface{}) error {
+	b, ok := src.([]byte)
+	if !ok {
+		return errors.New("scan source is not of type []byte")
+	}
+
+	if err := json.Unmarshal(b, m); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Value implements the driver.Valuer interface.
+// It allows to convert the map to a driver.value.
+func (m Map) Value() (driver.Value, error) {
+	b, err := json.Marshal(m)
+	if err != nil {
+		return nil, err
+	}
+
+	return string(b), nil
+}
+
+// UnmarshalJSON will unmarshall JSON value into
+// the map representation of this value.
+func (m Map) UnmarshalJSON(b []byte) error {
+	var stuff map[string]interface{}
+
+	if err := json.Unmarshal(b, &stuff); err != nil {
+		return err
+	}
+
+	for key, value := range stuff {
+		m[key] = value
+	}
+
+	return nil
+}
+
+// UnmarshalText will unmarshall text value into
+// the map representation of this value.
+func (m Map) UnmarshalText(text []byte) error {
+	if err := json.Unmarshal(text, &m); err != nil {
+		return err
+	}
+
+	return nil
+}
