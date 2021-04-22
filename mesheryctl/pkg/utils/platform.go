461289cca1b1e6630275dd5944a4f845e78ac0a7
s1ntaxe770r <cyberdev01@protonmail.com>
2021-04-22 19:36:12 +0100

    fix runtime panic caused by assertion error
    Signed-off-by: s1ntaxe770r <cyberdev01@protonmail.com>

--- mesheryctl/pkg/utils/platform.go
+++ mesheryctl/pkg/utils/platform.go
@@ -156,8 +156,11 @@ func GetLatestStableReleaseTag() (string, error) {
 	if err := json.Unmarshal(body, &dat); err != nil {
 		return "", errors.Wrap(err, "failed to unmarshal json into object")
 	}
-
-	return dat["tag_name"].(string), nil
+	null := ""
+	if dat["tag_name"] != nil {
+		null = dat["tag_name"].(string)
+	}
+	return null, nil
 }
 
 // IsAdapterValid checks if the adapter mentioned by the user is a valid adapter
