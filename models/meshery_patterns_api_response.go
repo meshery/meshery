55a519b951a3c283617382161bfb2a0fd37322f9
s1ntaxe770r <cyberdev01@protonmail.com>
2021-04-22 20:06:45 +0100

    fix merge conflict
    Signed-off-by: s1ntaxe770r <cyberdev01@protonmail.com>

--- models/meshery_patterns_api_response.go
+++ models/meshery_patterns_api_response.go
@@ -1,15 +1,8 @@
 package models
 
 type PatternsApiResponse struct {
-<<<<<<< HEAD
 	Page       uint             `json:"page"`
 	PageSize   uint             `json:"page_size"`
 	TotalCount uint             `json:"total_size"`
 	Patterns   []MesheryPattern `json:"patterns"`
-=======
-	Page       uint              `json:"page"`
-	PageSize   uint              `json:"page_size"`
-	TotalCount uint              `json:"total_size"`
-	Patterns   *[]MesheryPattern `json:"patterns"`
->>>>>>> 0753cdebf0bdc94854c8d3b13dea8017790aea4f
 }
