4307c3220a8ee98fecf9c503141fd06f2d1c6b96
Piyush Singariya <piyushsingariya@gmail.com>
2021-04-24 12:24:51 +0530

    golint errors corrected
    Signed-off-by: Piyush Singariya <piyushsingariya@gmail.com>

--- mesheryctl/internal/cli/root/system/check.go
+++ mesheryctl/internal/cli/root/system/check.go
@@ -28,32 +28,30 @@ func runPreDeploymentChecks() error {
 	//Check whether docker daemon is running or not
 	if err := exec.Command("docker", "ps").Run(); err != nil {
 		log.Error("Docker is not running")
-	} else {
-		log.Info("√ Docker is running")
 	}
+	log.Info("√ Docker is running")
+
 	//Check for installed docker-compose on client system
 	if err := exec.Command("docker-compose", "-v").Run(); err != nil {
 		log.Info("Docker-Compose is not available")
-	} else {
-		log.Info("√ docker-compose is available")
 	}
+	log.Info("√ docker-compose is available")
 
 	log.Info("\nKubernetes API \n--------------")
 	//Check whether k8s client can be initialized
 	client, err := meshkitkube.New([]byte(""))
 	if err != nil {
 		return errors.New("cannot initialize the client")
-	} else {
-		log.Info("√ can initialize the client")
 	}
+	log.Info("√ can initialize the client")
+
 	//Check whether kubernetes can be queried
 	podInterface := client.KubeClient.CoreV1().Pods("")
 	_, err = podInterface.List(context.TODO(), v1.ListOptions{})
 	if err != nil {
 		return errors.New("cannot query the Kubernetes API")
-	} else {
-		log.Info("√ can query the Kubernetes API")
 	}
+	log.Info("√ can query the Kubernetes API")
 
 	log.Info("\nKubernetes Version \n--------------")
 	//Check whether system has minimum supported versions of kubernetes and kubectl
