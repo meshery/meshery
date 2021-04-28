9f8ee15c257d2546c859f82e35cf9b72405e5f99
metonymic-smokey <ahuja.aditi@gmail.com>
2021-04-28 17:48:41 +0530

    minor changes for docker endpoint
    Signed-off-by: metonymic-smokey <ahuja.aditi@gmail.com>

--- mesheryctl/internal/cli/root/system/start.go
+++ mesheryctl/internal/cli/root/system/start.go
@@ -197,7 +197,7 @@ func start() error {
 		//check for container meshery_meshery_1 running status
 		for _, container := range containers {
 			if container.Names[0] == "/meshery_meshery_1" {
-				log.Info("Opening Meshery in your browser. If Meshery does not open, please point your browser to http://localhost:9081 to access Meshery.")
+				log.Info("Opening Meshery in your browser. If Meshery does not open, please point your browser to the endpoint URL to access Meshery.")
 
 				//check for os of host machine
 				if runtime.GOOS == "windows" {
