6da5e0f00c6afe0afcf4498bf5f9c871a114e0d0
Piyush Singariya <piyushsingariya@gmail.com>
2021-05-01 10:30:04 +0530

    finalised stuff
    Signed-off-by: Piyush Singariya <piyushsingariya@gmail.com>

--- mesheryctl/internal/cli/root/system/check.go
+++ mesheryctl/internal/cli/root/system/check.go
@@ -39,19 +39,33 @@ var PreCheckCmd = &cobra.Command{
 
 //Run preflight healthchecks to verify environment health
 func runPreflightHealthChecks(isPreRunExecution bool, subcommand string, platform string) error {
-	//Run docker healthchecks
-	if err := runDockerHealthCheck(isPreRunExecution, subcommand, platform); err != nil {
-		return err
+	//Docker healthchecks are only invoked when it's not a PreRunExecution
+	// or it's a PreRunExecution and current platform is docker
+	if !isPreRunExecution || (isPreRunExecution && platform == "docker") {
+		//Run docker healthchecks
+		if err := runDockerHealthCheck(isPreRunExecution, subcommand, platform); err != nil {
+			return err
+		}
 	}
-	//Run k8s API healthchecks
-	if err = runKubernetesAPIHealthCheck(); err != nil {
-		return err
+	//Kubernetes healthchecks are only invoked when it's not a PreRunExecution
+	// or it's a PreRunExecution and current platform is kubernetes
+	if !isPreRunExecution || (isPreRunExecution && platform == "kubernetes") {
+		//Run k8s API healthchecks
+		if err = runKubernetesAPIHealthCheck(isPreRunExecution); err != nil {
+			return err
+		}
 	}
-	//Run k8s plus kubectl minimum version healthchecks
-	if err := runKubernetesVersionHealthCheck(); err != nil {
-		return err
+	//KubernetesVersion healthchecks are only invoked when it's not a PreRunExecution
+	// or it's a PreRunExecution and current platform is kubernetes
+	if !isPreRunExecution || (isPreRunExecution && platform == "kubernetes") {
+		//Run k8s plus kubectl minimum version healthchecks
+		if err := runKubernetesVersionHealthCheck(isPreRunExecution); err != nil {
+			return err
+		}
 	}
-	log.Info("\n--------------\n--------------\n✓✓ Meshery prerequisites met")
+	if !isPreRunExecution {
+		log.Info("\n--------------\n--------------\n✓✓ Meshery prerequisites met")
+	}
 	return nil
 }
 
@@ -64,7 +78,7 @@ func runDockerHealthCheck(isPreRunExecution bool, subcommand string, platform st
 	if err := exec.Command("docker", "ps").Run(); err != nil {
 		log.Warn("!! Docker is not running")
 		//If preRunExecution and the current platform is docker then we trigger docker installation
-		if isPreRunExecution && platform == "docker" {
+		if isPreRunExecution {
 			//No auto installation of docker for windows
 			if runtime.GOOS == "windows" {
 				return errors.Wrapf(err, "Please start Docker. Run `mesheryctl system %s` once Docker is started.", subcommand)
@@ -82,7 +96,7 @@ func runDockerHealthCheck(isPreRunExecution bool, subcommand string, platform st
 	//Check for installed docker-compose on client system
 	if err := exec.Command("docker-compose", "-v").Run(); err != nil {
 		log.Warn("!! docker-compose is not available")
-		if isPreRunExecution && platform == "docker" {
+		if isPreRunExecution {
 			//No auto installation of Docker-compose for windows
 			if runtime.GOOS == "windows" {
 				return errors.Wrapf(err, "please install docker-compose. Run `mesheryctl system %s` after docker-compose is installed.", subcommand)
@@ -101,14 +115,18 @@ func runDockerHealthCheck(isPreRunExecution bool, subcommand string, platform st
 }
 
 //Run healthchecks to verify if kubernetes client can be initialized and can be queried
-func runKubernetesAPIHealthCheck() error {
-	log.Info("\nKubernetes API \n--------------")
+func runKubernetesAPIHealthCheck(isPreRunExecution bool) error {
+	if !isPreRunExecution {
+		log.Info("\nKubernetes API \n--------------")
+	}
 	//Check whether k8s client can be initialized
 	client, err := meshkitkube.New([]byte(""))
 	if err != nil {
 		return errors.New("!! cannot initialize the client")
 	}
-	log.Info("√ can initialize the client")
+	if !isPreRunExecution {
+		log.Info("√ can initialize the client")
+	}
 
 	//Check whether kubernetes can be queried
 	podInterface := client.KubeClient.CoreV1().Pods("")
@@ -116,15 +134,19 @@ func runKubernetesAPIHealthCheck() error {
 	if err != nil {
 		return errors.New("!! cannot query the Kubernetes API")
 	}
-	log.Info("√ can query the Kubernetes API")
+	if !isPreRunExecution {
+		log.Info("√ can query the Kubernetes API")
+	}
 
 	return nil
 }
 
 //Run healthchecks to verify kubectl and kubenetes version with
 // minimum compatible versions
-func runKubernetesVersionHealthCheck() error {
-	log.Info("\nKubernetes Version \n--------------")
+func runKubernetesVersionHealthCheck(isPreRunExecution bool) error {
+	if !isPreRunExecution {
+		log.Info("\nKubernetes Version \n--------------")
+	}
 	//Check whether system has minimum supported versions of kubernetes and kubectl
 	var kubeVersion *k8sVersion.Info
 	kubeVersion, err := utils.GetK8sVersionInfo()
@@ -134,12 +156,16 @@ func runKubernetesVersionHealthCheck() error {
 	if err = utils.CheckK8sVersion(kubeVersion); err != nil {
 		return err
 	}
-	log.Info("√ is running the minimum Kubernetes version")
+	if !isPreRunExecution {
+		log.Info("√ is running the minimum Kubernetes version")
+	}
 
 	if err = utils.CheckKubectlVersion(); err != nil {
 		return err
 	}
-	log.Info("√ is running the minimum kubectl version")
+	if !isPreRunExecution {
+		log.Info("√ is running the minimum kubectl version")
+	}
 
 	return nil
 }
