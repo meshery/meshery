a453ca930993172bda82cde98afddefcab2e5eb0
Piyush Singariya <piyushsingariya@gmail.com>
2021-05-13 12:19:56 +0530

    in progress
    Signed-off-by: Piyush Singariya <piyushsingariya@gmail.com>

--- mesheryctl/internal/cli/root/perf/list.go
+++ mesheryctl/internal/cli/root/perf/list.go
@@ -1,13 +1,9 @@
 package perf
 
 import (
-	"github.com/pkg/errors"
-
-	"github.com/layer5io/meshery/mesheryctl/internal/cli/root/config"
 	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var listCmd = &cobra.Command{
@@ -15,19 +11,16 @@ var listCmd = &cobra.Command{
 	Short:   "List Performance profiles",
 	Long:    `List all the available performance profiles`,
 	Example: "mesheryctl perf list",
-	Args:    cobra.NoArgs,
+	Args:    cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// Get viper instance used for context
-		mctlCfg, err := config.GetMesheryCtl(viper.GetViper())
-		if err != nil {
-			return errors.Wrap(err, "error processing config")
-		}
-		performanceProfiles := mctlCfg.PerfProfiles
-		var data [][]string
-		for _, profile := range performanceProfiles {
-			data = append(data, []string{profile.Name})
-		}
-		utils.PrintToTable([]string{"NAME", "RESULTS", "LAST-RUN"}, data)
+		// mctlCfg, err := config.GetMesheryCtl(viper.GetViper())
+		// if err != nil {
+		// 	return errors.Wrap(err, "error processing config")
+		// }
 
+		var data [][]string
+		utils.PrintToTable([]string{"NAME", "RESULTS", "LAST-RUN"}, data)
+		return nil
 	},
 }
