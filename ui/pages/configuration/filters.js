5ba67dc1ae95408a8c4902e23515373294973285
Gaurav Chadha <gauravchadha1676@gmail.com>
2021-05-28 13:45:45 +0530

    data-error
    Signed-off-by: Gaurav Chadha <gauravchadha1676@gmail.com>

--- ui/pages/configuration/filters.js
+++ ui/pages/configuration/filters.js
@@ -5,8 +5,8 @@ import { connect } from "react-redux";
 import { bindActionCreators } from 'redux'
 import Head from 'next/head';
 import { getPath } from "../../lib/path";
-import { resolveHref } from "next/dist/next-server/lib/router/router";
-import dataFetch from "../../lib/data-fetch";
+// import { resolveHref } from "next/dist/next-server/lib/router/router";
+// import dataFetch from "../../lib/data-fetch";
 
 const styles = {
   paper: {
@@ -22,27 +22,27 @@ class NewFilters extends React.Component {
     this.props.updatepagepath({ path: getPath() });
   }
 
-  async componentsDidMount() {
-    await new Promise(resolve => {
-      dataFetch(
-        `/api/experimental/filter${query}`,
-        {
-          credentials: "include",
-        },
-        (result) => {
-          console.log("FilterFile API", `/api/experimental/filter${query}`);
-          updateProgress({ showProgress: false });
-          if (result) {
-            setFilters(result.filters || []);
-            setPage(result.page || 0);
-            setPageSize(result.page_size || 0);
-            setCount(result.total_count || 0);
-          }
-        },
-        handleError
-      );
-    })
-  }
+  // async componentsDidMount() {
+  //   await new Promise(resolve => {
+  //     dataFetch(
+  //       `/api/experimental/filter${query}`,
+  //       {
+  //         credentials: "include",
+  //       },
+  //       (result) => {
+  //         console.log("FilterFile API", `/api/experimental/filter${query}`);
+  //         updateProgress({ showProgress: false });
+  //         if (result) {
+  //           setFilters(result.filters || []);
+  //           setPage(result.page || 0);
+  //           setPageSize(result.page_size || 0);
+  //           setCount(result.total_count || 0);
+  //         }
+  //       },
+  //       handleError
+  //     );
+  //   })
+  // }
 
   render () {
     return (
