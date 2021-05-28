a00a6fd23242ad74fadfde67d18be517f653da56
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-05-28 19:45:48 +0530

    add generic rollout interface
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

---
+++ models/pattern/patterns/application/application.go
@@ -0,0 +1,229 @@
+package application
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/layer5io/meshkit/models/oam/core/v1alpha1"
+	meshkube "github.com/layer5io/meshkit/utils/kubernetes"
+	"github.com/sirupsen/logrus"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+type serviceMesh string
+
+const (
+	istio serviceMesh = "istio"
+)
+
+type rolloutEngine string
+
+const (
+	argo rolloutEngine = "argo"
+)
+
+type ApplicationPatternSetting struct {
+	Replicas   int                      `json:"replicas,omitempty"`
+	Mesh       serviceMesh              `json:"mesh,omitempty"`
+	Containers []RolloutEngineContainer `json:"containers,omitempty"`
+}
+
+type ApplicationPatternConfiguration struct {
+	RolloutStrategy *ApplicationPatternConfigurationRolloutStrategy `json:"rollout_strategy,omitempty"`
+}
+
+type ApplicationPatternConfigurationRolloutStrategy struct {
+}
+
+// Deploy will deploy the application
+func Deploy(
+	kclient *meshkube.Client,
+	oamComp v1alpha1.Component,
+	oamConfig v1alpha1.Configuration,
+	isDel bool,
+) error {
+	if oamComp.Spec.Type == "Application" {
+		settings, err := getApplicationPatternSettings(oamComp)
+		if err != nil {
+			return err
+		}
+		config, err := getApplicationPatternConfiguration(oamComp.Name, oamConfig)
+		if err != nil {
+			return nil
+		}
+
+		mesh := settings.Mesh
+		if mesh == "" {
+			mesh = detectServiceMesh(kclient)
+		}
+
+		var engineName rolloutEngine
+		detectedEngine := detectRolloutEngine(kclient)
+		if detectedEngine == "" {
+			engineName = argo
+		} else {
+			engineName = detectedEngine
+		}
+
+		engine, err := NewRolloutEngine(kclient, string(engineName))
+		if err != nil {
+			return err
+		}
+
+		// If no engine was detected then proceed to install the default one
+		if detectedEngine == "" {
+			if err := engine.Install(); err != nil {
+				return err
+			}
+		}
+
+		if config.RolloutStrategy == nil {
+			if err := engine.Native(RolloutEngineGenericOptions{
+				Name:        oamComp.Name,
+				Namespace:   oamComp.Namespace,
+				ServiceMesh: string(mesh),
+				Replicas:    settings.Replicas,
+				Containers:  settings.Containers,
+				Delete:      isDel,
+			}); err != nil {
+				return err
+			}
+
+			return nil
+		}
+
+		return fmt.Errorf("strategy not supported")
+	}
+
+	return fmt.Errorf("%s is not an application pattern", oamComp.Spec.Type)
+}
+
+func getApplicationPatternSettings(oamComp v1alpha1.Component) (ApplicationPatternSetting, error) {
+	var settings ApplicationPatternSetting
+
+	jsonByt, err := json.Marshal(oamComp.Spec.Settings)
+	if err != nil {
+		return ApplicationPatternSetting{}, err
+	}
+
+	if err := json.Unmarshal(jsonByt, &settings); err != nil {
+		return settings, err
+	}
+
+	return settings, nil
+}
+
+func getApplicationPatternConfiguration(compName string, oamConfig v1alpha1.Configuration) (ApplicationPatternConfiguration, error) {
+	var config ApplicationPatternConfiguration
+
+	for _, cfg := range oamConfig.Spec.Components {
+		if cfg.ComponentName == compName {
+			// Find the "RolloutStrategy" trait
+			for _, trait := range cfg.Traits {
+				if trait.Name == "RolloutStrategy" {
+					jsonByt, err := json.Marshal(trait.Properties)
+					if err != nil {
+						return config, err
+					}
+
+					if err := json.Unmarshal(jsonByt, &config); err != nil {
+						return config, err
+					}
+
+					break
+				}
+			}
+		}
+	}
+
+	return config, nil
+}
+
+// detectServiceMesh will detect available service mesh in the k8s cluster
+func detectServiceMesh(kclient *meshkube.Client) serviceMesh {
+	if detectIstioServiceMesh(kclient) {
+		return istio
+	}
+
+	return ""
+}
+
+// detectRolloutEngine will detect available rollout engine in the k8s cluster
+func detectRolloutEngine(kclient *meshkube.Client) rolloutEngine {
+	if detectArgoRollout(kclient) {
+		return argo
+	}
+
+	return ""
+}
+
+// detectIstioServiceMesh returns true if k8s cluster has istio service mesh installed
+func detectIstioServiceMesh(kclient *meshkube.Client) bool {
+	svcs := []string{"istiod"}
+	nsList := findNamespaces(kclient)
+
+	for _, deployment := range svcs {
+		for _, ns := range nsList {
+			if _, err := kclient.
+				DynamicKubeClient.
+				Resource(schema.GroupVersionResource{
+					Group:    "",
+					Version:  "v1",
+					Resource: "services",
+				}).
+				Namespace(ns).
+				Get(context.TODO(), deployment, v1.GetOptions{}); err == nil {
+				logrus.Debug("found istio in the cluster")
+				return true
+			}
+
+			logrus.Debugf("%s not found in namespace %s", deployment, ns)
+		}
+	}
+
+	return true
+}
+
+// detectArgoRollout returns true if argo rollout is present in the cluster
+func detectArgoRollout(kclient *meshkube.Client) bool {
+	deployments := []string{"argo-rollouts"}
+	nsList := findNamespaces(kclient)
+
+	for _, deployment := range deployments {
+		for _, ns := range nsList {
+			if _, err := kclient.
+				DynamicKubeClient.
+				Resource(schema.GroupVersionResource{
+					Group:    "apps",
+					Version:  "v1",
+					Resource: "deployments",
+				}).
+				Namespace(ns).
+				Get(context.TODO(), deployment, v1.GetOptions{}); err == nil {
+				logrus.Debug("found argo rollout in the cluster")
+				return true
+			}
+
+			logrus.Debugf("%s not found in namespace %s", deployment, ns)
+		}
+	}
+
+	return false
+}
+
+func findNamespaces(kClient *meshkube.Client) []string {
+	list, err := kClient.KubeClient.CoreV1().Namespaces().List(context.TODO(), v1.ListOptions{})
+	if err != nil {
+		return []string{}
+	}
+
+	res := []string{}
+
+	for _, ns := range list.Items {
+		res = append(res, ns.Name)
+	}
+
+	return res
+}
---
+++ models/pattern/patterns/application/engine.go
@@ -0,0 +1,64 @@
+package application
+
+import (
+	"fmt"
+
+	meshkube "github.com/layer5io/meshkit/utils/kubernetes"
+	v1 "k8s.io/api/core/v1"
+)
+
+type RolloutEngineGenericOptions struct {
+	Name        string
+	Namespace   string
+	ServiceMesh string
+	Replicas    int
+	Containers  []RolloutEngineContainer
+	Delete      bool
+}
+
+type RolloutEngineContainer struct {
+	Name      string
+	Image     string
+	Ports     []RolloutEngineContainerPort
+	Resources []RolloutEngineContainerResource
+}
+
+type RolloutEngineContainerPort struct {
+	Name          string
+	ContainerPort int
+}
+
+type RolloutEngineContainerResource struct {
+	v1.ResourceRequirements
+}
+
+type RolloutEngineCanaryOptions struct {
+	RolloutEngineGenericOptions
+	Steps []RolloutEngineCanaryStep
+}
+
+type RolloutEngineCanaryStep struct {
+	SetWeight int
+	Steps     []RolloutEngineCanaryStepPause
+}
+
+type RolloutEngineCanaryStepPause struct {
+	Duration int
+	// Inline Metrics - for the future
+}
+
+type RolloutEngine interface {
+	// Canary(RolloutEngineCanaryOptions) error
+	Install() error
+	Native(RolloutEngineGenericOptions) error
+}
+
+func NewRolloutEngine(kubeclient *meshkube.Client, typ string) (RolloutEngine, error) {
+	if typ == "argo" {
+		return &ArgoRollout{
+			kubeclient: kubeclient,
+		}, nil
+	}
+
+	return nil, fmt.Errorf("invalid rollout engine type: \"%s\"", typ)
+}
