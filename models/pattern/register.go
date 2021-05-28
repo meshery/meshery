78a18702b162b3c572350bd6b0bae513a5e68d8b
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-05-28 19:42:30 +0530

    add application workloads
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- models/pattern/register.go
+++ models/pattern/register.go
@@ -175,7 +175,7 @@ func RegisterMesheryOAMWorkloads() error {
 	// accordingly
 	rootPath, _ := filepath.Abs("../oam/workloads")
 
-	return registerMesheryServerOAM(rootPath, []string{}, RegisterWorkload)
+	return registerMesheryServerOAM(rootPath, []string{"application"}, RegisterWorkload)
 }
 
 // registerMesheryServerOAM will read the oam definition file and its corresponding schema file
