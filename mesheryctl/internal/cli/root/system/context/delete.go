ff35ed4af01ea185ff9d393d0a53caa882050f0a
metonymic-smokey <ahuja.aditi@gmail.com>
2021-05-29 17:56:12 +0530

    interactive selection of new current context
    Signed-off-by: metonymic-smokey <ahuja.aditi@gmail.com>

--- mesheryctl/internal/cli/root/system/context/delete.go
+++ mesheryctl/internal/cli/root/system/context/delete.go
@@ -6,6 +6,11 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	"fmt"
+
+	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
+	"github.com/manifoldco/promptui"
 )
 
 // deleteContextCmd represents the delete command
@@ -23,13 +28,43 @@ var deleteContextCmd = &cobra.Command{
 		if !exists {
 			return errors.New("no context to delete")
 		}
-		delete(configuration.Contexts, args[0])
+
 		if viper.GetString("current-context") == args[0] {
-			viper.Set("current-context", "")
+			var res bool
+			res = utils.AskForConfirmation("Are you sure you want to delete the current context")
+
+			if !res {
+				log.Printf("Delete aborted")
+				return nil
+			}
+
+			var listContexts []string
+			for context := range configuration.Contexts {
+				if context != args[0] {
+					listContexts = append(listContexts, context)
+				}
+			}
+
+			prompt := promptui.Select{
+				Label: "Select context",
+				Items: listContexts,
+			}
+
+			_, result, err := prompt.Run()
+
+			if err != nil {
+				fmt.Printf("Prompt failed %v\n", err)
+				return err
+			}
+
+			fmt.Printf("The current context is now %q\n", result)
+			viper.Set("current-context", result)
 		}
+		delete(configuration.Contexts, args[0])
 		viper.Set("contexts", configuration.Contexts)
 		log.Printf("deleted context %s", args[0])
 		err = viper.WriteConfig()
+
 		return err
 	},
 }
