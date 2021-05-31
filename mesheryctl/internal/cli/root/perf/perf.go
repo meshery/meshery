f1f5d1b222315676317dfa123c95b2b5972b4cb4
Piyush Singariya <piyushsingariya@gmail.com>
2021-05-31 23:19:11 +0530

    perf changes
    Signed-off-by: Piyush Singariya <piyushsingariya@gmail.com>

--- mesheryctl/internal/cli/root/perf/perf.go
+++ mesheryctl/internal/cli/root/perf/perf.go
@@ -15,22 +15,14 @@
 package perf
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 
 	"github.com/pkg/errors"
 
-	"github.com/layer5io/meshery/mesheryctl/internal/cli/root/config"
 	"github.com/layer5io/meshery/mesheryctl/internal/cli/root/system"
 	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
 
-	log "github.com/sirupsen/logrus"
-
-	"github.com/asaskevich/govalidator"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var (
@@ -54,101 +46,6 @@ var PerfCmd = &cobra.Command{
 		return system.RunPreflightHealthChecks(true, cmd.Use)
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// Importing SMP Configuration from the file
-		var req *http.Request
-		if filePath != "" {
-			smpConfig, err := ioutil.ReadFile(filePath)
-
-			if err != nil {
-				return err
-			}
-
-			mctlCfg, err := config.GetMesheryCtl(viper.GetViper())
-			if err != nil {
-				return errors.Wrap(err, "error processing config")
-			}
-
-			req, err = http.NewRequest("POST", mctlCfg.GetBaseMesheryURL()+"/api/perf/load-test-smp", bytes.NewBuffer(smpConfig))
-			if err != nil {
-				return errors.Wrapf(err, utils.PerfError("Failed to invoke performance test"))
-			}
-		} else {
-			var err error
-
-			if testName == "" {
-				log.Debug("Test Name not provided")
-				testName = utils.StringWithCharset(8)
-				log.Debug("Using random test name: ", testName)
-			}
-
-			if testURL == "" {
-				return errors.New(utils.PerfError("please enter a test URL"))
-			}
-
-			// Method to check if the entered Test URL is valid or not
-			var validURL = govalidator.IsURL(testURL)
-
-			if !validURL {
-				return errors.New(utils.PerfError("please enter a valid test URL"))
-			}
-
-			mctlCfg, err := config.GetMesheryCtl(viper.GetViper())
-			if err != nil {
-				return errors.Wrap(err, "error processing config")
-			}
-
-			req, err = http.NewRequest("POST", mctlCfg.GetBaseMesheryURL()+"/api/perf/load-test", nil)
-			if err != nil {
-				return errors.Wrapf(err, utils.PerfError("Failed to invoke performance test"))
-			}
-
-			q := req.URL.Query()
-
-			q.Add("name", testName)
-			q.Add("loadGenerator", loadGenerator)
-			q.Add("c", concurrentRequests)
-			q.Add("url", testURL)
-			q.Add("qps", qps)
-
-			durLen := len(testDuration)
-
-			q.Add("dur", string(testDuration[durLen-1]))
-			q.Add("t", string(testDuration[:durLen-1]))
-
-			if testMesh != "" {
-				q.Add("mesh", testMesh)
-			}
-			req.URL.RawQuery = q.Encode()
-
-		}
-
-		if err := utils.AddAuthDetails(req, tokenPath); err != nil {
-			return errors.Wrap(err, utils.PerfError("failed to add auth details to request"))
-		}
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			return errors.Wrapf(err, utils.PerfError(fmt.Sprintf("failed to make request to %s", testURL)))
-		}
-		if utils.ContentTypeIsHTML(resp) {
-			return errors.New("failed to run test")
-		}
-		log.Debug("Initiating Performance test ...")
-		log.Debug(resp.Status)
-
-		defer utils.SafeClose(resp.Body)
-		data, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return errors.Wrap(err, utils.PerfError("failed to read response body"))
-		}
-		log.Debug(string(data))
-
-		if err := utils.UpdateAuthDetails(tokenPath); err != nil {
-			return errors.Wrap(err, utils.PerfError("failed to update auth details"))
-		}
-
-		log.Debug("Test Completed Successfully!")
 		return nil
 	},
 }
