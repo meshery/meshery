42ef483452663e1aa49809524720366609770522
dhruv0000 <patel.4@iitj.ac.in>
2021-06-22 04:24:51 +0530

    Fix auto config of graphs in presence of an old config
    Signed-off-by: dhruv0000 <patel.4@iitj.ac.in>

--- ui/components/PrometheusComponent.js
+++ ui/components/PrometheusComponent.js
@@ -128,20 +128,37 @@ class PrometheusComponent extends Component {
 
   componentDidMount() {
     const self = this;
-    let selector = {
-      serviceMesh: "ALL_MESH",
-    };
-    fetchAvailableAddons(selector).subscribe({
-      next: (res) => {
-        res?.addonsState?.forEach((addon) => {
-          if (addon.name === "prometheus" && self.state.prometheusURL === "") {
-            self.setState({ prometheusURL: "http://" + addon.endpoint })
-            submitPrometheusConfigure(self);
-          }
-        });
+
+    dataFetch(
+      "/api/prometheus/config",
+      {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        },
       },
-      error: (err) => console.log("error registering prometheus: " + err),
-    });
+      (result) => {
+        self.props.updateProgress({ showProgress: false });
+        if (typeof result !== "undefined" && result?.prometheusURL && result?.prometheusURL !="") {
+          let selector = {
+            serviceMesh: "ALL_MESH",
+          };
+          fetchAvailableAddons(selector).subscribe({
+            next: (res) => {
+              res?.addonsState?.forEach((addon) => {
+                if (addon.name === "prometheus" && self.state.prometheusURL === "") {
+                  self.setState({ prometheusURL: "http://" + addon.endpoint })
+                  submitPrometheusConfigure(self);
+                }
+              });
+            },
+            error: (err) => console.log("error registering prometheus: " + err),
+          });
+        }
+      },
+      self.handleError("There was an error communicating with grafana config")
+    )
   }
 
   static getDerivedStateFromProps(props, state) {
