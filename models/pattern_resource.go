188c67db8e6490b50b3ce79fae67082fa0acf322
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-06-23 19:54:20 +0530

    add pattern resource struct
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

---
+++ models/pattern_resource.go
@@ -0,0 +1,16 @@
+package models
+
+import "github.com/gofrs/uuid"
+
+// PatternResource represents a pattern resource that is provisioned
+// by meshery
+type PatternResource struct {
+	ID        *uuid.UUID `json:"id,omitempty"`
+	UserID    *uuid.UUID `json:"user_id,omitempty"`
+	Name      string     `json:"name,omitempty"`
+	Namespace string     `json:"namespace,omitempty"`
+	Type      string     `json:"type,omitempty"`
+	OAMType   string     `json:"oam_type,omitempty"`
+	Deleted   bool       `json:"deleted,omitempty"`
+	// History   []PatternResource `json:"history,omitempty"` // Maybe reused when audit trail arrives
+}
