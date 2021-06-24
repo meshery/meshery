3bd71f1a80ec966ee4977ea4803176f080262a3f
Piyush Singariya <piyushsingariya@gmail.com>
2021-06-24 08:39:42 +0530

    edited build test for mesheryctl
    Signed-off-by: Piyush Singariya <piyushsingariya@gmail.com>

--- mesheryctl/build_test.go
+++ mesheryctl/build_test.go
@@ -12,20 +12,12 @@ var update = flag.Bool("update", false, "update golden files")
 var binaryName = "mesheryctl"
 var binaryPath string
 
-func TestMain(m *testing.M) {
-	// move one directory up
-	err := os.Chdir(".")
-	if err != nil {
-		fmt.Printf("could not change dir: %v", err)
-		os.Exit(1)
-	}
+func TestMain(t *testing.T) {
 	// create a mesheryctl binary file
 	make := exec.Command("make")
-	err = make.Run()
+	err := make.Run()
 	if err != nil {
 		fmt.Printf("could not make binary for %s:%v", binaryName, err)
 		os.Exit(1)
 	}
-
-	os.Exit(m.Run())
 }
