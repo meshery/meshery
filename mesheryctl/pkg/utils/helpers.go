bbaa360679d3679f89cca597b80cc39554d9adca
Ayush PS <ayushpratap16@gmail.com>
2021-06-24 18:52:53 +0530

    Hack to fix the spinner info
    Signed-off-by: Ayush PS <ayushpratap16@gmail.com>

--- mesheryctl/pkg/utils/helpers.go
+++ mesheryctl/pkg/utils/helpers.go
@@ -798,6 +798,6 @@ func CreateDefaultSpinner(suffix string, finalMsg string) *spinner.Spinner {
 	s := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
 
 	s.Suffix = " " + suffix
-	s.FinalMSG = "\n" + finalMsg + "\n"
+	s.FinalMSG = finalMsg + "\n"
 	return s
 }
