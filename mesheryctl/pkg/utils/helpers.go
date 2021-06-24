3839e63f07bf18474013f4327d9f3a4c4311bff3
Ayush PS <ayushpratap16@gmail.com>
2021-06-24 18:52:53 +0530

    Hack to fix the spinner info
    Signed-off-by: Ayush PS <ayushpratap16@gmail.com>
    Signed-off-by: kaushambisharma <sharma.kaushambi@gmail.com>

--- mesheryctl/pkg/utils/helpers.go
+++ mesheryctl/pkg/utils/helpers.go
@@ -806,7 +806,7 @@ func CreateDefaultSpinner(suffix string, finalMsg string) *spinner.Spinner {
 	s := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
 
 	s.Suffix = " " + suffix
-	s.FinalMSG = "\n" + finalMsg + "\n"
+	s.FinalMSG = finalMsg + "\n"
 	return s
 =======
 // PrintToTableInStringFormat prints the given data into a table format but return as a string
