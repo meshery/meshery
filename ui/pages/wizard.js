f27957a560dc51c3402cdfc9ff90d5541a20fc98
Adithya Krishna <aadithya794@gmail.com>
2021-07-06 18:11:16 +0530

    FC v11
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- ui/pages/wizard.js
+++ ui/pages/wizard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import { withStyles, NoSsr } from '@material-ui/core';
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux'
@@ -10,43 +9,22 @@ import { getPath } from "../lib/path";
 import ConfigurationWizard from '../components/ConfigurationWizard'
 
 const styles = {
-=======
-import { updatepagepath, updatepagetitle } from "../lib/store";
-import { connect } from "react-redux";
-import { bindActionCreators } from 'redux'
-import Head from 'next/head';
-import { getPath } from "../lib/path";
-import { Paper, withStyles, NoSsr } from '@material-ui/core';
-
-const styles = {
-  paper: {
-    maxWidth: '90%',
-    margin: 'auto',
-    overflow: 'hidden',
-  }
->>>>>>> dec1c397 (Connect PopUp to config wizard path)
 }
 
 class Wizard extends React.Component {
-  componentDidMount () {
+  componentDidMount() {
     console.log(`path: ${getPath()}`);
     this.props.updatepagepath({ path: getPath() });
     this.props.updatepagetitle({ title: 'Configuration Wizard' });
   }
 
-  render () {
+  render() {
     return (
       <NoSsr>
         <Head>
           <title>Configuration Wizard | Meshery</title>
         </Head>
-<<<<<<< HEAD
-        <ConfigurationWizard/>
-=======
-        <Paper className={this.props.classes.paper}>
-          <h1>Wizard</h1>
-        </Paper>
->>>>>>> dec1c397 (Connect PopUp to config wizard path)
+        <ConfigurationWizard />
       </NoSsr>
     );
   }
