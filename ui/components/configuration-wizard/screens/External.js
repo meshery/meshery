29bc07ccd53dd796a407e4bbcbd85da05fdb7809
Adithya Krishna <aadithya794@gmail.com>
2021-07-06 17:50:37 +0530

    Fixed Package Lock Conflixts
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- ui/components/configuration-wizard/screens/External.js
+++ ui/components/configuration-wizard/screens/External.js
@@ -1,30 +1,94 @@
-import React from "react";
-import { makeStyles, Container, Fade } from "@material-ui/core/";
+import React from 'react'
+import {
+  makeStyles,
+  Container,
+  Fade,
+  Button
+} from '@material-ui/core/'
 
-import GrafanaIcon from "../icons/GrafanaIcon.js";
-import PrometheusIcon from "../icons/PrometheusIcon.js";
-import ConfigCard from "./ConfigCard";
+import GrafanaIcon from '../icons/GrafanaIcon'
+import PrometheusIcon from '../icons/PrometheusIcon'
+import ConfigCard from './ConfigCard'
 
 const useStyles = makeStyles({
-  cardContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    padding: "2rem 6rem",
+  container: {
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    height: '35rem',
+    padding: '2rem 6rem',
   },
-});
+  cardContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'center',
+    marginRight: '2rem',
+  },
+  infoContainer: {
+    boxSizing: 'border-box',
+    position: 'relative',
+    width: '12rem',
+    height: '8rem',
+    padding: '1rem',
+    margin: '3.5rem auto',
+    boxShadow: '0px 1px 6px 1px rgba(0,0,0,0.75)',
+  },
+  infoContext: {
+    display: 'inline',
+    padding: '0.25rem 0.5rem',
+    fontSize: '.75rem',
+    fontWeight: '300',
+    background: 'lightgray',
+  },
+  settingsButton: {
+    position: 'absolute',
+    bottom: '-2.5rem',
+    left: '1rem',
+    color: '#647881',
+    border: '1px solid #647881',
+    borderRadius: '.8rem',
+    fontSize: '.8rem',
+    fontWeight: '400',
+  },
+})
 
 const External = () => {
-  const classes = useStyles();
+  const classes = useStyles()
+  const [state, setState] = React.useState({})
+
+  const handleSwitch = (name, checked) => {
+    setState({
+      ...state,
+      [name]: checked,
+    })
+  }
 
   return (
-    <Fade timeout={{ enter: "500ms" }} in="true">
-      <Container className={classes.cardContainer}>
-        <ConfigCard name="Grafana" Icon={GrafanaIcon} topInputPlaceholder="URL" bottomInputPlaceholder="API Key"/>
-        <ConfigCard name="Prometheus" Icon={PrometheusIcon} topInputPlaceholder="" bottomInputPlaceholder=""/>
+    <Fade timeout={{ enter: '500ms' }} in='true'>
+      <Container className={classes.container}>
+        <div className={classes.cardContainer}>
+          <ConfigCard
+            name='grafana'
+            Icon={GrafanaIcon}
+            topInputPlaceholder='URL'
+            bottomInputPlaceholder='API Key'
+            handleSwitch={handleSwitch}
+          />
+        </div>
+        <div className={classes.cardContainer}>
+          <ConfigCard
+            name='prometheus'
+            Icon={PrometheusIcon}
+            topInputPlaceholder=''
+            bottomInputPlaceholder=''
+            handleSwitch={handleSwitch}
+          />
+        </div>
+        <Button className={classes.settingsButton}>Advanced Settings</Button>
       </Container>
     </Fade>
-  );
-};
+  )
+}
 
-export default External;
+export default External
