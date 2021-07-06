242c5df4e055653c06c4ffe5a0d6f746fd39e22a
Adithya Krishna <aadithya794@gmail.com>
2021-07-06 18:08:16 +0530

    FC v10
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- ui/pages/wizard.js
+++ ui/pages/wizard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import { withStyles, NoSsr } from '@material-ui/core';
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux'
@@ -9,6 +10,21 @@ import { getPath } from "../lib/path";
 import ConfigurationWizard from '../components/ConfigurationWizard'
 
 const styles = {
+=======
+import { updatepagepath, updatepagetitle } from "../lib/store";
+import { connect } from "react-redux";
+import { bindActionCreators } from 'redux'
+import Head from 'next/head';
+import { getPath } from "../lib/path";
+import { Paper, withStyles, NoSsr } from '@material-ui/core';
+
+const styles = {
+  paper: {
+    maxWidth: '90%',
+    margin: 'auto',
+    overflow: 'hidden',
+  }
+>>>>>>> dec1c397 (Connect PopUp to config wizard path)
 }
 
 class Wizard extends React.Component {
@@ -24,7 +40,13 @@ class Wizard extends React.Component {
         <Head>
           <title>Configuration Wizard | Meshery</title>
         </Head>
+<<<<<<< HEAD
         <ConfigurationWizard/>
+=======
+        <Paper className={this.props.classes.paper}>
+          <h1>Wizard</h1>
+        </Paper>
+>>>>>>> dec1c397 (Connect PopUp to config wizard path)
       </NoSsr>
     );
   }
