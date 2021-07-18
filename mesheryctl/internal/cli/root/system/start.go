8fab1b00c4b4e9bb91899237cb66e78bc08a0f87
metonymic-smokey <ahuja.aditi@gmail.com>
2021-07-18 21:07:51 +0530

    migrated some errors to meshkit
    Signed-off-by: metonymic-smokey <ahuja.aditi@gmail.com>

--- mesheryctl/internal/cli/root/system/start.go
+++ mesheryctl/internal/cli/root/system/start.go
@@ -28,6 +28,7 @@ import (
 
 	"github.com/pkg/errors"
 
+	"github.com/layer5io/meshery/handlers"
 	"github.com/layer5io/meshery/mesheryctl/internal/cli/root/config"
 	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
 
@@ -60,7 +61,7 @@ var startCmd = &cobra.Command{
 		}
 		hc, err := NewHealthChecker(hcOptions)
 		if err != nil {
-			return errors.Wrapf(err, "failed to initialize healthchecker")
+			return handlers.ErrHealthCheckFailed(err)
 		}
 		// execute healthchecks
 		err = hc.RunPreflightHealthChecks()
@@ -112,7 +113,7 @@ func start() error {
 
 		// download the docker-compose.yaml file corresponding to the current version
 		if err := utils.DownloadDockerComposeFile(currCtx, true); err != nil {
-			return errors.Wrapf(err, utils.SystemError(fmt.Sprintf("failed to download %s file", utils.DockerComposeFile)))
+			return handlers.ErrDownloadFile(err, utils.DockerComposeFile)
 		}
 
 		// Viper instance used for docker compose
@@ -125,7 +126,7 @@ func start() error {
 		compose := &utils.DockerCompose{}
 		err = utils.ViperCompose.Unmarshal(&compose)
 		if err != nil {
-			return err
+			return handlers.ErrUnmarshal(err, utils.DockerComposeFile)
 		}
 
 		//changing the port mapping in docker compose
@@ -437,7 +438,7 @@ func start() error {
 	}
 	hc, err := NewHealthChecker(hcOptions)
 	if err != nil {
-		return errors.Wrapf(err, "failed to initialize healthchecker")
+		return handlers.ErrHealthCheckFailed(err)
 	}
 	// If k8s is available in case of platform docker than we deploy operator
 	if err = hc.Run(); err == nil {
@@ -454,7 +455,7 @@ func start() error {
 		// Download operator manifest
 		err = utils.DownloadOperatorManifest()
 		if err != nil {
-			return err
+			return handlers.ErrDownloadFile(err, "operator manifest")
 		}
 
 		if !skipUpdateFlag {
