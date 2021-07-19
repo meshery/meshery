4d8ae5a3c8ac1cb9cf9bd827cf8b362463427a2f
Neha Lohia <nehapithadiya444@gmail.com>
2021-07-19 15:30:21 +0530

    added check for docker or kubernetes platform2
    Signed-off-by: Neha Lohia <nehapithadiya444@gmail.com>

--- mesheryctl/internal/cli/root/system/start.go
+++ mesheryctl/internal/cli/root/system/start.go
@@ -102,9 +102,6 @@ func start() error {
 		return err
 	}
 
-	// if utils.PlatformFlag != "" {
-	// 	currCtx.SetPlatform(utils.PlatformFlag)
-	// }
     if utils.PlatformFlag == "docker" || utils.PlatformFlag == "kubernetes" {
         currCtx.Platform = utils.PlatformFlag
         err := utils.ChangePlatform(mctlCfg.CurrentContext, currCtx)
@@ -113,9 +110,8 @@ func start() error {
             return err
         }
     } else{
-        return errors.New(fmt.Sprintf("neha the platform %s is not supported currently. The supported platforms are:\ndocker\nkubernetes\nPlease check %s/config.yaml file.", utils.PlatformFlag, utils.MesheryFolder))
+        return errors.New(fmt.Sprintf("the platform %s is not supported currently. The supported platforms are:\ndocker\nkubernetes\nPlease check %s/config.yaml file.", utils.PlatformFlag, utils.MesheryFolder))
     }
-
 
 	// Deploy to platform specified in the config.yaml
 	switch currCtx.GetPlatform() {
