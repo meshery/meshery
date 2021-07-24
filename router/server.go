a93243d7c31c6c6cda370415cfd35d6473e9d02f
Piyush Singariya <piyushsingariya@gmail.com>
2021-07-24 21:46:44 +0530

    few system endpoints
    Signed-off-by: Piyush Singariya <piyushsingariya@gmail.com>

--- router/server.go
+++ router/server.go
@@ -44,7 +44,7 @@ func NewRouter(ctx context.Context, h models.HandlerInterface, port int, g http.
 	// 	Handler(http.StripPrefix("/provider/", http.FileServer(http.Dir("../provider-ui/out/")))).
 	// 	Methods("GET")
 
-	gMux.Handle("/api/config/sync", h.ProviderMiddleware(h.AuthMiddleware(h.SessionInjectorMiddleware(h.SessionSyncHandler)))).
+	gMux.Handle("/api/system/sync", h.ProviderMiddleware(h.AuthMiddleware(h.SessionInjectorMiddleware(h.SessionSyncHandler)))).
 		Methods("GET")
 
 	gMux.Handle("/api/user", h.ProviderMiddleware(h.AuthMiddleware(h.SessionInjectorMiddleware(h.UserHandler)))).
