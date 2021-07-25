3abe76b38e7be1da6fd8c6445e0730ec0e3d1cde
Bariq <bariqhibat@gmail.com>
2021-07-25 22:46:49 +0800

    change e2e test according to bug fix
    Signed-off-by: Bariq <bariqhibat@gmail.com>

--- ui/cypress/integration/e2e/settings_spec.js
+++ ui/cypress/integration/e2e/settings_spec.js
@@ -1,17 +1,17 @@
 describe('Settings', () => {
   describe('Environment', () => {
     beforeEach(() => {
-      cy.intercept('GET', '/api/promGrafana/scan').as('getScan')
+      // cy.intercept('GET', '/api/promGrafana/scan').as('getScan')
 
       cy.selectProviderNone()
 
       cy.visit('/settings')
-      cy.wait('@getScan')
+      // cy.wait('@getScan')
     })
 
     it('click on Discover Cluster and send a ping to the cluster', () => {
       cy.intercept('GET', '/api/config/sync').as('getConfigSync')
-      cy.intercept('GET', '/api/k8sconfig/ping').as('getK8sConfigPing')
+      // cy.intercept('GET', '/api/k8sconfig/ping').as('getK8sConfigPing')
 
       cy.get('[data-cy="tabInClusterDeployment"]').click()
       cy.get('[data-cy="btnDiscoverCluster"]').click()
@@ -23,7 +23,7 @@ describe('Settings', () => {
         .should('have.text', 'kind-kind-cluster')
         .click()
 
-      cy.wait('@getK8sConfigPing')
+      // cy.wait('@getK8sConfigPing')
 
       cy
         .get('[data-cy="itemListContextName"] > .MuiListItemText-secondary')
@@ -33,12 +33,12 @@ describe('Settings', () => {
 
   describe('Service Meshes', () => {
     beforeEach(() => {
-      cy.intercept('GET', '/api/promGrafana/scan').as('getScan')
+      // cy.intercept('GET', '/api/promGrafana/scan').as('getScan')
       cy.intercept('GET', '/api/mesh/adapters').as('getMeshAdapters')
 
       cy.selectProviderNone()
       cy.visit('/settings')
-      cy.wait('@getScan')
+      // cy.wait('@getScan')
 
       cy.get('[data-cy="tabServiceMeshes"]').click()
       cy.wait('@getMeshAdapters')
