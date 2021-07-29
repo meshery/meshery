58369648207ed3adc8e3bb03109745451df482a9
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-07-29 18:33:19 +0530

    make queue internals local to the package
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- models/pattern/queue.go
+++ models/pattern/queue.go
@@ -4,7 +4,7 @@ import "sync"
 
 // Queue is the thread safe queue
 type Queue struct {
-	InternalQueue []string
+	internalQueue []string
 
 	sync.RWMutex
 }
@@ -14,7 +14,7 @@ func (q *Queue) Enqueue(str string) {
 	q.Lock()
 	defer q.Unlock()
 
-	q.InternalQueue = append(q.InternalQueue, str)
+	q.internalQueue = append(q.internalQueue, str)
 }
 
 // Dequeue removes data from the queue
@@ -22,8 +22,8 @@ func (q *Queue) Dequeue() string {
 	q.Lock()
 	defer q.Unlock()
 
-	val := q.InternalQueue[0]
-	q.InternalQueue = q.InternalQueue[1:]
+	val := q.internalQueue[0]
+	q.internalQueue = q.internalQueue[1:]
 
 	return val
 }
@@ -33,7 +33,7 @@ func (q *Queue) Length() int {
 	q.Lock()
 	defer q.Unlock()
 
-	return len(q.InternalQueue)
+	return len(q.internalQueue)
 }
 
 // Peek returns the first item in the queue
@@ -41,8 +41,8 @@ func (q *Queue) Peek() string {
 	q.Lock()
 	defer q.Unlock()
 
-	if len(q.InternalQueue) > 0 {
-		return q.InternalQueue[0]
+	if len(q.internalQueue) > 0 {
+		return q.internalQueue[0]
 	}
 
 	return ""
