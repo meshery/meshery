79b08c975b0a1e7aeffdef3a56f7dfdf45f0314b
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-08-05 03:11:06 +0530

    add resource selector
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

---
+++ models/pattern/resource/selector/selector.go
@@ -0,0 +1,44 @@
+package selector
+
+import (
+	"fmt"
+)
+
+const (
+	CoreResource = "pattern.meshery.io/core"
+	MeshResource = "pattern.meshery.io/mesh/workload"
+	K8sResource  = "pattern.meshery.io/k8s"
+)
+
+type SelectorHelpers interface {
+	GetServiceMesh() (name string, version string)
+	GetAPIVersionForKind() string
+}
+
+type Selector struct {
+	helpers SelectorHelpers
+}
+
+func New(helpers SelectorHelpers) *Selector {
+	return &Selector{
+		helpers: helpers,
+	}
+}
+
+func generateWorkloadKey(name string) string {
+	return fmt.Sprintf(
+		"/meshery/registry/definition/%s/%s/%s",
+		"core.oam.dev/v1alpha1",
+		"WorkloadDefinition",
+		name,
+	)
+}
+
+func generateTraitKey(name string) string {
+	return fmt.Sprintf(
+		"/meshery/registry/definition/%s/%s/%s",
+		"core.oam.dev/v1alpha1",
+		"TraitDefinition",
+		name,
+	)
+}
