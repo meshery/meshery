92b448f5d920b3e918b8866a34153b77c0bcf35c
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-08-05 19:50:02 +0530

    finalize the persitance stage
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

---
+++ models/pattern/stages/persist.go
@@ -0,0 +1,34 @@
+package stages
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+func Persist(prov ServiceInfoProvider, act ServiceActionProvider) ChainStageFunction {
+	return func(data *Data, err error, next ChainStageNextFunction) {
+		if err != nil {
+			act.Terminate(err)
+			return
+		}
+
+		for k := range data.Other {
+			// Find the services which where provisioned successfully
+			if strings.HasSuffix(k, ProvisionSuffixKey) {
+				k = strings.TrimSuffix(k, ProvisionSuffixKey)
+
+				_, ok := data.Other[fmt.Sprintf("%s%s", k, UpdateSuffixKey)]
+				if err := act.Persist(k, *data.Pattern.Services[k], ok); err != nil {
+					// Just log the error - non critical issue
+					logrus.Warn("failed to create resource entry:", err)
+				}
+			}
+		}
+
+		if next != nil {
+			next(data, nil)
+		}
+	}
+}
