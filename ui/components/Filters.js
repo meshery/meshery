a408c5b35723ff20a84e0106827613721ed6750a
Meghana-12 <meghana.cosmos@gmail.com>
2021-08-09 22:13:48 +0530

    added modal w/signoff
    Signed-off-by: Meghana-12 <meghana.cosmos@gmail.com>

--- ui/components/Filters.js
+++ ui/components/Filters.js
@@ -9,7 +9,7 @@ import {
   DialogContent,
   DialogActions,
   Divider,
-  Tooltip
+  Tooltip,
 } from "@material-ui/core";
 import { UnControlled as CodeMirror } from "react-codemirror2";
 import DeleteIcon from "@material-ui/icons/Delete";
@@ -22,33 +22,48 @@ import MUIDataTable from "mui-datatables";
 import Moment from "react-moment";
 import { withSnackbar } from "notistack";
 import CloseIcon from "@material-ui/icons/Close";
-import EditIcon from '@material-ui/icons/Edit';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import EditIcon from "@material-ui/icons/Edit";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 import { updateProgress } from "../lib/store";
 import TableSortLabel from "@material-ui/core/TableSortLabel";
 import dataFetch from "../lib/data-fetch";
+import URLUploader from "./URLUploader";
 
-const styles = (theme) => ({ grid : { padding : theme.spacing(2), },
-  tableHeader : { fontWeight : "bolder",
-    fontSize : 18, }, });
+const styles = (theme) => ({
+  grid : {
+    padding : theme.spacing(2),
+  },
+  tableHeader : {
+    fontWeight : "bolder",
+    fontSize : 18,
+  },
+});
 
 function CustomToolbar(onClick) {
   return function Toolbar() {
     return (
       <>
         <label htmlFor="upload-button">
-          <input type="file" accept=".yaml, .yml, .json" hidden onChange={onClick} id="upload-button" name="upload-button" />
+          <input
+            type="file"
+            accept=".yaml, .yml, .json"
+            hidden
+            onChange={onClick}
+            id="upload-button"
+            name="upload-button"
+          />
           <Tooltip title="Upload Filter">
             <IconButton aria-label="Upload" component="span">
               <UploadIcon />
             </IconButton>
           </Tooltip>
         </label>
+
+        <URLUploader />
       </>
     );
   };
 }
-
 function YAMLEditor({ filter, onClose, onSubmit }) {
   const [yaml, setYaml] = useState("");
 
@@ -86,10 +101,7 @@ function YAMLEditor({ filter, onClose, onSubmit }) {
   );
 }
 
-function MesheryFilters({
-  updateProgress, enqueueSnackbar, closeSnackbar, user, classes
-}) {
-  
+function MesheryFilters({ updateProgress, enqueueSnackbar, closeSnackbar, user, classes }) {
   const [page, setPage] = useState(0);
   const [search] = useState("");
   const [sortOrder] = useState("");
@@ -102,15 +114,23 @@ function MesheryFilters({
 
 
   const ACTION_TYPES = {
-    FETCH_FILTERS : { name : "FETCH_FILTERS" ,
-      error_msg : "Failed to fetch filter" },
-    DELETE_FILTERS : { name : "DELETE_FILTERS",
-      error_msg : "Failed to delete filter file" },
-    DEPLOY_FILTERS : { name : "DEPLOY_FILTERS",
-      error_msg : "Failed to deploy filter file" },
-    UPLOADFILTERS : { name : "UPLOAD_FILTERS",
-      error_msg : "Failed to upload filter file" },
-  }
+    FETCH_FILTERS : {
+      name : "FETCH_FILTERS",
+      error_msg : "Failed to fetch filter",
+    },
+    DELETE_FILTERS : {
+      name : "DELETE_FILTERS",
+      error_msg : "Failed to delete filter file",
+    },
+    DEPLOY_FILTERS : {
+      name : "DEPLOY_FILTERS",
+      error_msg : "Failed to deploy filter file",
+    },
+    UPLOADFILTERS : {
+      name : "UPLOAD_FILTERS",
+      error_msg : "Failed to upload filter file",
+    },
+  };
 
   const searchTimeout = useRef(null);
 
@@ -140,9 +160,8 @@ function MesheryFilters({
         updateProgress({ showProgress : false })
       },
       handleError(ACTION_TYPES.DEPLOY_FILTERS)
-    )
-  }
-
+    );
+  };
 
   function fetchFilters(page, pageSize, search, sortOrder) {
     if (!search) search = "";
@@ -173,10 +192,11 @@ function MesheryFilters({
   }
 
   // function handleError(error) {
-  const handleError = (action) => (error) =>  {
+  const handleError = (action) => (error) => {
     updateProgress({ showProgress : false });
 
-    enqueueSnackbar(`${action.error_msg}: ${error}`, { variant : "error",
+    enqueueSnackbar(`${action.error_msg}: ${error}`, {
+      variant : "error",
       action : function Action(key) {
         return (
           <IconButton key="close" aria-label="Close" color="inherit" onClick={() => closeSnackbar(key)}>
@@ -184,8 +204,9 @@ function MesheryFilters({
           </IconButton>
         );
       },
-      autoHideDuration : 8000, });
-  }
+      autoHideDuration : 8000,
+    });
+  };
 
   function resetSelectedRowData() {
     return () => {
@@ -194,7 +215,7 @@ function MesheryFilters({
   }
 
   function handleSubmit(data, id, name, type) {
-    updateProgress({ showProgress : true })
+    updateProgress({ showProgress : true });
     if (type === "delete") {
       dataFetch(
         `/api/filter/${id}`,
@@ -204,7 +225,7 @@ function MesheryFilters({
           console.log("FilterFile API", `/api/filter/${id}`);
           updateProgress({ showProgress : false });
           fetchFilters(page, pageSize, search, sortOrder);
-          resetSelectedRowData()()
+          resetSelectedRowData()();
         },
         // handleError
         handleError(ACTION_TYPES.DELETE_FILTERS)
@@ -236,18 +257,14 @@ function MesheryFilters({
     // Create a reader
     const reader = new FileReader();
     reader.addEventListener("load", (event) => {
-      handleSubmit(
-        event.target.result,
-        "",
-        file?.name || "meshery_" + Math.floor(Math.random() * 100),
-        "upload",
-      );
+      handleSubmit(event.target.result, "", file?.name || "meshery_" + Math.floor(Math.random() * 100), "upload");
     });
     reader.readAsText(file);
   }
 
   const columns = [
-    { name : "name",
+    {
+      name : "name",
       label : "Filter Name",
       options : {
         filter : false,
@@ -262,8 +279,10 @@ function MesheryFilters({
             </TableCell>
           );
         },
-      }, },
-    { name : "created_at",
+      },
+    },
+    {
+      name : "created_at",
       label : "Upload Timestamp",
       options : {
         filter : false,
@@ -281,8 +300,10 @@ function MesheryFilters({
         customBodyRender : function CustomBody(value) {
           return <Moment format="LLLL">{value}</Moment>;
         },
-      }, },
-    { name : "updated_at",
+      },
+    },
+    {
+      name : "updated_at",
       label : "Update Timestamp",
       options : {
         filter : false,
@@ -300,8 +321,10 @@ function MesheryFilters({
         customBodyRender : function CustomBody(value) {
           return <Moment format="LLLL">{value}</Moment>;
         },
-      }, },
-    { name : "Actions",
+      },
+    },
+    {
+      name : "Actions",
       options : {
         filter : false,
         sort : false,
@@ -314,7 +337,7 @@ function MesheryFilters({
           );
         },
         customBodyRender : function CustomBody(_, tableMeta) {
-          const rowData = filters[tableMeta.rowIndex]
+          const rowData = filters[tableMeta.rowIndex];
           return (
             <>
               <IconButton>
@@ -322,7 +345,8 @@ function MesheryFilters({
                   title="Config"
                   aria-label="config"
                   color="inherit"
-                  onClick={() => setSelectedRowData(filters[tableMeta.rowIndex])}/>
+                  onClick={() => setSelectedRowData(filters[tableMeta.rowIndex])}
+                />
               </IconButton>
               <IconButton>
                 <PlayArrowIcon
@@ -335,7 +359,8 @@ function MesheryFilters({
             </>
           );
         },
-      }, },
+      },
+    },
   ];
 
   columns.forEach((column, idx) => {
@@ -343,30 +368,29 @@ function MesheryFilters({
       columns[idx].options.sortDirection = sortOrder.split(" ")[1];
     }
   });
-  
+
   async function deleteFilter(id) {
     let response = await modalRef.current.show({
-      title: "Delete Filters?",
+      title : "Delete Filters?",
 
-      subtitle: "Are you sure you want to delete this filter?",
+      subtitle : "Are you sure you want to delete this filter?",
 
-      options: ["YES", "NO"],
-
-    })
-    if(response === "NO") return
+      options : ["YES", "NO"],
+    });
+    if (response === "NO") return;
     dataFetch(
       `/api/experimental/filter/${id}`,
       {
-        method: "DELETE",
-        credentials: "include",
+        method : "DELETE",
+        credentials : "include",
       },
       () => {
-        updateProgress({ showProgress: false });
+        updateProgress({ showProgress : false });
 
         enqueueSnackbar("Filter Successfully Deleted!", {
-          variant: "success",
-          autoHideDuration: 2000,
-          action: function Action(key) {
+          variant : "success",
+          autoHideDuration : 2000,
+          action : function Action(key) {
             return (
               <IconButton key="close" aria-label="Close" color="inherit" onClick={() => closeSnackbar(key)}>
                 <CloseIcon />
@@ -382,33 +406,33 @@ function MesheryFilters({
   }
 
   const options = {
-    filter: false,
-    sort: !(user && user.user_id === "meshery"),
-    search: !(user && user.user_id === "meshery"),
-    filterType: "textField",
-    responsive: "scrollFullHeight",
-    resizableColumns: true,
-    selectableRows: true,
-    serverSide: true,
+    filter : false,
+    sort : !(user && user.user_id === "meshery"),
+    search : !(user && user.user_id === "meshery"),
+    filterType : "textField",
+    responsive : "scrollFullHeight",
+    resizableColumns : true,
+    selectableRows : true,
+    serverSide : true,
     // selection: true,
     count,
     rowsPerPage : pageSize,
     rowsPerPageOptions : [10, 20, 25],
     fixedHeader : true,
     page,
-    print: false,
-    download: false,
+    print : false,
+    download : false,
     // handleDelete,
 
-    customToolbar: CustomToolbar(uploadHandler),
+    customToolbar : CustomToolbar(uploadHandler),
 
-    onRowsDelete: function handleDelete(row) {
-      const fid = Object.keys(row.lookup).map(idx => filters[idx]?.id)
-      fid.forEach(fid => deleteFilter(fid))
+    onRowsDelete : function handleDelete(row) {
+      const fid = Object.keys(row.lookup).map((idx) => filters[idx]?.id);
+      fid.forEach((fid) => deleteFilter(fid));
     },
     // selection:'mulitple',
 
-    onTableChange: (action, tableState) => {
+    onTableChange : (action, tableState) => {
       const sortInfo = tableState.announceText ? tableState.announceText.split(" : ") : [];
       let order = "";
       if (tableState.activeColumn) {
@@ -428,9 +452,7 @@ function MesheryFilters({
           }
           searchTimeout.current = setTimeout(() => {
             if (search !== tableState.searchText) {
-              fetchFilters(page, pageSize, tableState.searchText !== null
-                ? tableState.searchText
-                : "", sortOrder);
+              fetchFilters(page, pageSize, tableState.searchText !== null ? tableState.searchText : "", sortOrder);
             }
           }, 500);
           break;
@@ -456,22 +478,21 @@ function MesheryFilters({
         <YAMLEditor filter={selectedRowData} onClose={resetSelectedRowData()} onSubmit={handleSubmit} />
       )}
       <MUIDataTable
-        title={<div className={classes.tableHeader}>Filters</div>}
+        title={<div className={classes.tableHeader}>Filters?</div>}
         data={filters}
         columns={columns}
         // @ts-ignore
         options={options}
       />
       <PromptComponent ref={modalRef} />
-
     </NoSsr>
   );
 }
 
-const mapDispatchToProps = (dispatch) => ({ updateProgress : bindActionCreators(updateProgress, dispatch), });
+const mapDispatchToProps = (dispatch) => ({ updateProgress : bindActionCreators(updateProgress, dispatch) });
 
 const mapStateToProps = (state) => {
-  return { user : state.get("user").toObject(), };
+  return { user : state.get("user").toObject() };
 };
 
 // @ts-ignore
---
+++ ui/components/URLUploader.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import LinkIcon from '@material-ui/icons/Link';
+import { Modal , Tooltip, IconButton, TextField,Button, Grid} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+const styles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 600,
+    backgroundColor: theme.palette.background.paper,
+    border: '0px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    top: '50%',
+    left: '50%',
+    transform: `translate(-50%, -50%)`,
+  },
+  grid: {
+    width : '100%'
+  }
+}));
+
+function URLUploader() {
+  const classes = styles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <label htmlFor="url-upload-button">
+
+        <Tooltip title="Upload URL">
+          <IconButton aria-label="URL-Upload" component="span" onClick={handleOpen}>
+            <LinkIcon />
+          </IconButton>
+        </Tooltip>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="url-modal-title"
+          aria-describedby="url-modal-description"
+
+        >
+          <div
+            className={classes.paper}
+          >
+            <Grid
+              container spacing={2}
+            >
+              <Grid
+                item xs={12}>
+                <h2 id="simple-modal-title">Upload using URL</h2>
+              </Grid>
+              <Grid
+                item xs={12}>
+                <TextField id="standard-basic" label="Paste URL here" fullWidth/>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button fullWidth variant="contained" onClick={handleClose}>Cancel</Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button fullWidth variant="contained" color="primary" >Insert</Button>
+              </Grid>
+            </Grid>
+
+          </div>
+        </Modal>
+      </label>
+    </>
+  )
+}
+
+export default URLUploader
