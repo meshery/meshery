8893f2f65eceabfa8168724c92d14489dd2e7609
Rudraksh Pareek <rudrakshpareek3601@gmail.com>
2021-08-11 18:55:22 +0530

    fix some bugs in mesh sub commands
    Signed-off-by: Rudraksh Pareek <rudrakshpareek3601@gmail.com>

--- mesheryctl/internal/cli/root/mesh/validate.go
+++ mesheryctl/internal/cli/root/mesh/validate.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/layer5io/meshery/mesheryctl/internal/cli/root/config"
@@ -39,8 +38,14 @@ var validateCmd = &cobra.Command{
 	Args:  cobra.NoArgs,
 	Long:  `Validate service mesh conformance to different standard specifications`,
 	PreRunE: func(cmd *cobra.Command, args []string) error {
+		log.Infof("Verifying prerequisites...")
+		mctlCfg, err := config.GetMesheryCtl(viper.GetViper())
 		if err != nil {
-			return err
+			return errors.Wrap(err, "error processing config")
+		}
+		// sync with available adapters
+		if err = validateAdapter(mctlCfg, tokenPath, meshName); err != nil {
+			return errors.Wrap(err, "adapter not valid")
 		}
 		return nil
 	},
@@ -53,100 +58,17 @@ var validateCmd = &cobra.Command{
 			return errors.Wrap(err, "error processing config")
 		}
 
-		// sync
-		syncPath := mctlCfg.GetBaseMesheryURL() + "/api/system/sync"
-		method := "GET"
-
-		client := &http.Client{}
-
-		syncReq, err := http.NewRequest(method, syncPath, strings.NewReader(""))
+		_, err = sendValidateRequest(mctlCfg, meshName, false)
 		if err != nil {
-			return err
+			return errors.Wrap(err, "error sending request")
 		}
-
-		syncReq.Header.Add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
-
-		err = utils.AddAuthDetails(syncReq, tokenPath)
-		if err != nil {
-			return err
-		}
-
-		_, err = client.Do(syncReq)
-		if err != nil {
-			return err
-		}
-
-		s := utils.CreateDefaultSpinner("Validation started", "\nValidation complete")
-
-		path := mctlCfg.GetBaseMesheryURL() + "/api/mesh/ops"
-		method = "POST"
-
-		data := url.Values{}
-		data.Set("adapter", adapterURL)
-		data.Set("customBody", "")
-		data.Set("deleteOp", "")
-		data.Set("namespace", "meshery")
-
-		// Choose which specification to use for conformance test
-		switch spec {
-		case "smi":
-			{
-				data.Set("query", "smi_conformance")
-				break
-			}
-		case "smp":
-			{
-				return errors.New("support for SMP coming in a future release")
-			}
-		case "istio-vet":
-			{
-				if adapterURL == "meshery-istio:10000" {
-					data.Set("query", "istio-vet")
-					break
-				}
-				return errors.New("only Istio supports istio-vet operation")
-			}
-		default:
-			{
-				return errors.New("specified specification not found or not yet supported")
-			}
-		}
-
-		payload := strings.NewReader(data.Encode())
-
-		req, err := http.NewRequest(method, path, payload)
-
-		if err != nil {
-			return err
-		}
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
-
-		err = utils.AddAuthDetails(req, tokenPath)
-		if err != nil {
-			return err
-		}
-
-		res, err := client.Do(req)
-		if err != nil {
-			return err
-		}
-
-		defer res.Body.Close()
-		body, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			return err
-		}
-
-		log.Infof(string(body))
 
 		if watch {
 			log.Infof("Verifying Operation")
-			s.Start()
 			_, err = waitForValidateResponse(mctlCfg, "Smi conformance test")
 			if err != nil {
 				return errors.Wrap(err, "error verifying installation")
 			}
-			s.Stop()
 		}
 
 		return nil
@@ -164,8 +86,6 @@ func init() {
 }
 
 func waitForValidateResponse(mctlCfg *config.MesheryCtlConfig, query string) (string, error) {
-	var wg sync.WaitGroup
-	wg.Add(1)
 
 	path := mctlCfg.GetBaseMesheryURL() + "/api/events?client=cli_validate"
 	method := "GET"
@@ -186,31 +106,97 @@ func waitForValidateResponse(mctlCfg *config.MesheryCtlConfig, query string) (st
 		return "", err
 	}
 
-	event, _ := utils.ConvertRespToSSE(res)
+	event, err := utils.ConvertRespToSSE(res)
+	if err != nil {
+		return "", err
+	}
+
+	timer := time.NewTimer(time.Duration(1200) * time.Second)
+	eventChan := make(chan string)
 
 	//Run a goroutine to wait for the response
 	go func() {
 		for i := range event {
 			log.Infof("Event :" + i.Data)
 			if strings.Contains(i.Data, query) {
-				wg.Done()
+				eventChan <- "successful"
+			} else if strings.Contains(i.Data, "error") {
+				eventChan <- "error"
 			}
 		}
 	}()
 
-	//Run a goroutine to wait for time out of 20 mins
-	go func() {
-		time.Sleep(time.Second * 1200)
-		err = errors.New("timeout")
-		wg.Done()
-	}()
+	select {
+	case <- timer.C:
+		return "", errors.New("timeout")
+	case event := <- eventChan:
+		if event != "successful" {
+			return "", errors.New("SMI conformance tests failed")
+		}
+	}
 
-	//Wait till any one of the goroutines ends and return
-	wg.Wait()
+	return "", nil
+}
 
+func sendValidateRequest(mctlCfg *config.MesheryCtlConfig, query string, delete bool) (string, error) {
+	path := mctlCfg.GetBaseMesheryURL() + "/api/mesh/ops"
+	method := "POST"
+
+	data := url.Values{}
+	data.Set("adapter", adapterURL)
+	data.Set("query", query)
+	data.Set("customBody", "")
+	data.Set("namespace", namespace)
+	if delete {
+		data.Set("deleteOp", "on")
+	} else {
+		data.Set("deleteOp", "")
+	}
+
+	// Choose which specification to use for conformance test
+	switch spec {
+	case "smi":
+		{
+			data.Set("query", "smi_conformance")
+			break
+		}
+	case "istio-vet":
+		{
+			if adapterURL == "meshery-istio:10000" {
+				data.Set("query", "istio-vet")
+				break
+			}
+			return "", errors.New("only Istio supports istio-vet operation")
+		}
+	default:
+		{
+			return "", errors.New("specified specification not found or not yet supported")
+		}
+	}
+
+	payload := strings.NewReader(data.Encode())
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, path, payload)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+
+	err = utils.AddAuthDetails(req, tokenPath)
 	if err != nil {
 		return "", err
 	}
 
-	return "", nil
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
 }
