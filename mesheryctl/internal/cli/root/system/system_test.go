40ec2f7a82c860ccbe6e5ad67909c3e0b67fe713
Shreyas220 <shreyas.ny@gmail.com>
2021-08-13 00:31:23 +0530

    removed --skip-update
    Signed-off-by: Shreyas220 <shreyas.ny@gmail.com>

--- mesheryctl/internal/cli/root/system/system_test.go
+++ mesheryctl/internal/cli/root/system/system_test.go
@@ -52,13 +52,6 @@ func TestSystemCmdIntegration(t *testing.T) {
 			ExpectError:     false,
 			TimeoutRequired: 1,
 		},
-		{
-			Name:            "Restart Meshery",
-			Action:          "restart -skip docker",
-			Args:            []string{"restart --skip-update"},
-			ExpectError:     false,
-			TimeoutRequired: 1,
-		},
 		//status
 		{
 			Name:            "Printing Meshery status with Docker platform",
@@ -90,13 +83,6 @@ func TestSystemCmdIntegration(t *testing.T) {
 			Name:            "Restart Meshery",
 			Action:          "restart kubernetes",
 			Args:            []string{"restart"},
-			ExpectError:     false,
-			TimeoutRequired: 1,
-		},
-		{
-			Name:            "Restart Meshery",
-			Action:          "restart -skip kubernetes",
-			Args:            []string{"restart --skip-update"},
 			ExpectError:     false,
 			TimeoutRequired: 1,
 		},
