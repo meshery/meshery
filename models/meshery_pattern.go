3f9fbcc139d50254e444b9a082820313037555fc
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-08-19 13:17:27 +0530

    add docs for /api/pattern GET request
    Signed-off-by: Suryashankar Das <suryashankardas.2002@gmail.com>

--- models/meshery_pattern.go
+++ models/meshery_pattern.go
@@ -10,17 +10,21 @@ import (
 
 // MesheryPattern represents the patterns that needs to be saved
 type MesheryPattern struct {
+	// example: [0]
 	ID *uuid.UUID `json:"id,omitempty"`
-
-	Name        string `json:"name,omitempty"`
+	// example: string
+	Name string `json:"name,omitempty"`
+	// example: string
 	PatternFile string `json:"pattern_file"`
 	// Meshery doesn't have the user id fields
 	// but the remote provider is allowed to provide one
+	// example: string
 	UserID *string `json:"user_id" gorm:"-"`
-
+	// example: {"additionalProp1":{},"additionalProp2":{},"additionalProp3":{}}
 	Location sql.Map `json:"location"`
-
+	// example: 2021-08-02T18:30:22.995Z
 	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+	// example: 2021-08-02T18:30:22.995Z
 	CreatedAt *time.Time `json:"created_at,omitempty"`
 }
 
--- models/meshery_patterns_api_response.go
+++ models/meshery_patterns_api_response.go
@@ -2,8 +2,11 @@ package models
 
 // PatternsAPIResponse response retruned by patternfile endpoint on meshery server
 type PatternsAPIResponse struct {
+	// example: 0
 	Page       uint             `json:"page"`
+	// example: 0
 	PageSize   uint             `json:"page_size"`
+	// example: 0
 	TotalCount uint             `json:"total_count"`
 	Patterns   []MesheryPattern `json:"patterns"`
 }
