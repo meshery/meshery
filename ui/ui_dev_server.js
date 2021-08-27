38398ed1c2682476c2c5b3472fd57c13648e68f3
sayantan1413 <sayantanbose2001@gmail.com>
2021-08-27 23:54:34 +0530

    Removed some stuffs which are not needed
    Signed-off-by: sayantan1413 <sayantanbose2001@gmail.com>

--- ui/ui_dev_server.js
+++ ui/ui_dev_server.js
@@ -52,6 +52,5 @@ app.prepare().then(() => {
   server.listen(port, err => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
-    Linking.openURL(`"http://localhost:${port}`)
   })
 })
