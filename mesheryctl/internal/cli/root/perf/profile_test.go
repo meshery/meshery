abe68f66a7fa0667e8bb50d2c96147d0832a60df
Piyush Singariya <piyushsingariya@gmail.com>
2021-08-27 23:31:22 +0530

    profile tests done
    Signed-off-by: Piyush Singariya <piyushsingariya@gmail.com>

--- mesheryctl/internal/cli/root/perf/profile_test.go
+++ mesheryctl/internal/cli/root/perf/profile_test.go
@@ -26,13 +26,13 @@ var (
 	profile1003 = "1003.golden"
 	// api response of zero performance profile
 	profile1004 = "1004.golden"
-	// html output for not authenticated
+	// unable marshal response
 	profile1005 = "1005.golden"
 	// empty response
 	profile1006 = "1006.golden"
 )
 
-// golden file responses
+// golden file mesheryctl outputs
 var (
 	// mesheryctl response of 25 performance profile
 	profile1001output = "1001.golden"
@@ -46,18 +46,16 @@ var (
 	profile1005output = "1005.golden"
 	// mesheryctl response of 25 performance profile in json output
 	profile1006output = "1006.golden"
-	// mesheryctl response of 25 performance profile in json output
+	// mesheryctl response of 25 performance profile in yaml output
 	profile1007output = "1007.golden"
 	// mesheryctl response for invalid output format
 	profile1008output = "1008.golden"
-	// mesheryctl response for no authentication
-	profile1009output = "1009.golden"
 	// mesheryctl response for server response code error
-	profile1010output = "1010.golden"
+	profile1009output = "1009.golden"
 	// mesheryctl response for unmarshal error
-	profile1011output = "1011.golden"
+	profile1010output = "1010.golden"
 	// mesheryctl response for failing attach authentication
-	profile1012output = "1012.golden"
+	profile1011output = "1011.golden"
 )
 
 type profileTestStruct struct {
@@ -71,7 +69,7 @@ type profileTestStruct struct {
 
 func TestProfileCmd(t *testing.T) {
 	utils.SetupContextEnv(t)
-	// utils.StartMockery(t)
+	utils.StartMockery(t)
 	testContext := utils.NewTestHelper(t)
 
 	// get current directory
@@ -91,20 +89,16 @@ func TestProfileCmd(t *testing.T) {
 		{"profiles searching istio", []string{"profile", "istio"}, []utils.MockURL{{"GET", profileURL, profile1002, 200}}, profile1002output, testToken, false},
 		{"profiles searching test 3", []string{"profile", "test", "3"}, []utils.MockURL{{"GET", profileURL, profile1003, 200}}, profile1003output, testToken, false},
 		{"standard profiles in expand output", []string{"profile", "--expand"}, []utils.MockURL{{"GET", profileURL, profile1001, 200}}, profile1004output, testToken, false},
-		{"No profiles found", []string{"profile", "--expand"}, []utils.MockURL{{"GET", profileURL, profile1004, 200}}, profile1005output, testToken, false},
+		{"Unmarshal error", []string{"profile"}, []utils.MockURL{{"GET", profileURL, profile1005, 200}}, profile1010output, testToken, true},
+		{"failing add authentication test", []string{"profile"}, []utils.MockURL{}, profile1011output, testToken + "invalid-path", true},
+		{"Server Error 500", []string{"profile"}, []utils.MockURL{{"GET", profileURL, profile1006, 500}}, profile1009output, testToken, true},
 	}
 
-	testsWithOutputFormat := []profileTestStruct{
+	testsforLogrusOutputs := []profileTestStruct{
+		{"No profiles found", []string{"profile", "--expand"}, []utils.MockURL{{"GET", profileURL, profile1004, 200}}, profile1005output, testToken, false},
 		{"standard profiles in json output", []string{"profile", "-o", "json"}, []utils.MockURL{{"GET", profileURL, profile1001, 200}}, profile1006output, testToken, false},
 		{"standard profiles in yaml output", []string{"profile", "-o", "yaml"}, []utils.MockURL{{"GET", profileURL, profile1001, 200}}, profile1007output, testToken, false},
-	}
-
-	testsForErrors := []profileTestStruct{
-		{"invalid output format", []string{"profile", "-o", "invalid"}, []utils.MockURL{{"GET", profileURL, profile1001, 200}}, profile1008output, profileURL, true},
-		{"not authenticated", []string{"profile"}, []utils.MockURL{{"GET", profileURL, profile1005, 200}}, profile1009output, testToken, true},
-		{"Server Error 500", []string{"profile"}, []utils.MockURL{{"GET", profileURL, profile1006, 500}}, profile1010output, testToken, true},
-		{"Unmarshal error", []string{"profile"}, []utils.MockURL{{"GET", profileURL, profile1006, 200}}, profile1011output, testToken, true},
-		{"failing add authentication test", []string{"profile"}, []utils.MockURL{}, profile1012output, "testToken", true},
+		{"invalid output format", []string{"profile", "-o", "invalid"}, []utils.MockURL{{"GET", profileURL, profile1001, 200}}, profile1008output, testToken, true},
 	}
 
 	// Run tests in list format
@@ -129,6 +123,15 @@ func TestProfileCmd(t *testing.T) {
 			PerfCmd.SetOutput(rescueStdout)
 			err := PerfCmd.Execute()
 			if err != nil {
+				if tt.ExpectError {
+					if *update {
+						golden.Write(err.Error())
+					}
+					expectedResponse := golden.Load()
+
+					utils.Equals(t, expectedResponse, err.Error())
+					return
+				}
 				t.Error(err)
 			}
 
@@ -148,7 +151,7 @@ func TestProfileCmd(t *testing.T) {
 	}
 
 	// Run tests in list format
-	for _, tt := range testsWithOutputFormat {
+	for _, tt := range testsforLogrusOutputs {
 		t.Run(tt.Name, func(t *testing.T) {
 			tokenPath = tt.Token
 
@@ -166,6 +169,15 @@ func TestProfileCmd(t *testing.T) {
 			PerfCmd.SetOutput(b)
 			err := PerfCmd.Execute()
 			if err != nil {
+				if tt.ExpectError {
+					if *update {
+						golden.Write(err.Error())
+					}
+					expectedResponse := golden.Load()
+
+					utils.Equals(t, expectedResponse, err.Error())
+					return
+				}
 				t.Error(err)
 			}
 
@@ -180,37 +192,6 @@ func TestProfileCmd(t *testing.T) {
 		})
 	}
 
-	// Run tests in list format
-	for _, tt := range testsForErrors {
-		t.Run(tt.Name, func(t *testing.T) {
-			tokenPath = tt.Token
-
-			for _, mock := range tt.URLs {
-				apiResponse := utils.NewGoldenFile(t, mock.Response, fixturesDir).Load()
-				httpmock.RegisterResponder(mock.Method, mock.URL,
-					httpmock.NewStringResponder(mock.ResponseCode, apiResponse))
-			}
-			golden := utils.NewGoldenFile(t, tt.ExpectedResponse, testdataDir)
-
-			b := utils.SetupLogrusGrabTesting(t)
-
-			PerfCmd.SetArgs(tt.Args)
-			PerfCmd.SetOutput(b)
-			err := PerfCmd.Execute()
-			if err != nil {
-				t.Error(err)
-			}
-
-			// response being printed in console
-			actualResponse := b.String()
-			// write it in file
-			if *update {
-				golden.Write(actualResponse)
-			}
-			expectedResponse := golden.Load()
-			utils.Equals(t, expectedResponse, actualResponse)
-		})
-	}
 	// stop mock server
 	utils.StopMockery(t)
 }
