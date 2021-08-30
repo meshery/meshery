5dce31ec7f4458471da75c90b833cd42ad3ba6cd
Neha Lohia <nehapithadiya444@gmail.com>
2021-08-30 09:50:29 +0530

    added error code references in error.go
    Signed-off-by: Neha Lohia <nehapithadiya444@gmail.com>

--- mesheryctl/internal/cli/root/system/error.go
+++ mesheryctl/internal/cli/root/system/error.go
@@ -25,7 +25,7 @@ const (
 )
 
 func ErrHealthCheckFailed(err error) error {
-	return errors.New(ErrHealthCheckFailedCode, errors.Alert, []string{"Health checks failed"}, []string{err.Error()}, []string{}, []string{})
+	return errors.New(ErrHealthCheckFailedCode, errors.Alert, []string{"Health checks failed"}, []string{err.Error()}, []string{"health checks execution failed"}, []string{"health checks execution should passed to start Meshery server successfully"})
 }
 
 func ErrInvalidAdapter(err error, obj string) error {
@@ -33,15 +33,15 @@ func ErrInvalidAdapter(err error, obj string) error {
 }
 
 func ErrDownloadFile(err error, obj string) error {
-	return errors.New(ErrDownloadFileCode, errors.Alert, []string{"Error downloading file ", obj}, []string{err.Error()}, []string{}, []string{})
+	return errors.New(ErrDownloadFileCode, errors.Alert, []string{"Error downloading file ", obj}, []string{err.Error()}, []string{"failed downloading current docker-compose file"}, []string{"Make sure docker-compose file is downloaded successfully"})
 }
 
 func ErrStopMeshery(err error) error {
-	return errors.New(ErrStopMesheryCode, errors.Alert, []string{"Error stopping meshery"}, []string{err.Error()}, []string{}, []string{})
+	return errors.New(ErrStopMesheryCode, errors.Alert, []string{"Error stopping meshery"}, []string{err.Error()}, []string{"meshery server is not stopped, some of the docker containers are still running"}, []string{"Verify all docker containers of meshery server are stopped"})
 }
 
 func ErrResetMeshconfig(err error) error {
-	return errors.New(ErrResetMeshconfigCode, errors.Alert, []string{"Error resetting meshconfig"}, []string{err.Error()}, []string{}, []string{})
+	return errors.New(ErrResetMeshconfigCode, errors.Alert, []string{"Error resetting meshconfig"}, []string{err.Error()}, []string{"Meshery server config file is not reset to default settings"}, []string{"Verify Meshery server config file is reset to default settings by executing `mesheryctl system context view`"})
 }
 
 func ErrApplyManifest(err error, deleteStatus, updateStatus bool) error {
@@ -65,21 +65,21 @@ func ErrUnsupportedPlatform(platform string, config string) error {
 }
 
 func ErrRetrievingCurrentContext(err error) error {
-	return errors.New(ErrRetrievingCurrentContextCode, errors.Alert, []string{"Error retrieving current context"}, []string{err.Error()}, []string{}, []string{})
+	return errors.New(ErrRetrievingCurrentContextCode, errors.Alert, []string{"Error retrieving current context"}, []string{err.Error()}, []string{"current context is not retrieved successfully"}, []string{"Verify current context is retrieved successfully and valid"})
 }
 
 func ErrSettingTemporaryContext(err error) error {
-	return errors.New(ErrSettingTemporaryContextCode, errors.Alert, []string{"Error setting temporary context"}, []string{err.Error()}, []string{}, []string{})
+	return errors.New(ErrSettingTemporaryContextCode, errors.Alert, []string{"Error setting temporary context"}, []string{err.Error()}, []string{"temporary context is not set properly"}, []string{"Verify the temporary context is set properly using the -c flag provided"})
 }
 
 func ErrCreateManifestsFolder(err error) error {
-	return errors.New(ErrCreateManifestsFolderCode, errors.Alert, []string{"Error creating manifest folder"}, []string{err.Error()}, []string{}, []string{})
+	return errors.New(ErrCreateManifestsFolderCode, errors.Alert, []string{"Error creating manifest folder"}, []string{err.Error()}, []string{"system error in creating manifest folder"}, []string{"Make sure manifest folder (.meshery/manifests) is created properly"})
 }
 
 func ErrProcessingMctlConfig(err error) error {
-	return errors.New(ErrProcessingMctlConfigCode, errors.Alert, []string{"Error processing config"}, []string{err.Error()}, []string{}, []string{})
+	return errors.New(ErrProcessingMctlConfigCode, errors.Alert, []string{"Error processing config"}, []string{err.Error()}, []string{"Error due to invalid format of Mesheryctl config"}, []string{"Make sure Mesheryctl config is in correct format and valid"})
 }
 
 func ErrRestartMeshery(err error) error {
-	return errors.New(ErrRestartMesheryCode, errors.Alert, []string{"Error restarting Meshery"}, []string{err.Error()}, []string{}, []string{})
+	return errors.New(ErrRestartMesheryCode, errors.Alert, []string{"Error restarting Meshery"}, []string{err.Error()}, []string{"Meshery is not running"}, []string{"Restart Meshery instance"})
 }
