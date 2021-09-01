f85fbd6945d288fa8da5c8d0645a0be5799f6456
suod-NithishKarthik <nithishkarthik01@gmail.com>
2021-09-01 21:43:14 +0530

    refactor: Update graphql schema path
    Signed-off-by: suod-NithishKarthik <nithishkarthik01@gmail.com>

--- ui/app/relay.config.js
+++ ui/app/relay.config.js
@@ -3,6 +3,6 @@ module.exports = {
   // ...
   // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
   src: "../features",
-  schema: "./schema.graphql",
+  schema: "../../internal/graphql/schema/schema.graphql",
   exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
 };
