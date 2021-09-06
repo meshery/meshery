ef661a45b21698894b59a58b0e46db051d9b2ad3
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-09-06 12:04:42 +0530

    replace legacy k8sconfig reader with new implementation and setup data in http request contexts
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- handlers/middlewares.go
+++ handlers/middlewares.go
@@ -3,9 +3,11 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/layer5io/meshery/models"
+	"github.com/layer5io/meshkit/utils/kubernetes"
 	"github.com/sirupsen/logrus"
 )
 
@@ -95,22 +97,44 @@ func (h *Handler) SessionInjectorMiddleware(next func(http.ResponseWriter, *http
 		}
 
 		user, _ := provider.GetUserDetails(req)
-
 		prefObj, err := provider.ReadFromPersister(user.UserID)
 		if err != nil {
 			logrus.Warn("unable to read session from the session persister, starting with a new one")
 		}
 
-		err = h.checkIfK8SConfigExistsOrElseLoadFromDiskOrK8S(req, user, prefObj, provider)
+		token := provider.UpdateToken(w, req)
+
+		ctx := context.WithValue(req.Context(), models.TokenCtxKey, token)
+		ctx = context.WithValue(ctx, models.PerfObjCtxKey, prefObj)
+		ctx = context.WithValue(ctx, models.UserCtxKey, user)
+
+		k8scontext, err := h.GetCurrentContext(token, provider)
 		if err != nil {
-			logrus.Errorf("Unable to load default kubernetes config: %v", err)
+			logrus.Warn("failed to find kubernetes context")
+
+			// Set some defaults in the context so that the casting doesn't fails
+			ctx = context.WithValue(ctx, models.KubeContextKey, nil)
+			ctx = context.WithValue(ctx, models.KubeHanderKey, nil)
+			ctx = context.WithValue(ctx, models.KubeConfigKey, nil)
+		} else {
+			cfg, err := k8scontext.GenerateKubeConfig()
+			if err != nil {
+				logrus.Warn("failed to load kube config for the user")
+			}
+
+			// Create mesherykube handler
+			client, err := kubernetes.New(cfg)
+			if err != nil {
+				fmt.Printf("%s\n", err)
+				http.Error(w, "failed to create kubeconfig handler for the user", http.StatusInternalServerError)
+				return
+			}
+
+			ctx = context.WithValue(ctx, models.KubeContextKey, k8scontext)
+			ctx = context.WithValue(ctx, models.KubeHanderKey, client)
+			ctx = context.WithValue(ctx, models.KubeConfigKey, cfg)
 		}
 
-		token := provider.UpdateToken(w, req)
-		//lint:ignore SA1029 we want to make sure that no two results of errors
-		ctx := context.WithValue(req.Context(), models.TokenCtxKey, token) // nolint
-		ctx = context.WithValue(ctx, models.UserCtxKey, user)              // nolint
-		ctx = context.WithValue(ctx, models.PerfObjCtxKey, prefObj)        // nolint
 		req1 := req.WithContext(ctx)
 
 		next(w, req1, prefObj, user, provider)
