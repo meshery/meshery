2cd5a79c906518b47d75a54c300c90f9bf3defa4
Krithik Vaidya <krithikvaidya@gmail.com>
2021-09-13 15:07:37 +0530

    Use golden files for sse-client tests
    Signed-off-by: Krithik Vaidya <krithikvaidya@gmail.com>

--- mesheryctl/pkg/utils/sse-client_test.go
+++ mesheryctl/pkg/utils/sse-client_test.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"bufio"
 	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"reflect"
@@ -38,38 +39,37 @@ func TestConvertRespToSSE(t *testing.T) {
 
 func Test_loop(t *testing.T) {
 	tests := []struct {
-		name string
-		body string
-		want []Event
+		name    string
+		body    string
+		fixture string
+		golden  string
 	}{
 		{
-			name: "Successful SMI test event",
-			body: "data: {\"summary\":\"Smi conformance test completed successfully\",\"details\":\"Result-Id: ce336fe4-6e01-4779-949b-6526e44e3d55\",\"operation_id\":\"b33e2b2d-4826-4ea7-8c41-f8f76e8e4bf7\"}\nid: test1\nevent: testevent1\n:\"Comment to be ignored\"\n\n",
-			want: []Event{
-				{
-					ID:   "test1\n",
-					Name: "testevent1",
-					Data: EventData{Details: "Result-Id: ce336fe4-6e01-4779-949b-6526e44e3d55", OperationID: "b33e2b2d-4826-4ea7-8c41-f8f76e8e4bf7", Summary: "Smi conformance test completed successfully"},
-				},
-			},
+			name:    "Successful SMI test event",
+			body:    "data: {\"summary\":\"Smi conformance test completed successfully\",\"details\":\"Result-Id: ce336fe4-6e01-4779-949b-6526e44e3d55\",\"operation_id\":\"b33e2b2d-4826-4ea7-8c41-f8f76e8e4bf7\"}\nid: test1\nevent: testevent1\n:\"Comment to be ignored\"\n\n",
+			fixture: "loop.success.golden",
+			golden:  "loop.expect.success.golden",
 		},
 		{
-			name: "Failed SMI test event",
-			body: "data:{\"event_type\":2,\"summary\":\"Error while running SMI Conformance test\",\"details\":\"Get \\\"https://192.168.49.2:8443/api?timeout=32s\\\": dial tcp 192.168.49.2:8443: i/o timeout\",\"operation_id\":\"95c27959-a4d7-4b35-95d2-50faa04ab641\"}\nid:test2\nevent:testevent2\nretry:\"test\"\n\n",
-			want: []Event{
-				{
-					ID:   "test2\n",
-					Name: "testevent2",
-					Data: EventData{Details: "Get \"https://192.168.49.2:8443/api?timeout=32s\": dial tcp 192.168.49.2:8443: i/o timeout", OperationID: "95c27959-a4d7-4b35-95d2-50faa04ab641", Summary: "Error while running SMI Conformance test"},
-				},
-			},
+			name:    "Failed SMI test event",
+			body:    "data:{\"event_type\":2,\"summary\":\"Error while running SMI Conformance test\",\"details\":\"Get \\\"https://192.168.49.2:8443/api?timeout=32s\\\": dial tcp 192.168.49.2:8443: i/o timeout\",\"operation_id\":\"95c27959-a4d7-4b35-95d2-50faa04ab641\"}\nid:test2\nevent:testevent2\nretry:\"test\"\n\n",
+			fixture: "loop.error.golden",
+			golden:  "loop.expect.error.golden",
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// Read fixture
+			bodyBytes, err := ioutil.ReadFile("fixtures/sse_client/" + tt.fixture)
+			if err != nil {
+				t.Fatalf("Error while reading fixture: %v", err)
+			}
+
+			body := string(bodyBytes)
+
 			resp := http.Response{
-				Body: ioutil.NopCloser(bytes.NewBufferString(tt.body)),
+				Body: ioutil.NopCloser(bytes.NewBufferString(body)),
 			}
 
 			reader := bufio.NewReader(resp.Body)
@@ -77,17 +77,34 @@ func Test_loop(t *testing.T) {
 
 			go loop(reader, events)
 
+			gotEvents := []Event{}
 			i := 0
+
 			for event := range events {
-				if i == len(tt.want) {
-					t.Fatalf("Error: Got more events than expected")
-				}
-
-				if !reflect.DeepEqual(event, tt.want[i]) {
-					t.Fatalf("Error: Wanted event \n%v\nGot event\n%v\n", tt.want[i], event)
-				}
-
+				gotEvents = append(gotEvents, event)
 				i++
+			}
+
+			// Read golden file
+			golden := NewGoldenFile(t, tt.golden, "testdata/sse_client/")
+			if *update {
+				data, _ := json.Marshal(gotEvents)
+				golden.Write(string(data))
+			}
+
+			wantEvents := []Event{}
+			err = json.Unmarshal([]byte(golden.Load()), &wantEvents)
+
+			if err != nil {
+				t.Fatalf("Error in unmarshalling golden file: %v", err)
+			}
+
+			if i > len(wantEvents) {
+				t.Fatalf("Error: Got more events [%v] than expected [%v]", i, wantEvents)
+			}
+
+			if !reflect.DeepEqual(gotEvents, wantEvents) {
+				t.Fatalf("Error: Wanted events \n%v\nGot events\n%v\n", wantEvents, gotEvents)
 			}
 		})
 	}
