814ba2198398da7581570e550fbe439b89bc55db
Shreyas220 <shreyas.ny@gmail.com>
2021-09-15 01:36:18 +0530

    added filter deploy post and delete
    Signed-off-by: Shreyas220 <shreyas.ny@gmail.com>

--- handlers/meshery_filter_handler.go
+++ handlers/meshery_filter_handler.go
@@ -252,3 +252,29 @@ func formatFilterOutput(rw http.ResponseWriter, content []byte, format string) {
 	rw.Header().Set("Content-Type", "application/json")
 	fmt.Fprint(rw, string(data))
 }
+
+// swagger:route POST /api/filter/deploy ApplicationsAPI idPostDeployApplicationFile
+// Handle POST request for Application File Deploy
+//
+// Deploy an attached filter file with the request
+// responses:
+//  200: FilterFilesResponseWrapper
+
+// swagger:route DELETE /api/filter/deploy ApplicationsAPI idDeleteApplicationFile
+// Handle DELETE request for Filter File Deploy
+//
+// Delete a deployed filter file with the request
+// responses:
+//  200:
+
+// FilterFileHandler handles the requested related to application files
+func (h *Handler) FilterFileHandler(
+	rw http.ResponseWriter,
+	r *http.Request,
+	prefObj *models.Preference,
+	user *models.User,
+	provider models.Provider,
+) {
+	// Filter files are just pattern files
+	h.PatternFileHandler(rw, r, prefObj, user, provider)
+}
