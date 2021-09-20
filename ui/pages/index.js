cf32cc2892b910b8a49b7ef194c666105dc6fcdf
suod-NithishKarthik <nithishkarthik01@gmail.com>
2021-09-20 11:26:33 +0530

    chore: Update .esllintignore and .prettierignore
    Signed-off-by: suod-NithishKarthik <nithishkarthik01@gmail.com>

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -1,9 +1,5 @@
-import { AdaptersChipList, AdaptersListContainer, ComponentsStatusContainer } from "@/features/mesheryComponents";
-import { Button, useTheme } from "@material-ui/core";
 import React from "react";
 
 export default function Home() {
-  // const theme = useTheme();
-  // return <div style={{ color: theme.palette.newcolor }}>Hello</div>;
-  return <AdaptersListContainer render={() => <Button>Hello</Button>} />;
+  return <div>Hello</div>;
 }
