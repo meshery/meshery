dc3a5a4ab5f25b3f687f0466e5240693f8b503c0
suod-NithishKarthik <nithishkarthik01@gmail.com>
2021-09-21 12:36:56 +0530

    feat: Add pagetitle to the pages
    Signed-off-by: suod-NithishKarthik <nithishkarthik01@gmail.com>

--- ui/components/Layout/Layout.js
+++ ui/components/Layout/Layout.js
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { pathToPageTitleMapper } from "@/utils/path";
 import { StyledEngineProvider, CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import React from "react";
@@ -14,7 +15,11 @@ export const Layout = ({ children }) => (
         <CssBaseline />
         <NavbarContainer render={(props) => <Navbar {...props} />} />
         <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
-          <Header drawerOpen={false} onDrawerToggle={() => null} pageTitle={"Connection Wizard"} />
+          <Header
+            drawerOpen={false}
+            onDrawerToggle={() => null}
+            pageTitle={pathToPageTitleMapper[window.location.pathname]}
+          />
           <main style={{ flex: 1, padding: "48px 36px 24px", background: "#eaeff1" }}>{children}</main>
 
           <footer>Hello</footer>
--- ui/utils/path.js
+++ ui/utils/path.js
@@ -1,5 +1,5 @@
 /**
- * @returns {string} path - Returns current location pathname
+ * @returns {string} path - Returns current location pathname - Not the full path but the first part of the path string separated by "/"
  */
 export function getPath() {
   let path = typeof window !== "undefined" ? window.location.pathname : "";
@@ -9,3 +9,24 @@ export function getPath() {
   path += typeof window !== "undefined" ? window.location.search : "";
   return path;
 }
+
+export const pathToPageTitleMapper = {
+  "/": "Dashboard",
+  "/configuration/applications": "Applications",
+  "/configuration/patterns": "Patterns",
+  "/configuration/filters": "Filters",
+  "/management/consul": "Consul",
+  "/management/istio": "Istio",
+  "/management/citrix": "Citrix Service Mesh",
+  "/management/kuma": "Kuma",
+  "/management/linkerd": "Linkerd",
+  "/management/nsm": "Network Service Mesh",
+  "/management/octarine": "Octarine",
+  "/management/nginx": "NGINX Service Mesh",
+  "/management/osm": "Open Service Mesh",
+  "/management/traefik-mesh": "Traefik Mesh",
+  "/performance": "Performance",
+  "/smi_results": "Conformance",
+  "/system/connections": "Connection Wizard",
+  "/settings": "Settings",
+};
