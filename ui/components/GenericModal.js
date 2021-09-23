e471812830d9b7a9e678f867e02f5424fa07506b
Aditya Chaterjee <speak2adi@gmail.com>
2021-09-23 15:28:24 +0530

    fixed oopsie 2
    Signed-off-by: Aditya Chaterjee <speak2adi@gmail.com>

--- ui/components/GenericModal.js
+++ ui/components/GenericModal.js
@@ -3,18 +3,28 @@ import { Modal, Backdrop, Fade } from '@mui/core'
 import { styled } from "@mui/material/styles";
 import { useTheme } from "@mui/system";
 
-const CustomModal = styled(Modal)(({theme}) => ({
+const CustomModal = styled(Modal)(({ theme }) => ({
    display : "flex",
    alignItems : "center",
    justifyContent : "center", 
 }))
-
+/**
+ *
+ * @param {{
+ *  open?: boolean,
+ *  Content?: JSX.Element,
+ *  handleClose?: (event: {}, reason: "backdropClick" | "escapeKeyDown") => void,
+ *  container?: React.ReactInstance | (() => React.ReactInstance)
+ * }} props
+ * @returns
+ */
 
 export const GenericModal = ({
     open, Content, handleClose, container
   }) =>  { const theme = useTheme();
     return (
       <CustomModal
+        sx={{theme}}
         open={open}
         onClose={handleClose}
         closeAfterTransition
