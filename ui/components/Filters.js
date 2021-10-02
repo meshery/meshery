2acbb1d17bdb3992c6785eae8f83497ffe9ec09e
Gaurav Chadha <gauravchadha1676@gmail.com>
2021-10-03 01:07:38 +0530

    x style filter
    Signed-off-by: Gaurav Chadha <gauravchadha1676@gmail.com>

--- ui/components/Filters.js
+++ ui/components/Filters.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
+import { withStyles, makeStyles, MuiThemeProvider } from "@material-ui/core/styles";
+import {  createTheme } from '@material-ui/core/styles';
 import {
   NoSsr,
   TableCell,
@@ -164,6 +165,43 @@ function MesheryFilters({
   const [selectedRowData, setSelectedRowData] = useState(null);
   const DEPLOY_URL = '/api/filter/deploy';
 
+
+  const getMuiTheme = () => createTheme({
+    overrides : {
+      MuiInput : {
+        underline : {
+          "&:hover:not(.Mui-disabled):before" : {
+            borderBottom : "2px solid #222"
+          },
+          "&:after" : {
+            borderBottom : "2px solid #222"
+          }
+        }
+      },
+      MUIDataTableSearch : {
+        searchIcon : {
+          color : "#607d8b" ,
+          marginTop : "7px",
+          marginRight : "8px",
+        },
+        clearIcon : {
+          "&:hover" : {
+            color : "#607d8b"
+          }
+        },
+      },
+      MUIDataTableToolbar : {
+        iconActive : {
+          color : "#222"
+        },
+        icon : {
+          "&:hover" : {
+            color : "#607d8b"
+          }
+        },
+      }
+    }
+  })
 
   const ACTION_TYPES = {
     FETCH_FILTERS : {
@@ -534,13 +572,15 @@ function MesheryFilters({
       {selectedRowData && Object.keys(selectedRowData).length > 0 && (
         <YAMLEditor filter={selectedRowData} onClose={resetSelectedRowData()} onSubmit={handleSubmit} />
       )}
-      <MUIDataTable
-        title={<div className={classes.tableHeader}>Filters</div>}
-        data={filters}
-        columns={columns}
-        // @ts-ignore
-        options={options}
-      />
+      <MuiThemeProvider theme={getMuiTheme()}>
+        <MUIDataTable
+          title={<div className={classes.tableHeader}>Filters</div>}
+          data={filters}
+          columns={columns}
+          // @ts-ignore
+          options={options}
+        />
+      </MuiThemeProvider>
       <PromptComponent ref={modalRef} />
     </NoSsr>
   );
