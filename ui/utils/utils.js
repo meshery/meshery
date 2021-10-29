2fcdb59c634a9f95a69e5a25296fe26c5d944abe
Abhishek-kumar09 <abhimait1909@gmail.com>
2021-10-30 00:54:02 +0530

    Add utility functions for the ui
    Signed-off-by: Abhishek-kumar09 <abhimait1909@gmail.com>

---
+++ ui/utils/utils.js
@@ -0,0 +1,22 @@
+/**
+ * Check if an object is empty
+ *
+ * @param {Object} obj
+ *
+ * @returns {Boolean} if obj is empty
+ */
+export function isEmptyObj(obj) {
+  return obj
+    && Object.keys(obj).length === 0
+    && Object.getPrototypeOf(obj) === Object.prototype
+}
+
+/**
+ * Check if array is empty
+ *
+ * @param {Array} arr
+ * @returns {Boolean} if arr is empty
+ */
+export function isEmptyArr(arr) {
+  return arr && arr.length === 0
+}
