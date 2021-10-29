5ea16304c840a7330285839d358f496ed81c556e
Abhishek-kumar09 <abhimait1909@gmail.com>
2021-10-30 02:41:48 +0530

    Add debouuncing mechanism for state propagation for optimized rerenders
    Signed-off-by: Abhishek-kumar09 <abhimait1909@gmail.com>

--- ui/components/MesheryMeshInterface/PatternService/RJSF.js
+++ ui/components/MesheryMeshInterface/PatternService/RJSF.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { withTheme } from "@rjsf/core";
 import { Theme as MaterialUITheme } from "@rjsf/material-ui";
 import { Button, TextField, Typography } from "@material-ui/core";
@@ -13,65 +13,65 @@ import CustomObjectFieldTemplate from "./RJSFCustomComponents/ObjectFieldTemplat
 const Form = withTheme(MaterialUITheme);
 
 const muiTheme = createTheme({
-  palette : {
-    primary : {
-      main : '#607d8b',
+  palette: {
+    primary: {
+      main: '#607d8b',
     },
   },
-  typography : {
-    fontSize : 13,
+  typography: {
+    fontSize: 13,
   },
-  props : {
-    MuiTextField : {
-      variant : 'outlined',
-      margin : 'dense',
+  props: {
+    MuiTextField: {
+      variant: 'outlined',
+      margin: 'dense',
     },
   },
-  overrides : {
-    MuiButton : {
-      textSecondary : {
-        color : '#00b39f',
-        "&:hover" : "00b39f"
+  overrides: {
+    MuiButton: {
+      textSecondary: {
+        color: '#00b39f',
+        "&:hover": "00b39f"
       }
     },
-    MuiBox : {
-      root : {
-        marginTop : 0
+    MuiBox: {
+      root: {
+        marginTop: 0
       }
     },
-    MuiDivider : {
-      root : {
-        height : '0.5px'
+    MuiDivider: {
+      root: {
+        height: '0.5px'
       }
     },
-    MuiFormLabel : {
-      root : {
-        color : "#333",
-        fontSize : '0.8rem',
-        textTransform : 'capitalize',
+    MuiFormLabel: {
+      root: {
+        color: "#333",
+        fontSize: '0.8rem',
+        textTransform: 'capitalize',
       }
     },
-    MuiTypography : {
-      body1 : {
-        fontSize : '0.8rem'
+    MuiTypography: {
+      body1: {
+        fontSize: '0.8rem'
       },
-      h5 : {
-        textTransform : 'capitalize',
-        fontSize : '1.2rem',
+      h5: {
+        textTransform: 'capitalize',
+        fontSize: '1.2rem',
       }
     },
-    MuiGrid : {
-      root : {
-        "& > *" : {
-          border : 'none !important'
+    MuiGrid: {
+      root: {
+        "& > *": {
+          border: 'none !important'
         },
-        marginTop : '0.2rem !important',
+        marginTop: '0.2rem !important',
       },
-      'spacing-xs-2' : {
-        padding : 0,
-        '& > *' : {
-          paddingTop : '0 !important',
-          paddingBottom : '0 !important'
+      'spacing-xs-2': {
+        padding: 0,
+        '& > *': {
+          paddingTop: '0 !important',
+          paddingBottom: '0 !important'
         }
 
       }
@@ -80,14 +80,14 @@ const muiTheme = createTheme({
 });
 
 function deleteTitleFromJSONSchema(jsonSchema) {
-  return { ...jsonSchema, title : "" };
+  return { ...jsonSchema, title: "" };
 }
 
 function deleteDescriptionFromJSONSchema(jsonSchema) {
-  return { ...jsonSchema, description : "" };
+  return { ...jsonSchema, description: "" };
 }
 
-function formatString(text){
+function formatString(text) {
   if (!text) return null
 
   // format string for prettified camelCase
@@ -96,7 +96,7 @@ function formatString(text){
   return result;
 }
 
-function camelCaseToCapitalize(text){
+function camelCaseToCapitalize(text) {
   if (!text) return null
 
   const result = text.replace(/([A-Z])/g, " $1");
@@ -107,29 +107,29 @@ function camelCaseToCapitalize(text){
 function addTitleToPropertiesJSONSchema(jsonSchema) {
   const newProperties = jsonSchema?.properties
 
-  if (newProperties && typeof newProperties === 'object'){
+  if (newProperties && typeof newProperties === 'object') {
     Object.keys(newProperties).map(key => {
-      if (Object.prototype.hasOwnProperty.call(newProperties, key)){
+      if (Object.prototype.hasOwnProperty.call(newProperties, key)) {
         let defaultValue;
         let types = []
-        if (!Array.isArray(newProperties[key].type) && Object.prototype.hasOwnProperty.call(newProperties[key], 'type')){
+        if (!Array.isArray(newProperties[key].type) && Object.prototype.hasOwnProperty.call(newProperties[key], 'type')) {
           types.push(newProperties[key].type)
         } else {
           types.push(...newProperties[key].type)
         }
-        if (types.includes('null')){
+        if (types.includes('null')) {
           defaultValue = null
-        } else if (types.includes('integer')){
+        } else if (types.includes('integer')) {
           defaultValue = 0
-        } else if (types.includes('string')){
+        } else if (types.includes('string')) {
           defaultValue = ''
-        } else if (types.includes('array')){
+        } else if (types.includes('array')) {
           defaultValue = []
         }
         newProperties[key] = {
           ...newProperties[key],
-          title : camelCaseToCapitalize(formatString(key)),
-          default : defaultValue
+          title: camelCaseToCapitalize(formatString(key)),
+          default: defaultValue
         }
         // if (typeof newProperties[key] === 'object' && Object.prototype.hasOwnProperty.call(newProperties[key], 'properties')){
         //   newProperties[key] = {
@@ -141,7 +141,7 @@ function addTitleToPropertiesJSONSchema(jsonSchema) {
 
     })
 
-    return { ...jsonSchema, properties : newProperties };
+    return { ...jsonSchema, properties: newProperties };
   }
   return undefined
 }
@@ -151,16 +151,16 @@ const CustomInputField = (props) => {
   const prettifiedName = camelCaseToCapitalize(formatString(name)) || 'Input'
   return (
     <div key={props.id}>
-      <Typography variant="body1" style={{ fontWeight : "bold" }}>{prettifiedName}
+      <Typography variant="body1" style={{ fontWeight: "bold" }}>{prettifiedName}
         {props.schema?.description && (
           <Tooltip title={props.schema?.description}>
             <IconButton component="span" size="small">
-              <HelpOutlineIcon style={{ fontSize : 17 }} />
+              <HelpOutlineIcon style={{ fontSize: 17 }} />
             </IconButton>
           </Tooltip>
         )}
       </Typography>
-      <TextField variant="outlined" size="small" style={{ margin : '10px 0 ' }} autoFocus key={props.id} value={props.value} id={props.id} onChange={e => props?.onChange(e.target.value)} placeholder={`${prettifiedName}`}/>
+      <TextField variant="outlined" size="small" style={{ margin: '10px 0 ' }} autoFocus key={props.id} value={props.value} id={props.id} onChange={e => props?.onChange(e.target.value)} placeholder={`${prettifiedName}`} />
     </div>
   )
 }
@@ -169,49 +169,58 @@ const MemoizedCustomInputField = React.memo(CustomInputField)
 
 function RJSFButton({ handler, text, ...restParams }) {
   return (
-    <Button variant="contained" color="primary" style={{ marginRight : "0.5rem" }} onClick={handler} {...restParams}>
+    <Button variant="contained" color="primary" style={{ marginRight: "0.5rem" }} onClick={handler} {...restParams}>
       {text}
     </Button>
   );
 }
 
 function RJSF({ formData, jsonSchema, onChange, hideSubmit, hideTitle, onSubmit, onDelete, renderAsTooltip, ...restparams }) {
-  let uiJsonSchema = {}
-  // TODO: needs to do recursively for deep fields
-  Object.keys(jsonSchema.properties).map(key => {
-    uiJsonSchema = {
+  const [data, setData] = React.useState(prev => ({ ...formData, ...prev }));
+  const [uiSchema, setUiSchema] = useState()
+
+  useEffect(() => {
+    let uiJsonSchema = {}
+    // TODO: needs to do recursively for deep fields
+    Object.keys(jsonSchema.properties).map(key => {
+      uiJsonSchema = {
+        ...uiJsonSchema,
+        [key]: {
+          'ui:description': ' '
+        },
+      }
+    })
+
+    setUiSchema({
+      // hide all descriptions from fields
       ...uiJsonSchema,
-      [key] : {
-        'ui:description' : ' '
+      replicas: {
+        "ui:widget": "range"
       },
-    }
-  })
+      "ui:order": [
+        "name",
+        "namespace",
+        "*",
+      ],
 
-  const uiSchema = {
-    // hide all descriptions from fields
-    ...uiJsonSchema,
-    replicas : {
-      "ui:widget" : "range"
-    },
-    "ui:order" : [
-      "name",
-      "namespace",
-      "*",
-    ],
+    })
+  }, [])
 
-  };
+  useEffect(() => {
+    // Apply debouncing mechanism for the state propagation
+    const timer = setTimeout(() => {
+      onChange?.(data);
+    }, 600);
+
+    return () => clearTimeout(timer)
+  }, [data]);
 
   // define new string field
-  const fields =  {
+  const fields = {
     // eslint-disable-next-line
-    StringField : ({idSchema, formData, ...props}) => <MemoizedCustomInputField id={idSchema['$id']} value={formData} idSchema={idSchema} {...props} />
+    StringField: ({ idSchema, formData, ...props }) => <MemoizedCustomInputField id={idSchema['$id']} value={formData} idSchema={idSchema} {...props} />
   }
 
-  const [data, setData] = React.useState(prev => ({ ...formData, ...prev }));
-
-  React.useEffect(() => {
-    onChange?.(data);
-  }, [data]);
 
   return (
     <>
@@ -247,7 +256,7 @@ function RJSF({ formData, jsonSchema, onChange, hideSubmit, hideTitle, onSubmit,
             uiSchema={uiSchema}
           // noHtml5Validate
           >
-            <button style={{ opacity : '0' }} />
+            <button style={{ opacity: '0' }} />
           </Form>
         </MuiThemeProvider>
       )}
