0b02be8efbf54b4ab73d05a7ce901833f6e73b31
sayantan1413 <sayantanbose2001@gmail.com>
2021-11-10 21:14:11 +0530

    Removed
    Signed-off-by: sayantan1413 <sayantanbose2001@gmail.com>

--- ui/components/AppBar.js
+++ ui/components/AppBar.js
@@ -1,7 +1,6 @@
 import { AppBar } from '@mui/material';
 import { styled } from "@mui/material/styles";
 import { useTheme } from "@mui/system";
-import theme from '../styles/theme'
 import { appBarColor } from '../styles/colors';
 
 const CustomAppBarWrapper = styled(AppBar)(({theme})=>({
@@ -11,14 +10,6 @@ const CustomAppBarWrapper = styled(AppBar)(({theme})=>({
     padding: theme.spacing(1.4),
     zIndex: 1100,
 }))
-
-export const CustomAppBar = () =>  { const theme = useTheme();
-    return (
-      <CustomAppBarWrapper>
-        
-      </CustomAppBarWrapper>
-    );
-  }
 
 export default CustomAppBarWrapper;
 
--- ui/components/LoadingSpinner.js
+++ ui/components/LoadingSpinner.js
@@ -1,17 +1,11 @@
 import CircularProgress from '@mui/material/CircularProgress';
 import { styled } from "@mui/material/styles";
-import { useTheme } from "@mui/system";
+import { appBarColor } from '../styles/colors';
 
 const CustomLoadingSpinnerWrapper = styled(CircularProgress)(({theme})=>({
-    backgroundColor: '#396679', 
+    backgroundColor: appBarColor, 
     size: 30,
     thickness: 2.7,
 }))
-
-export const CustomLoadingSpinner = () =>  { const theme = useTheme();
-    return (
-      <CustomLoadingSpinnerWrapper/>
-    );
-  }
 
 export default CustomLoadingSpinnerWrapper;
