6a70e3c7655dec82194da1e023fa136581c57d5f
Ayan Choudhary <ayanchoudhary1025@gmail.com>
2021-12-07 00:36:58 +0530

    create instance creation util for meshkit/logger in mesheryctl
    Signed-off-by: Ayan Choudhary <ayanchoudhary1025@gmail.com>

--- mesheryctl/pkg/utils/formatter.go
+++ mesheryctl/pkg/utils/formatter.go
@@ -1,6 +1,10 @@
 package utils
 
 import (
+	"os"
+
+	"github.com/layer5io/meshkit/logger"
+	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -17,4 +21,16 @@ func (f *TerminalFormatter) Format(entry *log.Entry) ([]byte, error) {
 func SetupLogrusFormatter() {
 	//log formatter for improved UX
 	log.SetFormatter(new(TerminalFormatter))
+}
+
+// Initialize Meshkit Logger instance
+func MeshkitLogger() (logger.Handler, error) {
+	log, err := logger.New("mesheryctl", logger.Options{
+		Format: logger.SyslogLogFormat,
+	})
+	if err != nil {
+		logrus.Error(err)
+		os.Exit(1)
+	}
+	return log, nil
 }
