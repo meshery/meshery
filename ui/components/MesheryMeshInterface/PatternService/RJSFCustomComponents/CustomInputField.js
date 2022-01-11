8af767fccbb0ef5e966ddea8710eda069ea0cfd7
Abhishek-kumar09 <abhimait1909@gmail.com>
2022-01-11 21:28:56 +0530

    [RJSF/Extension] Fix Name and Namespace Fileds
    Signed-off-by: Abhishek-kumar09 <abhimait1909@gmail.com>

--- ui/components/MesheryMeshInterface/PatternService/RJSFCustomComponents/CustomInputField.js
+++ ui/components/MesheryMeshInterface/PatternService/RJSFCustomComponents/CustomInputField.js
@@ -4,14 +4,25 @@ import HelpOutlineIcon from "../HelpOutlineIcon";
 import EnlargedTextTooltip from "../EnlargedTextTooltip";
 import { formatString } from "../../helpers"
 
+// The special input fields
+export const omitTitleFields = ["name", "namespace"]
+
 const CustomInputField = (props) => {
-  const name = props?.name || props?.idSchema['$id']?.split('_')[1]
+  const name = props?.name || props?.idSchema['$id']?.split('_')[1].trim()
   const prettifiedName = formatString(name) || 'Input'
-  const style= {
+  const style = {
     display : "flex",
     alignItems : "center",
     justifyContent : "space-between"
   }
+
+  if (omitTitleFields.includes(name)) {
+    console.log("Fuck that's cool", prettifiedName)
+    return <div key={props.id} style={style}>
+      <TextField label={prettifiedName} variant="outlined" size="small" autoFocus key={props.id} value={props.value} id={props.id} onChange={e => props?.onChange(e.target.value)} placeholder={`${prettifiedName}`} />
+    </div>
+  }
+
   return (
     <div key={props.id} style={style}>
       <Typography variant="body1" style={{ fontWeight : "bold" }}>{prettifiedName}
@@ -23,7 +34,7 @@ const CustomInputField = (props) => {
           </EnlargedTextTooltip>
         )}
       </Typography>
-      <TextField variant="outlined" size="small" autoFocus key={props.id} value={props.value} id={props.id} onChange={e => props?.onChange(e.target.value)} placeholder={`${prettifiedName}`}/>
+      <TextField variant="outlined" size="small" autoFocus key={props.id} value={props.value} id={props.id} onChange={e => props?.onChange(e.target.value)} placeholder={`${prettifiedName}`} />
     </div>
   )
 }
