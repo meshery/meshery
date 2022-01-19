2d69a3d2a5a0b8853c21e05f19487690a6336730
Aditya Chatterjee <speak2adi@gmail.com>
2022-01-19 11:53:05 -0500

    4001
    Signed-off-by: Aditya Chatterjee <speak2adi@gmail.com>

--- ui/ui_dev_server.js
+++ ui/ui_dev_server.js
@@ -4,7 +4,7 @@ const { parse } = require("url");
 const next = require("next");
 const detect = require('detect-port');
 
-var port = parseInt(process.env.PORT, 10) || 4000
+var port = parseInt(process.env.PORT, 10) || 4001
 detect(port, (err, _port) => {
   while (port !== _port) {
     port = port + 1;
