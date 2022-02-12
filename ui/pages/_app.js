9132e8cf8192808245db43d8c6cfca6da34c9269
Abhishek-kumar09 <abhimait1909@gmail.com>
2022-02-12 19:47:35 +0530

    Redux: Extract drawer collapse variable with redux
    Signed-off-by: Abhishek-kumar09 <abhimait1909@gmail.com>

--- ui/pages/_app.js
+++ ui/pages/_app.js
@@ -10,7 +10,7 @@ import PropTypes from 'prop-types';
 import Hidden from '@material-ui/core/Hidden';
 import withRedux from "next-redux-wrapper";
 import { makeStore, actionTypes } from '../lib/store';
-import { Provider } from "react-redux";
+import { connect, Provider } from "react-redux";
 import { fromJS } from 'immutable';
 import { NoSsr, Typography } from '@material-ui/core';
 import FavoriteIcon from '@material-ui/icons/Favorite';
@@ -59,19 +59,14 @@ class MesheryApp extends App {
     super();
     this.pageContext = getPageContext();
 
-    this.state = { mobileOpen : false,
-      isDrawerCollapsed : false };
+    this.state = {
+      mobileOpen : false,
+    };
   }
 
   handleDrawerToggle = () => {
     this.setState(state => ({ mobileOpen : !state.mobileOpen }));
   }
-
-  handleCollapseDrawer = (open = null) => {
-    if (typeof(open) === 'boolean')
-      return this.setState({ isDrawerCollapsed : open });
-    this.setState(state => ({ isDrawerCollapsed : !state.isDrawerCollapsed }));
-  };
 
   handleL5CommunityClick = () => {
     if (typeof window !== 'undefined'){
@@ -174,75 +169,60 @@ class MesheryApp extends App {
 
   render() {
     const {
-      Component, store, pageProps, classes
+      Component, pageProps, classes, isDrawerCollapsed
     } = this.props;
-    const { isDrawerCollapsed } = this.state;
     return (
       <NoSsr>
-        <Provider store={store}>
-          <Head>
-            <link rel="shortcut icon" href="/ui/public/static/img/meshery-logo/meshery-logo.svg" />
-            <title>Meshery</title>
-          </Head>
-          <MuiThemeProvider theme={theme}>
-            <MuiPickersUtilsProvider utils={MomentUtils}>
-              <div className={classes.root}>
-                <CssBaseline />
-                <nav className={isDrawerCollapsed
-                  ? classes.drawerCollapsed
-                  : classes.drawer} data-test="navigation">
-                  <Hidden smUp implementation="js">
-                    <Navigator
-                      variant="temporary"
-                      open={this.state.mobileOpen}
-                      onClose={this.handleDrawerToggle}
-                      onCollapseDrawer={(open = null) => this.handleCollapseDrawer(open)}
-                      isDrawerCollapsed={isDrawerCollapsed}
-                    />
-                  </Hidden>
-                  <Hidden xsDown implementation="css">
-                    <Navigator
-                      onCollapseDrawer={(open = null) => this.handleCollapseDrawer(open)}
-                      isDrawerCollapsed={isDrawerCollapsed} />
-                  </Hidden>
-                </nav>
-                <div className={classes.appContent}>
-                  <SnackbarProvider
-                    anchorOrigin={{ vertical : 'bottom',
-                      horizontal : 'right', }}
-                    iconVariant = {{
-                      success : <CheckCircle style={{ marginRight : "0.5rem" }} />,
-                      error : <Error style={{ marginRight : "0.5rem" }} />,
-                      warning : <Warning style={{ marginRight : "0.5rem" }} />,
-                      info : <Info style={{ marginRight : "0.5rem" }} />
-                    }}
-                    classes={{
-                      variantSuccess : classes.notifSuccess,
-                      variantError : classes.notifError,
-                      variantWarning : classes.notifWarn,
-                      variantInfo : classes.notifInfo,
-                    }}
-                    maxSnack={10}
-                  >
-                    <MesheryProgressBar />
-                    <Header onDrawerToggle={this.handleDrawerToggle} onDrawerCollapse={isDrawerCollapsed}/>
-                    <main className={classes.mainContent}>
-                      <MuiPickersUtilsProvider utils={MomentUtils}>
-                        <Component pageContext={this.pageContext} {...pageProps} />
-                      </MuiPickersUtilsProvider>
-                    </main>
-                  </SnackbarProvider>
-                  <footer className={classes.footer}>
-                    <Typography variant="body2" align="center" color="textSecondary" component="p">
-                      <span onClick={this.handleL5CommunityClick} className={classes.footerText}>
-                                   Built with <FavoriteIcon className={classes.footerIcon} /> by the Layer5 Community</span>
-                    </Typography>
-                  </footer>
-                </div>
-              </div>
-            </MuiPickersUtilsProvider>
-          </MuiThemeProvider>
-        </Provider>
+        <div className={classes.root}>
+          <CssBaseline />
+          <nav className={isDrawerCollapsed
+            ? classes.drawerCollapsed
+            : classes.drawer} data-test="navigation">
+            <Hidden smUp implementation="js">
+              <Navigator
+                variant="temporary"
+                open={this.state.mobileOpen}
+                onClose={this.handleDrawerToggle}
+              />
+            </Hidden>
+            <Hidden xsDown implementation="css">
+              <Navigator />
+            </Hidden>
+          </nav>
+          <div className={classes.appContent}>
+            <SnackbarProvider
+              anchorOrigin={{ vertical : 'bottom',
+                horizontal : 'right', }}
+              iconVariant = {{
+                success : <CheckCircle style={{ marginRight : "0.5rem" }} />,
+                error : <Error style={{ marginRight : "0.5rem" }} />,
+                warning : <Warning style={{ marginRight : "0.5rem" }} />,
+                info : <Info style={{ marginRight : "0.5rem" }} />
+              }}
+              classes={{
+                variantSuccess : classes.notifSuccess,
+                variantError : classes.notifError,
+                variantWarning : classes.notifWarn,
+                variantInfo : classes.notifInfo,
+              }}
+              maxSnack={10}
+            >
+              <MesheryProgressBar />
+              <Header onDrawerToggle={this.handleDrawerToggle} onDrawerCollapse={isDrawerCollapsed}/>
+              <main className={classes.mainContent}>
+                <MuiPickersUtilsProvider utils={MomentUtils}>
+                  <Component pageContext={this.pageContext} {...pageProps} />
+                </MuiPickersUtilsProvider>
+              </main>
+            </SnackbarProvider>
+            <footer className={classes.footer}>
+              <Typography variant="body2" align="center" color="textSecondary" component="p">
+                <span onClick={this.handleL5CommunityClick} className={classes.footerText}>
+                              Built with <FavoriteIcon className={classes.footerIcon} /> by the Layer5 Community</span>
+              </Typography>
+            </footer>
+          </div>
+        </div>
       </NoSsr>
     );
   }
@@ -250,5 +230,29 @@ class MesheryApp extends App {
 
 MesheryApp.propTypes = { classes : PropTypes.object.isRequired, };
 
+const mapStateToProps = state => ({
+  isDrawerCollapsed : state.get("isDrawerCollapsed")
+})
+
+const MesheryWithRedux = connect(mapStateToProps)(MesheryApp);
+
+const MesheryAppWrapper = (props) => {
+  return (
+    <Provider store={props.store}>
+      <Head>
+        <link rel="shortcut icon" href="/ui/public/static/img/meshery-logo/meshery-logo.svg" />
+        <title>Meshery</title>
+      </Head>
+      <MuiThemeProvider theme={theme}>
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <MesheryWithRedux {...props} />
+        </MuiPickersUtilsProvider>
+      </MuiThemeProvider>
+    </Provider>
+  );
+}
+
 export default withStyles(styles)(withRedux(makeStore, { serializeState : state => state.toJS(),
-  deserializeState : state => fromJS(state) })(MesheryApp));
+  deserializeState : state => fromJS(state) })(MesheryAppWrapper));
+
+
