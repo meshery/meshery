c7ec9f33b1b8a8b3e360e0e3fad3505c5eef4157
chinmaym07 <b418020@iiit-bh.ac.in>
2022-02-25 02:40:14 +0530

    Updated the UX
    'Started Meshery logging ...' will be shown only if meshery is running .
    
    Signed-off-by: chinmaym07 <b418020@iiit-bh.ac.in>

--- mesheryctl/internal/cli/root/system/logs.go
+++ mesheryctl/internal/cli/root/system/logs.go
@@ -75,7 +75,6 @@ It also shows the logs of a specific component.`,
 		return err
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		log.Info("Starting Meshery logging...")
 
 		// Get viper instance used for context
 		mctlCfg, err := config.GetMesheryCtl(viper.GetViper())
@@ -108,6 +107,8 @@ It also shows the logs of a specific component.`,
 			if !ok {
 				log.Error("No logs to show. Meshery is not running.")
 				return nil
+			} else {
+				log.Info("Starting Meshery logging...")
 			}
 
 			if _, err := os.Stat(utils.DockerComposeFile); os.IsNotExist(err) {
@@ -145,6 +146,8 @@ It also shows the logs of a specific component.`,
 			if !ok {
 				log.Error("No logs to show. Meshery is not running.")
 				return nil
+			} else {
+				log.Info("Starting Meshery logging...")
 			}
 
 			// create an kubernetes client
