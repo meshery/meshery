d6242d68a8e1adc194d38b734eb321fa3f61ff46
Aditya Chatterjee <speak2adi@gmail.com>
2022-04-28 20:12:14 +0530

    change
    Signed-off-by: Aditya Chatterjee <speak2adi@gmail.com>

--- install/docker-extension/ui/src/img/meshery-logo/CustomMesheryLogo.js
+++ install/docker-extension/ui/src/img/meshery-logo/CustomMesheryLogo.js
@@ -1,8 +1,5 @@
 
 import React from "react";
-import SvgIcon from '@mui/material/SvgIcon';
-
-
 
 const MesheryIcon = ({CustomColor}) => (
 <svg width="519" height="100" viewBox="0 0 519 100" fill="none" xmlns="http://www.w3.org/2000/svg">
