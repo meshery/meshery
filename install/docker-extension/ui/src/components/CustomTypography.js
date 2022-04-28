6b3d68e144fc84ee60644a97a55d431e101687ae
Aditya Chatterjee <speak2adi@gmail.com>
2022-04-28 20:09:01 +0530

    remove custom typography
    Signed-off-by: Aditya Chatterjee <speak2adi@gmail.com>

--- install/docker-extension/ui/src/components/CustomTypography.js
+++
@@ -1,9 +0,0 @@
-import Typography from "@mui/material/Typography";
-import { styled } from "@mui/material/styles";
-
-const CustomTypography = styled(Typography)(({theme})=>({  
-  root: {
-  color: theme.palette.common.white
-}})) 
-  
-export default CustomTypography;
