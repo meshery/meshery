8945a0caefab83763211de7d731070502349d359
ashish <ashishjaitiwari15112000@gmail.com>
2022-05-11 21:07:34 +0530

    remove redundant comments
    Signed-off-by: ashish <ashishjaitiwari15112000@gmail.com>

--- internal/graphql/model/models_gen.go
+++ internal/graphql/model/models_gen.go
@@ -214,7 +214,6 @@ const (
 	MeshTypeTanzu              MeshType = "TANZU"
 	MeshTypeOpenServiceMesh    MeshType = "OPEN_SERVICE_MESH"
 	MeshTypeNginxServiceMesh   MeshType = "NGINX_SERVICE_MESH"
-	MeshTypeCilium MeshType = "CILIUM_SERVICE_MESH"
 )
 
 var AllMeshType = []MeshType{
