f2f9dcd64e1ac38247bbca4419d2449b101542d4
ashish <ashishjaitiwari15112000@gmail.com>
2022-06-02 13:25:06 +0530

    Considerations for "all" cluster_id
    Signed-off-by: ashish <ashishjaitiwari15112000@gmail.com>

--- internal/graphql/model/addons_helper.go
+++ internal/graphql/model/addons_helper.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"github.com/layer5io/meshery/models"
+	"gorm.io/gorm"
 
 	meshsyncmodel "github.com/layer5io/meshsync/pkg/model"
 )
@@ -13,14 +14,26 @@ func GetAddonsState(selectors []MeshType, provider models.Provider, cid []string
 	for _, selector := range selectors {
 		//subquery1 := r.DBHandler.Select("id").Where("kind = ? AND key = ? AND value = ?", meshsyncmodel.KindAnnotation, "meshery/component-type", "control-plane").Table("key_values")
 		//subquery2 := r.DBHandler.Select("id").Where("id IN (?) AND kind = ? AND key = ? AND value IN (?)", subquery1, meshsyncmodel.KindAnnotation, "meshery/maintainer", selectors).Table("key_values")
-		result := provider.GetGenericPersister().
-			Where("cluster_id IN ?", cid).
-			Preload("ObjectMeta", "namespace = ?", controlPlaneNamespace[selector]).
-			Preload("ObjectMeta.Labels", "kind = ?", meshsyncmodel.KindLabel).
-			Preload("ObjectMeta.Annotations", "kind = ?", meshsyncmodel.KindAnnotation).
-			Preload("Spec").
-			Preload("Status").
-			Find(&objects, "kind = ?", "Service")
+		var result *gorm.DB
+		if len(cid) == 1 && cid[0] == "all" {
+			result = provider.GetGenericPersister().
+				Preload("ObjectMeta", "namespace = ?", controlPlaneNamespace[selector]).
+				Preload("ObjectMeta.Labels", "kind = ?", meshsyncmodel.KindLabel).
+				Preload("ObjectMeta.Annotations", "kind = ?", meshsyncmodel.KindAnnotation).
+				Preload("Spec").
+				Preload("Status").
+				Find(&objects, "kind = ?", "Service")
+		} else {
+			result = provider.GetGenericPersister().
+				Where("cluster_id IN ?", cid).
+				Preload("ObjectMeta", "namespace = ?", controlPlaneNamespace[selector]).
+				Preload("ObjectMeta.Labels", "kind = ?", meshsyncmodel.KindLabel).
+				Preload("ObjectMeta.Annotations", "kind = ?", meshsyncmodel.KindAnnotation).
+				Preload("Spec").
+				Preload("Status").
+				Find(&objects, "kind = ?", "Service")
+		}
+
 		if result.Error != nil {
 			return nil, ErrQuery(result.Error)
 		}
