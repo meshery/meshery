da8aed18a1af18f2748444cf36447f50296af809
Gaurav Chadha <gauravchadha1676@gmail.com>
2022-06-24 01:26:26 +0530

    refactor patters card
    Signed-off-by: Gaurav Chadha <gauravchadha1676@gmail.com>

--- ui/components/MesheryPatterns/MesheryPatternCard.js
+++ ui/components/MesheryPatterns/MesheryPatternCard.js
@@ -14,85 +14,10 @@ import { UnControlled as CodeMirror } from "react-codemirror2";
 import FullscreenExit from "@material-ui/icons/FullscreenExit";
 import UndeployIcon from "../../public/static/img/UndeployIcon";
 import DoneAllIcon from '@material-ui/icons/DoneAll';
+import useStyles from "./Cards.styles";
 
 const INITIAL_GRID_SIZE = { xl : 4, md : 6, xs : 12 };
 
-const useStyles= makeStyles(() => ({
-  cardButtons : {
-    display : "flex",
-    justifyContent : "flex-end",
-    alignItems : "center",
-    marginTop : "50px"
-  },
-  testsButton : {
-    marginRight : "0.5rem"
-  },
-  perfResultsContainer : {
-    marginTop : "0.5rem"
-  },
-  backGrid : {
-    marginBottom : "0.25rem",
-    minHeight : "6rem",
-    position : "relative"
-  },
-  updateDeleteButtons : {
-    width : "fit-content",
-    margin : "10 0 0 auto",
-    position : "absolute",
-    right : 0,
-    bottom : 0,
-  },
-  yamlDialogTitle : {
-    display : "flex",
-    alignItems : "center"
-  },
-  yamlDialogTitleText : {
-    flexGrow : 1
-  },
-  fullScreenCodeMirror : {
-    height : '100%',
-    width : '100%',
-    '& .CodeMirror' : {
-      minHeight : "300px",
-      height : '100%',
-      width : '100%'
-    }
-  },
-  maximizeButton : {
-    width : "fit-content",
-    margin : "0 0 0 auto",
-    position : "absolute",
-    right : 0,
-    top : 0
-  },
-  noOfResultsContainer : {
-    margin : "0 0 1rem",
-    '& div' : {
-      display : "flex",
-      alignItems : "center"
-    },
-  },
-  bottomPart : {
-    display : "flex",
-    justifyContent : "flex-end",
-    alignItems : "center",
-  },
-  lastRunText : {
-    marginRight : "0.5rem"
-
-  },
-  iconPatt : {
-    width : "24px",
-    height : "24px",
-    marginRight : "5px"
-  },
-  undeployButton : {
-    backgroundColor : "#B32700",
-    color : "#ffffff"
-  }
-}))
-
-//Full screen Dialog: Similar to the dialog on the table view, with few modification on parameters
 const YAMLDialog = ({
   fullScreen,
   name,
@@ -165,9 +90,7 @@ function MesheryPatternCard({
   updateHandler,
   deleteHandler,
   setSelectedPatterns,
-  setYaml,
-  // requestFullSize,
-  // requestSizeRestore,
+  setYaml
 }) {
 
   function genericClickHandler(ev, fn) {
