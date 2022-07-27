d71f54fd9f94dddd6483a88e0da095842387968c
Jared Byers <j.byers@f5.com>
2022-07-27 16:37:08 -0700

    add validation options to usage
    Signed-off-by: Jared Byers <j.byers@f5.com>

--- mesheryctl/internal/cli/root/mesh/validate.go
+++ mesheryctl/internal/cli/root/mesh/validate.go
@@ -87,7 +87,7 @@ mesheryctl mesh validate --adapter [name of the adapter] --tokenPath [path to to
 				}
 				return errors.New("only Istio supports istio-vet operation")
 			default:
-				return errors.New("specified specification not found or not yet supported")
+				return errors.New("specification not found or not yet supported")
 			}
 
 			_, err = sendOperationRequest(mctlCfg, query, false)
@@ -97,7 +97,7 @@ mesheryctl mesh validate --adapter [name of the adapter] --tokenPath [path to to
 
 			if watch {
 				log.Infof("Verifying Operation")
-				_, err = waitForValidateResponse(mctlCfg, "Smi conformance test")
+				_, err = waitForValidateResponse(mctlCfg, "SMI conformance test")
 				if err != nil {
 					log.Fatalln(err)
 				}
@@ -109,8 +109,13 @@ mesheryctl mesh validate --adapter [name of the adapter] --tokenPath [path to to
 )
 
 func init() {
-	validateCmd.Flags().StringVarP(&spec, "spec", "s", "smi", "specification to be used for conformance test")
-	_ = validateCmd.MarkFlagRequired("spec")
+	validateCmd.Flags().StringVarP(
+		&spec,
+		"spec",
+		"s",
+		"smi",
+		"Specification to be used for conformance test [smi (default), istio-vet]",
+	)
 	validateCmd.Flags().StringVarP(
 		&namespace, "namespace", "n", "default",
 		"Kubernetes namespace where the mesh is deployed",
