309e9712a90e1e114c4e6e5ca1aec317d18fca68
Gaurav Chadha <gauravchadha1676@gmail.com>
2022-07-30 02:57:36 +0530

    fix-minor-ui-crash-bug
    Signed-off-by: Gaurav Chadha <gauravchadha1676@gmail.com>

--- ui/components/MesheryApplications/ApplicationsGrid.js
+++ ui/components/MesheryApplications/ApplicationsGrid.js
@@ -139,7 +139,7 @@ function MesheryApplicationGrid({ applications=[],handleDeploy, handleUnDeploy, 
                 No Applications Found
               </Typography>
               <div>
-                <UploadImport aria-label="URL upload button" handleUrlUpload={urlUploadHandler} handleUpload={uploadHandler} configuration="Application"  />
+                <UploadImport supportedTypes="null" aria-label="URL upload button" handleUrlUpload={urlUploadHandler} handleUpload={uploadHandler} configuration="Application"  />
 
               </div>
             </div>
