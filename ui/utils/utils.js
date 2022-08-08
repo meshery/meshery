b61b3d56f1b9e91043de707f0b6bf424bda37f3b
subhradwip01 <subhradwip01@gmail.com>
2022-08-08 21:06:48 +0530

    removed utils file
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- ui/utils/utils.js
+++
@@ -1,9 +0,0 @@
-/**
-* @param {object} obj check obj is empty or not
-* @returns {boolean} true or false based on object is empty
-*/
-
-export const isEmptyObj= (obj)=> {
-    for (let i in obj) return true;
-    return false;
-  }
