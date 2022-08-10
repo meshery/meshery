ac37cb247a16969ae3b36185c91793ea4528aebc
Mario Arriaga <ing.mario.arriaga@gmail.com>
2022-08-10 14:55:05 -0500

    Fix cypress integration test
    Signed-off-by: Mario Arriaga <ing.mario.arriaga@gmail.com>

--- ui/cypress/integration/integration/configuration_filters_spec.js
+++ ui/cypress/integration/integration/configuration_filters_spec.js
@@ -45,6 +45,9 @@ describe('Configuration Management', () => {
           // It internally calls https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/dispatchEvent
           // with a Custom 'change' input event.
           //import button shifted to Filters main UI under service_mesh_config_management_spec.js
+          cy.get('[data-cy="import-button"]').click();
+          cy.get('[data-cy="file-upload-button"]').attachFile(testFilePath);
+
           cy.wait('@postFilter').then(interception => {
 
             cy.wrap(interception.request).then(req => {
