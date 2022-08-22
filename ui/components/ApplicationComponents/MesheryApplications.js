2efa4aa7679b485432dde4b2ced385311da19bba
YashKamboj <yashkamboj29@gmail.com>
2022-08-22 23:43:17 +0530

    added generated application graphql
    Signed-off-by: YashKamboj <yashkamboj29@gmail.com>

--- ui/components/ApplicationComponents/MesheryApplications.js
+++ ui/components/ApplicationComponents/MesheryApplications.js
@@ -25,6 +25,8 @@ function MesheryApplications({user}) {
     
  const [page, setPage] = useState(0);
  const [count, setCount] = useState(0);
+ const [search] = useState("");
+ const [sortOrder] = useState("");
  const [pageSize, setPageSize] = useState(10)
     const [applications, setApplications] = useState([]);
     const [selectedRowData, setSelectedRowData] = useState(null);
@@ -75,25 +77,6 @@ function MesheryApplications({user}) {
   })
 }
 
-    const applications1 = [
-      {
-        id: "e7ccec75-bec6-4b28-b450-272aefa8a182",
-        name: "IstioFilterPattern.yaml",
-        user_id: "f714c166-5113-4f52-844c-38f0672b5e60",
-        created_at: "2022-07-14T13:50:46.375252Z",
-updated_at: "2022-07-14T13:53:35.479756Z"
-        },
-        {
-        id: "fb43eb24-de45-481a-955c-3916440276eb",
-        name: "IstioFilterPattern (1).yaml",
-        user_id: "f714c166-5113-4f52-844c-38f0672b5e60",
-        created_at: "2022-07-14T13:50:46.375252Z",
-updated_at: "2022-07-14T13:53:35.479756Z"
-  }
-    ];
-
-   
-    const handleClick = () => setApplications(applications1);;
     
     const handleModalClose = () => {
       setModalOpen({
@@ -115,8 +98,7 @@ updated_at: "2022-07-14T13:53:35.479756Z"
     <div> 
                     {selectedRowData && Object.keys(selectedRowData).length > 0 && (
           <YAMLEditor application={selectedRowData} onClose={resetSelectedRowData()}  />
-          )}
-      <Button onClick={handleClick}>QWE</Button>     
+          )}  
       {!selectedApplication.show  &&  (applications.length > 0 || viewType === "table") &&
       <Box sx={{display: "flex"}}>  
        <UploadImport configuration="Applications" />
