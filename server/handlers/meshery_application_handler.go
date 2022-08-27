162001e7d9085ee1d89436688f2bc1e3087c011e
Gaurav Chadha <gauravchadha1676@gmail.com>
2022-08-28 00:45:24 +0530

    add fetch swagger response code for apps_handler_go
    Signed-off-by: Gaurav Chadha <gauravchadha1676@gmail.com>

--- server/handlers/meshery_application_handler.go
+++ server/handlers/meshery_application_handler.go
@@ -32,6 +32,7 @@ type MesheryApplicationRequestBody struct {
 
 // swagger:route POST /api/application/deploy ApplicationsAPI idPostDeployApplicationFile
 // Handle POST request for Application File Deploy
+//
 // Deploy an attached application file with the request
 // responses:
 //  200: applicationFilesResponseWrapper
@@ -58,7 +59,14 @@ func (h *Handler) ApplicationFileHandler(
 // swagger:route POST /api/application/ ApplicationsAPI idPostApplicationFileRequest
 // Handle POST request for Application Files
 //
-// Save attached Meshery Application File
+// Creates a new application with source-content
+// responses:
+//  200: mesheryApplicationResponseWrapper
+
+// swagger:route PUT /api/application/ ApplicationsAPI idPUTApplicationFileRequest
+// Handle PUT request for Application Files
+//
+// Updates the design for given application
 // responses:
 //  200: mesheryApplicationResponseWrapper
 
@@ -461,6 +469,13 @@ func (h *Handler) GetMesheryApplicationHandler(
 	fmt.Fprint(rw, string(resp))
 }
 
+// swagger:route GET /api/application/types ApplicationsAPI typeGetMesheryApplication
+// Handle GET request for Meshery Application with the provided type
+//
+// Get application file type
+// responses:
+//  200: mesheryApplicationResponseWrapper
+
 // GetMesheryApplicationHandler fetched the application with the given id
 func (h *Handler) GetMesheryApplicationTypesHandler(
 	rw http.ResponseWriter,
@@ -480,6 +495,13 @@ func (h *Handler) GetMesheryApplicationTypesHandler(
 	rw.Header().Set("Content-Type", "application/json")
 	fmt.Fprint(rw, string(b))
 }
+
+// swagger:route GET /api/application/download/{id}/{sourcetype} ApplicationsAPI typeGetApplication
+// Handle GET request for Meshery Application with of source content
+//
+// Get the application source-content
+// responses:
+//  200: mesheryApplicationResponseWrapper
 
 // GetMesheryApplicationHandler fetched the application with the given id
 func (h *Handler) GetMesheryApplicationSourceHandler(
