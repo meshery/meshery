d060c98fb8c0e27711779ae263f16267ef63bac8
YASH KAMBOJ <75125203+YashKamboj@users.noreply.github.com>
2022-09-15 19:49:01 +0530

    Update CustomAvatar.js

--- ui/components/CustomAvatar.js
+++ ui/components/CustomAvatar.js
@@ -7,17 +7,6 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
   backgroundColor : '#44b700',
     color : '#44b700',
     boxShadow : `0 0 0 2px ${theme.palette.background.paper}`,
-    // '&::after' : {
-    //   position : 'absolute',
-    //   top : 0,
-    //   left : 0,
-    //   width : '100%',
-    //   height : '100%',
-    //   borderRadius : '50%',
-    //   animation : '$ripple 1.2s infinite ease-in-out',
-    //   border : '1px solid currentColor',
-    //   content : '""',
-    // }
 }));
 
 const CustomBox = styled(Box)(({ theme }) => ({
@@ -28,34 +17,6 @@ const CustomBox = styled(Box)(({ theme }) => ({
     },
 
 }));
-// const StyledBadge = withStyles((theme) => ({
-//   badge : {
-//     backgroundColor : '#44b700',
-//     color : '#44b700',
-//     boxShadow : `0 0 0 2px ${theme.palette.background.paper}`,
-//     '&::after' : {
-//       position : 'absolute',
-//       top : 0,
-//       left : 0,
-//       width : '100%',
-//       height : '100%',
-//       borderRadius : '50%',
-//       animation : '$ripple 1.2s infinite ease-in-out',
-//       border : '1px solid currentColor',
-//       content : '""',
-//     },
-//   },
-//   '@keyframes ripple' : {
-//     '0%' : {
-//       transform : 'scale(.8)',
-//       opacity : 1,
-//     },
-//     '100%' : {
-//       transform : 'scale(1.8)',
-//       opacity : 0,
-//     },
-//   },
-// }))(Badge);
 
 
 export default function BadgeAvatars({ children }) {
