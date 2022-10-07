71c407a2c10762fd13c293660d91c2cb2e527bae
harkiratsm <multaniharry714@gmail.com>
2022-10-07 08:22:14 +0530

    fixed Error msg position
    Signed-off-by: harkiratsm <multaniharry714@gmail.com>

--- ui/components/MesheryMeshInterface/PatternService/RJSFCustomComponents/CustomInputField.js
+++ ui/components/MesheryMeshInterface/PatternService/RJSFCustomComponents/CustomInputField.js
@@ -14,28 +14,30 @@ const CustomInputField = (props) => {
   }
 
   return (
-    <div key={props.id} style={style}>
-      <TextField
-        variant="outlined"
-        size="small"
-        key={props.id}
-        value={props.value}
-        id={props.id}
-        error={props.rawErrors?.length > 0}
-        onChange={e => props?.onChange(e.target.value)}
-        label={`${prettifiedName}`}
-        InputProps={{
-          style : { padding : "2px 0px 3px 0px", backgroundColor : "rgba(255, 255, 255, 0.4)" },
-          endAdornment : (<InputAdornment position="start">
-            {props.schema?.description && (
-              <EnlargedTextTooltip title={props.schema?.description}>
-                <IconButton component="span" size="small">
-                  <HelpOutlineIcon />
-                </IconButton>
-              </EnlargedTextTooltip>
-            )}
-          </InputAdornment>),
-        }} />
+    <>
+      <div key={props.id} style={style}>
+        <TextField
+          variant="outlined"
+          size="small"
+          key={props.id}
+          value={props.value}
+          id={props.id}
+          error={props.rawErrors?.length > 0}
+          onChange={e => props?.onChange(e.target.value)}
+          label={`${prettifiedName}`}
+          InputProps={{
+            style : { padding : "2px 0px 3px 0px", backgroundColor : "rgba(255, 255, 255, 0.4)" },
+            endAdornment : (<InputAdornment position="start">
+              {props.schema?.description && (
+                <EnlargedTextTooltip title={props.schema?.description}>
+                  <IconButton component="span" size="small">
+                    <HelpOutlineIcon />
+                  </IconButton>
+                </EnlargedTextTooltip>
+              )}
+            </InputAdornment>),
+          }} />
+      </div>
       <div style={{ display : "flex" }}>
         {props.rawErrors?.map((errormsg, i) => (
           (errormsg === "is a required property" ? null
@@ -43,7 +45,7 @@ const CustomInputField = (props) => {
           )
         ))}
       </div>
-    </div>
+    </>
   )
 }
 
