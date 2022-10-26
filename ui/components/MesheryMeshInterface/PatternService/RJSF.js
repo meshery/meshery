2bcffa0fcd49f3bff4658d26022cced6b5a77a0f
harkiratsm <multaniharry714@gmail.com>
2022-10-26 12:16:03 +0530

    Field Error Template
    Signed-off-by: harkiratsm <multaniharry714@gmail.com>

--- ui/components/MesheryMeshInterface/PatternService/RJSF.js
+++ ui/components/MesheryMeshInterface/PatternService/RJSF.js
@@ -8,6 +8,7 @@ import { recursiveCleanObject } from "../helpers";
 import MesheryArrayFieldTemplate from "./RJSFCustomComponents/ArrayFieldTemlate";
 import MesheryCustomObjFieldTemplate from "./RJSFCustomComponents/ObjectFieldTemplate";
 // import MesheryWrapIfAdditionalTemplate from './RJSFCustomComponents/WrapIfAdditionalTemplate';
+import MesheryFieldErrorTemplate from "./RJSFCustomComponents/FieldErrorTemplate"
 import { customizeValidator } from "@rjsf/validator-ajv6";
 import CustomInputField from "./RJSFCustomComponents/CustomInputField";
 import _ from "lodash"
@@ -49,6 +50,7 @@ function RJSFForm(props) {
     ArrayFieldTemplate = MesheryArrayFieldTemplate,
     ObjectFieldTemplate = MesheryCustomObjFieldTemplate,
     // WrapIfAdditionalTemplate = MesheryWrapIfAdditionalTemplate,
+    FieldErrorTemplate= MesheryFieldErrorTemplate,
     LoadingComponent,
     ErrorList,
     // prop should be present in order for the cloned element to override this property
@@ -58,6 +60,7 @@ function RJSFForm(props) {
     ArrayFieldTemplate,
     ObjectFieldTemplate,
     // WrapIfAdditionalTemplate, // todo: enable it with some fixes
+    FieldErrorTemplate
   }
 
   useEffect(() => {
