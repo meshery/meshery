afe215fc30b348cde9cfc4cf2b71c25c44f0d136
Lee Calcote <leecalcote@gmail.com>
2022-11-01 11:41:59 -0500

    Apply suggestions from code review
    Signed-off-by: Lee Calcote <leecalcote@gmail.com>

--- mesheryctl/pkg/utils/error.go
+++ mesheryctl/pkg/utils/error.go
@@ -128,7 +128,7 @@ func formatError(msg string, cmd cmdType) string {
 
 func ErrAttachAuthToken(err error) error {
 	return errors.New(ErrAttachAuthTokenCode, errors.Alert, []string{err.Error()},
-		[]string{"Authentication token not found. Please supply a valid user token using the --token (or -t) flag. Or login with `mesheryctl system login`"}, []string{}, []string{})
+		[]string{"Authentication token not found. Login with `mesheryctl system login` or supply a valid user token using the --token (or -t) flag."}, []string{}, []string{})
 }
 
 func ErrFailRequest(err error) error {
