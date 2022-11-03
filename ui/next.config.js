8a78cafa44c8a1daa57225b70cee9a06c72cae4b
Abhishek kr <abhimait1909@gmail.com>
2022-11-03 20:58:50 +0530

    add next config to build selective page
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,23 +1,15 @@
-const withCSS = require('@zeit/next-css');
-const compose = require('next-compose');
+// const withCSS = require('@zeit/next-css');
+// const compose = require('next-compose');
 
-module.exports = compose([
-  [withCSS, {}],
-  { exportPathMap : function () {
-    return {
-      '/' : { page : '/' },
-      '/index' : { page : '/index' },
-      '/performance/*' : { page : '/performance/[page]' },
-      '/configure' : { page : '/configure' },
-      '/playground' : { page : '/playground' },
-      '/provider/*' : { page : '/provider' },
-      '/settings/*' : { page : '/settings' },
-      '/404' : { page : '/_error' },
-    };
-  }, },
-  { webpack : (config)  => {
-    config.resolve.alias = { ...config.resolve.alias,
-      "remote-component.config.js" : __dirname + "/remote-component.config.js" };
-    return config;
-  } }
-]);
+module.exports = {
+  exportPathMap : function (pathMap) {
+    if (process.env.PLAYGROUND === "true") {
+      return {
+        '/' : { page : '/' },
+        '/extension/*' : { page : "/extension/[component]" }
+      }
+    } else {
+      return pathMap
+    }
+  },
+}
