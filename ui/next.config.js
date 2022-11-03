9137d9f9a9d442a1092f8b72f41a0634231d1d77
Abhishek kr <abhimait1909@gmail.com>
2022-11-04 01:59:24 +0530

    fix next build failure in ci
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,20 +1,29 @@
-// const withCSS = require('@zeit/next-css');
-// const compose = require('next-compose');
+const withCSS = require('@zeit/next-css');
+const compose = require('next-compose');
 
-module.exports = {
-  exportPathMap : function (pathMap) {
-    if (process.env.PLAYGROUND === "true") {
-      return {
-        '/' : { page : '/' },
-        '/extension/*' : { page : "/extension/[component]" }
-      }
-    } else {
-      return pathMap
-    }
-  },
-  webpack : (config)  => {
+// module.exports = {
+//   exportPathMap : function (pathMap) {
+//     if (process.env.PLAYGROUND === "true") {
+//       return {
+//         '/' : { page : '/' },
+//         '/extension/*' : { page : "/extension/[component]" }
+//       }
+//     } else {
+//       return pathMap
+//     }
+//   },
+//   webpack : (config)  => {
+//     config.resolve.alias = { ...config.resolve.alias,
+//       "remote-component.config.js" : __dirname + "/remote-component.config.js" };
+//     return config;
+//   }
+// }
+
+module.exports = compose([
+  [withCSS, {}],
+  { webpack : (config)  => {
     config.resolve.alias = { ...config.resolve.alias,
       "remote-component.config.js" : __dirname + "/remote-component.config.js" };
     return config;
-  }
-}
+  } }
+]);
