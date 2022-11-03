0db8e5d3ec7add029eccb128e7f9e1c04381b118
Aditya Chatterjee <speak2adi@gmail.com>
2022-11-04 00:12:21 +0530

    update
    Signed-off-by: Aditya Chatterjee <speak2adi@gmail.com>

--- ui/components/Popup.js
+++ ui/components/Popup.js
@@ -2,10 +2,11 @@ import React, {
   useEffect,
   useState } from 'react';
 import Cookies from 'universal-cookie';
-import GenericModal from './GenericModal';
+// import GenericModal from './GenericModal';
 import { makeStyles, MuiThemeProvider } from '@material-ui/core/styles';
 import { createTheme } from '@material-ui/core/styles';
-import { Typography, Grid, Button } from '@material-ui/core';
+import { Typography, Grid, Button,Modal, IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 
 
 const getMuiTheme = () => createTheme({
@@ -26,19 +27,35 @@ const getMuiTheme = () => createTheme({
 const styles = makeStyles((theme) => ({
   paper : {
     position : 'absolute',
-    width : 600,
+    width : 450,
     backgroundColor : theme.palette.background.paper,
     border : '0px solid #000',
     boxShadow : theme.shadows[5],
-    padding : theme.spacing(2, 4, 3),
-    top : '50%',
-    left : '50%',
+    padding : theme.spacing(1, 2, 3, 4),
+    top : '70%',
+    left : '83%',
     transform : `translate(-50%, -50%)`,
     borderRadius : 10,
   },
   grid : {
     width : '100%'
   },
+  drawer : {
+    height : 300,
+    position : "relative"
+  },
+
+  canvas : {
+    width : 275,
+    position : "absolute",
+    paddingTop : "1rem",
+    right : 15
+  },
+  header : {
+    paddingBottom : "0.5rem",
+    paddingTop : "0.6rem",
+    position : "absolute"
+  }
 
 
 }));
@@ -70,27 +87,53 @@ function Popup() {
     setIsOpen(false);
   }
 
+  const handleSignUp = (e) => {
+    window.open("https://layer5.io/meshmap", "_blank")
+    e.stopPropagation();
+  };
+
   return (
     <div>
-      <GenericModal
+
+      <Modal hideBackdrop={true}    open={isOpen} onClose={handleClose}>
+
+        <MuiThemeProvider theme={getMuiTheme()}>
+          <div
+            className={classes.paper}
+          >
+
+            <Typography className={classes.header} variant="h6" >Signup for Beta access of MeshMap!
+            </Typography>
+
+            <div style={{ display : "flex", justifyContent : "flex-end", whiteSpace : "nowrap", position : "relative" }}>
+              <IconButton  key="close" aria-label="Close" color="inherit" onClick={() => setIsOpen(false)}>
+                <CloseIcon />
+              </IconButton>
+
+            </div>
+            <img className={classes.drawer} src="/static/img/designer-drawer.png" />
+            <img className={classes.canvas} src="/static/img/designer-canvas.png" />
+            <Typography variant="subtitle1"><i>Friends dont let friends GitOps alone.</i></Typography>
+            <div style={{ display : "flex", justifyContent : "flex-end" }}>
+              <Grid item xs={3}>
+                <Button fullWidth variant="contained" color="primary" onClick={(e) => handleSignUp(e)}>Sign up</Button>
+              </Grid>
+              {/* <Grid item xs={3}>
+                <Button fullWidth variant="contained" color="primary" onClick={() => setIsOpen(false)}>close</Button>
+              </Grid> */}
+            </div>
+          </div>
+        </MuiThemeProvider>
+
+      </Modal>
+
+      {/* <GenericModal
         open={isOpen}
         handleClose={handleClose}
         Content={
-          <MuiThemeProvider theme={getMuiTheme()}>
-            <div
-              className={classes.paper}
-            >
-              <Typography variant="h5" >Signup for Beta access of MeshMap!</Typography>
-              <div style={{ display : "flex", justifyContent : "flex-end" }}>
-                <Grid item xs={3}>
-                  <Button fullWidth variant="contained" color="primary" onClick={handleClose}>Close</Button>
-                </Grid>
-              </div>
-            </div>
-          </MuiThemeProvider>
 
         }
-      />
+      /> */}
     </div>
   )
 }
