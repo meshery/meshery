a859bd2b5f7cb3c5668483334c67d73892b5f4f3
harkiratsm <multaniharry714@gmail.com>
2022-11-07 21:26:08 +0530

    removing margin
    Signed-off-by: harkiratsm <multaniharry714@gmail.com>

--- ui/components/MesheryMeshInterface/PatternService/RJSFCustomComponents/FieldTemplate.js
+++ ui/components/MesheryMeshInterface/PatternService/RJSFCustomComponents/FieldTemplate.js
@@ -4,7 +4,6 @@ export const CustomFieldTemplate=(props) => {
   const {  classNames, children } = props;
   return (
     <div className={classNames}>
-      <div style={{ marginTop : "5px" }}> {" "}  </div>
       {children}
     </div>
   )
