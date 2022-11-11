b1cd5248e3381755cc975e12c5a93a0a6dc105a8
MUzairS15 <muzair.shaikh810@gmail.com>
2022-11-11 19:07:00 +0530

    Add support to revoke and introspect JWT
    Signed-off-by: MUzairS15 <muzair.shaikh810@gmail.com>

--- server/models/error.go
+++ server/models/error.go
@@ -87,6 +87,8 @@ const (
 	ErrBrokerSubscriptionCode             = "2238"
 	ErrContextAlreadyPersistedCode        = "2241"
 	ErrGetPackageCode                     = "2252"
+	ErrTokenRevokeCode                    = "2253"
+	ErrTokenIntrospectCode                = "2254"
 )
 
 var (
@@ -283,6 +285,14 @@ func ErrPrometheusQueryRange(err error, query string, startTime, endTime time.Ti
 
 func ErrTokenRefresh(err error) error {
 	return errors.New(ErrTokenRefreshCode, errors.Alert, []string{"Error occurred while Refresing the token"}, []string{err.Error()}, []string{}, []string{})
+}
+
+func ErrTokenRevoke(err error) error {
+	return errors.New(ErrTokenRevokeCode, errors.Alert, []string{"Error occurred while revoking the token"}, []string{err.Error()}, []string{}, []string{})
+}
+
+func ErrTokenIntrospect(err error) error {
+	return errors.New(ErrTokenIntrospectCode, errors.Alert, []string{"Error occurred while introspecting the token"}, []string{err.Error()}, []string{}, []string{})
 }
 
 func ErrGetToken(err error) error {
