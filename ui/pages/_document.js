b37cb173b8581d75ac31ce2bc552e2b0ad0ed60b
Lee Calcote <leecalcote@gmail.com>
2022-11-29 18:05:10 -0600

    Update ui/pages/_document.js
    Signed-off-by: Lee Calcote <leecalcote@gmail.com>

--- ui/pages/_document.js
+++ ui/pages/_document.js
@@ -37,6 +37,9 @@ class MesheryDocument extends Document {
       <Html lang="en" dir="ltr">
         <Head>
           <meta charSet="utf-8" />
+          {/**
+           * content="no-referrer" included to avoid 403 errors on Google avatars
+          */}
           <meta name="referrer" content="no-referrer" />
           <link rel="icon" href="/static/favicon.png" />
 
