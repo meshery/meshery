ffaa7784b108898f6e24a731b9c1a7a5a3b71747
leecalcote <leecalcote@gmail.com>
2022-12-08 17:03:58 -0600

    files
    Signed-off-by: leecalcote <leecalcote@gmail.com>

--- .github/scripts/integration_publisher.sh
+++ .github/scripts/integration_publisher.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-results=$(< results.json)
+# Setup for local testing
+# INTEGRATIONS=$(< results.json)
+# integration_count=$(jq ' .results[].result.rawData | length' results.json)
 
-for i in $results
-  do
-    echo $i | jq ".results[].result.rawData[$i][0]"
-  done
+# Setup for repository-based run
+#results=$(< results.json)
+integration_count=$(jq ' .results[].result.rawData | length' $INTEGRATIONS)
+
+for (( i=0; i<integration_count; i++ ))
+do 
+    integration_name=$(jq " .results[].result.rawData[${i}][0]" -r $INTEGRATIONS)
+    integration_data=$(jq " .results[].result.rawData[${i}][1]" -r $INTEGRATIONS)
+    mkdir -p ${integration_name}/icon/color
+    mkdir -p ${integration_name}/icon/white
+    echo $integration_data > ${integration_name}/index.mdx   
+done
