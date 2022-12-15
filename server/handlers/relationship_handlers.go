2e3e0445e4bb31f520f43608595a587882a53fdf
Nithish <nithishkarthik01@gmail.com>
2022-12-16 01:46:37 +0530

    feat: Add endpoints for querying and adding relationships
    Signed-off-by: Nithish <nithishkarthik01@gmail.com>

---
+++ server/handlers/relationship_handlers.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/layer5io/meshkit/models/meshmodel"
+	"github.com/layer5io/meshkit/models/meshmodel/core/types"
+	"github.com/layer5io/meshkit/models/meshmodel/core/v1alpha1"
+)
+
+func (h *Handler) GetAllMeshmodelRelationships(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Add("Content-Type", "application/json")
+	enc := json.NewEncoder(rw)
+	res := h.registryManager.GetEntities(&v1alpha1.RelationshipFilter{})
+	if err := enc.Encode(res); err != nil {
+		h.log.Error(ErrWorkloadDefinition(err)) //TODO: Add appropriate meshkit error
+		http.Error(rw, ErrWorkloadDefinition(err).Error(), http.StatusInternalServerError)
+	}
+}
+
+func (h *Handler) RegisterMeshmodelRelationships(rw http.ResponseWriter, r *http.Request) {
+	dec := json.NewDecoder(r.Body)
+	var cc meshmodel.MeshModelRegistrantData
+	err := dec.Decode(&cc)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+	switch cc.EntityType {
+	case types.RelationshipDefinition:
+		var r v1alpha1.RelationshipDefinition
+		err = json.Unmarshal(cc.Entity, &r)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusBadRequest)
+			return
+		}
+		err = h.registryManager.RegisterEntity(cc.Host, r)
+	}
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+// TODO
+func RegisterStaticMeshmodelRelationship() {}
