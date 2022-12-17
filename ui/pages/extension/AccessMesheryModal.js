d3cfd64448297e372ef2a5888bf7ed1992d652df
Gaurav Chadha <gauravchadha1676@gmail.com>
2022-12-17 14:07:22 +0530

    use dialog component and updated fix
    Signed-off-by: Gaurav Chadha <gauravchadha1676@gmail.com>

--- ui/pages/extension/AccessMesheryModal.js
+++ ui/pages/extension/AccessMesheryModal.js
@@ -1,72 +1,37 @@
-import React, {
-} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Grid, Button, IconButton } from '@material-ui/core';
+import { Typography, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 
 const styles = makeStyles((theme) => ({
-  paper : {
-    position : 'absolute',
-    width : "30rem",
-    height : "20rem",
-    backgroundColor : theme.palette.background.paper,
-    boxShadow : theme.shadows[5],
-    borderRadius : "4px",
-    top : "0",
-    bottom : "0",
-    left : "0",
-    right : "0",
-    margin : "auto",
-    ["@media (max-width: 455px)"] : {
-      width : "100%"
-    },
-    zIndex : 5
+  root : {
+    margin : 0,
+    padding : theme.spacing(2),
   },
-  header : {
-    display : "flex",
-    justifyContent : "center",
+  closeButton : {
     position : "absolute",
-    width : "100%",
-    top : "0.7rem",
-    fontWeight : "bold",
-    ["@media (max-width: 455px)"] : {
-      fontSize : "1rem"
-    },
-  },
-  imgWrapper : {
-    display : "flex",
-    justifyContent : "center"
-  },
-  headerWrapper : {
-    marginBottom : 12,
-    borderRadius : "4px 4px 0 0",
-    textAlign : 'center',
-    width : "100%",
-    padding : theme.spacing(1),
-    color : '#fff',
-    backgroundColor : "#252E31",
-    fontSize : "1rem",
-  },
-  closing : {
+    right : theme.spacing(1),
+    top : theme.spacing(1),
+    color : theme.palette.common.white,
     transform : "rotate(-90deg)",
     "&:hover" : {
       transform : "rotate(90deg)",
-      transition : "all .3s ease-in",
-      cursor : "pointer",
-    }
+      transition : "all .3s ease-in"
+    },
+  },
+  dialogTitle : {
+    textAlign : "center",
+    backgroundColor : "#252E31",
+    color : "#fff"
+  },
+  imgWrapper : {
+    display : "flex",
+    justifyContent : "center",
+    padding : "1rem"
   }
-
 }));
 
-function PlaygroundMeshDeploy(props) {
 
-  if (props.isOpen) {
-    return <PlayCard closeForm={props.closeForm} rootStyle={props.rootStyle} />
-  }
-  return <></>;
-}
-
-export function PlayCard({ rootStyle, closeForm }) {
+export default function PlaygroundMeshDeploy(props) {
   const classes = styles();
 
   const handlePage = (e) => {
@@ -75,33 +40,31 @@ export function PlayCard({ rootStyle, closeForm }) {
   };
 
   return (
-    <div
-      className={classes.paper}
-      style={rootStyle}
-    >
-      <div className={classes.headerWrapper}>
-        <Typography className={classes.header} variant="h6">Access all features</Typography>
-        <div style={{ display : "flex", justifyContent : "flex-end", whiteSpace : "nowrap", position : "relative" }}>
-          <IconButton key="close" aria-label="Close" color="inherit" onClick={closeForm}>
-            <CloseIcon className={classes.closing} />
+    <div>
+      <Dialog aria-labelledby="customized-dialog-title" open={props.isOpen} >
+        <DialogTitle id="customized-dialog-title" className={classes.dialogTitle}>
+          <Typography variant="h6">The Cloud Native Playground</Typography>
+          <IconButton aria-label="close" className={classes.closeButton} onClick={props.closeForm}>
+            <CloseIcon />
           </IconButton>
-        </div>
-      </div>
-      <div className={classes.imgWrapper}>
-        <img width="60%" height="60%" src="/static/img/meshery-logo-light-text-side.png" />
-      </div>
-      <div style={{ display : "flex", justifyContent : "center", padding : "1.5rem", width : "100%" }} >
-        <Typography>Access is limited in Meshery <span style={{ color : "#F0A303" }}>Playground.</span> <br />
-                    To get full access, Deploy Meshery locally.</Typography>
-      </div>
-      <Typography style={{ textAlign : "center", marginBottom : "1rem" }}>&#x26A1; Deploy Meshery in seconds</Typography>
-      <div style={{ display : "flex", justifyContent : "center" }}>
-        <Grid item xs={3}>
-          <Button fullWidth variant="outlined" color="primary" onClick={(e) => handlePage(e)}>Get Started</Button>
-        </Grid>
-      </div>
+        </DialogTitle>
+        <DialogContent>
+          <div className={classes.imgWrapper}>
+            <img width="60%" height="60%" src="/static/img/meshery-logo-light-text-side.png" />
+          </div>
+          <Typography gutterBottom>
+            Meshery Playground gives you hands-on experience using not just Kubernetes, but every CNCF project and surrounding ecosystem.
+            Choose your <a href="https://layer5.io/learn/learning-paths">Learning Path</a> and follow their labs in your browser as you visually and collaboratively learn by doing without having to install a single thing.
+          </Typography>
+          <Typography gutterBottom>
+            To ensure that Meshery Playground remains a clean sandbox of all to use, many of Meshery&apos;s features are disabled.
+            aaFor full access to all of Meshery&apos;s features, deploy your own instance of Meshery.
+          </Typography>
+        </DialogContent>
+        <DialogActions style={{ diasplay : "flex", justifyContent : "center" }}>
+          <Button size="medium" variant="outlined" color="primary" onClick={(e) => handlePage(e)}>Get Started</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
-
-export default PlaygroundMeshDeploy;
