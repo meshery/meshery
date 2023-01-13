8142567e8fc1e1856d51716844458eab6ddbd64a
Antonette Caldwell <pullmana8@gmail.com>
2023-01-13 08:01:16 -0600

    Upgraded to Next@12 and React@17
    Signed-off-by: Antonette Caldwell <pullmana8@gmail.com>

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,26 +1,5 @@
-const compose = require('next-compose');
-
-// module.exports = {
-//   exportPathMap : function (pathMap) {
-//     console.log(pathMap)
-//     if (process.env.PLAYGROUND === "true") {
-//       return {
-//         '/' : { page : '/' },
-//         '/extension/*' : { page : "/extension/[component]" }
-//       }
-//     } else {
-//       return pathMap
-//     }
-//   },
-//   webpack : (config)  => {
-//     config.resolve.alias = { ...config.resolve.alias,
-//       "remote-component.config.js" : __dirname + "/remote-component.config.js" };
-//     return config;
-//   }
-// }
-
-module.exports = compose([
-  { exportPathMap : function () {
+module.exports = {
+  exportPathMap : function () {
     return {
       '/404' : { page : '/404' },
       '/configuration/applications' : { page : '/configuration/applications' },
@@ -39,10 +18,23 @@ module.exports = compose([
       '/system/connections' : { page : '/system/connections' },
       '/user/preferences' : { page : '/user/preferences' }
     };
-  }, },
-  { webpack : (config)  => {
-    config.resolve.alias = { ...config.resolve.alias,
-      "remote-component.config.js" : __dirname + "/remote-component.config.js" };
+  },
+  //  exportPathMap: function (pathMap) {
+  //    console.log(pathMap)
+  //    if (process.env.PLAYGROUND === "true") {
+  //      return {
+  //        '/': { page: '/' },
+  //        '/extension/*': { page: "/extension/[component]" }
+  //      }
+  //    } else {
+  //      return pathMap
+  //    }
+  //  },
+  webpack : (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "remote-component.config.js" : __dirname + "/remote-component.config.js"
+    };
     return config;
-  } }
-]);
+  }
+}
