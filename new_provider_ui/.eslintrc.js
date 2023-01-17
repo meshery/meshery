f3279186df811a87651f4404d26c46fc9b19247b
Antonette Caldwell <pullmana8@gmail.com>
2023-01-17 11:32:38 -0600

    Added eslint rules
    Signed-off-by: Antonette Caldwell <pullmana8@gmail.com>

---
+++ new_provider_ui/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
