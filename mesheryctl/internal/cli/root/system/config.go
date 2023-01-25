b18e1237826c8ab3033a1edd2924930febe50bda
kernelpanic77 <shanware.ishan@gmail.com>
2023-01-25 15:58:18 +0530

    improved error message for system cmd - config, logs
    Signed-off-by: kernelpanic77 <shanware.ishan@gmail.com>

--- mesheryctl/internal/cli/root/system/config.go
+++ mesheryctl/internal/cli/root/system/config.go
@@ -346,7 +346,9 @@ Example: mesheryctl system config eks
 Description: Configure the Kubernetes cluster used by Meshery.`
 
 		if len(args) == 0 {
-			return fmt.Errorf("config name not provided\n\n%v", errMsg)
+			return fmt.Errorf("name of kubernetes cluster to configure meshery not provided\n\n%v", errMsg)
+		} else if len(args) > 1 {
+			return fmt.Errorf("Expected one argument recieved multiple arguments")
 		}
 		return nil
 	},
