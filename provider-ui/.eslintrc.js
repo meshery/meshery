f5f50a56a4b01c96f55319d64c0751f28d9ea97b
Antonette Caldwell <pullmana8@gmail.com>
2023-01-26 12:20:57 -0600

    Renamed .eslintrc file
    Signed-off-by: Antonette Caldwell <pullmana8@gmail.com>

--- provider-ui/.eslintrc.js
+++ provider-ui/.eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     extends: [
       'plugin:react/recommended',
       'plugin:@next/next/recommended',
+      "plugin:cypress/recommended",
       'next/core-web-vitals',
     ],
     overrides: [
