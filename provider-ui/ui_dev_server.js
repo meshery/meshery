6502a61b263b56835e68744ccf3d91d5ab21c080
Annu Singh <annu4444.as@gmail.com>
2023-01-28 16:37:04 +0000

    changes for codespace doc
    Signed-off-by: Annu Singh <annu4444.as@gmail.com>

--- provider-ui/ui_dev_server.js
+++ provider-ui/ui_dev_server.js
@@ -24,7 +24,7 @@ app.prepare().then(() => {
     // This tells it to parse the query portion of the URL.
     const { pathname } = parse(req.url, true);
     if (pathname.startsWith('/api') || pathname.startsWith('/user/logout') || pathname.startsWith('/user/login')) {
-      proxy.web(req, res, { target: 'http://localhost:9018' });
+      proxy.web(req, res, { target: 'http://localhost:9081' });
     } else {
       if (req.url.startsWith('/provider')) {
         req.url = req.url.replace('/provider', '');
