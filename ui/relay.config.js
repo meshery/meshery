db7a60048717103a298a2c41c4206990f8f795f6
Pranav Singh <pranavsingh02@hotmail.com>
2023-02-15 20:26:02 +0530

    fix caching issue
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

--- ui/relay.config.js
+++ ui/relay.config.js
@@ -8,8 +8,7 @@ module.exports = {
   // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
   src : "./components",
   schema : "../server/internal/graphql/schema/schema.graphql",
-  language : "javascript",
-  exclude : ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
+  language : "javascript"
 }
 
 
