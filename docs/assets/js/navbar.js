5505cfc3fffe09863c4febde5562a11afad164cb
vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>
2023-02-28 00:43:36 +0530

    update mode toggle icons in docs
    Signed-off-by: vishalvivekm <110674407+vishalvivekm@users.noreply.github.com>

--- docs/assets/js/navbar.js
+++ docs/assets/js/navbar.js
@@ -1,26 +1,23 @@
 "use strict";
 
-
-// DARK/LIGHT MODE TOGGLE
-
 let toggleBtn = document.getElementById("mode-toggle-btn")
 toggleBtn.onclick = setMode;
+document.body.classList.add("dark-mode");
 if (localStorage.getItem("mode")) setMode();
 function setMode() {
-    document.body.classList.toggle("dark-mode")
-    // let layer5Logos = document.querySelectorAll("#layer5-logo");
-    // let allLogos=document.querySelectorAll("#logo-dark-light");
-    // console.log(allLogos)
-    if (document.body.classList.contains("dark-mode")) {
-        // layer5Logos.forEach(e => e.src = '../images/company-logo/layer5-dark-mode-logo.svg')
-        // allLogos.forEach(e=>e.src=e.dataset.logoForDark)
-    } else {
-        // layer5Logos.forEach(e => e.src = '../images/company-logo/layer5-no-trim.svg')
-        // allLogos.forEach(e=>e.src=e.dataset.logoForLight)
-    }
-    if (document.body.classList.contains("dark-mode")) {
-        localStorage.setItem("mode", "dark-mode")
-    } else {
-        localStorage.setItem("mode", "")
-    }
+  document.body.classList.toggle("dark-mode")
+
+  let allLogos = document.querySelectorAll("#logo-dark-light");
+
+  if (document.body.classList.contains("dark-mode")) {
+    allLogos.forEach(e => e.src = e.dataset.logoForDark)
+  } else {
+
+    allLogos.forEach(e => e.src = e.dataset.logoForLight)
+  }
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("mode", "dark-mode")
+  } else {
+    localStorage.setItem("mode", "")
+  }
 }
