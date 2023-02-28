e783509462821bc79005c5edf4e2fa5488c38aa4
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2023-02-28 11:49:09 +0530

    flickering in dark mode is resolved
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>

--- docs/assets/js/navbar.js
+++ docs/assets/js/navbar.js
@@ -1,36 +1,26 @@
 "use strict";
 
+
 // DARK/LIGHT MODE TOGGLE
 
-let toggleBtn = document.getElementById("mode-toggle-btn");
+let toggleBtn = document.getElementById("mode-toggle-btn")
 toggleBtn.onclick = setMode;
-
-// Set dark mode by default
-document.body.classList.add("dark-mode");
-
-if (localStorage.getItem("mode") !== null && localStorage.getItem("mode") !== "dark-mode") {
-  document.body.classList.remove("dark-mode");
-}
- 
-// Set logo for dark mode
-if (document.body.classList.contains("dark-mode")) {
-  let allLogos = document.querySelectorAll("#logo-dark-light");
-  allLogos.forEach(e => e.src = e.dataset.logoForDark);
-  localStorage.setItem("mode", "dark-mode");
-}
-
-// Set logo for light mode
+if (localStorage.getItem("mode")) setMode();
 function setMode() {
-  document.body.classList.toggle("dark-mode");
-  let allLogos = document.querySelectorAll("#logo-dark-light");
-
-  if (document.body.classList.contains("dark-mode")) {
-    allLogos.forEach(e => e.src = e.dataset.logoForDark);
-    localStorage.setItem("mode", "dark-mode");
-  } 
-  
-  else {
-    allLogos.forEach(e => e.src = e.dataset.logoForLight);
-    localStorage.setItem("mode", "");
-  }
+    document.body.classList.toggle("dark-mode")
+    // let layer5Logos = document.querySelectorAll("#layer5-logo");
+    // let allLogos=document.querySelectorAll("#logo-dark-light");
+    // console.log(allLogos)
+    if (document.body.classList.contains("dark-mode")) {
+        // layer5Logos.forEach(e => e.src = '../images/company-logo/layer5-dark-mode-logo.svg')
+        // allLogos.forEach(e=>e.src=e.dataset.logoForDark)
+    } else {
+        // layer5Logos.forEach(e => e.src = '../images/company-logo/layer5-no-trim.svg')
+        // allLogos.forEach(e=>e.src=e.dataset.logoForLight)
+    }
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("mode", "dark-mode")
+    } else {
+        localStorage.setItem("mode", "")
+    }
 }
