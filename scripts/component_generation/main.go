f9135a22388cd5d2a37828871c46d42d4a7b26f8
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2023-03-17 22:25:14 +0530

    Add env variable in gh step
    Signed-off-by: Ashish Tiwari <ashishjaitiwari15112000@gmail.com>

--- scripts/component_generation/main.go
+++ scripts/component_generation/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -14,6 +16,9 @@ import (
 
 	"github.com/layer5io/meshkit/models/meshmodel/core/v1alpha1"
 	"github.com/layer5io/meshkit/utils/artifacthub"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
 	"gopkg.in/yaml.v3"
 )
 
@@ -61,6 +66,9 @@ func convertCompModelsToPackages(models []ComponentModel) []artifacthub.AhPackag
 }
 
 func main() {
+	if len(os.Args) > 1 {
+		spreadsheetID = os.Args[1]
+	}
 	if _, err := os.Stat(OutputDirectoryPath); err != nil {
 		err := os.Mkdir(OutputDirectoryPath, 0744)
 		if err != nil {
@@ -468,4 +476,88 @@ func SplitYamlIntoFiles(file *os.File) error {
 	}
 
 	return nil
+}
+
+var spreadsheetID = "1DZHnzxYWOlJ69Oguz4LkRVTFM79kC2tuvdwizOJmeMw"
+
+const sheetID = 0
+
+func Spreadsheet(srv *sheets.Service, sheetName string, spreadsheet chan struct {
+	comps []v1alpha1.ComponentDefinition
+	model string
+}, am map[string]bool, acpm map[string]map[string]bool) {
+	start := time.Now()
+	rangeString := sheetName + "!A4:AB4"
+	for entry := range spreadsheet {
+		if len(entry.comps) == 0 {
+			continue
+		}
+		values := [][]interface{}{}
+		for _, comp := range entry.comps {
+			if acpm[entry.model][comp.Kind] {
+				fmt.Println("[Debug][Spreadsheet] Skipping spreadsheet updation for ", entry.model, comp.Kind)
+				continue
+			}
+			// Get the value of the specified cell.
+			resp, err := srv.Spreadsheets.Values.Get(spreadsheetID, rangeString).Do()
+			if err != nil {
+				fmt.Println("Unable to retrieve data from sheet: ", err)
+				return
+			}
+			newValues := resp.Values[0]
+			newValues[6] = comp.Kind
+			newValues[1] = entry.model
+			newValues[0] = entry.model
+			values = append(values, newValues)
+			time.Sleep(1 * time.Second) //To keep rps under 1rps
+		}
+		if am[entry.model] {
+			fmt.Println("[Debug][Spreadsheet] Skipping spreadsheet updation for ", entry.model)
+			continue
+		}
+		// Get the value of the specified cell.
+		resp, err := srv.Spreadsheets.Values.Get(spreadsheetID, rangeString).Do()
+		if err != nil {
+			fmt.Println("Unable to retrieve data from sheet: ", err)
+			return
+		}
+		newValues := resp.Values[0]
+		newValues[1] = entry.model
+		newValues[0] = entry.model
+		newValues[4] = len(entry.comps)
+		values = append(values, newValues)
+		row := &sheets.ValueRange{
+			Values: values,
+		}
+		// // srv.Spreadsheets.Values.Get(spreadsheetID, sheetName)
+		// // srv.Spreadsheets.Values.Update()
+		response2, err := srv.Spreadsheets.Values.Append(spreadsheetID, sheetName, row).ValueInputOption("USER_ENTERED").InsertDataOption("INSERT_ROWS").Context(context.Background()).Do()
+		if err != nil || response2.HTTPStatusCode != 200 {
+			fmt.Println(err)
+			continue
+		}
+		time.Sleep(1 * time.Second) //To keep rps under 1rps
+	}
+	elapsed := time.Now().Sub(start)
+	fmt.Printf("Time taken by spreadsheet updater (including the time it required to generate components): %f", elapsed.Minutes())
+}
+
+func NewSheetSRV() *sheets.Service {
+	ctx := context.Background()
+	byt, _ := base64.StdEncoding.DecodeString(os.Getenv("CRED"))
+	// authenticate and get configuration
+	config, err := google.JWTConfigFromJSON(byt, "https://www.googleapis.com/auth/spreadsheets")
+	if err != nil {
+		fmt.Println("ERR2", err)
+		return nil
+	}
+	// create client with config and context
+	client := config.Client(ctx)
+	// create new service using client
+	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		fmt.Println("ERR3", err)
+		return nil
+	}
+	return srv
 }
