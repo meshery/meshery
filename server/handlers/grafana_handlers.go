ac5e6b2ec1eeff21373a777fb2d7588f706b3e6d
Azanul <azanulhaque@gmail.com>
2023-03-23 01:06:24 +0530

    unused parameters
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- server/handlers/grafana_handlers.go
+++ server/handlers/grafana_handlers.go
@@ -101,7 +101,7 @@ func (h *Handler) GrafanaConfigHandler(w http.ResponseWriter, req *http.Request,
 // 	200:
 
 // GrafanaPingHandler - used to initiate a Grafana ping
-func (h *Handler) GrafanaPingHandler(w http.ResponseWriter, req *http.Request, prefObj *models.Preference, user *models.User, p models.Provider) {
+func (h *Handler) GrafanaPingHandler(w http.ResponseWriter, req *http.Request, prefObj *models.Preference, _ *models.User, _ models.Provider) {
 	// if req.Method != http.MethodGet {
 	// 	w.WriteHeader(http.StatusNotFound)
 	// 	return
