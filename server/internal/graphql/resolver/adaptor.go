e16f99d59ab078f2e10abf1ecccefc1580a68252
Azanul <azanulhaque@gmail.com>
2023-03-24 18:09:40 +0530

    adaptor deploy function update
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- server/internal/graphql/resolver/adaptor.go
+++ server/internal/graphql/resolver/adaptor.go
@@ -2,104 +2,59 @@ package resolver
 
 import (
 	"context"
-	"path"
-
-	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
-	mesherykube "github.com/layer5io/meshkit/utils/kubernetes"
+	"os"
+	"os/exec"
 
 	"github.com/layer5io/meshery/server/internal/graphql/model"
 	"github.com/layer5io/meshery/server/models"
 )
 
-func (r *Resolver) changeAdapterStatus(ctx context.Context, provider models.Provider, adapter models.Adapter, status model.Status, ctxID string) (model.Status, error) {
-	delete := true
+func (r *Resolver) changeAdapterStatus(ctx context.Context, provider models.Provider, adapter models.Adapter, status model.Status, adaptorPort string) (model.Status, error) {
+	deleteAdaptor := true
 
 	if status == model.StatusEnabled {
-		r.Log.Info("Installing Adapter")
-		delete = false
+		r.Log.Info("Deploying Adapter")
+		deleteAdaptor = false
 	} else {
-		r.Log.Info("Uninstalling Adapter in context ", ctxID)
+		r.Log.Info("Undeploying Adapter")
 	}
 
-	var kubeclient *mesherykube.Client
-	var k8scontext models.K8sContext
-	var err error
-	if ctxID != "" {
-		allContexts, ok := ctx.Value(models.AllKubeClusterKey).([]models.K8sContext)
-		if !ok || len(allContexts) == 0 {
-			r.Log.Error(ErrNilClient)
-			return model.StatusUnknown, ErrNilClient
-		}
-		for _, ctx := range allContexts {
-			if ctx.ID == ctxID {
-				k8scontext = ctx
-				break
-			}
-		}
-	} else {
-		k8scontexts, ok := ctx.Value(models.KubeClustersKey).([]models.K8sContext)
-		if !ok || len(k8scontexts) == 0 {
-			return model.StatusUnknown, ErrMesheryClient(nil)
-		}
-		k8scontext = k8scontexts[0]
-	}
-
-	kubeclient, err = k8scontext.GenerateKubeHandler()
-	if err != nil {
-		return model.StatusUnknown, ErrMesheryClient(err)
-	}
-
-	if kubeclient.KubeClient == nil {
-		r.Log.Error(ErrNilClient)
-		return model.StatusUnknown, ErrNilClient
-	}
-
-	go func(del bool, kubeclient *mesherykube.Client) {
-		var act mesherykube.HelmChartAction
+	go func(del bool) {
+		// Update AdapterTracker
 		if del {
 			r.Config.AdapterTracker.RemoveAdapter(ctx, adapter)
-			act = mesherykube.UNINSTALL
 		} else {
 			r.Config.AdapterTracker.AddAdapter(ctx, adapter)
-			act = mesherykube.INSTALL
 		}
-		err := kubeclient.ApplyHelmChart(mesherykube.ApplyHelmChartConfig{
-			Namespace:       utils.MesheryNamespace,
-			ReleaseName:     "meshery",
-			CreateNamespace: true,
-			ChartLocation: mesherykube.HelmChartLocation{
-				Repository: utils.HelmChartURL,
-				Chart:      utils.HelmChartName,
-				Version:    "latest",
-			},
-			OverrideValues: map[string]interface{}{},
-			Action:         act,
-			// the helm chart will be downloaded to ~/.meshery/manifests if it doesn't exist
-			DownloadLocation: path.Join(utils.MesheryFolder, utils.ManifestsFolder),
-			DryRun:           true,
-		})
 
+		// Clone the Meshery-Istio repository
+		cmd := exec.Command("git", "clone", "https://github.com/layer5io/"+adapter.Name+".git")
+		err := cmd.Run()
 		if err != nil {
 			r.Log.Error(err)
 			return
 		}
 
-		r.Log.Info("Adapter operation executed")
-
-		if !del {
-			endpoint, err := model.SubscribeToBroker(provider, kubeclient, r.brokerChannel, r.BrokerConn, connectionTrackerSingleton)
-			r.Log.Debug("Endpoint: ", endpoint)
-			if err != nil {
-				r.Log.Error(err)
-				return
-			}
-			connectionTrackerSingleton.Set(k8scontext.ID, endpoint)
-			r.Log.Info("Connected to broker at:", endpoint)
-			connectionTrackerSingleton.Log(r.Log)
+		// Navigate to the Meshery-Istio directory
+		err = os.Chdir(adapter.Name)
+		if err != nil {
+			r.Log.Error(err)
 		}
 
-		r.Log.Info("Meshsync operation executed")
-	}(delete, kubeclient)
+		// Run the Meshery-Istio
+		cmd = exec.Command("make", "run")
+		err = cmd.Run()
+		if err != nil {
+			r.Log.Error(err)
+		}
+
+		if status == model.StatusEnabled {
+			r.Log.Info("Deployed Adapter")
+			deleteAdaptor = false
+		} else {
+			r.Log.Info("Undeployed Adapter")
+		}
+	}(deleteAdaptor)
 
 	return model.StatusProcessing, nil
 }
