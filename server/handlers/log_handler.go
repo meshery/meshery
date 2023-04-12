c605abd97cddffc25b5f95cbb5a9c8d2d692859f
Abhishek kr <abhimait1909@gmail.com>
2023-04-13 03:22:44 +0530

    add log header to log file
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

--- server/handlers/log_handler.go
+++ server/handlers/log_handler.go
@@ -2,9 +2,12 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 	"path/filepath"
+	"runtime"
+	"time"
 
 	"github.com/layer5io/meshery/server/models"
 	"github.com/sirupsen/logrus"
@@ -30,47 +33,70 @@ func (h *Handler) LogHandler(w http.ResponseWriter, req *http.Request, prefObj *
 		return
 	}
 
-	err := createLogFile(user, parsedBody.SessionId, parsedBody.Logs)
+	err := writeLogToFile(user, parsedBody.SessionId, parsedBody.Logs)
 	if err != nil {
 		w.Write([]byte(err.Error()))
 		w.WriteHeader(500)
 		return
 	}
 
-	w.Write([]byte("Response provided!!"))
+	w.Write([]byte("Logs written on filesystem"))
 	w.WriteHeader(200)
 }
 
-func createLogFile(user *models.User, sessionId string, logs []Log) error {
-	filePath, err := getLogFilePath()
-	// create set of folders if doesn't exist
-	errMkdir := os.MkdirAll(filePath, os.ModePerm)
-	if err != nil || errMkdir != nil {
-		logrus.Error("Error doing File operation on operating system", err)
-		return err
-	}
-
-	f, err := os.OpenFile(filepath.Join(filePath, sessionId), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+func writeLogToFile(user *models.User, sessionId string, logs []Log) error {
+	f, err := createLogFile(sessionId)
 	if err != nil {
-		logrus.Errorf("Failed to open/create/append to logFile with session Id %s", sessionId)
 		return err
 	}
-
 	defer f.Close()
 
-	byte, err := json.Marshal(logs)
-	if err != nil {
-		logrus.Error("error marshalling json")
-		return err
+	var stringBytes []byte
+
+	// check for file info and if the file is empty add the log-headers
+	fileInfo, _ := f.Stat()
+	if fileInfo.Size() == 0 {
+		stringBytes = getLogFileHeaders(user)
 	}
 
-	if _, err = f.Write(byte); err != nil {
+	// go through logs to write to file as a JSON entry where each line is a log on the file
+	for _, log := range logs {
+		bytes, err := json.Marshal(log)
+		if err != nil {
+			logrus.Error(fmt.Sprintf("failed to marshal error log %s", err.Error()))
+			continue
+		}
+		stringBytes = append(stringBytes, bytes...)
+		stringBytes = append(stringBytes, []byte("\n")...)
+	}
+
+	if _, err = f.Write(stringBytes); err != nil {
 		return err
 	}
 
 	return nil
 }
 
+// createLogFile returns reference to the opened file as a result of request to create or append
+func createLogFile(sessionId string) (*os.File, error) {
+	filePath, err := getLogFilePath()
+	// create set of folders if doesn't exist
+	errMkdir := os.MkdirAll(filePath, os.ModePerm)
+	if err != nil || errMkdir != nil {
+		logrus.Error("Error doing File operation on operating system", err)
+		return nil, err
+	}
+
+	f, err := os.OpenFile(filepath.Join(filePath, sessionId), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		logrus.Errorf("Failed to open/create/append to logFile with session Id %s [%s]", sessionId, err.Error())
+		return nil, err
+	}
+
+	return f, nil
+}
+
+// getLogFilePath gets the log directory where the log file has to be placed
 func getLogFilePath() (string, error) {
 	homDir, e := os.UserHomeDir()
 	if e != nil {
@@ -78,4 +104,15 @@ func getLogFilePath() (string, error) {
 	}
 	logDir := "/.meshery/logs/"
 	return filepath.Join(homDir, logDir), nil
+}
+
+func getLogFileHeaders(user *models.User) []byte {
+	return []byte(fmt.Sprintf(`// Meshery Log File, please share this file to raise issues or problems you report in Meshery
+// System Information: 
+// Operating System: %s
+// CPUs: %d
+// Os Architecture: %s
+// 
+// Time Created: %s
+`, runtime.GOOS, runtime.NumCPU(), runtime.GOARCH, time.Now().String()))
 }
