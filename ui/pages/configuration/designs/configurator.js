6cd3e45055622b7ae29d85d24f504dc2ac0a3785
Abhishek kr <abhimait1909@gmail.com>
2023-04-21 03:24:18 +0530

    Add title and beta badge for design configurator
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

--- ui/pages/configuration/designs/configurator.js
+++ ui/pages/configuration/designs/configurator.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NoSsr, withStyles } from "@material-ui/core";
-import { updatepagepath } from "../../../lib/store";
+import { updatebetabadge, updatepagepath, updatepagetitle } from "../../../lib/store";
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
 import Head from 'next/head';
@@ -15,10 +15,12 @@ const styles = {
   }
 };
 
-class Patterns extends React.Component {
+class DesignConfiguratorPage extends React.Component {
   componentDidMount() {
     console.log(`path: ${getPath()}`);
-    this.props.updatepagepath({ path : getPath() });
+    this.props.updatepagepath({ path : getPath(), isBeta : true, title : "Configure Design" });
+    this.props.updatepagetitle({ title : "Configure Design" })
+    this.props.updatebetabadge({ isBeta : true })
   }
 
   render() {
@@ -33,9 +35,13 @@ class Patterns extends React.Component {
   }
 }
 
-const mapDispatchToProps = dispatch => ({ updatepagepath : bindActionCreators(updatepagepath, dispatch) })
+const mapDispatchToProps = dispatch => ({
+  updatepagepath : bindActionCreators(updatepagepath, dispatch),
+  updatepagetitle : bindActionCreators(updatepagetitle, dispatch),
+  updatebetabadge : bindActionCreators(updatebetabadge, dispatch)
+})
 
 export default withStyles(styles)(connect(
   null,
   mapDispatchToProps
-)(Patterns));
+)(DesignConfiguratorPage));
