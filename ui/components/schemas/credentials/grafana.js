1ffb213b96c0b4c1ac4076e1e84db631eacadf04
harkiratsm <multaniharry714@gmail.com>
2023-04-30 15:08:21 +0530

    add creds icon and isolate the schemas
    Signed-off-by: harkiratsm <multaniharry714@gmail.com>

---
+++ ui/components/schemas/credentials/grafana.js
@@ -0,0 +1,34 @@
+export const grafanaSchema = {
+  type : "object",
+  title : "Grafana",
+  properties : {
+    key : {
+      type : "string",
+      default : "grafana"
+    },
+    grafanaCredentialName : {
+      title : "Credential Name",
+      type : "string",
+      description : "Name of your credential"
+    },
+    secret : {
+      type : "object",
+      title : "Credential Secret",
+      description : "Credential secret for the Grafana instance",
+      properties : {
+        grafanaURL : {
+          type : "string",
+          title : "URL",
+          description : "URL of the Grafana instance"
+        },
+        grafanaAPIKey : {
+          type : "string",
+          title : "API Key",
+          description : "API Key for the Grafana instance"
+        }
+      },
+      required : ["grafanaURL", "grafanaAPIKey"]
+    }
+  },
+  required : ["grafanaCredentialName"]
+};
