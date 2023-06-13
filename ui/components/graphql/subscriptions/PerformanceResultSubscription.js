6ab37cd60af8bfa5eaa12ab49885cbf79143d01e
Abhishek kr <abhimait1909@gmail.com>
2023-06-14 04:51:54 +0530

    add the gql subscriptions
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

---
+++ ui/components/graphql/subscriptions/PerformanceResultSubscription.js
@@ -0,0 +1,34 @@
+import { graphql, requestSubscription } from "react-relay";
+import { createRelayEnvironment } from "../../../lib/relayEnvironment";
+
+const performanceResultSubscription = graphql`
+subscription PerformanceResultSubscription($selector: PageFilter!, $profileID: String!) {
+    subscribePerfResults(selector: $selector, profileID: $profileID) {
+            page
+            page_size
+            total_count
+            results {
+                meshery_id
+                name
+                mesh
+                performance_profile
+                test_id
+                server_metrics
+                test_start_time
+                created_at
+                user_id
+                updated_at
+                runner_results
+            }
+        }
+    }
+`;
+export default function subscribePerformanceProfiles(dataCB, variables) {
+  const environment = createRelayEnvironment({});
+  return requestSubscription(environment, {
+    subscription : performanceResultSubscription,
+    variables : variables,
+    onNext : dataCB,
+    onError : (error) => console.log(`requestSubscription error:`, error),
+  });
+}
