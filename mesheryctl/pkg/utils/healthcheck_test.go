d65c3b1a0c92c43be5e67fa97a29d83612e003e7
Freedisch <freeproduc@gmail.com>
2023-06-19 23:35:28 +0300

    added tests for healthcheck
    Signed-off-by: Freedisch <freeproduc@gmail.com>

--- mesheryctl/pkg/utils/healthcheck_test.go
+++ mesheryctl/pkg/utils/healthcheck_test.go
@@ -1,7 +1,13 @@
 package utils
 
 import (
+	"context"
+	"fmt"
 	"testing"
+
+	meshkitkube "github.com/layer5io/meshkit/utils/kubernetes"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var versionCheck = []struct {
@@ -93,5 +99,132 @@ func TestCheckKubectlVersion(t *testing.T) {
 				t.Errorf("checkKubectlVersion() = %v, want %v", got, tt.expected)
 			}
 		})
+	}
+}
+
+func TestCheckK8sVersion(t *testing.T) {
+	tests := []struct {
+		version string
+		expected [3]int
+	}{
+		{
+			version: "v1.12.0",
+			expected: [3]int{1, 12, 0},
+		},
+		{
+			version: "v1.12.1",
+			expected: [3]int{1, 12, 1},
+		},
+		{
+			version: "v1.12.2",
+			expected: [3]int{1, 12, 2},
+		},
+		{
+			version: "v1.12.3",
+			expected: [3]int{1, 12, 3},
+		},
+		{
+			version: "v1.12.4",
+			expected: [3]int{1, 12, 4},
+		},
+		{
+			version: "v1.12.5",
+			expected: [3]int{1, 12, 5},
+		},
+		{
+			version: "v1.12.6",
+			expected: [3]int{1, 12, 6},
+		},
+		{
+			version: "v1.12.7",
+			expected: [3]int{1, 12, 7},
+		},
+		{
+			version: "v1.12.8",
+			expected: [3]int{1, 12, 8},
+		},
+		{
+			version: "v1.12.9",
+			expected: [3]int{1, 12, 9},
+		},
+		{
+			version: "v1.12.10",
+			expected: [3]int{1, 12, 10},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.version, func(t *testing.T) {
+			if got, _:= getK8sVersion(tt.version); got != tt.expected {
+				t.Errorf("getK8sVersion() = %v, want %v", got, tt.expected)
+			}
+		})
+	}
+
+}
+
+
+func TestIsPodRunning(t *testing.T) {
+	tests := []struct {
+		namespace string
+		podname string
+		expected string
+	}{
+		{
+			namespace: "test0",
+			podname: "test-pod0",
+			expected: "Running",
+		},
+		{
+			namespace: "test1",
+			podname: "test-pod1",
+			expected: "Running",
+		},
+		{
+			namespace: "test2",
+			podname: "test-pod2",
+			expected: "Running",
+		},
+		{
+			namespace: "test3",
+			podname: "test-pod3",
+			expected: "Running",
+		},
+		{
+			namespace: "test4",
+			podname: "test-pod4",
+			expected: "Running",
+		},
+		{
+			namespace: "",
+			podname: "test-pod",
+			expected: "Running",
+		},
+		{
+			namespace: "test",
+			podname: "",
+			expected: "Running",
+		},
+		{
+			namespace: "",
+			podname: "",
+			expected: "Running",
+		},
+		{
+			podname: "test-pod",
+			expected: "Failed",
+		},
+		{
+			namespace: "test",
+			expected: "Failed",
+		},
+	}
+
+	for _, tt := range tests {
+		c, _ := meshkitkube.New([]byte(""))
+		got, _ := c.KubeClient.CoreV1().Pods(tt.namespace).Get(context.TODO(), tt.podname, metav1.GetOptions{})
+		if got.Status.Phase != v1.PodPhase(tt.expected) {
+			fmt.Errorf("Pod is Running  got %v want %v", got, tt.expected)
+		}
 	}
 }
