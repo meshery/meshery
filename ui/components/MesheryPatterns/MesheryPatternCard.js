165af7ce2fbafdd0283d9d2e53c3456c6097bb78
Lee Calcote <leecalcote@gmail.com>
2023-06-19 20:44:04 -0500

    Update ui/components/MesheryPatterns/MesheryPatternCard.js
    Signed-off-by: Lee Calcote <leecalcote@gmail.com>

--- ui/components/MesheryPatterns/MesheryPatternCard.js
+++ ui/components/MesheryPatterns/MesheryPatternCard.js
@@ -93,7 +93,7 @@ function MesheryPatternCard({
               </Typography>
               <div style={{ border : "1px solid rgb(118 118 118)",color : "rgb(118 118 118)",marginTop : "auto",fontSize : "0.6rem",padding : "0.2rem 0.5rem",borderRadius : "0.2rem" }}>
                PUBLISHED
-               </div>
+              </div>
             </div>
             <div className={classes.lastRunText} >
               <div>
