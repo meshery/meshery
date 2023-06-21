699cf4bacf67fa3445a631dbabbcf773e3dbb0e3
Akash Mishra <80618499+helloakash1701@users.noreply.github.com>
2023-06-21 13:23:10 +0530

    Update script.sh
    Improvements made in this version:
    
    1- Added informative echo statements to provide feedback during the execution of the script.
    
    2 -Assigned the file name and target directory to variables (zip_file and target_dir, respectively) for easier customization and maintenance.
    
    3- Added an error check after the zip operation (zip -r "$zip_file" "$target_dir") using $? to determine the exit status of the previous command. If the exit status is zero, it indicates success, and a corresponding message is displayed. Otherwise, a failure message is shown.
    
    These modifications enhance the script by providing clearer output and making it more flexible for customization.
    
    Signed-off-by: Akash Mishra <80618499+helloakash1701@users.noreply.github.com>

--- docs/script.sh
+++ docs/script.sh
@@ -1,6 +1,21 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
-if [ -f site-dir.zip ]; then
-    rm -rf site-dir.zip
+zip_file="site-dir.zip"
+target_dir="./docs/_site"
+
+# Check if the zip file exists
+if [ -f "$zip_file" ]; then
+    echo "Removing existing $zip_file..."
+    rm "$zip_file"
 fi
-zip -r site-dir.zip ./docs/_site
+
+# Create a new zip file
+echo "Creating $zip_file..."
+zip -r "$zip_file" "$target_dir"
+
+# Check the zip operation result
+if [ $? -eq 0 ]; then
+    echo "Zip file created successfully."
+else
+    echo "Failed to create zip file."
+fi
