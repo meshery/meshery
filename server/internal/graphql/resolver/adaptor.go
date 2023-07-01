d0a8d8cd6d627eac802b3a1945ef67e58ef4d304
vaibhav bisht <vaibhavbisht592@gmail.com>
2023-07-01 19:10:42 +0530

    added adapters in extensions
    Signed-off-by: vaibhav bisht <vaibhavbisht592@gmail.com>

--- server/internal/graphql/resolver/adaptor.go
+++ server/internal/graphql/resolver/adaptor.go
@@ -2,13 +2,43 @@ package resolver
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/layer5io/meshery/server/helpers"
 	"github.com/layer5io/meshery/server/helpers/utils"
 	"github.com/layer5io/meshery/server/internal/graphql/model"
 	"github.com/layer5io/meshery/server/models"
 )
 
+func getAdapterInformationByName(adapterName string) *models.Adapter {
+	var adapter *models.Adapter
+
+	for _, v := range models.ListAvailableAdapters {
+		if adapterName == v.Name {
+			adapter = &v
+		}
+	}
+
+	return adapter
+}
+
 func (r *Resolver) changeAdapterStatus(_ context.Context, _ models.Provider, targetStatus model.Status, adapterName, targetPort string) (model.Status, error) {
+	// not able to perform any operation when the name is not there
+	if adapterName == "" && targetPort == "" {
+		return model.StatusUnknown, helpers.ErrAdapterInsufficientInformation(fmt.Errorf("either of adapter name or target port is not provided, please provide a name of adapter or target-port to perform operation on"))
+	}
+
+	// in case of empty target, prefer the default ports
+	if targetPort == "" {
+		r.Log.Warn(fmt.Errorf("target port is not specified in the request body, searching for default ports"))
+		selectedAdapter := getAdapterInformationByName(adapterName)
+		if selectedAdapter == nil {
+			return model.StatusUnknown, helpers.ErrAdapterInsufficientInformation(fmt.Errorf("adapter name is not available, not able to figure out target port"))
+		}
+
+		targetPort = selectedAdapter.Location
+	}
+
 	platform := utils.GetPlatform()
 	if platform == "kubernetes" {
 		r.Log.Info("Feature for kuberenetes disabled")
@@ -24,7 +54,7 @@ func (r *Resolver) changeAdapterStatus(_ context.Context, _ models.Provider, tar
 		r.Log.Info("Undeploying Adapter")
 	}
 
-	r.Log.Debug(adapterName, targetPort)
+	r.Log.Debug(fmt.Printf("changing adapter status of %s on port %s to status %s \n", adapterName, targetPort, targetStatus))
 	adapter := models.Adapter{Name: adapterName, Location: targetPort}
 	go func(routineCtx context.Context, del bool) {
 		var operation string
