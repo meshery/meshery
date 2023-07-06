d36d41c1a520ba044231a4c5096963eb60607247
harkiratsm <multaniharry714@gmail.com>
2023-07-06 23:53:36 +0530

    separate the publish schema
    Signed-off-by: harkiratsm <multaniharry714@gmail.com>

--- ui/components/PublishModal.js
+++ ui/components/PublishModal.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button, Grid } from '@material-ui/core';
-// import { createTheme } from '@material-ui/core/styles';
 import validator from "@rjsf/validator-ajv8";
 import {
   Dialog, DialogActions,
@@ -10,76 +9,11 @@ import {
 import { Form } from '@rjsf/material-ui';
 import useStyles from "./MesheryPatterns/Cards.styles";
 import PublicIcon from '@material-ui/icons/Public';
-// const getMuiTheme = () => createTheme({
-//   palette : {
-//     primary : {
-//       main : "#607d8b"
-//     },
-//     secondary : {
-//       main : "#666666"
-//     }
-//   },
-//   overrides : {
-//     MuiGrid : {
-//       input : {
-//         color : '#607d8b'
-//       }
-//     },
-//   }
-// })
+import { publishSchema } from './schemas/connections/publishSchema';
 
 export default function PublishModal(props) {
   const { open, handleClose, resourceType, handlePublish, title } = props;
   const classes = useStyles();
-  const schema = {
-    "type" : "object",
-    "properties" : {
-      "compatibility" : {
-        "type" : "array",
-        "items" : {
-          "enum" : [
-            "Kubernetes",
-            "Argo CD",
-            "AWS App Mesh",
-            "Consul",
-            "Fluentd",
-            "Istio",
-            "Jaeger",
-            "Kuma",
-            "Linkerd",
-            "Network Service Mesh",
-            "NGINX Service Mesh",
-            "Open Service Mesh",
-            "Prometheus",
-            "Traefik Mesh"
-          ],
-          "type" : "string"
-        },
-        "uniqueItems" : true,
-
-      },
-      "pattern_caveats" : {
-        "type" : "string"
-      },
-      "pattern_info" : {
-        "type" : "string"
-      },
-      "type" : {
-        "type" : "string",
-        "examples" : [
-          "deployment",
-          "observability",
-          "resiliency",
-          "scaling",
-          "security",
-          "traffic-management",
-          "troubleshooting",
-          "workloads"
-        ]
-      }
-    },
-    "required" : ["compatibility", "pattern_caveats", "pattern_info", "type"] // TODO: to be injected dynamically and backed by RJSF
-  }
 
   const data = resourceType?.catalog_data || {};
 
@@ -93,13 +27,13 @@ export default function PublishModal(props) {
         </DialogTitle>
         <DialogContent>
           <Grid container spacing={24} alignItems="center">
-            <Form schema={schema} formData={data} validator={validator}
+            <Form schema={publishSchema} formData={data} validator={validator}
               onSubmit={(data) => {
-                handleClose();
                 handlePublish({
                   id : resourceType.id,
                   catalog_data : data.formData
                 })
+                handleClose();
               }}
             >
               <Button
