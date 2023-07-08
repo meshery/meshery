2433c70d6b46c438bd14f8d2112966c031da1fd9
aabidsofi19 <mailtoaabid01@gmail.com>
2023-07-08 20:55:43 +0530

    make chart colors go round-robin
    Signed-off-by: aabidsofi19 <mailtoaabid01@gmail.com>

---
+++ ui/utils/chartColors.js
@@ -0,0 +1,17 @@
+export const CHART_COLORS = ["#00B39F" ,"#396679" ,"#83B71E" , '#00382D','#477E96','#396679']
+
+export const dataToColors = (data) => {
+  const columns = data.map((item) => item[0] )
+  const colors = { }
+  let colorIdx = 0
+
+  columns.forEach((col) => {
+    if (colorIdx >= CHART_COLORS.length) {
+      colorIdx = 0
+    }
+    colors[col] = CHART_COLORS[colorIdx]
+    colorIdx +=1
+  })
+
+  return colors
+}
