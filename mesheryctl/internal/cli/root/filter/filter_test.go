05c2f75f75ed68880f06e0a193c732a1ff261620
Philip-21 <philipuzomaobiora@gmail.com>
2023-07-09 01:13:18 +0100

    first commit
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

---
+++ mesheryctl/internal/cli/root/filter/filter_test.go
@@ -0,0 +1,127 @@
+package filter
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
+)
+
+func TestFilterCmd(t *testing.T) {
+	utils.SetupContextEnv(t)
+
+	// initialize mock server for handling requests
+	utils.StartMockery(t)
+
+	// create a test helper
+	testContext := utils.NewTestHelper(t)
+
+	// get current directory
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		t.Fatal("Not able to get current working directory")
+	}
+	currentDir := filepath.Dir(filename)
+	fixturesDir := filepath.Join(currentDir, "fixtures")
+
+	FilterTestCases := []struct {
+		Name             string
+		Args             []string
+		URL              string
+		Fixture          string
+		Token            string
+		ExpectedResponse string
+		ExpectHelp       bool
+		ExpectErr        bool
+	}{
+		{
+			Name:             "view",
+			Args:             []string{"view"},
+			Token:            filepath.Join(fixturesDir, "token.golden"),
+			ExpectedResponse: "filter.output.golden",
+			Fixture:          "filter.view.api.response.golden",
+			URL:              testContext.BaseURL + "/api/filter/view",
+			ExpectHelp:       false,
+			ExpectErr:        false,
+		},
+		{
+			Name:             "delete",
+			Args:             []string{"delete"},
+			Token:            filepath.Join(fixturesDir, "token.golden"),
+			ExpectedResponse: "filter.output.golden",
+			Fixture:          "filter.delete.api.response.golden",
+			URL:              testContext.BaseURL + "/api/filter/delete",
+			ExpectHelp:       false,
+			ExpectErr:        false,
+		},
+		{
+			Name:             "list",
+			Args:             []string{"list"},
+			Token:            filepath.Join(fixturesDir, "token.golden"),
+			ExpectedResponse: "filter.output.golden",
+			Fixture:          "filter.list.api.response.golden",
+			URL:              testContext.BaseURL + "/api/filter/list",
+			ExpectHelp:       false,
+			ExpectErr:        false,
+		},
+	}
+	for _, tc := range FilterTestCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			// View api response from golden files
+			apiResponse := utils.NewGoldenFile(t, tc.Fixture, fixturesDir).Load()
+
+			//set token
+			utils.TokenFlag = tc.Token
+			// mock response
+			httpmock.RegisterResponder("GET", tc.URL,
+				httpmock.NewStringResponder(200, apiResponse))
+
+			//Expected Response
+			testdataDir := filepath.Join(currentDir, "testdata")
+			golden := utils.NewGoldenFile(t, tc.ExpectedResponse, testdataDir)
+
+			//Console Prints
+			StdOut := os.Stdout
+			//return expected pair of files/output
+			read, write, _ := os.Pipe()
+			os.Stdout = write
+			_ = utils.SetupMeshkitLoggerTesting(t, false)
+			cmd := FilterCmd
+			cmd.SetArgs(tc.Args)
+			cmd.SetOutput(StdOut)
+
+			err := cmd.Execute()
+			if err != nil {
+				if tc.ExpectHelp && tc.ExpectErr {
+					//write in file
+					if *update {
+						golden.Write(err.Error())
+					}
+					expectedResponse := golden.Load()
+					utils.Equals(t, expectedResponse, err.Error())
+					return
+				}
+				t.Fatal(err)
+			}
+			write.Close()
+			output, _ := io.ReadAll(read)
+			os.Stdout = StdOut
+			//print response string to console
+			Response := string(output)
+			// write it in file
+			if *update {
+				golden.Write(Response)
+			}
+			expectedResponse := golden.Load()
+
+			utils.Equals(t, expectedResponse, Response)
+
+		})
+	}
+	// stop mock server
+	utils.StopMockery(t)
+}
