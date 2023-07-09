35ef049e87547a405893c9d33a53ac64a91543c5
Lee Calcote <lee.calcote@layer5.io>
2023-07-08 19:45:46 -0500

    tweak
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- ui/components/DashboardMeshModelGraph.js
+++ ui/components/DashboardMeshModelGraph.js
@@ -1,48 +1,56 @@
 import Grid from "@material-ui/core/Grid";
-import React, { useEffect,  useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Typography
 } from "@material-ui/core";
-import  BBChart from "./BBChart"
-import  { donut,pie } from "billboard.js";
-import { getAllComponents,getMeshModels,getRelationshipsDetail,fetchCategories, getModelFromCategoryApi } from "../api/meshmodel"
+import BBChart from "./BBChart"
+import { donut, pie } from "billboard.js";
+import { getAllComponents, getMeshModels, getRelationshipsDetail, fetchCategories, getModelFromCategoryApi } from "../api/meshmodel"
 import { dataToColors } from "../utils/chartColors"
 
-const useFetchTotal = ( fetchr ) => {
-  const [total , setTotal] = useState(0)
+const useFetchTotal = (fetchr) => {
+  const [total, setTotal] = useState(0)
 
   useEffect(() => {
     fetchr().then((json) => {
       setTotal(json["total_count"])
-    }).catch(e => console.log("Api Error : ",e))
-  },[])
+    }).catch(e => console.log("Api Error : ", e))
+  }, [])
   return total
 }
 
-function MeshModelContructs({ classes }){
-  const totalModels = useFetchTotal(() => getMeshModels(1,1))
-  const totalComponents = useFetchTotal(() => getAllComponents(1,1))
-  const totalRelationships = useFetchTotal(() => getRelationshipsDetail(1 , 1))
+function MeshModelContructs({ classes }) {
+  const totalModels = useFetchTotal(() => getMeshModels(1, 1))
+  const totalComponents = useFetchTotal(() => getAllComponents(1, 1))
+  const totalRelationships = useFetchTotal(() => getRelationshipsDetail(1, 1))
   const data = [
     ["Models", totalModels],
-    ["Components", totalComponents], 
-    
-    ["Relationships",totalRelationships]
+    ["Components", totalComponents],
+    ["Relationships", totalRelationships]
   ]
   const chartOptions = {
-    data : {
-      columns : data ,
-      type : donut(),
-      colors : dataToColors(data)
+    data: {
+      columns: data,
+      type: donut(),
+      colors: dataToColors(data),
     },
-    tooltip : {
-      format : {
-        value : function(v){
+    arc: {
+      cornerRadius: {
+        ratio: 0.1
+      }
+    },
+    bindto: "#donutCornerRadius",
+    donut: {
+      title: "Capabilities\nRegistry"
+    },
+    bindto: "#multilineTitle",
+    tooltip: {
+      format: {
+        value: function (v) {
           return v
         }
       }
     },
-
   }
 
   return (
@@ -62,24 +70,24 @@ function MeshModelContructs({ classes }){
 
 function MeshModelCategories({ classes }) {
 
-  const [categoryMap , setCategoryMap] = useState({})
-  const cleanedData = Object.keys(categoryMap).map((key) => ([key,categoryMap[key]]) )
+  const [categoryMap, setCategoryMap] = useState({})
+  const cleanedData = Object.keys(categoryMap).map((key) => ([key, categoryMap[key]]))
   const chartOptions = {
 
-    data : {
-      columns : cleanedData ,
-      colors : dataToColors(cleanedData),
-      type : pie(),
+    data: {
+      columns: cleanedData,
+      colors: dataToColors(cleanedData),
+      type: pie(),
     },
-    tooltip : {
-      format : {
-        value : function(v){
+    tooltip: {
+      format: {
+        value: function (v) {
           return `${v} Models`
         }
       }
     },
-    legend : {
-      show : false
+    legend: {
+      show: false
     }
   }
   useEffect(() => {
@@ -87,11 +95,11 @@ function MeshModelCategories({ classes }) {
       categoriesJson['categories'].forEach((category) => {
         let categoryName = category.name
         getModelFromCategoryApi(categoryName).then((modelsJson) => {
-          setCategoryMap((prevState) => ( { ...prevState,[categoryName] : modelsJson["total_count"] }) )
+          setCategoryMap((prevState) => ({ ...prevState, [categoryName]: modelsJson["total_count"] }))
         })
       })
     })
-  },[])
+  }, [])
 
   return (
     <div className={classes.dashboardSection}>
@@ -111,11 +119,11 @@ export default function DashboardMeshModelGraph({ classes }) {
   return (
     <Grid container spacing={4}>
       <Grid item xs={12} md={6}>
-        <MeshModelCategories classes={classes}/>
+        <MeshModelCategories classes={classes} />
       </Grid>
 
       <Grid item xs={12} md={6}>
-        <MeshModelContructs classes={classes}/>
+        <MeshModelContructs classes={classes} />
       </Grid>
 
     </Grid>)
