f446ccf935d9a667c843eafaeba1d3746de77c66
Pranav Singh <pranavsingh02@hotmail.com>
2023-07-12 13:17:28 +0530

    add supported system names
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

---
+++ scripts/component_updation/pkg/utility.go
@@ -0,0 +1,23 @@
+package pkg
+
+type SystemType int
+
+const (
+	Meshery SystemType = iota
+	Docs
+	RemoteProvider
+)
+
+func (dt SystemType) String() string {
+	switch dt {
+		case Meshery:
+			return "meshery"
+
+		case Docs:
+			return "docs"
+
+		case RemoteProvider:
+			return "remote-provider"
+	}
+	return ""
+}
