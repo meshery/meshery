538e6e762eae4063bd2be53844531a9bd4495d6e
MUzairS15 <muzair.shaikh810@gmail.com>
2023-07-17 14:16:10 +0530

    undo eslint changes
    Signed-off-by: MUzairS15 <muzair.shaikh810@gmail.com>

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -13,8 +13,7 @@ module.exports = {
   "extends" : ["eslint:recommended", "plugin:react/recommended", "plugin:cypress/recommended", "next"],
   "globals" : {
     "Atomics" : "readonly",
-    "SharedArrayBuffer" : "readonly",
-    "globalThis": "readonly"
+    "SharedArrayBuffer" : "readonly"
   },
   "parser" : "@babel/eslint-parser",
   "parserOptions" : {
