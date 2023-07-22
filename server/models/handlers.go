94bb1f1b2f871d2b935daa86a894e5dfbcd8c43e
Freedisch <freeproduc@gmail.com>
2023-07-22 10:41:59 +0300

    remove func
    Signed-off-by: Freedisch <freeproduc@gmail.com>

--- server/models/handlers.go
+++ server/models/handlers.go
@@ -108,7 +108,6 @@ type HandlerInterface interface {
 	ValidationHandler(rw http.ResponseWriter, r *http.Request)
 	MeshModelGenerationHandler(rw http.ResponseWriter, r *http.Request)
 	GetMeshmodelModels(rw http.ResponseWriter, r *http.Request)
-	GetMeshmodelRegistery(rw http.ResponseWriter, r *http.Request)
 	RegisterMeshmodelComponents(rw http.ResponseWriter, r *http.Request)
 
 	GetMeshmodelComponentByModel(rw http.ResponseWriter, r *http.Request)
