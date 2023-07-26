295612cad871983089b271cea2f3d57363120629
Abhishek kr <abhimait1909@gmail.com>
2023-07-27 03:00:13 +0530

    Enhancement: Dont crash the UI on RJSF Form Errors
    Signed-off-by: Abhishek kr <abhimait1909@gmail.com>

---
+++ ui/components/ErrorBoundary.js
@@ -0,0 +1,40 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+
+/**
+ * ErrorBoundary is a React component that catches JavaScript errors in its child components and renders a fallback UI when an error occurs.
+ * It should be used as a wrapper around components that might throw errors.
+ */
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError : false, error : null };
+  }
+
+  /** Update state so the next render will show the fallback UI. */
+  static getDerivedStateFromError(error) {
+    return { hasError : true, error : error };
+  }
+
+  resetErrorBoundary = () => {
+    this.setState({ hasError : false, error : null });
+  }
+
+  /** You can render any custom fallback UI */
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="alert alert-danger">
+          <p>Couldn&apos;t open RJSF Form, the following error was encountered:</p>
+          <pre>{this.state.error.message}</pre>
+          <Button color="primary" variant="contained" onClick={this.resetErrorBoundary}>
+            Refresh Form
+          </Button>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
