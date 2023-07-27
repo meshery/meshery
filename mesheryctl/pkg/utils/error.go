40d893ed8df9b18cbe50d863ed18b41ad2348687
Philip-21 <philipuzomaobiora@gmail.com>
2023-07-27 13:47:54 +0100

    creating probable cause and remedy fro the error wrappers
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- mesheryctl/pkg/utils/error.go
+++ mesheryctl/pkg/utils/error.go
@@ -8,17 +8,16 @@ import (
 )
 
 var (
-	ErrFailRequestCode     = "1044"
-	ErrFailReqStatusCode   = "1045"
-	ErrAttachAuthTokenCode = "1046"
-	ErrReadConfigFileCode          = "replace_me"
-	ErrMarshalIndentCode           = "replace_me"
-	ErrMakeRequestCode             = "replace_me"
-	ErrProcessingConfigCode  ="replace_me"
-	ErrResponseStatusBodyCode      = "replace_me"
-	ErrResponseStatusCode          = "replace_me"
-	ErrJSONToYAMLCode              = "replace_me"
-	ErrOutFormatFlagCode           = "replace_me"
+	ErrFailRequestCode        = "1044"
+	ErrFailReqStatusCode      = "1045"
+	ErrAttachAuthTokenCode    = "1046"
+	ErrReadConfigFileCode     = "replace_me"
+	ErrMarshalIndentCode      = "replace_me"
+	ErrProcessingConfigCode   = "replace_me"
+	ErrResponseStatusBodyCode = "replace_me"
+	ErrResponseStatusCode     = "replace_me"
+	ErrJSONToYAMLCode         = "replace_me"
+	ErrOutFormatFlagCode      = "replace_me"
 )
 
 // RootError returns a formatted error message with a link to 'root' command usage page at
@@ -229,58 +228,61 @@ func formatError(msg string, cmd cmdType) string {
 }
 
 func ErrAttachAuthToken(err error) error {
-	return errors.New(ErrAttachAuthTokenCode, errors.Alert, []string{err.Error()},
-		[]string{"Authentication token not found. Login with `mesheryctl system login` or supply a valid user token using the --token (or -t) flag."}, []string{}, []string{})
+	return errors.New(ErrAttachAuthTokenCode, errors.Alert, []string{"Authentication token not found"},
+		[]string{"Authentication token not found", err.Error()}, []string{"User not logged in to generate token"}, []string{ "Login with `mesheryctl system login` or supply a valid user token using the --token (or -t) flag."})
 }
 
 func ErrFailRequest(err error) error {
 	return errors.New(ErrFailRequestCode, errors.Alert, []string{"Failed to make a request"},
-		[]string{err.Error()}, []string{}, []string{})
+		[]string{"Failed to make request" + err.Error()}, []string{"Probable network connection problem"}, []string{"Check your network connection and check the status of meshery server via 'mesheryctl system status'"})
 }
 
 func ErrUnauthenticated() error {
-	return errors.New(ErrFailRequestCode, errors.Alert, []string{},
-		[]string{"Authentication token is invalid. Please supply a valid user token. Login with `mesheryctl system login`"}, []string{}, []string{})
+	return errors.New(ErrFailRequestCode, errors.Alert, []string{"Authentication token is invalid"},
+		[]string{"Authentication token is invalid. Please supply a valid user token. "}, []string{"User not logged in correctly to generate token"}, []string{"Please login with `mesheryctl system login`"})
 }
 
 func InvalidToken() error {
 	return errors.New(ErrFailRequestCode, errors.Alert, []string{},
-		[]string{"Authentication token is expired/invalid. Please login with `mesheryctl system login` to generate a new token"}, []string{}, []string{})
+		[]string{"Authentication token is expired/invalid."}, []string{"Token expired"}, []string{" Please login with `mesheryctl system login` to generate a new token"})
 }
 
 func ErrFailReqStatus(statusCode int) error {
-	return errors.New(ErrFailReqStatusCode, errors.Alert, []string{},
-		[]string{"Response Status Code " + strconv.Itoa(statusCode) + ", possible Server Error"}, []string{}, []string{})
+	return errors.New(ErrFailReqStatusCode, errors.Alert, []string{"Failed response Server Error"},
+		[]string{"Response Status Code " + strconv.Itoa(statusCode) + ", possible Server Error"}, []string{"Bad api call"}, []string{"Check your network connection and check the status of meshery server via 'mesheryctl system status'"})
 }
 
 func ErrProcessingConfig(err error) error {
-	return errors.New(ErrProcessingConfigCode, errors.Alert, []string{"Error processing config"}, []string{"Error processing config", err.Error()}, []string{}, []string{})
+	return errors.New(ErrProcessingConfigCode, errors.Alert, []string{"Error processing config"},
+		[]string{"Error processing config", err.Error()}, []string{"Couldn't load mesh config"}, []string{"Ensure mesheryctl has the right configurations"})
 }
 
 func ErrMarshalIndent(err error) error {
-	return errors.New(ErrMarshalIndentCode, errors.Alert, []string{"Error indenting json body"}, []string{"Can't marshal indent body filters", "" + err.Error()}, []string{}, []string{})
-}
-
-func ErrMakeRequest(err error) error {
-	return errors.New(ErrMakeRequestCode, errors.Alert, []string{"Error in making request"}, []string{"Can't return response from the new request made", "" + err.Error()}, []string{}, []string{})
+	return errors.New(ErrMarshalIndentCode, errors.Alert,
+		[]string{"Error indenting json body"}, []string{err.Error()}, []string{"Can't marshal indent body filters due to invalid content"}, []string{"Please check the data structure you are providing for Marshalling"})
 }
 
 func ErrResponseStatusBody(statusCode int, body string) error {
-	return errors.New(ErrResponseStatusBodyCode, errors.Alert, []string{"Wrong status code"}, []string{"Server returned with status code: " + fmt.Sprint(statusCode) + "\n" + "Response: " + body}, []string{}, []string{})
+	return errors.New(ErrResponseStatusBodyCode, errors.Alert, []string{"Wrong status code"},
+		[]string{"Server returned with status code: " + fmt.Sprint(statusCode) + "\n" + "Response: " + body}, []string{"Error occured while generating a response body"}, []string{"Check your network connection and check the status of meshery server via 'mesheryctl system status'"})
 }
 
 func ErrResponseStatus(statusCode int) error {
-	return errors.New(ErrResponseStatusCode, errors.Alert, []string{"Wrong status code"}, []string{"Server returned with status code: " + fmt.Sprint(statusCode)}, []string{}, []string{})
+	return errors.New(ErrResponseStatusCode, errors.Alert, []string{"Wrong status code"},
+		[]string{"Server returned with status code: " + fmt.Sprint(statusCode)}, []string{"Error occured while generating a respons"}, []string{"Check your network connection and check the status of meshery server via 'mesheryctl system status'"})
 }
 
 func ErrJSONToYAML(err error) error {
-	return errors.New(ErrJSONToYAMLCode, errors.Alert, []string{"Can't convert JSON to yaml"}, []string{"Failed to convert json to yaml", "" + err.Error()}, []string{}, []string{})
+	return errors.New(ErrJSONToYAMLCode, errors.Alert, []string{"Can't convert JSON to yaml"},
+		[]string{"Failed to convert json to yaml", "" + err.Error()}, []string{"The Content provided to Marshall is invalid"}, []string{"Please check the json structure you are providing for yaml conversion"})
 }
 
 func ErrOutFormatFlag() error {
-	return errors.New(ErrOutFormatFlagCode, errors.Alert, []string{"Output-format choice invalid"}, []string{"Output-format choice invalid, use [json|yaml]"}, []string{}, []string{})
+	return errors.New(ErrOutFormatFlagCode, errors.Alert, []string{"Output-format choice invalid"},
+		[]string{"Output-format choice invalid, use [json|yaml]"}, []string{"Invalid json or yaml content"}, []string{"Please check the json or yaml structure"})
 }
 
 func ErrReadConfigFile(err error) error {
-	return errors.New(ErrReadConfigFileCode, errors.Alert, []string{"Unable to read config file"}, []string{"Can't read config file from Path", "" + err.Error()}, []string{}, []string{})
+	return errors.New(ErrReadConfigFileCode, errors.Alert, []string{"Unable to read config file"},
+		[]string{"Can't read config file from Path", "" + err.Error()}, []string{"Provided file is not present or invalid path"}, []string{"Please provide a valid file path with a valid config file"})
 }
