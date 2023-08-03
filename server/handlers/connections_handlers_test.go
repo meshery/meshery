1abf051ea400414acf0c7b15f646f87d90b9592d
Philip-21 <philipuzomaobiora@gmail.com>
2023-08-03 16:05:15 +0100

    adding get function
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- server/handlers/connections_handlers_test.go
+++ server/handlers/connections_handlers_test.go
@@ -9,26 +9,30 @@ import (
 
 	"testing"
 
+	"github.com/gorilla/mux"
 	"github.com/layer5io/meshery/server/models"
 )
 
-var saveconn = models.ConnectionPayload{
-	Kind:    "meshery",
-	SubType: "management",
-	Type:    "platform",
-	MetaData: map[string]interface{}{
-		"server_id":        "d058accd-290d-4123-9c19-3ee7c0b1cbdb",
-		"server_version":   "v0.6.91",
-		"server_location":  "https://playground.meshery.io",
-		"server_build_sha": "4bf9a717",
-	},
-	Status: models.CONNECTED,
-	CredentialSecret: map[string]interface{}{
-		"token": "eyJhY2Nlc3NfdG9rZW4iOiJleUpoYkdjaU9pSlNVekkxTmlJc0ltdHBaQ0k2SW5CMVlteHBZenBvZVdSeVlTNXFkM1F1WVdOalpYTnpMWFJ2YTJWdUlpd2lkSGx3SWpvaVNsZFVJbjAuZXlKaGRXUWlPbHRkTENKamJHbGxiblJmYVdRaU9pSnRaWE5vWlhKNUxXTnNiM1ZrSWl3aVpYaHdJam94TmpnNU56TXlNRE16TENKbGVIUWlPbnQ5TENKcFlYUWlPakUyT0RrMk5EVTJNeklzSW1semN5STZJbWgwZEhBNkx5OXNiMk5oYkdodmMzUTZPVEF3TUM4aUxDSnFkR2tpT2lJek9UVmpOMlV3TnkwME1XTXhMVFF6T1RrdE9ERXhaQzFqWmpKak56ZGlabVZtTVRraUxDSnVZbVlpT2pFMk9EazJORFUyTXpJc0luTmpjQ0k2V3lKdmNHVnVhV1FpTENKdlptWnNhVzVsSWl3aWIyWm1iR2x1WlY5aFkyTmxjM01pWFN3aWMzVmlJam9pWWxkV2VtRkhNV2hqUlVKeldWaHNiR05xVlhWaFZ6ZzlPbUpYVm5waFIxWjVaVk14YW1KSE9URmFRVDA5SW4wLmpVelhmMkRrTi15NVpiWThSVkFuTjJZeXFTS05BT2tCZ0dDNHMxb2dnUFoxQS1CakdNbkFyMTZjRVBLcmVPVENXSzNfaU1DdWctcmltZG9QeGlqWFllX1d5bGpqeU9Ta1RyR0xpY0xubE1WNmhXNndJbXptT0E1VWh2UDVWZ09oeXRDZ1VQU3U0OFRVRFFTUE1NU1VkRnRFTzY2c0tvM3pXeURiV01lbm1rTHp6bGJwaFhFbXJpTDFhcnZkRHBFZV9ndHlWa0xjbktCS3p6U2Q2WWF3eDUzS0NhWGhGa2Q0Y0FVaUFVX2lGZjZnYW9ib0VRNUVfV2ZtRDJJaG9BaEtTWHJNbjdmTkxJTERwOFVXRUk3VGdwUUVFZ1QyY1VXdWY4eXlhV2NxOWpmRW9ia3ZQLXpTa29oXzJYZWlLRmtFRjAydTJ2VGp6Z3VpSmJRRl9DWmtQZGlVcGt1VlU0VEJHZVk5U2h6SWxUeGxYdWNySnp1UDI0YVJOMjl6cnNxN1hUMTktTVd2WVRkb3RCek1Dd3hhOTV2Q1BkdUFOTXhPTzVoYWJ3X2hfVUoyTGwycFFfT1dkeHJUSjFNV2pQR25hdzBFcGtqcVlDV1lSdzVJMnBwZGctWGRJUzM3aEpBbTk0MXpLNFhVanhIZzluTVJxSE53Y1RjWnFYRGU0UUstejBVWTJ0Nm0tNjBTZjZJRDg1anpkeFlxMmVfQ3BFMno3MFhaSmJZT1A1OGthU0Zfekxac1FUSkwyUFBDRmpGOGl2bnFFRzlzLUZqemRSSUxmUi1Tc2VsUjZzWk4tb3JteXlLbWF4MnlIYmljZkxTRjhveWQzWW5QSHk5ZWpYSkY1bVpwd3lPWUpmS1VRNEtOVTZ3QXg2cjljRnVvQ0NSRjJFRnJDdnJTVWE0IiwidG9rZW5fdHlwZSI6ImJlYXJlciIsInJlZnJlc2hfdG9rZW1iOiIteXI0Z2VDeEExWlJPTFJyeTlsa3doUXl5VGR4SzV4clFzNzY1ejBkYUxnLmEzSVdLVGZCVHQ5VzRkZ0Mwdl93U0dZX1ozYTZPSndWYl9jU1h4c1hUalUiLCJleHBpcnkiOiIyMDIzLTA3LTE5VDA3OjMwOjMyLjkxNjozNpswNToqMCA1",
+var saveconn = []models.ConnectionPayload{
+	{
+		Kind:    "meshery",
+		SubType: "management",
+		Type:    "platform",
+		MetaData: map[string]interface{}{
+			"server_id":        "d058accd-290d-4123-9c19-3ee7c0b1cbdb",
+			"server_version":   "v0.6.91",
+			"server_location":  "https://playground.meshery.io",
+			"server_build_sha": "4bf9a717",
+		},
+		Status: models.CONNECTED,
+		CredentialSecret: map[string]interface{}{
+			"token": "eyJhY2Nlc3NfdG9rZW4iOiJleUpoYkdjaU9pSlNVekkxTmlJc0ltdHBaQ0k2SW5CMVlteHBZenBvZVdSeVlTNXFkM1F1WVdOalpYTnpMWFJ2YTJWdUlpd2lkSGx3SWpvaVNsZFVJbjAuZXlKaGRXUWlPbHRkTENKamJHbGxiblJmYVdRaU9pSnRaWE5vWlhKNUxXTnNiM1ZrSWl3aVpYaHdJam94TmpnNU56TXlNRE16TENKbGVIUWlPbnQ5TENKcFlYUWlPakUyT0RrMk5EVTJNeklzSW1semN5STZJbWgwZEhBNkx5OXNiMk5oYkdodmMzUTZPVEF3TUM4aUxDSnFkR2tpT2lJek9UVmpOMlV3TnkwME1XTXhMVFF6T1RrdE9ERXhaQzFqWmpKak56ZGlabVZtTVRraUxDSnVZbVlpT2pFMk9EazJORFUyTXpJc0luTmpjQ0k2V3lKdmNHVnVhV1FpTENKdlptWnNhVzVsSWl3aWIyWm1iR2x1WlY5aFkyTmxjM01pWFN3aWMzVmlJam9pWWxkV2VtRkhNV2hqUlVKeldWaHNiR05xVlhWaFZ6ZzlPbUpYVm5waFIxWjVaVk14YW1KSE9URmFRVDA5SW4wLmpVelhmMkRrTi15NVpiWThSVkFuTjJZeXFTS05BT2tCZ0dDNHMxb2dnUFoxQS1CakdNbkFyMTZjRVBLcmVPVENXSzNfaU1DdWctcmltZG9QeGlqWFllX1d5bGpqeU9Ta1RyR0xpY0xubE1WNmhXNndJbXptT0E1VWh2UDVWZ09oeXRDZ1VQU3U0OFRVRFFTUE1NU1VkRnRFTzY2c0tvM3pXeURiV01lbm1rTHp6bGJwaFhFbXJpTDFhcnZkRHBFZV9ndHlWa0xjbktCS3p6U2Q2WWF3eDUzS0NhWGhGa2Q0Y0FVaUFVX2lGZjZnYW9ib0VRNUVfV2ZtRDJJaG9BaEtTWHJNbjdmTkxJTERwOFVXRUk3VGdwUUVFZ1QyY1VXdWY4eXlhV2NxOWpmRW9ia3ZQLXpTa29oXzJYZWlLRmtFRjAydTJ2VGp6Z3VpSmJRRl9DWmtQZGlVcGt1VlU0VEJHZVk5U2h6SWxUeGxYdWNySnp1UDI0YVJOMjl6cnNxN1hUMTktTVd2WVRkb3RCek1Dd3hhOTV2Q1BkdUFOTXhPTzVoYWJ3X2hfVUoyTGwycFFfT1dkeHJUSjFNV2pQR25hdzBFcGtqcVlDV1lSdzVJMnBwZGctWGRJUzM3aEpBbTk0MXpLNFhVanhIZzluTVJxSE53Y1RjWnFYRGU0UUstejBVWTJ0Nm0tNjBTZjZJRDg1anpkeFlxMmVfQ3BFMno3MFhaSmJZT1A1OGthU0Zfekxac1FUSkwyUFBDRmpGOGl2bnFFRzlzLUZqemRSSUxmUi1Tc2VsUjZzWk4tb3JteXlLbWF4MnlIYmljZkxTRjhveWQzWW5QSHk5ZWpYSkY1bVpwd3lPWUpmS1VRNEtOVTZ3QXg2cjljRnVvQ0NSRjJFRnJDdnJTVWE0IiwidG9rZW5fdHlwZSI6ImJlYXJlciIsInJlZnJlc2hfdG9rZW1iOiIteXI0Z2VDeEExWlJPTFJyeTlsa3doUXl5VGR4SzV4clFzNzY1ejBkYUxnLmEzSVdLVGZCVHQ5VzRkZ0Mwdl93U0dZX1ozYTZPSndWYl9jU1h4c1hUalUiLCJleHBpcnkiOiIyMDIzLTA3LTE5VDA3OjMwOjMyLjkxNjozNpswNToqMCA1",
+		},
 	},
 }
 
 var provide models.Provider
+var preference *models.Preference
 
 func TestSaveConnections(t *testing.T) {
 	t.Run("TestSave", func(t *testing.T) {
@@ -45,8 +49,11 @@ func TestSaveConnections(t *testing.T) {
 		}
 		// Create a response recorder
 		rr := httptest.NewRecorder()
-		handlers.SaveConnection(rr, req, nil, nil, provide)
-
+		http.HandleFunc("/api/integrations/connections",func(w http.ResponseWriter, r *http.Request){
+			handlers.SaveConnection(rr, req, nil, nil, provide)
+		})
+		// Serve the request
+		http.DefaultServeMux.ServeHTTP(rr, req)
 		// Check the response status code
 		if rr.Code != http.StatusCreated {
 			t.Errorf("Expected status code %d, got %d", http.StatusCreated, rr.Code)
@@ -54,3 +61,24 @@ func TestSaveConnections(t *testing.T) {
 		t.Log("Saved Connection Successfully")
 	})
 }
+func TestGetConnections(t *testing.T) {
+	t.Run("GetConnection", func(t *testing.T) {
+		h := Handler{}
+		req, err := http.NewRequest(http.MethodGet, "/api/integrations/connections/{connectionKind}", nil)
+		if err != nil {
+			t.Fatalf("Error in making request with payload: %v", err)
+		}
+		req.URL.RawQuery = "page=0&pagesize=10&order=created_at+desc"
+		rr := httptest.NewRecorder()
+		r := mux.NewRouter()
+		r.HandleFunc("/api/integrations/connections/{connectionKind}", func(w http.ResponseWriter, req *http.Request) {
+			h.GetConnections(w, req, preference, &models.User{ID: "user123"}, provide)
+		})
+		// Serve the request
+		r.ServeHTTP(rr, req)
+		if rr.Code != http.StatusOK {
+			t.Errorf("Expected status code %d, got %d", http.StatusOK, rr.Code)
+		}
+	})
+}
+
