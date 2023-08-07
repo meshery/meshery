f5abd4af0cabd4f4bf9b04c9fea167e1847e4430
MUzairS15 <muzair.shaikh810@gmail.com>
2023-08-07 11:59:27 +0530

    rm old components dir
    Signed-off-by: MUzairS15 <muzair.shaikh810@gmail.com>

--- server/meshmodel/helper.go
+++ server/meshmodel/helper.go
@@ -54,6 +54,10 @@ func (erh *EntityRegistrationHelper) SeedComponents() {
 		return
 	}
 
+	relationships := make([]string, 0)
+	policies :=  make([]string, 0)
+	
+	// change to queue approach to register comps, relationships and policies
 	// Read component and relationship definitions from files and send them to respective channels
 	for _, model  := range models {
 		entitiesPath := filepath.Join(ModelsPath, model.Name())
@@ -68,17 +72,19 @@ func (erh *EntityRegistrationHelper) SeedComponents() {
 			if entity.IsDir() {
 				switch entity.Name() {
 				case "relationships":
-					erh.generateRelationships(entityPath)
+					relationships = append(relationships, entityPath)
 				case "policies": 
-					break
+					policies = append(policies, entityPath)
 				default:
-					erh.generateComponents(entityPath)
+					erh.generateComponents(entityPath) // register components first
 				}
 			}
 		}
-		
 	}
 
+	for _, relationship := range relationships {
+		erh.generateRelationships(relationship)
+	}
 }
 
 // reads component definitions from files and sends them to the component channel
