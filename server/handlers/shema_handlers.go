321155d50f75db624a540f5f678c798233feba3a
Pranav Singh <pranavsingh02@hotmail.com>
2023-08-09 21:43:20 +0530

    Update server/handlers/shema_handlers.go
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

--- server/handlers/shema_handlers.go
+++ server/handlers/shema_handlers.go
@@ -25,7 +25,6 @@ func (h *Handler) HandleResourceSchemas(rw http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Println("see this, came here", rscName)
 
 	var rjsfSchemaJSON map[string]interface{}
 	var uiSchemaJSON map[string]interface{}
