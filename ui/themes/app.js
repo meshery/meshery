db06c9058c9f2ac4080ec04ffc89d143e877105f
Gaurav Chadha <65453826+Chadha93@users.noreply.github.com>
2023-08-16 16:09:31 +0530

    Update ui/themes/app.js
    Signed-off-by: Gaurav Chadha <65453826+Chadha93@users.noreply.github.com>

--- ui/themes/app.js
+++ ui/themes/app.js
@@ -6,7 +6,7 @@ const drawerWidth = 256;
 export var darkTheme = createTheme({
   typography : {
     useNextVariants : true,
-    fontFamily: "Qanelas Soft",
+    fontFamily : "Qanelas Soft",
     h5 : {
       fontWeight : 'bolder',
       fontSize : 26,
