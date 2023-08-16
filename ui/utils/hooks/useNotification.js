e270e8c338da8375462b32e3a8ee0ce96231b0d2
aabidsofi19 <mailtoaabid01@gmail.com>
2023-08-16 15:58:32 +0530

    Add Custom Snackbar Hook'
    Signed-off-by: aabidsofi19 <mailtoaabid01@gmail.com>

---
+++ ui/utils/hooks/useNotification.js
@@ -0,0 +1,75 @@
+import { IconButton } from "@material-ui/core"
+import { ToggleButtonGroup } from "@mui/material"
+import { useSnackbar } from "notistack"
+import { iconMedium } from "../../css/icons.styles"
+import CloseIcon from "@material-ui/icons/Close";
+import { NorthEast } from "@mui/icons-material";
+import { useDispatch } from "react-redux";
+import { pushEvent ,openEventInNotificationCenter, toggleNotificationCenter } from "../../lib/store";
+import moment from "moment";
+import { v4 } from "uuid";
+
+// Meshery Event Interface
+// event = {
+//   event_type ,
+//   summary,
+//   deleteEvent,
+//   eventCause,
+//   eventRemediation,
+//   eventErrorCode,
+//   componentType,
+//   componentName,
+// }
+
+export const useNotification = () => {
+
+  const { enqueueSnackbar,closeSnackbar }= useSnackbar()
+
+  const dispatch = useDispatch()
+
+  const openEvent = (eventId) => {
+    dispatch(openEventInNotificationCenter({
+      eventId
+    }))
+    dispatch(toggleNotificationCenter())
+  }
+  const notify = ({ id,message,description,event_type,timestamp,customEvent,showInNotificationCenter=true }) => {
+
+    timestamp = timestamp ?? moment.utc().valueOf()
+    id = id  || v4()
+
+
+    if (showInNotificationCenter) {
+      dispatch(pushEvent({
+        summary : message ,
+        id,
+        event_type,
+        timestamp,
+        eventCause : description,
+        ...customEvent
+      }))
+    }
+
+    enqueueSnackbar(message,{
+      variant : event_type.type,
+      action : function Action(key) {
+        return (
+          <ToggleButtonGroup>
+            <IconButton key={`closeevent-${id}`} aria-label="Close" color="inherit" onClick={() => closeSnackbar(key)}>
+              <CloseIcon style={iconMedium} />
+            </IconButton>
+            {showInNotificationCenter &&
+              <IconButton key={`openevent-${id}`} aria-label="Open" color="inherit" onClick={() => openEvent(id)}>
+                <NorthEast style={iconMedium} />
+              </IconButton> }
+          </ToggleButtonGroup>
+        );
+      },
+    })
+  }
+
+  return {
+    notify
+  }
+
+}
