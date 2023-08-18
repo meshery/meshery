5e20717e714300c1afaffbe4b25a21b33ed24f03
Philip-21 <philipuzomaobiora@gmail.com>
2023-08-18 08:22:56 +0100

    adjustments for lint checks
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- mesheryctl/internal/cli/root/filter/list_test.go
+++ mesheryctl/internal/cli/root/filter/list_test.go
@@ -1,116 +1,118 @@
-// package filter
+/*
+package filter
 
-// import (
-// 	"flag"
-// 	"io"
-// 	"os"
-// 	"path/filepath"
-// 	"runtime"
-// 	"testing"
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"runtime"
+	"testing"
 
-// 	"github.com/jarcoal/httpmock"
-// 	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/jarcoal/httpmock"
+	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
+	"github.com/stretchr/testify/assert"
+)
 
+//var update = flag.Bool("update", false, "update golden files")
 
-// func TestListCmd(t *testing.T) {
-// 	// setup current context
-// 	utils.SetupContextEnv(t)
+func TestListCmd(t *testing.T) {
+	// setup current context
+	utils.SetupContextEnv(t)
 
-// 	// initialize mock server for handling requests
-// 	utils.StartMockery(t)
+	// initialize mock server for handling requests
+	utils.StartMockery(t)
 
-// 	// create a test helper
-// 	testContext := utils.NewTestHelper(t)
+	// create a test helper
+	testContext := utils.NewTestHelper(t)
 
-// 	// get current directory
-// 	_, filename, _, ok := runtime.Caller(0)
-// 	if !ok {
-// 		t.Fatal("Not able to get current working directory")
-// 	}
-// 	currDir := filepath.Dir(filename)
-// 	fixturesDir := filepath.Join(currDir, "fixtures")
+	// get current directory
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		t.Fatal("Not able to get current working directory")
+	}
+	currDir := filepath.Dir(filename)
+	fixturesDir := filepath.Join(currDir, "fixtures")
 
-// 	// test scenrios for fetching data
-// 	tests := []struct {
-// 		Name             string
-// 		Args             []string
-// 		URL              string
-// 		Fixture          string
-// 		Token            string
-// 		ExpectedResponse string
-// 		ExpectError      bool
-// 	}{
-// 		{
-// 			Name:             "Fetch Filter List",
-// 			Args:             []string{"list"},
-// 			ExpectedResponse: "list.filter.output.golden",
-// 			Fixture:          "list.filter.api.response.golden",
-// 			URL:              testContext.BaseURL + "/api/filter",
-// 			Token:            filepath.Join(fixturesDir, "token.golden"),
-// 			ExpectError:      false,
-// 		},
-// 	}
+	// test scenrios for fetching data
+	tests := []struct {
+		Name             string
+		Args             []string
+		URL              string
+		Fixture          string
+		Token            string
+		ExpectedResponse string
+		ExpectError      bool
+	}{
+		{
+			Name:             "Fetch Filter List",
+			Args:             []string{"list"},
+			ExpectedResponse: "list.filter.output.golden",
+			Fixture:          "list.filter.api.response.golden",
+			URL:              testContext.BaseURL + "/api/filter",
+			Token:            filepath.Join(fixturesDir, "token.golden"),
+			ExpectError:      false,
+		},
+	}
 
-// 	// Run tests
-// 	for _, tt := range tests {
-// 		t.Run(tt.Name, func(t *testing.T) {
-// 			// View api response from golden files
-// 			apiResponse := utils.NewGoldenFile(t, tt.Fixture, fixturesDir).Load()
+	// Run tests
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			// View api response from golden files
+			apiResponse := utils.NewGoldenFile(t, tt.Fixture, fixturesDir).Load()
 
-// 			// set token
-// 			utils.TokenFlag = tt.Token
+			// set token
+			utils.TokenFlag = tt.Token
 
-// 			// mock response
-// 			httpmock.RegisterResponder("GET", tt.URL,
-// 				httpmock.NewStringResponder(200, apiResponse))
+			// mock response
+			httpmock.RegisterResponder("GET", tt.URL,
+				httpmock.NewStringResponder(200, apiResponse))
 
-// 			// Expected response
-// 			testdataDir := filepath.Join(currDir, "testdata")
-// 			golden := utils.NewGoldenFile(t, tt.ExpectedResponse, testdataDir)
+			// Expected response
+			testdataDir := filepath.Join(currDir, "testdata")
+			golden := utils.NewGoldenFile(t, tt.ExpectedResponse, testdataDir)
 
-// 			// Grab console prints
-// 			rescueStdout := os.Stdout
-// 			r, w, _ := os.Pipe()
-// 			os.Stdout = w
-// 			_ = utils.SetupMeshkitLoggerTesting(t, false)
-// 			FilterCmd.SetArgs(tt.Args)
-// 			FilterCmd.SetOutput(rescueStdout)
-// 			err := FilterCmd.Execute()
-// 			if err != nil {
-// 				// if we're supposed to get an error
-// 				if tt.ExpectError {
-// 					// write it in file
-// 					if *update {
-// 						golden.Write(err.Error())
-// 					}
-// 					expectedResponse := golden.Load()
+			// Grab console prints
+			rescueStdout := os.Stdout
+			r, w, _ := os.Pipe()
+			os.Stdout = w
+			_ = utils.SetupMeshkitLoggerTesting(t, false)
+			FilterCmd.SetArgs(tt.Args)
+			FilterCmd.SetOutput(rescueStdout)
+			err := FilterCmd.Execute()
+			if err != nil {
+				// if we're supposed to get an error
+				if tt.ExpectError {
+					// write it in file
+					if *update {
+						golden.Write(err.Error())
+					}
+					expectedResponse := golden.Load()
 
-// 					utils.Equals(t, expectedResponse, err.Error())
-// 					return
-// 				}
-// 				t.Fatal(err)
-// 			}
+					utils.Equals(t, expectedResponse, err.Error())
+					return
+				}
+				t.Fatal(err)
+			}
 
-// 			w.Close()
-// 			out, _ := io.ReadAll(r)
-// 			os.Stdout = rescueStdout
+			w.Close()
+			out, _ := io.ReadAll(r)
+			os.Stdout = rescueStdout
 
-// 			// response being printed in console
-// 			actualResponse := string(out)
+			// response being printed in console
+			actualResponse := string(out)
 
-// 			// write it in file
-// 			if *update {
-// 				golden.Write(actualResponse)
-// 			}
-// 			expectedResponse := golden.Load()
+			// write it in file
+			if *update {
+				golden.Write(actualResponse)
+			}
+			expectedResponse := golden.Load()
 
-// 			//utils.Equals(t, expectedResponse, actualResponse)
-// 			assert.Equal(t, expectedResponse, actualResponse)
-// 		})
-// 		t.Log("List Filter test Passed")	
-// 	}
-// 	// stop mock server
-// 	utils.StopMockery(t)
-// }
+			//utils.Equals(t, expectedResponse, actualResponse)
+			assert.Equal(t, expectedResponse, actualResponse)
+		})
+		t.Log("List Filter test Passed")	
+	}
+	// stop mock server
+	utils.StopMockery(t)
+}
+*/
