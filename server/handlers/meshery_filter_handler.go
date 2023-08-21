2e5d21f78ba12eb15bb985414d9b144d3aa6b979
Aritra Majumder <aritramajumder8438@gmail.com>
2023-08-21 11:45:06 +0530

    Update meshery_filter_handler.go
    Signed-off-by: Aritra Majumder <aritramajumder8438@gmail.com>

--- server/handlers/meshery_filter_handler.go
+++ server/handlers/meshery_filter_handler.go
@@ -204,7 +204,7 @@ func (h *Handler) handleFilterPOST(
 //
 // ```?pagesize={pagesize}``` Default pagesize is 10
 //
-// ```?visibility={visibility}``` Default visibility is public
+// ```?visibility={visibility}``` Default visibility is private
 // responses:
 //
 //	200: mesheryFiltersResponseWrapper
