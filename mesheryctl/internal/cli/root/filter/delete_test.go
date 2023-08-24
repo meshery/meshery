09c1dfe9801837d0203e9b1aa5a5d3f2c027d2a5
Philip-21 <philipuzomaobiora@gmail.com>
2023-08-24 13:26:40 +0100

    adjusting workflows
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- mesheryctl/internal/cli/root/filter/delete_test.go
+++ mesheryctl/internal/cli/root/filter/delete_test.go
@@ -1,100 +1,109 @@
 package filter
 
-//commenting to pass workflow
+import (
+	"path/filepath"
+	"runtime"
+	"testing"
 
-func TestDeleteCmd() {
-	// // setup current context
-	// utils.SetupContextEnv(t)
+	"github.com/jarcoal/httpmock"
 
-	// // initialize mock server for handling requests
-	// utils.StartMockery(t)
+	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
+	"github.com/stretchr/testify/assert"
+)
 
-	// // create a test helper
-	// testContext := utils.NewTestHelper(t)
+func TestDeleteCmd(t *testing.T) {
+	// setup current context
+	utils.SetupContextEnv(t)
 
-	// // get current directory
-	// _, filename, _, ok := runtime.Caller(0)
-	// if !ok {
-	// 	t.Fatal("Not able to get current working directory")
-	// }
-	// currDir := filepath.Dir(filename)
-	// fixturesDir := filepath.Join(currDir, "fixtures")
+	// initialize mock server for handling requests
+	utils.StartMockery(t)
 
-	// testcase := []struct {
-	// 	Name             string
-	// 	Args             []string
-	// 	URL              string
-	// 	Fixture          string
-	// 	Token            string
-	// 	ExpectedResponse string
-	// 	ExpectedError    bool
-	// 	Method           string
-	// 	ResponseCode     int
-	// }{
-	// 	{
-	// 		Name:             "Delete Kuma-Test",
-	// 		Args:             []string{"delete", "c0c6035a-b1b9-412d-aab2-4ed1f1d51f84", "Kuma-Test"},
-	// 		URL:              testContext.BaseURL + "/api/filter/c0c6035a-b1b9-412d-aab2-4ed1f1d51f84",
-	// 		Token:            filepath.Join(fixturesDir, "token.golden"),
-	// 		Fixture:          "delete.kuma.api.response.golden",
-	// 		ExpectedResponse: "delete.kuma.output.golden",
-	// 		ExpectedError:    false,
-	// 		Method:           "DELETE",
-	// 		ResponseCode:     200,
-	// 	}, {
-	// 		Name:             "Delete RolloutAndIstio",
-	// 		Args:             []string{"delete", "d0e09134-acb6-4c71-b051-3d5611653f70", "RolloutAndIstio"},
-	// 		URL:              testContext.BaseURL + "/api/filter/d0e09134-acb6-4c71-b051-3d5611653f70",
-	// 		Token:            filepath.Join(fixturesDir, "token.golden"),
-	// 		Fixture:          "delete.rollout.api.response.golden",
-	// 		ExpectedResponse: "delete.rollout.output.golden",
-	// 		ExpectedError:    false,
-	// 		Method:           "DELETE",
-	// 		ResponseCode:     200,
-	// 	},
-	// }
-	// for _, tt := range testcase {
-	// 	t.Run(tt.Name, func(t *testing.T) {
-	// 		apiResponse := utils.NewGoldenFile(t, tt.Fixture, fixturesDir).Load()
-	// 		// set token
-	// 		utils.TokenFlag = tt.Token
-	// 		// mock response
-	// 		httpmock.RegisterResponder(tt.Method, tt.URL,
-	// 			httpmock.NewStringResponder(200, apiResponse))
-	// 		// Expected response
-	// 		testdataDir := filepath.Join(currDir, "testdata")
-	// 		golden := utils.NewGoldenFile(t, tt.ExpectedResponse, testdataDir)
+	// create a test helper
+	testContext := utils.NewTestHelper(t)
 
-	// 		b := utils.SetupMeshkitLoggerTesting(t, false)
-	// 		FilterCmd.SetArgs(tt.Args)
-	// 		FilterCmd.SetOutput(b)
-	// 		err := FilterCmd.Execute()
-	// 		if err != nil {
-	// 			// if we're supposed to get an error
-	// 			if tt.ExpectedError {
-	// 				// write it in file
-	// 				if *update {
-	// 					golden.Write(err.Error())
-	// 				}
-	// 				expectedResponse := golden.Load()
+	// get current directory
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		t.Fatal("Not able to get current working directory")
+	}
+	currDir := filepath.Dir(filename)
+	fixturesDir := filepath.Join(currDir, "fixtures")
 
-	// 				utils.Equals(t, expectedResponse, err.Error())
-	// 				return
-	// 			}
-	// 			t.Fatal(err)
-	// 		}
-	// 		// response being printed in console
-	// 		output := b.String()
-	// 		actualResponse := output
+	testcase := []struct {
+		Name             string
+		Args             []string
+		URL              string
+		Fixture          string
+		Token            string
+		ExpectedResponse string
+		ExpectedError    bool
+		Method           string
+		ResponseCode     int
+	}{
+		{
+			Name:             "Delete Kuma-Test",
+			Args:             []string{"delete", "c0c6035a-b1b9-412d-aab2-4ed1f1d51f84", "Kuma-Test"},
+			URL:              testContext.BaseURL + "/api/filter?/c0c6035a-b1b9-412d-aab2-4ed1f1d51f84",
+			Token:            filepath.Join(fixturesDir, "token.golden"),
+			Fixture:          "delete.kuma.api.response.golden",
+			ExpectedResponse: "delete.kuma.output.golden",
+			ExpectedError:    false,
+			Method:           "DELETE",
+			ResponseCode:     200,
+		}, {
+			Name:             "Delete RolloutAndIstio",
+			Args:             []string{"delete", "d0e09134-acb6-4c71-b051-3d5611653f70", "RolloutAndIstio"},
+			URL:              testContext.BaseURL + "/api/filter?/d0e09134-acb6-4c71-b051-3d5611653f70",
+			Token:            filepath.Join(fixturesDir, "token.golden"),
+			Fixture:          "delete.rollout.api.response.golden",
+			ExpectedResponse: "delete.rollout.output.golden",
+			ExpectedError:    false,
+			Method:           "DELETE",
+			ResponseCode:     200,
+		},
+	}
+	for _, tt := range testcase {
+		t.Run(tt.Name, func(t *testing.T) {
+			apiResponse := utils.NewGoldenFile(t, tt.Fixture, fixturesDir).Load()
+			// set token
+			utils.TokenFlag = tt.Token
+			// mock response
+			httpmock.RegisterResponder(tt.Method, tt.URL,
+				httpmock.NewStringResponder(200, apiResponse))
+			// Expected response
+			testdataDir := filepath.Join(currDir, "testdata")
+			golden := utils.NewGoldenFile(t, tt.ExpectedResponse, testdataDir)
 
-	// 		// write it in file
-	// 		if *update {
-	// 			golden.Write(actualResponse)
-	// 		}
-	// 		expectedResponse := golden.Load()
+			buff := utils.SetupMeshkitLoggerTesting(t, false)
+			cmd := FilterCmd
+			cmd.SetArgs(tt.Args)
+			cmd.SetOutput(buff)
+			err := cmd.Execute()
+			if err != nil {
+				// if we're supposed to get an error
+				if tt.ExpectedError {
+					// write it in file
+					if *update {
+						golden.Write(err.Error())
+					}
+					expectedResponse := golden.Load()
 
-	// 		assert.Equal(t, expectedResponse, actualResponse)
-	// 	})
-	// }
-	// utils.StopMockery(t)
+					utils.Equals(t, expectedResponse, err.Error())
+					return
+				}
+				t.Fatal(err)
+			}
+
+			actualResponse := buff.String()
+
+			// write it in file
+			if *update {
+				golden.Write(actualResponse)
+			}
+			expectedResponse := golden.Load()
+
+			assert.Equal(t, expectedResponse, actualResponse)
+		})
+	}
+	utils.StopMockery(t)
 }
