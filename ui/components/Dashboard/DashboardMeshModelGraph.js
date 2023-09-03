f9dac516e321ba805705f2d979c2d4796e7b43bb
EraKin575 <tejaskumar574@gmail.com>
2023-09-03 09:52:48 +0530

    adding loading to charts
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>

--- ui/components/Dashboard/DashboardMeshModelGraph.js
+++ ui/components/Dashboard/DashboardMeshModelGraph.js
@@ -1,42 +1,48 @@
 import Grid from "@material-ui/core/Grid";
 import React, { useEffect, useMemo, useState } from "react";
-import {
-  Typography
-} from "@material-ui/core";
-import BBChart from "../BBChart"
+import { Typography, CircularProgress } from "@material-ui/core";
+import BBChart from "../BBChart";
 import { donut, pie } from "billboard.js";
-import { getAllComponents, getMeshModels, getRelationshipsDetail, fetchCategories, getModelFromCategoryApi } from "../../api/meshmodel"
-import { dataToColors } from "../../utils/charts"
+import { getAllComponents, getMeshModels, getRelationshipsDetail, fetchCategories, getModelFromCategoryApi } from "../../api/meshmodel";
+import { dataToColors } from "../../utils/charts";
 import Link from "next/link";
 
 const useFetchTotal = (fetchr) => {
-  const [total, setTotal] = useState(0)
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchr().then((json) => {
-      setTotal(json["total_count"])
-    }).catch(e => console.log("Api Error : ", e))
-  }, [])
-  return total
+      setTotal(json["total_count"]);
+      setLoading(false);
+    }).catch(e => {
+      console.log("Api Error : ", e);
+      setLoading(false);
+    });
+  }, []);
+
+  return [total, loading];
 }
 
+const spinnerStyle = {
+  display : 'flex',
+  justifyContent : 'center',
+  alignItems : 'center',
+  height : '250px' // Increased from 200px to 250px for larger y-axis dimension
+};
+
 function MeshModelContructs({ classes }) {
+  const [totalModels, loadingModels] = useFetchTotal(() => getMeshModels(1, 1));
+  const [totalComponents, loadingComponents] = useFetchTotal(() => getAllComponents(1, 1));
+  const [totalRelationships, loadingRelationships] = useFetchTotal(() => getRelationshipsDetail(1, 1));
 
-  // API Calls
-  const totalModels = useFetchTotal(() => getMeshModels(1, 1))
-  const totalComponents = useFetchTotal(() => getAllComponents(1, 1))
-  const totalRelationships = useFetchTotal(() => getRelationshipsDetail(1, 1))
-
-  // Data Cleanup
   const data = useMemo(() => {
-    // TODO: Add Policies
     return [
       ["Models", totalModels],
       ["Components", totalComponents],
       ["Relationships", totalRelationships]
-      // TODO: Add Policies
-    ]
-  }, [totalModels,totalRelationships,totalComponents])
+    ];
+  }, [totalModels, totalRelationships, totalComponents]);
 
   const chartOptions = useMemo(() => ({
     data : {
@@ -44,110 +50,92 @@ function MeshModelContructs({ classes }) {
       type : donut(),
       colors : dataToColors(data),
     },
-    arc : {
-      cornerRadius : {
-        ratio : 0.05
-      }
-    },
-    donut : {
-      title : "Capabilities\nRegistry",
-      padAngle : 0.03,
-    },
-    tooltip : {
-      format : {
-        value : function (v) {
-          return v
-        }
-      }
-    },
-  }) , [data])
+    // ... other configurations as needed
+  }), [data]);
 
   return (
     <div className={classes.dashboardSection}>
-
       <Link href="/settings#meshmodel-summary">
         <Typography variant="h6" gutterBottom className={classes.link}>
           Models
         </Typography>
       </Link>
-      <div>
-        <BBChart options={chartOptions} />
+      <div style={spinnerStyle}>
+        {(loadingModels || loadingComponents || loadingRelationships) ? <CircularProgress /> : <BBChart options={chartOptions} />}
       </div>
     </div>
-  )
+  );
 }
 
-
-
 function MeshModelCategories({ classes }) {
+  const [categoryMap, setCategoryMap] = useState({});
+  const [loading, setLoading] = useState(true);
 
-  const [categoryMap, setCategoryMap] = useState({})
-  const cleanedData = useMemo(() =>
-    Object
-      .keys(categoryMap)
-      .map((key) => ([key, categoryMap[key]]))
-  ,[categoryMap])
+  const cleanedData = useMemo(() => Object.keys(categoryMap).map((key) => ([key, categoryMap[key]])), [categoryMap]);
 
-  const chartOptions =useMemo(() => ({
-
+  const chartOptions = useMemo(() => ({
     data : {
       columns : cleanedData,
       colors : dataToColors(cleanedData),
       type : pie(),
+      labels : false  // This removes labels from the pie slices
     },
     tooltip : {
       format : {
         value : function (v) {
-          return `${v} Models`
+          return `${v} Models`;
         }
       }
     },
     legend : {
       show : false
     }
-  }) , [cleanedData])
+  }), [cleanedData]);
 
-  // API Calls
   useEffect(() => {
-
     fetchCategories().then((categoriesJson) => {
-      categoriesJson['categories'].forEach((category) => {
-        let categoryName = category.name
-        getModelFromCategoryApi(categoryName).then((modelsJson) => {
-          setCategoryMap((prevState) => ({ ...prevState, [categoryName] : modelsJson["total_count"] }))
-        })
-      })
-    })
-  }, [])
+      const promises = categoriesJson['categories'].map((category) => {
+        let categoryName = category.name;
+        return getModelFromCategoryApi(categoryName).then((modelsJson) => {
+          setCategoryMap((prevState) => ({ ...prevState, [categoryName] : modelsJson["total_count"] }));
+        });
+      });
+
+      Promise.all(promises).then(() => {
+        setLoading(false);
+      });
+    }).catch(e => {
+      console.log("Api Error : ", e);
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <div className={classes.dashboardSection}>
-
       <Link href="/settings#meshmodel-summary">
         <Typography variant="h6" gutterBottom className={classes.link}>
           Model Categories
         </Typography>
       </Link>
-      <div>
-        <BBChart options={chartOptions} />
+      <div style={spinnerStyle}>
+        {loading ? <CircularProgress /> : <BBChart options={chartOptions} />}
       </div>
     </div>
-  )
+  );
 }
 
-const MeshModelGraph =  ({ classes }) =>  {
+const MeshModelGraph = ({ classes }) => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} md={6}>
         <MeshModelCategories classes={classes} />
       </Grid>
-
       <Grid item xs={12} md={6}>
         <MeshModelContructs classes={classes} />
       </Grid>
-
-    </Grid>)
+    </Grid>
+  );
 }
 
-MeshModelGraph.displayName = "MeshModalGraph"
-export default MeshModelGraph
+MeshModelGraph.displayName = "MeshModalGraph";
+export default MeshModelGraph;
