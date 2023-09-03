12b131bf7c1914fa3c5921034448bc9e09948c10
EraKin575 <tejaskumar574@gmail.com>
2023-09-03 10:37:18 +0530

    added loading to connection stats chart
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>

--- ui/components/Dashboard/ConnectionCharts.js
+++ ui/components/Dashboard/ConnectionCharts.js
@@ -1,24 +1,25 @@
-import React , { useEffect,useState } from "react";
-import {  Box , Typography } from "@material-ui/core";
+import React , { useEffect, useState } from "react";
+import { CircularProgress, Box , Typography } from "@material-ui/core";
 import { donut } from "billboard.js";
 import BBChart from "../BBChart";
-import { dataToColors , isValidColumnName } from "../../utils/charts"
+import { dataToColors , isValidColumnName } from "../../utils/charts";
 import { getConnectionStatusSummary } from "../../api/connections";
 import ConnectClustersBtn from "../General/ConnectClustersBtn";
 import Link from "next/link";
 
-
 export default function ConnectionStatsChart({ classes }) {
-
-  const [chartData,setChartData] = useState([])
+  const [chartData, setChartData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);  // New isLoading state
 
   useEffect(() => {
+    setIsLoading(true);  // Set loading state to true when fetch begins
     getConnectionStatusSummary().then(json => {
       setChartData(json.connections_status
         .filter(data => isValidColumnName(data.status))
-        .map(data => [data.status,data.count]))
-    })
-  },[])
+        .map(data => [data.status, data.count]));
+      setIsLoading(false);  // Set loading state to false when fetch completes
+    });
+  }, []);
 
   const chartOptions = {
     data : {
@@ -49,8 +50,6 @@ export default function ConnectionStatsChart({ classes }) {
     },
   };
 
-
-
   return (
     <div className={classes.dashboardSection}>
       <Link href="/management/connections">
@@ -58,25 +57,36 @@ export default function ConnectionStatsChart({ classes }) {
           Connections
         </Typography>
       </Link>
-      <Box  sx={{ display : "flex",justifyContent : "center",alignItems : "center",alignContent : "center",height : "100%" }}>
-        { chartData.length > 0 ?
-          <BBChart options={chartOptions} />
-          : (
-            <div
-              style={{
-                padding : "2rem",
-                display : "flex",
-                justifyContent : "center",
-                alignItems : "center",
-                flexDirection : "column",
-              }}
-            >
-              <Typography style={{ fontSize : "1.5rem", marginBottom : "1rem" }} align="center" >
+      <Box
+        sx={{
+          display : "flex",
+          justifyContent : "center",
+          alignItems : "center",
+          alignContent : "center",
+          height : "300px",  // Fixed height for consistency
+          width : "100%",    // Using 100% width to occupy the full width of its parent
+        }}
+      >
+        {isLoading ?
+          <CircularProgress />  // Show loading spinner if isLoading is true
+          : chartData.length > 0 ?
+            <BBChart options={chartOptions} />
+            : (
+              <div
+                style={{
+                  padding : "2rem",
+                  display : "flex",
+                  justifyContent : "center",
+                  alignItems : "center",
+                  flexDirection : "column",
+                }}
+              >
+                <Typography style={{ fontSize : "1.5rem", marginBottom : "1rem" }} align="center">
                  No connections found in your clusters
-              </Typography>
-              <ConnectClustersBtn/>
-            </div>
-          )
+                </Typography>
+                <ConnectClustersBtn/>
+              </div>
+            )
         }
       </Box>
     </div>
