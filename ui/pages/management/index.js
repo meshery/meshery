fb30c9ce718e2378eaeeecf9d470e01ab5e99eb6
Dhiraj Bari <djirajbari123@gmail.com>
2023-09-08 09:57:53 +0530

    change management/index.js to functional behavior
    Signed-off-by: Dhiraj Bari <djirajbari123@gmail.com>
    
    change management/index.js to functional behavior

--- ui/pages/management/index.js
+++ ui/pages/management/index.js
@@ -1,27 +1,31 @@
 import { NoSsr } from "@material-ui/core";
 import Head from "next/head";
 import { withRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import MesheryPlayComponent from "../../components/MesheryPlayComponent";
 import { updatepagepath } from "../../lib/store";
-class Manage extends React.Component {
-  render() {
-    return (
-      <NoSsr>
-        <Head>
-          <title>Management | Meshery </title>
-        </Head>
-        <MesheryPlayComponent />
-      </NoSsr>
-    );
-  }
+
+function Manage(props) {
+  useEffect(() => {
+    document.title = "Management | Meshery";
+  }, []);
+
+  return (
+    <NoSsr>
+      <Head>
+        <title>Management | Meshery</title>
+      </Head>
+      <MesheryPlayComponent />
+    </NoSsr>
+  );
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return { updatepagepath : bindActionCreators(updatepagepath, dispatch), };
+  return {
+    updatepagepath: bindActionCreators(updatepagepath, dispatch),
+  };
 };
 
 export default connect(null, mapDispatchToProps)(withRouter(Manage));
-
