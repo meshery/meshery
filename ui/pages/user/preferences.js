262f15281a3398ffbad05b57cd36fa135a2c8ad1
Mukesh Kuiry <mukeshkk3162@gmail.com>
2023-09-10 05:06:07 +0530

    [UI] ConvertedUserPreferences and preferences form class-based to functional component
    Signed-off-by: Mukesh Kuiry <mukeshkk3162@gmail.com>

--- ui/pages/user/preferences.js
+++ ui/pages/user/preferences.js
@@ -2,84 +2,84 @@ import UserPreferences from "../../components/UserPreferences";
 import { NoSsr, Paper, withStyles } from "@material-ui/core";
 import { updatepagepath, updatepagetitle } from "../../lib/store";
 import { connect } from "react-redux";
-import { bindActionCreators } from 'redux';
+import { bindActionCreators } from "redux";
 import { getPath } from "../../lib/path";
-import Head from 'next/head';
-import dataFetch from '../../lib/data-fetch';
+import Head from "next/head";
+import dataFetch from "../../lib/data-fetch";
 import { ctxUrl } from "../../utils/multi-ctx";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const styles = { paper : { maxWidth : '90%',
-  margin : 'auto',
-  overflow : 'hidden', } };
+const styles = { paper : { maxWidth : "90%", margin : "auto", overflow : "hidden" } };
 
-class UserPref extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={};
-  }
+const UserPref = (props) => {
+  const [anonymousStats, setAnonymousStats] = useState(false);
+  const [perfResultStats, setPerfResultStats] = useState(false);
 
-  async componentDidMount() {
+  useEffect(() => {
+    handleFetchData();
+  }, []);
+
+  const handleFetchData = async () => {
     console.log(`path: ${getPath()}`);
-    this.props.updatepagepath({ path : getPath() });
-    this.props.updatepagetitle({ title : "User Preferences" });
+    props.updatepagepath({ path : getPath() });
+    props.updatepagetitle({ title : "User Preferences" });
 
-    await new Promise(resolve => {
+    await new Promise((resolve) => {
       dataFetch(
-        ctxUrl('/api/user/prefs', this.props.selectedK8sContexts),
+        ctxUrl("/api/user/prefs", props.selectedK8sContexts),
         {
-          method : 'GET',
-          credentials : 'include',
-        }, (result) => {
+          method : "GET",
+          credentials : "include",
+        },
+        (result) => {
           resolve();
           console.log(result);
-          if (typeof result !== 'undefined') {
-            this.setState({
-              anonymousStats : result.anonymousUsageStats||false,
-              perfResultStats : result.anonymousPerfResults||false,
-            });
+          if (typeof result !== "undefined") {
+            setAnonymousStats(result.anonymousUsageStats);
+            setPerfResultStats(result.perfResultDeletionPolicy);
           }
         },
         // Ignore error because we will fallback to default state
         // and to avoid try catch due to async await functions
-        resolve);
+        resolve
+      );
     });
-  }
+  };
 
-  render () {
-    const { anonymousStats, perfResultStats }=this.state;
-    console.log(this.state)
-    if (anonymousStats===undefined){
-      // Skip rendering till data is not loaded
-      return <div></div>
-    }
-    return (
-      <NoSsr>
-        <Head>
-          <title>Preferences | Meshery</title>
-        </Head>
-        <Paper className={this.props.classes.paper}>
-          {/* {should meshmap specific user preferences be placed along with general preferences or from the remote provider} */}
-          <UserPreferences anonymousStats={anonymousStats} perfResultStats={perfResultStats}  theme={this.props.theme} themeSetter={this.props.themeSetter}/>
-        </Paper>
-      </NoSsr>
-    );
-  }
-}
+  return (
+    <>
+      {anonymousStats === undefined ? (
+        <div></div>
+      ) : (
+        <NoSsr>
+          <Head>
+            <title>Preferences | Meshery</title>
+          </Head>
+          <Paper className={props.classes.paper}>
+            {/* {should meshmap specific user preferences be placed along with general preferences or from the remote provider} */}
+            <UserPreferences
+              anonymousStats={anonymousStats}
+              perfResultStats={perfResultStats}
+              theme={props.theme}
+              themeSetter={props.themeSetter}
+            />
+          </Paper>
+        </NoSsr>
+      )}
+    </>
+  );
+};
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   updatepagepath : bindActionCreators(updatepagepath, dispatch),
   updatepagetitle : bindActionCreators(updatepagetitle, dispatch),
-})
+});
 const mapStateToProps = (state) => {
-  const selectedK8sContexts = state.get('selectedK8sContexts');
+  const selectedK8sContexts = state.get("selectedK8sContexts");
 
   return {
     selectedK8sContexts,
   };
 };
 
-export default withStyles(styles)(connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(UserPref));
+export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(UserPref));
