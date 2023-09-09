8c87f0e18e60c7472882a109b0e3590dda08b74c
Lauquik <laukik.22010776@viit.ac.in>
2023-09-09 17:09:10 +0530

    Refactor dataFetch to use axios with async/await
    Signed-off-by: Lauquik <laukik.22010776@viit.ac.in>

--- provider-ui/lib/data-fetch.js
+++ provider-ui/lib/data-fetch.js
@@ -1,40 +1,42 @@
-import fetch from "isomorphic-unfetch";
+import axios from 'axios';
 
-// This can be migrated as a custom hook in React
-const dataFetch = (url, options = {}, successFn, errorFn) => {
-  // const controller = new AbortController();
-  // const signal = controller.signal;
-  // options.signal = signal;
-  // setTimeout(() => controller.abort(), 10000); // nice to have but will mess with the load test
-  if (errorFn === undefined) {
+const dataFetch = async (url, options = {}, successFn, errorFn) => {
+  // Set default error function if not provided
+  if (!errorFn) {
     errorFn = (err) => {
       console.error(`Error fetching ${url} --DataFetch`, err);
     };
   }
-  fetch(url, options)
-    .then((res) => {
-      if (res.status === 401 || res.redirected) {
-        if (window.location.host.endsWith("3000")) {
-          window.location = "/user/login"; // for local dev thru node server
-        } else {
-          window.location.reload(); // for use with Go server
-        }
-      }
-      let result;
-      if (res.ok) {
-        // console.log(`res type: ${res.type}`);
-        try {
-          result = res.json();
-        } catch (e) {
-          result = res.text();
-        }
-        return result;
+
+  try {
+    const response = await axios(url, options);
+
+    // Handle redirects or unauthorized status
+    if (response.status === 401 || (response.request && response.request.responseURL !== url)) {
+      if (window.location.host.endsWith("3000")) {
+        window.location = "/user/login"; // for local dev thru node server
       } else {
-        res.text().then(errorFn);
+        window.location.reload(); // for use with Go server
       }
-    })
-    .then(successFn)
-    .catch(errorFn);
+      return;
+    }
+
+    // Call the success function with the response data
+    successFn(response.data);
+  } catch (error) {
+    // Handle the error
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      errorFn(error.response.data);
+    } else if (error.request) {
+      // The request was made but no response was received
+      errorFn(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      errorFn(error.message);
+    }
+  }
 };
 
 export default dataFetch;
