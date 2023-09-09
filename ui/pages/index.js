9431cd4443d400900aa17172c5fd9beb905abfe9
ShatilKhan <shatilshahriar009@gmail.com>
2023-09-10 03:18:10 +0600

    [UI] Convert Class Components into Functional Ones
    Signed-off-by: ShatilKhan<shatilshahriar009@gmail.com>
    Signed-off-by: ShatilKhan <shatilshahriar009@gmail.com>

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -1,28 +1,26 @@
 import { NoSsr } from "@material-ui/core";
 import Head from 'next/head';
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
 import DashboardComponent from "../components/DashboardComponent";
 import { getPath } from "../lib/path";
 import { updatepagepath } from "../lib/store";
 
-class Index extends React.Component {
-  componentDidMount () {
+function Index(props) {
+  useEffect(() => {
     console.log(`path: ${getPath()}`);
-    this.props.updatepagepath({ path : getPath() });
-  }
+    props.updatepagepath({ path : getPath() });
+  }, []);
 
-  render () {
-    return (
-      <NoSsr>
-        <Head>
-          <title>Dashboard | Meshery</title>
-        </Head>
-        <DashboardComponent />
-      </NoSsr>
-    );
-  }
+  return (
+    <NoSsr>
+      <Head>
+        <title>Dashboard | Meshery</title>
+      </Head>
+      <DashboardComponent />
+    </NoSsr>
+  );
 }
 
 const mapDispatchToProps = dispatch => ({ updatepagepath : bindActionCreators(updatepagepath, dispatch) })
