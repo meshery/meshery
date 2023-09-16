3c73dc5892fe9c5eb85942536729238ce90cd533
aabidsofi19 <mailtoaabid01@gmail.com>
2023-09-16 22:06:51 +0530

    Swap icons
    Signed-off-by: aabidsofi19 <mailtoaabid01@gmail.com>

--- ui/assets/icons/ReadIcon.js
+++ ui/assets/icons/ReadIcon.js
@@ -2,19 +2,23 @@ import React from "react";
 
 const ReadIcon = ({ height, width, fill, style = {} }) => {
   return (
-    <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
       x="0px"
       y="0px"
       style={style}
       height={height}
       width={width}
       fill="#fff"
-      viewBox="0 0 20 16"
+      viewBox="0 0 20 19"
     >
-      <path d="M18,0H2C0.9,0,0,0.9,0,2l0,12c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V2C20,0.9,19.1,0,18,0z M18,4l-8,5L2,4V2l8,5l8-5V4z" fill={fill} />
+      <path
+        d="M20,7c0-0.7-0.4-1.3-0.9-1.7L10,0L1,5.3C0.4,5.7,0,6.3,0,7v10c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2L20,7z M10,12L1.7,6.8
+		L10,2l8.3,4.8L10,12z"
+        fill={fill}
+      />
     </svg>
-
-
   );
 };
 
