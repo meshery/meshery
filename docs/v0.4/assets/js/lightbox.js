b13aeb390bd02e547ab857e63f1490c2ad429932
EraKin575 <tejaskumar574@gmail.com>
2023-09-17 14:45:41 +0530

    removed unnecessary spaces
    Signed-off-by: EraKin575 <tejaskumar574@gmail.com>

--- docs/v0.4/assets/js/lightbox.js
+++ docs/v0.4/assets/js/lightbox.js
@@ -1,4 +1,3 @@
-
 function is_youtubelink(url) {
     var p = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
     return (url.match(p)) ? RegExp.$1 : false;
