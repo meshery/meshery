05603aa79f0d03b5cf4fe10557d37f5d8d85d2d9
Archit Sharma <archit8679@gmail.com>
2023-09-17 14:52:24 +0530

    load-test-timer-dialog.js updated to functional component
    Signed-off-by: Archit Sharma <archit8679@gmail.com>

--- ui/components/load-test-timer-dialog.js
+++ ui/components/load-test-timer-dialog.js
@@ -6,37 +6,37 @@ if (typeof window !== 'undefined') {
   ReactCountdownClock = require('react-countdown-clock');
 }
 
-class LoadTestTimerDialog extends React.Component {
-  render() {
-    const { countDownComplete, t, open, } = this.props;
-    if (!open) {
-      return '';
-    }
-    let tNum = 0; let dur;
-    try {
-      tNum = parseInt(t.substring(0, t.length - 1));
-    } catch (ex) {
-      console.error('Unexpected Error');
-    }
-    switch (t.substring(t.length - 1, t.length).toLowerCase()) {
-      case 'h':
-        dur = tNum * 60 * 60;
-        break;
-      case 'm':
-        dur = tNum * 60;
-        break;
-      default:
-        dur = tNum;
-    }
-    return (
-      <NoSsr>
-        {/* <Menu
+const LoadTestTimerDialog = (props) => {
+
+  const { countDownComplete, t, open, } = props;
+  if (!open) {
+    return '';
+  }
+  let tNum = 0; let dur;
+  try {
+    tNum = parseInt(t.substring(0, t.length - 1));
+  } catch (ex) {
+    console.error('Unexpected Error');
+  }
+  switch (t.substring(t.length - 1, t.length).toLowerCase()) {
+    case 'h':
+      dur = tNum * 60 * 60;
+      break;
+    case 'm':
+      dur = tNum * 60;
+      break;
+    default:
+      dur = tNum;
+  }
+  return (
+    <NoSsr>
+      {/* <Menu
           // id="simple-menu"
           anchorEl={container}
           open={open}
           // onClose={this.handleClose}
         > */}
-        {/* <div id="anc1"></div>
+      {/* <div id="anc1"></div>
         <Popper open={open} anchorEl={() => document.querySelector('#anc1')}
            placement='bottom'
             modifiers={{
@@ -44,43 +44,43 @@ class LoadTestTimerDialog extends React.Component {
                 enabled: false,
               },
             }}> */}
-        {/* <Paper> */}
-        {/* <ClickAwayListener onClickAway={this.handleClose}></ClickAwayListener> */}
+      {/* <Paper> */}
+      {/* <ClickAwayListener onClickAway={this.handleClose}></ClickAwayListener> */}
 
-        {/* <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+      {/* <MenuItem onClick={this.handleClose}>Profile</MenuItem>
           <MenuItem onClick={this.handleClose}>My account</MenuItem>
           <MenuItem onClick={this.handleClose}>Logout</MenuItem> */}
 
-        <div style={{
-          marginLeft : 'auto',
-          marginRight : 'auto',
-          width : '400px',
-          position : "relative",
-          zIndex : "0"
-          // height: '400',
-        }}
-        >
-          <ReactCountdownClock
-            seconds={dur}
-            color="#667C89"
-            alpha={0.9}
-            size={400}
-            onComplete={countDownComplete}
-          />
-        </div>
-        {/* </Paper> */}
-        {/* </Popper> */}
-        {/* </Menu> */}
+      <div style={{
+        marginLeft : 'auto',
+        marginRight : 'auto',
+        width : '400px',
+        position : "relative",
+        zIndex : "0"
+        // height: '400',
+      }}
+      >
+        <ReactCountdownClock
+          seconds={dur}
+          color="#667C89"
+          alpha={0.9}
+          size={400}
+          onComplete={countDownComplete}
+        />
+      </div>
+      {/* </Paper> */}
+      {/* </Popper> */}
+      {/* </Menu> */}
 
-        {/* <Dialog onClose={this.handleTimerDialogClose}
+      {/* <Dialog onClose={this.handleTimerDialogClose}
             {...other} hideBackdrop={true} >
             <DialogContent>
 
             </DialogContent>
         </Dialog> */}
-      </NoSsr>
-    );
-  }
-}
+    </NoSsr>
+  );
+
+};
 
 export default LoadTestTimerDialog;
