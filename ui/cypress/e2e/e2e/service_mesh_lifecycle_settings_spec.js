b3516dca38c67a6b5672e830006b7b18b37ad062
Mario Arriaga <ing.mario.arriaga@gmail.com>
2023-09-22 10:13:55 -0600

    fix test - adapters not ready yet
    Signed-off-by: Mario Arriaga <ing.mario.arriaga@gmail.com>

--- ui/cypress/e2e/e2e/service_mesh_lifecycle_settings_spec.js
+++ ui/cypress/e2e/e2e/service_mesh_lifecycle_settings_spec.js
@@ -1,5 +1,4 @@
 /// <reference types="cypress" />
-import { aliasQuery } from "../../actionHelpers/graphql-test-utils";
 
 describe("Lifecycle Service Mesh", () => {
   beforeEach(() => {
@@ -30,7 +29,7 @@ describe("Lifecycle Service Mesh", () => {
         cy.get('[data-cy="mesh-adapter-url"]').type(`${ADAPTER_LOCATION}{enter}`);
         // "Connect" Button
         cy.get('[data-cy="btnSubmitMeshAdapter"]').click();
-        cy.contains("Adapter was configured!").should("exist");
+        cy.contains("Adapter was configured!", { timeout: 10_000 }).should("exist");
       } else {
         // TODO: Implement test code for Deploying from Settings > Adapters Page's "Available Mesh Adapter" Dropdown
       }
@@ -48,7 +47,7 @@ describe("Lifecycle Service Mesh", () => {
       // Lifecycle > Service Mesh Page
       cy.visit('/management');
       selectServiceMeshType(ADAPTER_LOCATION);
-      cy.get('[data-cy="lifecycle-service-mesh-type"]').should("contain.text", ADAPTER_LOCATION);
+      cy.get('[data-cy="lifecycle-service-mesh-type"]', { timeout: 10_000 }).should("contain.text", ADAPTER_LOCATION);
       // "Manage Service Mesh" Card's Ping adapter chip
       cy.get('[data-cy="adapter-chip-ping"]').click();
       cy.contains("Adapter pinged!").should("exist");
