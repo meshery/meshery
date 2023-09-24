47c925c21c7e48a1e3133625384b96557acb88ee
Mukesh Kuiry <99157367+mukeshkuiry@users.noreply.github.com>
2023-09-25 05:02:36 +0530

    Update MesheryChartDialog.js
    Signed-off-by: Mukesh Kuiry <99157367+mukeshkuiry@users.noreply.github.com>

--- ui/components/MesheryChartDialog.js
+++ ui/components/MesheryChartDialog.js
@@ -1,50 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 const styles = () => ({});
 
-class MesheryChartDialog extends React.Component {
-  render() {
-    const { classes, open, title, handleClose, content } = this.props;
-
-    return (
-      <React.Fragment>
-        <Dialog
-          fullWidth
-          maxWidth="md"
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="chart-dialog-title"
-        >
-          <DialogTitle id="chart-dialog-title">
-            {title && title.length ? title : 'Comparison'}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText className={classes.dialogContent}>{content}</DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose} color="primary">
-              Close
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </React.Fragment>
-    );
-  }
-}
+const MesheryChartDialog = ({ classes, open, title, handleClose, content }) => {
+  return (
+    <React.Fragment>
+      <Dialog fullWidth maxWidth="md" open={open} onClose={handleClose} aria-labelledby="chart-dialog-title">
+        <DialogTitle id="chart-dialog-title">{title && title.length ? title : "Comparison"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText className={classes.dialogContent}>{content}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+};
 
 MesheryChartDialog.propTypes = {
-  classes: PropTypes.object.isRequired,
-  open: PropTypes.bool.isRequired,
-  handleClose: PropTypes.func.isRequired,
-  content: PropTypes.node.isRequired,
+  classes : PropTypes.object.isRequired,
+  open : PropTypes.bool.isRequired,
+  handleClose : PropTypes.func.isRequired,
+  content : PropTypes.node.isRequired,
 };
 
 export default withStyles(styles)(MesheryChartDialog);
