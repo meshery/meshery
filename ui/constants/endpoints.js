3add2c20042d0453632b2f397585da7c3bb8b5d2
yash sharma <yashsharma2572@gmail.com>
2023-09-27 04:03:59 +0530

    added new icon and updated endpoint constants
    Signed-off-by: yash sharma <yashsharma2572@gmail.com>

--- ui/constants/endpoints.js
+++ ui/constants/endpoints.js
@@ -2,9 +2,9 @@
 export const MESHMODEL_ENDPOINT = '/api/meshmodels/models';
 export const MESHMODEL_RELATIONSHIPS_ENDPOINT = '/api/meshmodels/models/core/relationships';
 export const MESHERY_CLOUD_PROD = 'https://meshery.layer5.io';
-export const PATTERN_ENDPOINT = "/api/pattern";
-export const FILTER_ENDPOINT = "/api/filter";
-export const RJSF_SCHEMAS = "/api/schema/resource";
+export const PATTERN_ENDPOINT = '/api/pattern';
+export const FILTER_ENDPOINT = '/api/filter';
+export const RJSF_SCHEMAS = '/api/schema/resource';
 export const SORT = {
   ASCENDING: 'asc',
   DESCENDING: 'desc',
