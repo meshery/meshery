6302bd7ee944cf33ca539353d884c0cc5273890d
yash sharma <yashsharma2572@gmail.com>
2023-09-29 19:27:02 +0530

    fix JSDoc comment
    Signed-off-by: yash sharma <yashsharma2572@gmail.com>

--- ui/components/Connect/helpers/common.js
+++ ui/components/Connect/helpers/common.js
@@ -1,5 +1,5 @@
 /**
- * Very connection status
+ * Verify connection status
  * If connection already exist response will be connection object
  * If connection is not exist response will be 200 empty
  * 
