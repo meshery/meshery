8a638f7f3e485f99ed36daec46dafd242cb01831
yash sharma <yashsharma2572@gmail.com>
2023-10-03 19:02:15 +0530

    fix build
    Signed-off-by: yash sharma <yashsharma2572@gmail.com>

--- ui/components/General/TipsCarousel.js
+++ ui/components/General/TipsCarousel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Box, Typography } from '@material-ui/core';
-import TipsIcon from '../../assets/icons/TipsIcon'; // add this icon
+import TipsIcon from '../../assets/icons/Tipsicon'; // add this icon
 import { useTheme } from '@material-ui/core';
 
 const TipsCarousel = ({ tips }) => {
