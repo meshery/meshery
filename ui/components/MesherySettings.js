d669280858ef23eaa0434a9e27dcd0468b7e2aed
MeenuyD <meenu.coninja@gmail.com>
2023-10-08 06:16:08 +0530

    fix: Settings: Hide Credentials Menu
    Signed-off-by: MeenuyD <meenu.coninja@gmail.com>

--- ui/components/MesherySettings.js
+++ ui/components/MesherySettings.js
@@ -393,14 +393,14 @@ class MesherySettings extends React.Component {
                 tab="meshmodelSummary"
               />
             </Tooltip>
-            <Tooltip title="Credential" placement="top">
+            {/* <Tooltip title="Credential" placement="top">
               <Tab
                 className={classes.tab}
                 icon={<CredentialIcon width="1.5rem" />}
                 label="Credentials"
                 tab="credential"
               />
-            </Tooltip>
+            </Tooltip> */}
 
             {/*NOTE: Functionality of performance tab will be modified, until then keeping it and the related code commented */}
 
