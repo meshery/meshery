9f89ad7b736bd7d64f6145f13e9147e19fc663c3
Pranav Singh <pranavsingh02@hotmail.com>
2023-10-29 20:04:52 +0530

    fix provider-lint
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

--- provider-ui/components/Provider.style.js
+++ provider-ui/components/Provider.style.js
@@ -2,7 +2,6 @@ import { styled } from '@mui/material/styles'
 import MenuItem from '@mui/material/MenuItem'
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
-import Details from "@mui/material/Accordion";
 
 export const CustomDiv = styled('div')(({ theme }) => ({
   width : '60%',
@@ -30,14 +29,14 @@ export const CustomDialog = styled(Dialog)(({ theme }) => ({
     padding : theme.spacing(1),
   },
   '& .MuiDialogContentText-root > a' : {
-    color:"#222",
+    color : "#222",
   },
 }));
 export const CustomDialogActions = styled(DialogActions)(({ theme }) => ({
-  display:"flex", justifyContent:"space-between",  
-  background: "#eee",
+  display : "flex", justifyContent : "space-between",
+  background : "#eee",
   padding : theme.spacing(2),
   '& div > a' : {
-    color:"#222",    
+    color : "#222",
   },
 }));
