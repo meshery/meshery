cd7da39bdb6b0486b7d14308b15af3246d18295b
Pranav Singh <pranavsingh02@hotmail.com>
2023-11-13 16:39:48 +0530

    update node
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

---
+++ ui/components/DashboardComponent/nodes.js
@@ -0,0 +1,291 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import { TableCell, Tooltip, TableSortLabel } from '@material-ui/core';
+import Moment from 'react-moment';
+import dataFetch from '../../lib/data-fetch';
+import { useNotification } from '../../utils/hooks/useNotification';
+import { EVENT_TYPES } from '../../lib/event-types';
+import ResponsiveDataTable from '../../utils/data-table';
+// import { FormattedMetadata } from '../NotificationCenter/metadata';
+import CustomColumnVisibilityControl from '../../utils/custom-column';
+import useStyles from '../../assets/styles/general/tool.styles';
+import SearchBar from '../../utils/custom-search';
+
+const ACTION_TYPES = {
+  FETCH_MESHSYNC_RESOURCES: {
+    name: 'FETCH_MESHSYNC_RESOURCES',
+    error_msg: 'Failed to fetch meshsync resources',
+  },
+};
+
+const Nodes = ({ classes, updateProgress }) => {
+  const [meshSyncResources, setMeshSyncResources] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [count, setCount] = useState(0);
+  const [pageSize, setPageSize] = useState(0);
+  const [search, setSearch] = useState('');
+  const [sortOrder, setSortOrder] = useState('');
+
+  const StyleClass = useStyles();
+
+  const { notify } = useNotification();
+
+  const getMeshsyncResources = (page, pageSize, search, sortOrder) => {
+    setLoading(true);
+    if (!search) search = '';
+    if (!sortOrder) sortOrder = '';
+    dataFetch(
+      `/api/system/meshsync/resources?kind=Node&page=${page}&pagesize=${pageSize}&search=${encodeURIComponent(
+        search,
+      )}&order=${encodeURIComponent(sortOrder)}`,
+      {
+        credentials: 'include',
+        method: 'GET',
+      },
+      (res) => {
+        setMeshSyncResources(res?.resources || []);
+        setPage(res?.page || 0);
+        setCount(res?.total_count || 0);
+        setPageSize(res?.page_size || 0);
+        setLoading(false);
+      },
+      handleError(ACTION_TYPES.FETCH_MESHSYNC_RESOURCES),
+    );
+  };
+
+  const columns = [
+    {
+      name: 'id',
+      label: 'ID',
+      options: {
+        display: false,
+      },
+    },
+    {
+      name: 'metadata.name',
+      label: 'Name',
+      options: {
+        sort: false,
+        sortThirdClickReset: true,
+        display: true,
+        customBodyRender: (value) => {
+          const maxCharLength = 30;
+          const shouldTruncate = value?.length > maxCharLength;
+
+          return (
+            <Tooltip title={value} placement="top">
+              <div
+                style={{
+                  overflow: 'hidden',
+                  whiteSpace: 'nowrap',
+                  textOverflow: shouldTruncate ? 'ellipsis' : 'none',
+                }}
+              >
+                {shouldTruncate ? `${value.slice(0, maxCharLength)}...` : value}
+              </div>
+            </Tooltip>
+          );
+        },
+      },
+    },
+    {
+      name: 'apiVersion',
+      label: 'API version',
+      options: {
+        sort: false,
+        display: true,
+        customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+          return (
+            <SortableTableCell
+              index={index}
+              columnData={column}
+              columnMeta={columnMeta}
+              onSort={() => sortColumn(index)}
+            />
+          );
+        },
+      },
+    },
+    {
+      name: 'cluster_id', // TODO: chande to cluster name
+      label: 'Cluster ID',
+      options: {
+        sort: false,
+        sortThirdClickReset: true,
+        customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+          return (
+            <SortableTableCell
+              index={index}
+              columnData={column}
+              columnMeta={columnMeta}
+              onSort={() => sortColumn(index)}
+            />
+          );
+        },
+      },
+    },
+    {
+      name: 'metadata.creationTimestamp',
+      label: 'Discovered At',
+      options: {
+        sort: false,
+        sortThirdClickReset: true,
+        customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+          return (
+            <SortableTableCell
+              index={index}
+              columnData={column}
+              columnMeta={columnMeta}
+              onSort={() => sortColumn(index)}
+            />
+          );
+        },
+        customBodyRender: function CustomBody(value) {
+          return (
+            <Tooltip
+              title={
+                <Moment startOf="day" format="LLL">
+                  {value}
+                </Moment>
+              }
+              placement="top"
+              arrow
+              interactive
+            >
+              <Moment format="LL">{value}</Moment>
+            </Tooltip>
+          );
+        },
+      },
+    },
+  ];
+
+  const options = useMemo(
+    () => ({
+      filter: false,
+      viewColumns: false,
+      search: false,
+      responsive: 'standard',
+      serverSide: true,
+      selectableRows: false,
+      count,
+      rowsPerPage: pageSize,
+      rowsPerPageOptions: [10, 25, 30],
+      fixedHeader: true,
+      page,
+      print: false,
+      download: false,
+      textLabels: {
+        selectedRows: {
+          text: 'nodes(s) selected',
+        },
+      },
+      enableNestedDataAccess: '.',
+      onTableChange: (action, tableState) => {
+        const sortInfo = tableState.announceText ? tableState.announceText.split(' : ') : [];
+        let order = '';
+        if (tableState.activeColumn) {
+          order = `${columns[tableState.activeColumn].name} desc`;
+        }
+        switch (action) {
+          case 'changePage':
+            setPage(tableState.page.toString());
+            break;
+          case 'changeRowsPerPage':
+            setPageSize(tableState.rowsPerPage.toString());
+            break;
+          case 'sort':
+            if (sortInfo.length == 2) {
+              if (sortInfo[1] === 'ascending') {
+                order = `${columns[tableState.activeColumn].name} asc`;
+              } else {
+                order = `${columns[tableState.activeColumn].name} desc`;
+              }
+            }
+            if (order !== sortOrder) {
+              setSortOrder(order);
+            }
+            break;
+        }
+      },
+    }),
+    [page, pageSize],
+  );
+
+  useEffect(() => {
+    if (!loading) {
+      getMeshsyncResources(page, pageSize, search, sortOrder);
+    }
+  }, [page, pageSize, search, sortOrder]);
+
+  const handleError = (action) => (error) => {
+    updateProgress({ showProgress: false });
+    notify({
+      message: `${action.error_msg}: ${error}`,
+      event_type: EVENT_TYPES.ERROR,
+      details: error.toString(),
+    });
+  };
+
+  const [tableCols, updateCols] = useState(columns);
+
+  const [columnVisibility, setColumnVisibility] = useState(() => {
+    // Initialize column visibility based on the original columns' visibility
+    const initialVisibility = {};
+    columns.forEach((col) => {
+      initialVisibility[col.name] = col.options?.display !== false;
+    });
+    return initialVisibility;
+  });
+
+  return (
+    <>
+      <div className={StyleClass.toolWrapper} style={{ marginBottom: '5px', marginTop: '1rem' }}>
+        <div className={classes.createButton}>{/* <MesherySettingsEnvButtons /> */}</div>
+        <div
+          className={classes.searchAndView}
+          style={{
+            display: 'flex',
+            borderRadius: '0.5rem 0.5rem 0 0',
+          }}
+        >
+          <SearchBar
+            onSearch={(value) => {
+              setSearch(value);
+            }}
+            placeholder="Search nodes..."
+          />
+
+          <CustomColumnVisibilityControl
+            columns={columns}
+            customToolsProps={{ columnVisibility, setColumnVisibility }}
+          />
+        </div>
+      </div>
+      <ResponsiveDataTable
+        data={meshSyncResources}
+        columns={columns}
+        options={options}
+        className={classes.muiRow}
+        tableCols={tableCols}
+        updateCols={updateCols}
+        columnVisibility={columnVisibility}
+      />
+    </>
+  );
+};
+
+const SortableTableCell = ({ index, columnData, columnMeta, onSort }) => {
+  return (
+    <TableCell key={index} onClick={onSort}>
+      <TableSortLabel
+        active={columnMeta.name === columnData.name}
+        // direction={columnMeta.direction || 'asc'}
+      >
+        <b>{columnData.label}</b>
+      </TableSortLabel>
+    </TableCell>
+  );
+};
+
+export default Nodes;
