f30f36d62d668ead459a841133b3cbbba2a64efb
Pranav Singh <pranavsingh02@hotmail.com>
2023-11-16 19:17:47 +0530

    fix
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

--- ui/components/DashboardComponent/view.js
+++ ui/components/DashboardComponent/view.js
@@ -5,109 +5,174 @@ import { Paper, IconButton } from '@material-ui/core';
 import { useRouter } from 'next/router';
 // import { FormatStructuredData } from '../DataFormatter';
 import NameValueTable from '../DataFormatter/NameValueTable';
-// import ResponsiveDataTable from '../../utils/data-table';
+import ResponsiveDataTable from '../../utils/data-table';
 
 const View = (props) => {
   const { type, setView, resource } = props;
   console.log(type, setView, resource);
   console.log('inside view component');
 
-  // function renerDynamicTable(key, value) {
-  //   console.log('inside render dynamic table');
-  //   const allKeys = value.reduce((keys, obj) => {
-  //     Object.keys(obj).forEach((key) => {
-  //       if (!keys.includes(key)) {
-  //         keys.push(key);
-  //       }
-  //     });
-  //     return keys;
-  //   }, []);
+  // TODO: handle the condition for images
+  function RenderDynamicTable(key, value) {
+    console.log('inside render dynamic table');
+    const allKeys = value.reduce((keys, obj) => {
+      Object.keys(obj).forEach((key) => {
+        if (!keys.includes(key)) {
+          keys.push(key);
+        }
+      });
+      return keys;
+    }, []);
 
-  //   const columns = allKeys.map((key) => ({
-  //     name: key,
-  //     label: key,
-  //     options: {
-  //       filter: false,
-  //       sort: false,
-  //     },
-  //   }));
+    const columns = allKeys.map((key) => ({
+      name: key,
+      label: key,
+      options: {
+        filter: false,
+        sort: false,
+      },
+    }));
 
-  //   let options = {
-  //     filter: false,
-  //     download: false,
-  //     print: false,
-  //     search: false,
-  //     viewColumns: false,
-  //     selectableRows: 'none',
-  //     pagination: false,
-  //     responsive: 'standard',
-  //   };
+    let options = {
+      filter: false,
+      download: false,
+      print: false,
+      search: false,
+      viewColumns: false,
+      selectableRows: 'none',
+      pagination: false,
+      responsive: 'standard',
+    };
 
-  //   console.log("columns", columns)
-  //   console.log("value", value)
+    console.log('columns', columns);
+    console.log('value', value);
 
-  //   return (
-  //     <>
-  //       {key}
+    return (
+      <>
+        {key}
 
-  //       <ResponsiveDataTable
-  //         data={value}
-  //         columns={columns}
-  //         options={options}
-  //         tableCols={columns}
-  //         updateCols={() => {}}
-  //       />
-  //     </>
-  //   );
-  // }
+        <ResponsiveDataTable
+          data={value}
+          columns={columns}
+          options={options}
+          tableCols={columns}
+          updateCols={() => {}}
+          columnVisibility={{}}
+        />
+      </>
+    );
+  }
 
-  // function sanitizeResourceCopy(obj, parentKey = '') {
-  //   Object.entries(obj).forEach(([key, value]) => {
-  //     const currentKey = parentKey ? `${parentKey}.${key}` : key;
+  const RenderObject = (obj) => {
+    const processObjForKeyValTable = (obj, parentKey = '') => {
+      let rows = [];
+      for (const [key, value] of Object.entries(obj)) {
+        if (Array.isArray(value)) {
+          continue;
+        }
 
-  //       //       console.log("val", value)
-  //       // console.log("Array.isArray(value)", Array.isArray(value))
-
-  //     if (Array.isArray(value)) {
-  //      return renerDynamicTable(key, value);
-  //     }
-  //       if (typeof value === 'object' && value !== null) {
-  //         return sanitizeResource(value, currentKey);
-  //       } else {
-  //         if (key === 'attribute') {
-  //           return sanitizeResource(JSON.parse(value), currentKey);
-  //         }
-  //         if (key === 'id') {
-  //           return { name: currentKey, value: value, hide: true };
-  //         }
-  //         return { name: currentKey, value };
-  //       }
-
-  //   });
-  // }
-
-  function sanitizeResource(obj, parentKey = '') {
-    return Object.entries(obj).flatMap(([key, value]) => {
-      const currentKey = parentKey ? `${parentKey}.${key}` : key;
-
-      //       console.log("val", value)
-      // console.log("Array.isArray(value)", Array.isArray(value))
-
-      if (!Array.isArray(value)) {
+        const currentKey = parentKey ? `${parentKey}.${key}` : key;
         if (typeof value === 'object' && value !== null) {
-          return sanitizeResource(value, currentKey);
+          return processObjForKeyValTable(value, currentKey);
         } else {
           if (key === 'attribute') {
-            return sanitizeResource(JSON.parse(value), currentKey);
+            return processObjForKeyValTable(JSON.parse(value), currentKey);
           }
           if (key === 'id') {
-            return { name: currentKey, value: value, hide: true };
+            return rows.push({ name: currentKey, value: value, hide: true });
           }
-          return { name: currentKey, value };
+          return rows.push({ name: currentKey, value });
         }
       }
-    });
-  }
+
+      console.log('rows', rows);
+
+      return (
+        <>
+          <NameValueTable rows={rows} />
+        </>
+      );
+    };
+
+    const processObjForKeyDataTable = (obj, parentKey = '') => {
+      let results = [];
+      for (const [key, value] of Object.entries(obj)) {
+        console.log('key', key);
+        console.log('value', value);
+        const currentKey = parentKey ? `${parentKey}.${key}` : key;
+        if (
+          Array.isArray(value) &&
+          value.length > 0 &&
+          typeof value[0] === 'object' &&
+          value[0] !== null
+        ) {
+          results.push(RenderDynamicTable(key, value));
+        }
+        if (typeof value === 'object' && value !== null) {
+          results.push(processObjForKeyDataTable(value, currentKey));
+        } else {
+          if (key === 'attribute') {
+            results.push(processObjForKeyDataTable(JSON.parse(value), currentKey));
+          }
+        }
+      }
+      return results;
+    };
+
+    return (
+      <>
+        {processObjForKeyDataTable(obj)}
+        {processObjForKeyValTable(obj)}
+      </>
+    );
+  };
+
+  //   const RecursiveTable = ({ data }) => {
+  //     const [accumulatedData, setAccumulatedData] = useState([]);
+  //     let currentArray = [];
+
+  //     const renderTable = () => {
+  //       if (accumulatedData.length > 0) {
+  //         const result = <NameValueTable rows={accumulatedData} />;
+  //         setAccumulatedData([]);
+  //         return result;
+  //       }
+  //       return null;
+  //     };
+
+  //     const renderRecursiveTable = (obj, parentKey = '') => {
+  //     const rows = [];
+
+  //     for (const [key, value] of Object.entries(obj)) {
+  //       const currentKey = parentKey ? `${parentKey}.${key}` : key;
+
+  //       if (Array.isArray(value)) {
+  //         // If value is an array, render a subtable
+  //         rows.push(<RenderDynamicTable key={currentKey} value={value} />);
+  //       } else if (typeof value === 'object' && value !== null) {
+  //         // If value is an object, recursively render a subtable
+  //         rows.push(renderRecursiveTable(value, currentKey));
+  //       } else if (key === 'attribute') {
+  //           return rows.push(renderRecursiveTable(JSON.parse(value), currentKey));
+  //         }
+  //         else if (key === 'id') {
+  //           return rows.push({ name: currentKey, value: value, hide: true });
+  //         } else {
+  //         // For non-object values, accumulate data for rendering a NameValueTable
+  //         rows.push({ name: currentKey, value });
+  //       }
+  //     }
+
+  //     if (rows.length > 0) {
+  //       // If there are accumulated rows, render a NameValueTable
+  //       return <NameValueTable rows={rows} />;
+  //     }
+
+  //     return null;
+  //     };
+
+  //      return <>{renderRecursiveTable(data)}</>;
+  // };
 
   const router = useRouter();
 
@@ -127,24 +192,24 @@ const View = (props) => {
     return <></>;
   };
 
-  const ResourceDetails = (resource) => {
-    console.log(resource);
-    return (
-      <>
-        <div
-          style={{
-            margin: '1rem auto',
-          }}
-        >
-          {/* <FormatStructuredData data={resource} /> */}
-          <NameValueTable rows={resource.resource} />
-        </div>
-      </>
-    );
-  };
+  // const ResourceDetails = (resource) => {
+  //   console.log(resource);
+  //   return (
+  //     <>
+  //       <div
+  //         style={{
+  //           margin: '1rem auto',
+  //         }}
+  //       >
+  //         {/* <FormatStructuredData data={resource} /> */}
+  //         <NameValueTable rows={resource.resource} />
+  //       </div>
+  //     </>
+  //   );
+  // };
 
-  let cleanedResourceMetadata = sanitizeResource(resource?.metadata);
-  let cleanedResourceStatus = sanitizeResource(resource?.status);
+  // let cleanedResourceMetadata = sanitizeResource(resource?.metadata);
+  // let cleanedResourceStatus = sanitizeResource(resource?.status);
   // let cleanedResourceSpec = sanitizeResource(resource?.spec);
 
   return (
@@ -158,8 +223,13 @@ const View = (props) => {
           <HeaderComponent />
           <div style={{ margin: '1rem 7rem' }}>
             <ResourceMetrics />
-            <ResourceDetails resource={cleanedResourceMetadata} />
-            <ResourceDetails resource={cleanedResourceStatus} />
+            {/* <ResourceDetails resource={cleanedResourceMetadata} />
+            <ResourceDetails resource={cleanedResourceStatus} /> */}
+            {/* <SanitizeResourceCopy obj={resource?.status} /> */}
+            {/* <RecursiveTable data={resource?.metadata} />
+            <RecursiveTable data={resource?.status} /> */}
+            <RenderObject obj={resource?.metadata} />
+            <RenderObject obj={resource?.status} />
           </div>
         </Paper>
       </div>
