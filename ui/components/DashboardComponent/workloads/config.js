f7317c6c45cd39a5ed8572b258f582c0dbf93afb
Pranav Singh <pranavsingh02@hotmail.com>
2023-11-17 20:21:56 +0530

    finalize workloads
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

--- ui/components/DashboardComponent/workloads/config.js
+++ ui/components/DashboardComponent/workloads/config.js
@@ -881,7 +881,7 @@ const StandardWorkloadTable = (props) => {
           },
         },
         {
-          name: 'status.attribute',
+          name: 'spec.attribute',
           label: 'Desired Replicas',
           options: {
             sort: false,
@@ -909,8 +909,8 @@ const StandardWorkloadTable = (props) => {
             sort: false,
             customBodyRender: function CustomBody(val) {
               let attribute = JSON.parse(val);
-              let availableReplicas = attribute.availableReplicas;
-              return <>{availableReplicas}</>;
+              let replicas = attribute.replicas;
+              return <>{replicas}</>;
             },
             customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
               return (
@@ -1037,15 +1037,520 @@ const StandardWorkloadTable = (props) => {
     },
     REPLICATIONCONTROLLERS: {
       name: 'ReplicationController',
-      columns: [],
+      columns: [
+        {
+          name: 'id',
+          label: 'ID',
+          options: {
+            display: false,
+          },
+        },
+        {
+          name: 'metadata.name',
+          label: 'Name',
+          options: {
+            sort: false,
+            sortThirdClickReset: true,
+            customBodyRender: function CustomBody(value, tableMeta) {
+              return (
+                <>
+                  <div
+                    style={{
+                      color: 'inherit',
+                      textDecorationLine: 'underline',
+                      cursor: 'pointer',
+                      marginBottom: '0.5rem',
+                    }}
+                    onClick={() =>
+                      swtichView(SINGLE_WORKLOAD, meshSyncResources[tableMeta.rowIndex])
+                    }
+                  >
+                    {value}
+                  </div>
+                </>
+              );
+            },
+          },
+        },
+        {
+          name: 'apiVersion',
+          label: 'API version',
+          options: {
+            sort: false,
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+          },
+        },
+        {
+          name: 'spec.attribute',
+          label: 'Desired Replicas',
+          options: {
+            sort: false,
+            customBodyRender: function CustomBody(val) {
+              let attribute = JSON.parse(val);
+              let replicas = attribute.replicas;
+              return <>{replicas}</>;
+            },
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+          },
+        },
+        {
+          name: 'status.attribute',
+          label: 'Current Replicas',
+          options: {
+            sort: false,
+            customBodyRender: function CustomBody(val) {
+              let attribute = JSON.parse(val);
+              let replicas = attribute.replicas;
+              return <>{replicas}</>;
+            },
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+          },
+        },
+        {
+          name: 'metadata.namespace',
+          label: 'Namespace',
+          options: {
+            sort: false,
+            sortThirdClickReset: true,
+            customBodyRender: function CustomBody(value, tableMeta) {
+              return (
+                <>
+                  <div
+                    style={{
+                      color: 'inherit',
+                      textDecorationLine: 'underline',
+                      cursor: 'pointer',
+                      marginBottom: '0.5rem',
+                    }}
+                    onClick={() =>
+                      swtichView(SINGLE_WORKLOAD, meshSyncResources[tableMeta.rowIndex])
+                    }
+                  >
+                    {value}
+                  </div>
+                </>
+              );
+            },
+          },
+        },
+        {
+          name: 'cluster_id',
+          label: 'Cluster',
+          options: {
+            sort: false,
+            sortThirdClickReset: true,
+            customBodyRender: function CustomBody(val) {
+              let clusterName = getClusterNameFromClusterId(val, k8sConfig);
+              return (
+                <>
+                  <a
+                    href={'#'}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{
+                      color: 'inherit',
+                      textDecorationLine: 'underline',
+                      cursor: 'pointer',
+                      marginBottom: '0.5rem',
+                    }}
+                  >
+                    {clusterName}
+                  </a>
+                </>
+              );
+            },
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+          },
+        },
+        {
+          name: 'metadata.creationTimestamp',
+          label: 'Age',
+          options: {
+            sort: false,
+            sortThirdClickReset: true,
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+            customBodyRender: function CustomBody(value) {
+              let time = timeAgo(value);
+              return <>{time}</>;
+            },
+          },
+        },
+      ],
     },
     JOBS: {
       name: 'Job',
-      columns: [],
+      columns: [
+        {
+          name: 'id',
+          label: 'ID',
+          options: {
+            display: false,
+          },
+        },
+        {
+          name: 'metadata.name',
+          label: 'Name',
+          options: {
+            sort: false,
+            sortThirdClickReset: true,
+            customBodyRender: function CustomBody(value, tableMeta) {
+              return (
+                <>
+                  <div
+                    style={{
+                      color: 'inherit',
+                      textDecorationLine: 'underline',
+                      cursor: 'pointer',
+                      marginBottom: '0.5rem',
+                    }}
+                    onClick={() =>
+                      swtichView(SINGLE_WORKLOAD, meshSyncResources[tableMeta.rowIndex])
+                    }
+                  >
+                    {value}
+                  </div>
+                </>
+              );
+            },
+          },
+        },
+        {
+          name: 'apiVersion',
+          label: 'API version',
+          options: {
+            sort: false,
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+          },
+        },
+        {
+          name: 'metadata.namespace',
+          label: 'Namespace',
+          options: {
+            sort: false,
+            sortThirdClickReset: true,
+            customBodyRender: function CustomBody(value, tableMeta) {
+              return (
+                <>
+                  <div
+                    style={{
+                      color: 'inherit',
+                      textDecorationLine: 'underline',
+                      cursor: 'pointer',
+                      marginBottom: '0.5rem',
+                    }}
+                    onClick={() =>
+                      swtichView(SINGLE_WORKLOAD, meshSyncResources[tableMeta.rowIndex])
+                    }
+                  >
+                    {value}
+                  </div>
+                </>
+              );
+            },
+          },
+        },
+        {
+          name: 'cluster_id',
+          label: 'Cluster',
+          options: {
+            sort: false,
+            sortThirdClickReset: true,
+            customBodyRender: function CustomBody(val) {
+              let clusterName = getClusterNameFromClusterId(val, k8sConfig);
+              return (
+                <>
+                  <a
+                    href={'#'}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{
+                      color: 'inherit',
+                      textDecorationLine: 'underline',
+                      cursor: 'pointer',
+                      marginBottom: '0.5rem',
+                    }}
+                  >
+                    {clusterName}
+                  </a>
+                </>
+              );
+            },
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+          },
+        },
+        {
+          name: 'metadata.creationTimestamp',
+          label: 'Age',
+          options: {
+            sort: false,
+            sortThirdClickReset: true,
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+            customBodyRender: function CustomBody(value) {
+              let time = timeAgo(value);
+              return <>{time}</>;
+            },
+          },
+        },
+      ],
     },
     CRONJOBS: {
       name: 'CronJob',
-      columns: [],
+      columns: [
+        {
+          name: 'id',
+          label: 'ID',
+          options: {
+            display: false,
+          },
+        },
+        {
+          name: 'metadata.name',
+          label: 'Name',
+          options: {
+            sort: false,
+            sortThirdClickReset: true,
+            customBodyRender: function CustomBody(value, tableMeta) {
+              return (
+                <>
+                  <div
+                    style={{
+                      color: 'inherit',
+                      textDecorationLine: 'underline',
+                      cursor: 'pointer',
+                      marginBottom: '0.5rem',
+                    }}
+                    onClick={() =>
+                      swtichView(SINGLE_WORKLOAD, meshSyncResources[tableMeta.rowIndex])
+                    }
+                  >
+                    {value}
+                  </div>
+                </>
+              );
+            },
+          },
+        },
+        {
+          name: 'apiVersion',
+          label: 'API version',
+          options: {
+            sort: false,
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+          },
+        },
+        {
+          name: 'spec.attribute',
+          label: 'Schedule',
+          options: {
+            sort: false,
+            customBodyRender: function CustomBody(val) {
+              let attribute = JSON.parse(val);
+              let schedule = attribute.schedule;
+              return <>{schedule}</>;
+            },
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+          },
+        },
+        {
+          name: 'spec.attribute',
+          label: 'Suspend',
+          options: {
+            sort: false,
+            customBodyRender: function CustomBody(val) {
+              let attribute = JSON.parse(val);
+              let suspend = attribute.suspend;
+              return <>{suspend}</>;
+            },
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+          },
+        },
+        {
+          name: 'metadata.namespace',
+          label: 'Namespace',
+          options: {
+            sort: false,
+            sortThirdClickReset: true,
+            customBodyRender: function CustomBody(value, tableMeta) {
+              return (
+                <>
+                  <div
+                    style={{
+                      color: 'inherit',
+                      textDecorationLine: 'underline',
+                      cursor: 'pointer',
+                      marginBottom: '0.5rem',
+                    }}
+                    onClick={() =>
+                      swtichView(SINGLE_WORKLOAD, meshSyncResources[tableMeta.rowIndex])
+                    }
+                  >
+                    {value}
+                  </div>
+                </>
+              );
+            },
+          },
+        },
+        {
+          name: 'cluster_id',
+          label: 'Cluster',
+          options: {
+            sort: false,
+            sortThirdClickReset: true,
+            customBodyRender: function CustomBody(val) {
+              let clusterName = getClusterNameFromClusterId(val, k8sConfig);
+              return (
+                <>
+                  <a
+                    href={'#'}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{
+                      color: 'inherit',
+                      textDecorationLine: 'underline',
+                      cursor: 'pointer',
+                      marginBottom: '0.5rem',
+                    }}
+                  >
+                    {clusterName}
+                  </a>
+                </>
+              );
+            },
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+          },
+        },
+        {
+          name: 'metadata.creationTimestamp',
+          label: 'Age',
+          options: {
+            sort: false,
+            sortThirdClickReset: true,
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+            customBodyRender: function CustomBody(value) {
+              let time = timeAgo(value);
+              return <>{time}</>;
+            },
+          },
+        },
+      ],
     },
   };
 
