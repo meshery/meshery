1b4baefb70a23096761284e930c571b63fffa455
Pranav Singh <pranavsingh02@hotmail.com>
2023-11-17 14:28:31 +0530

    finalize view component
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

--- ui/components/DashboardComponent/view.js
+++ ui/components/DashboardComponent/view.js
@@ -61,90 +61,67 @@ const View = (props) => {
   }
 
   const RenderObject = (obj) => {
-    function processObjForKeyValTable(obj, parentKey = '') {
-      // return Object.entries(obj).flatMap(([key, value]) => {
-      //   const currentKey = parentKey ? `${parentKey}.${key}` : key;
-      //   if (Array.isArray(value)) {
-      //     return []; // Skip processing arrays
-      //   }
-      //   if (typeof value === 'object' && value !== null) {
-      //     return processObjForKeyValTable(value, currentKey);
-      //   } else {
-      //     if (key === 'attribute') {
-      //       return processObjForKeyValTable(JSON.parse(value), currentKey);
-      //     }
-      //     if (key === 'id') {
-      //       return { name: currentKey, value: value, hide: true };
-      //     }
-      //     return { name: currentKey, value };
-      //   }
-      // });
-      //  let rows = [];
+    function processObjForKeyValTable(obj) {
+      const [processedData, setProcessedData] = React.useState([]);
 
-      // for (const [key, value] of Object.entries(obj)) {
-      //   const currentKey = parentKey ? `${parentKey}.${key}` : key;
+      function processObj(obj, parentKey = '') {
+        let rows = [];
+        let currentGroup = [];
 
-      //   if (Array.isArray(value)) {
-      //     // Skip processing arrays
-      //     continue;
-      //   }
+        for (const [key, value] of Object.entries(obj)) {
+          const currentKey = parentKey ? `${parentKey}.${key}` : key;
 
-      //   let row = [];
-
-      // if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
-      //   row = [{ [currentKey]: processObjForKeyValTable(value, currentKey) }];
-      // } else {
-      //   if (key === 'attribute') {
-      //     row = processObjForKeyValTable(JSON.parse(value), currentKey);
-      //   } else if (key === 'id') {
-      //     row.push({ name: currentKey, value: value, hide: true });
-      //   } else {
-      //     row.push({ name: currentKey, value });
-      //   }
-      // }
-
-      // rows.push(...row); // Push the row into the main rows array
-      // }
-
-      let rows = [];
-      let currentGroup = [];
-
-      for (const [key, value] of Object.entries(obj)) {
-        const currentKey = parentKey ? `${parentKey}.${key}` : key;
-
-        if (Array.isArray(value)) {
-          // Skip the key if the value is an array
-          continue;
-        } else if (typeof value === 'object' && value !== null) {
-          // For objects, recursively process and add to the current group
-          currentGroup.push(...processObjForKeyValTable(value, currentKey));
-        } else {
-          // For non-objects, add to the rows directly
-          if (key === 'attribute') {
-            currentGroup.push(...processObjForKeyValTable(JSON.parse(value), currentKey));
-          } else if (key === 'id') {
-            currentGroup.push({ name: currentKey, value: value, hide: true });
+          if (Array.isArray(value)) {
+            // Skip the key if the value is an array
+            continue;
+          } else if (typeof value === 'object' && value !== null) {
+            // For objects, recursively process and add to the current group
+            currentGroup.push(...processObj(value, currentKey));
           } else {
-            currentGroup.push({ name: currentKey, value });
+            // For non-objects, add to the rows directly
+            if (key === 'attribute') {
+              currentGroup.push(...processObj(JSON.parse(value), currentKey));
+            } else if (key === 'id') {
+              currentGroup.push({ name: currentKey, value: value, hide: true });
+            } else {
+              currentGroup.push({ name: currentKey, value });
+            }
           }
         }
+
+        // Group by the parent key
+        if (parentKey !== '' && currentGroup.length > 0) {
+          if (Array.isArray(currentGroup)) {
+            setProcessedData((prev) => [...prev, { [parentKey]: currentGroup }]);
+          }
+        }
+
+        return rows;
       }
 
-      // Group by the parent key
-      if (parentKey !== '' && currentGroup.length > 0) {
-        rows.push({ [parentKey]: currentGroup });
-      } else {
-        rows.push(...currentGroup);
-      }
+      React.useEffect(() => {
+        console.log(processObj(obj));
+      }, [obj]);
 
-      return rows;
+      return (
+        <>
+          {processedData.map((obj, index) => (
+            <div key={index}>
+              {Object.entries(obj).map(([key, value], innerIndex) => (
+                <div key={innerIndex}>
+                  {key}
+                  <NameValueTable rows={value} />
+                </div>
+              ))}
+            </div>
+          ))}
+        </>
+      );
     }
 
     const processObjForKeyDataTable = (obj, parentKey = '') => {
       let results = [];
       for (const [key, value] of Object.entries(obj)) {
-        console.log('key', key);
-        console.log('value', value);
         const currentKey = parentKey ? `${parentKey}.${key}` : key;
         if (
           Array.isArray(value) &&
@@ -165,34 +142,10 @@ const View = (props) => {
       return results;
     };
 
-    let cleanedObj = processObjForKeyValTable(obj)[0]?.obj;
-
-    const processObjectForTable = (obj, parentKey = '') => {
-      console.log('inside processObjectForTable', obj, parentKey);
-      for (const entry of obj) {
-        const currentKey = entry.name;
-
-        if (Array.isArray(entry.value)) {
-          console.log('inside array', entry.value);
-          // If the value is an array, call the Table component
-          // with the parentKey and the array as data
-          return <NameValueTable rows={entry.value} />;
-        }
-
-        if (typeof entry.value === 'object' && entry.value !== null) {
-          // If the value is an object, recursively process it
-          return processObjectForTable(entry.value, currentKey);
-        }
-      }
-    };
-
     return (
       <>
         {processObjForKeyDataTable(obj)}
-        {/* <NameValueTable rows={processObjForKeyValTable(obj)[0]?.obj} /> */}
-        {cleanedObj.map((entry) => {
-          return processObjectForTable([entry]);
-        })}
+        {processObjForKeyValTable(obj)}
       </>
     );
   };
