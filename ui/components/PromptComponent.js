7410ec9d641a8150dd6aaf47e757ea602c5036c9
Raj <rajkasat@yo.com>
2023-11-24 01:08:37 +0530

    Changed hover effect on kubernetes config modal

--- ui/components/PromptComponent.js
+++ ui/components/PromptComponent.js
@@ -71,7 +71,7 @@ const PromptActionButton = styled(Button)(({ theme, promptVariant }) => ({
   backgroundColor: theme.palette.secondary[promptVariant || 'primary'],
   color: '#fff',
   '&:hover': {
-    backgroundColor: theme.palette.secondary[promptVariant || 'primary'],
+    backgroundColor: theme.palette.secondary.focused,
     boxShadow:
       '0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)',
   },
