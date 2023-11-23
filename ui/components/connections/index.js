d756152d34de6f38431a84baf659ad03cd96ef22
Senali <senalidilumika@gmail.com>
2023-11-24 04:51:58 +0530

    Next status from connection definition
    Signed-off-by: Senali <senalidilumika@gmail.com>

--- ui/components/connections/index.js
+++ ui/components/connections/index.js
@@ -52,7 +52,7 @@ import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 import SyncIcon from '@mui/icons-material/Sync';
 import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
 import ExploreIcon from '@mui/icons-material/Explore';
-import { CONNECTION_KINDS, CONNECTION_STATES } from '../../utils/Enum';
+import { CONNECTION_STATES } from '../../utils/Enum';
 import { FormatConnectionMetadata } from './metadata';
 import useKubernetesHook from '../hooks/useKubernetesHook';
 import theme from '../../themes/app';
@@ -151,7 +151,7 @@ function Connections({
   /*onOpenCreateConnectionModal,*/ operatorState,
   selectedK8sContexts,
   k8sconfig,
-  /*connectionMetadataState,*/
+  connectionMetadataState,
 }) {
   const modalRef = useRef(null);
   const [page, setPage] = useState(0);
@@ -166,7 +166,6 @@ function Connections({
   const [rowData, setSelectedRowData] = useState({});
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [_operatorState, _setOperatorState] = useState(operatorState || []);
-  const [connectionStatus, setConnectionStatus] = useState(null);
   const [tab, setTab] = useState(0);
   const ping = useKubernetesHook();
 
@@ -250,9 +249,7 @@ function Connections({
                 }
               }}
               iconSrc={
-                getColumnValue(tableMeta.rowData, 'kind', columns) === CONNECTION_KINDS.MESHERY
-                  ? '/static/img/meshery-logo.png'
-                  : '/static/img/kubernetes.svg'
+                getColumnValue(tableMeta.rowData, 'kindLogo', columns).colorIcon.split('public')[1]
               }
               style={{ maxWidth: '120px' }}
             />
@@ -420,7 +417,7 @@ function Connections({
           } else {
             nextStatus.push(value);
           }
-          const disabled = value === 'deleted' || originalNextStatus === undefined ? true : false;
+          const disabled = value === 'deleted' ? true : false;
           return (
             <>
               <FormControl className={classes.chipFormControl}>
@@ -513,6 +510,13 @@ function Connections({
     {
       name: 'nextStatus',
       label: 'nextStatus',
+      options: {
+        display: false,
+      },
+    },
+    {
+      name: 'kindLogo',
+      label: 'kindLogo',
       options: {
         display: false,
       },
@@ -624,16 +628,10 @@ function Connections({
    * fetch connections when the page loads
    */
   useEffect(() => {
-    if (!loading && connectionStatus) {
+    if (!loading && connectionMetadataState) {
       getConnections(page, pageSize, search, sortOrder);
     }
-  }, [page, pageSize, search, sortOrder, connectionStatus]);
-
-  useEffect(() => {
-    Object.keys(CONNECTION_KINDS).map((kind) => {
-      getConnectionStatus(CONNECTION_KINDS[kind]);
-    });
-  }, []);
+  }, [page, pageSize, search, sortOrder, connectionMetadataState]);
 
   const getConnections = (page, pageSize, search, sortOrder) => {
     setLoading(true);
@@ -649,7 +647,11 @@ function Connections({
       },
       (res) => {
         res?.connections.forEach((connection) => {
-          connection.nextStatus = connectionStatus[connection.kind];
+          (connection.nextStatus =
+            connection.nextStatus === undefined &&
+            connectionMetadataState[connection.kind].transitions),
+            (connection.kindLogo =
+              connection.kindLogo === undefined && connectionMetadataState[connection.kind].icon);
         });
         setConnections(res?.connections || []);
         setPage(res?.page || 0);
@@ -658,26 +660,6 @@ function Connections({
         setLoading(false);
       },
       handleError(ACTION_TYPES.FETCH_CONNECTIONS),
-    );
-  };
-
-  const getConnectionStatus = (connectionKind) => {
-    setLoading(true);
-    dataFetch(
-      `  /api/integrations/connections/${connectionKind}/transitions`,
-      {
-        credentials: 'include',
-        method: 'GET',
-      },
-      (res) => {
-        setConnectionStatus((prevState) => ({
-          ...prevState,
-          [connectionKind]: res,
-        }));
-        setLoading(false);
-      },
-      handleError(ACTION_TYPES.FETCH_CONNECTION_STATUS_TRANSITIONS),
-      setLoading(false),
     );
   };
 
