821f2d602f8ad527c19dafb601d417f23e345558
sudhanshu dasgupta <dasguptashivam23@gmail.com>
2023-12-01 21:10:16 +0530

    make sub tabs full width
    Signed-off-by: sudhanshu dasgupta <dasguptashivam23@gmail.com>

--- ui/components/DashboardComponent/resources/resources-sub-menu.js
+++ ui/components/DashboardComponent/resources/resources-sub-menu.js
@@ -121,7 +121,7 @@ const ResourcesSubMenu = (props) => {
       <div className={classes.wrapperClss}>
         <Paper className={classes.wrapperClss}>
           <div className={classes.subMenuTab}>
-            <Box sx={{ margin: '0 auto', width: '100%', maxWidth: { xs: 490, sm: 880 } }}>
+            <Box sx={{ margin: '0 auto', width: '100%', maxWidth: { xs: 490, sm: 880, md: 1200 } }}>
               <Tabs
                 value={tabVal}
                 className={classes.tabs}
@@ -129,6 +129,7 @@ const ResourcesSubMenu = (props) => {
                 variant="scrollable"
                 indicatorColor="primary"
                 textColor="primary"
+                centered
               >
                 {Object.keys(resource.tableConfig()).map((key, index) => {
                   return (
