e78e5719c3bb001514e0842a7bb030568a0252f3
Shlok Mishra <99207534+Jougan-0@users.noreply.github.com>
2023-12-11 18:27:01 +0530

    Update server/cmd/main.go
    Co-authored-by: Lee Calcote <leecalcote@gmail.com>
    Signed-off-by: Shlok Mishra <99207534+Jougan-0@users.noreply.github.com>

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -296,14 +296,18 @@ func main() {
 	connToInstanceTracker := machines.ConnectionToStateMachineInstanceTracker{
 		ConnectToInstanceMap: make(map[uuid.UUID]*machines.StateMachine, 0),
 	}
-	hosts, _, err := regManager.GetRegistrants(&v1alpha1.HostFilter{})
-	if err != nil {
-		log.Error(err)
-	}
-	for _, host := range hosts {
-		summary := host.Summary
-		log.Info(fmt.Sprintf("For Registrant %s Successfully imported %d models %d components %d relationships %d policy",
-			host.Hostname, summary.Models, summary.Components, summary.Relationships, summary.Policies))
+	
+		hosts, _, err := regManager.GetRegistrants(&v1alpha1.HostFilter{})
+		if err != nil {
+			log.Error(err)
+		}
+		for _, host := range hosts {
+			log.Infof("For Registrant %s Successfully imported %d models %d components %d relationships %d policy", host.Hostname, host.Summary.Models, host.Summary.Components, host.Summary.Relationships, host.Summary.Policies)
+			nonRegisteredEntityCount := meshmodel.NonImportModel[host.Hostname]
+			if nonRegisteredEntityCount.Models > 0 || nonRegisteredEntityCount.Components > 0 || nonRegisteredEntityCount.Relationships > 0 || nonRegisteredEntityCount.Policies > 0 {
+				log.Errorf("Registrant %s: Failed to import %d models, %d components, %d relationships, and %d policies", host.Hostname, nonRegisteredEntityCount.Models, nonRegisteredEntityCount.Components, nonRegisteredEntityCount.Relationships, nonRegisteredEntityCount.Policies)
+			}
+		}
 
 		nonImportModel := meshmodel.NonImportModel[host.Hostname]
 		if nonImportModel.Models > 0 || nonImportModel.Components > 0 || nonImportModel.Relationships > 0 || nonImportModel.Policies > 0 {
@@ -336,7 +340,6 @@ func main() {
 			fmt.Println("Error writing JSON data to file:", err)
 			return
 		}
-	})
 
 	k8sComponentsRegistrationHelper := models.NewComponentsRegistrationHelper(log)
 	rego, err := policies.NewRegoInstance(PoliciesPath, RelationshipsPath)
