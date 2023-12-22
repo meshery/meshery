fe8c9d3c9cb4ec22b7edbde37fdc87615becaed7
sudhanshu dasgupta <dasguptashivam23@gmail.com>
2023-12-22 20:19:17 +0530

    prevent expansion on chip
    Signed-off-by: sudhanshu dasgupta <dasguptashivam23@gmail.com>

--- ui/components/connections/index.js
+++ ui/components/connections/index.js
@@ -260,7 +260,8 @@ function Connections({
                   getColumnValue(tableMeta.rowData, 'kind', columns),
                 )
               }
-              handlePing={() => {
+              handlePing={(e) => {
+                e.stopPropagation();
                 if (tableMeta.rowData[4] === CONNECTION_KINDS.KUBERNETES) {
                   ping(tableMeta.rowData[3], tableMeta.rowData[2], tableMeta.rowData[0]);
                 }
