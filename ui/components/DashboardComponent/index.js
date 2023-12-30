3accde2452943ab9de244aff6eb0e4eccaf33072
Jougan-0 <prasantmishra2018@gmail.com>
2023-12-30 06:37:36 +0530

    server side event call w/signoff
    Signed-off-by: Jougan-0 <prasantmishra2018@gmail.com>

--- ui/components/DashboardComponent/index.js
+++ ui/components/DashboardComponent/index.js
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux';
 import { withRouter } from 'next/router';
 import { updateGrafanaConfig, updatePrometheusConfig, updateTelemetryUrls } from '../../lib/store';
 import { withStyles } from '@material-ui/core/styles';
-import { withNotify, useNotification } from '../../utils/hooks/useNotification';
+import { withNotify } from '../../utils/hooks/useNotification';
 import { Tooltip, Tabs, Tab, Paper, Typography } from '@material-ui/core';
 import { updateProgress } from '../../lib/store';
 import { ResourcesConfig } from './resources/config';
@@ -13,7 +13,6 @@ import ResourcesSubMenu from './resources/resources-sub-menu';
 import Overview from './overview';
 import KubernetesIcon from '../../assets/icons/technology/kubernetes';
 import MesheryIcon from './images/meshery-icon.js';
-import { EVENT_TYPES } from '../../lib/event-types';
 
 const styles = (theme) => ({
   wrapperClss: {
@@ -109,7 +108,6 @@ const styles = (theme) => ({
 
 const DashboardComponent = (props) => {
   const { classes, k8sconfig, selectedK8sContexts, updateProgress } = props;
-  const { notify } = useNotification();
 
   const [tabVal, setTabVal] = React.useState(0);
 
@@ -123,44 +121,14 @@ const DashboardComponent = (props) => {
 
       if (data.total_count > 0) {
         for (const registrant of data.registrants) {
-          const { hostname, summary } = registrant;
-
-          let successMessage = `For registrant ${hostname} successfully imported `;
-          if (summary.models > 0) successMessage += ` ${summary.models} models`;
-          if (summary.components > 0) successMessage += ` ${summary.components} components`;
-          if (summary.relationships > 0)
-            successMessage += ` ${summary.relationships} relationships`;
-          if (summary.policies > 0) successMessage += ` ${summary.policies} policies`;
-
-          notify({
-            message: successMessage,
-            event_type: EVENT_TYPES.SUCCESS,
-          });
-
-          const nonRegisteredResponse = await fetch('api/meshmodels/nonRegisterEntity', {
+          console.log(registrant);
+          const { hostname } = registrant;
+          await fetch('api/meshmodels/nonRegisterEntity', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
             },
             body: JSON.stringify({ Hostname: hostname }),
-          });
-
-          const nonRegisteredData = await nonRegisteredResponse.json();
-          console.log(nonRegisteredData);
-
-          let errorMessage = `For registrant ${hostname} failed to import`;
-          if (nonRegisteredData.summary.models > 0)
-            errorMessage += ` ${nonRegisteredData.summary.models} models`;
-          if (nonRegisteredData.summary.components > 0)
-            errorMessage += ` ${nonRegisteredData.summary.components} components`;
-          if (nonRegisteredData.summary.relationships > 0)
-            errorMessage += ` ${nonRegisteredData.summary.relationships} relationships`;
-          if (nonRegisteredData.summary.policies > 0)
-            errorMessage += ` ${nonRegisteredData.summary.policies} policies`;
-
-          notify({
-            message: errorMessage,
-            event_type: EVENT_TYPES.ERROR,
           });
         }
       }
@@ -172,7 +140,6 @@ const DashboardComponent = (props) => {
   useEffect(() => {
     fetchData();
   }, []);
-
   function TabContainer(props) {
     return (
       <Typography component="div" style={{ paddingTop: 2 }}>
