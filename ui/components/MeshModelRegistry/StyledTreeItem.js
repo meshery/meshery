b0e7d85e39f1f4504db50577531c4cfdd218b718
yash sharma <yashsharma2572@gmail.com>
2024-01-12 20:31:30 +0530

    Make styled tree item separate component
    Signed-off-by: yash sharma <yashsharma2572@gmail.com>

---
+++ ui/components/MeshModelRegistry/StyledTreeItem.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { Box, Typography, useTheme } from '@material-ui/core';
+import Checkbox from '@mui/material/Checkbox';
+import SearchBar from '@/utils/custom-search';
+import debounce from '@/utils/debounce';
+import { StyledTreeItemRoot } from './MeshModel.style';
+import { useWindowDimensions } from '@/utils/dimension';
+
+/**
+ * Customized item component in mui-x-tree
+ */
+const StyledTreeItem = React.forwardRef(function StyledTreeItem(props, ref) {
+  const [checked, setChecked] = useState(false);
+  const [hover, setHover] = useState(false);
+  const { check, labelText, root, search, setSearchText, ...other } = props;
+  const theme = useTheme();
+  const { width } = useWindowDimensions();
+  const [isSearchExpanded, setIsSearchExpanded] = useState(false);
+
+  return (
+    <StyledTreeItemRoot
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      root={root}
+      lineColor={theme.palette.secondary.text}
+      label={
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            py: check ? 0.5 : search ? 0.2 : 1.5,
+            px: 0,
+          }}
+        >
+          {width < 1370 && isSearchExpanded ? null : (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}
+            >
+              <Typography variant={'body'} style={{ color: `${root}` }}>
+                {labelText}
+              </Typography>
+            </div>
+          )}
+
+          {/* Currently the functionality of checkbox is not supported */}
+
+          {check && (
+            <Checkbox
+              onClick={() => setChecked((prevcheck) => !prevcheck)}
+              size="small"
+              checked={checked}
+              sx={{
+                visibility: hover || checked ? 'visible' : 'hidden',
+                color: '#00B39F',
+                '&.Mui-checked': {
+                  color: '#00B39F',
+                },
+              }}
+            />
+          )}
+          {search && (
+            <SearchBar
+              onSearch={debounce((value) => setSearchText(value), 200)}
+              expanded={isSearchExpanded}
+              setExpanded={setIsSearchExpanded}
+              placeholder="Search"
+            />
+          )}
+        </Box>
+      }
+      {...other}
+      ref={ref}
+    />
+  );
+});
+
+export default StyledTreeItem;
