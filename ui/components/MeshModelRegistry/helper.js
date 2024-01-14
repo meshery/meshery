7f06ea4fb3b30330ccd932f4fd3bc5355e74ea14
yash sharma <yashsharma2572@gmail.com>
2024-01-15 02:05:18 +0530

    Added helper function
    Signed-off-by: yash sharma <yashsharma2572@gmail.com>

---
+++ ui/components/MeshModelRegistry/helper.js
@@ -0,0 +1,31 @@
+import {
+    MODELS,
+    REGISTRANTS,
+  } from '@/constants/navigator';
+
+/**
+ * Retrieves filtered data for the details component based on the selected item ID.
+ *
+ * @param {Array} data - An array of data representing the tree.
+ * @param {Array} selectedIdArr - An array containing the IDs of the selected item. As the length increases, it represents sub-child levels. For example, selectedIdArr[0] is the main parent, selectedIdArr[1] is a sub-child of it, and so on.
+ * @returns {Object} - An object containing the selected component, model, and relationship.
+ */
+export const getFilteredDataForDetailsComponent = (data, selectedItemUUID, view) => {
+  let selectedIdArr = selectedItemUUID.split('.')
+  const selectedParent = data.find((parentItem) => parentItem.id === selectedIdArr[0]);
+  let selectedComponent = [];
+  let selectedModel = {};
+  let selectedRelationship = [];
+
+  selectedModel = view === REGISTRANTS ? selectedParent?.models : selectedParent;
+  const componentKey = view === MODELS ? 'components' : 'relationships';
+  selectedComponent = selectedModel?.[componentKey]?.find(
+    (component) => component && `${selectedModel.id}.1.${component.id}` === selectedItemUUID,
+  );
+  selectedRelationship = selectedModel?.relationships?.find(
+    (relationship) =>
+      relationship && `${selectedModel.id}.2.${relationship.id}` === selectedItemUUID,
+  );
+
+  return { selectedComponent, selectedModel, selectedRelationship };
+};
