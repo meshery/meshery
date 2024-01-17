a5a8d7067c055fba2ec6d329746df0f438759c81
Pranav Singh <pranavsingh02@hotmail.com>
2024-01-17 18:51:29 +0530

    add layer5io, mesheryio, mesherydocs generator
    Signed-off-by: Pranav Singh <pranavsingh02@hotmail.com>

--- scripts/component_updater/pkg/utility.go
+++ scripts/component_updater/pkg/utility.go
@@ -1,10 +1,11 @@
 package pkg
 
 import (
-	"log"
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -45,14 +46,14 @@ func GetIndexForRegisterCol(cols []string, shouldRegister string) int {
 	return shouldRegisterColIndex
 }
 
-func formatName(input string) string {
+func FormatName(input string) string {
 	formattedName := strings.ReplaceAll(input, " ", "-")
 	formattedName = strings.ToLower(formattedName)
 	return formattedName
 }
 
 func CreateFiles(path, filetype, name, content string) {
-	formattedName := formatName(name)
+	formattedName := FormatName(name)
 
 	fullPath := path + "/" + formattedName + filetype
 
@@ -70,3 +71,130 @@ func CreateFiles(path, filetype, name, content string) {
 	}
 }
 
+func GenerateLayer5Docs(model ModelCSV, components []ComponentCSV, path string) error {
+	// create dir for model
+	modelDir := filepath.Join(path, model.Model)
+	err := os.MkdirAll(modelDir, 0777)
+	if err != nil {
+		return err
+	}
+
+	// create dir for color model icons
+	iconsDir := filepath.Join(modelDir, "icons", "color")
+	err = os.MkdirAll(iconsDir, 0777)
+	if err != nil {
+		return err
+	}
+
+	err = WriteSVG(filepath.Join(iconsDir, model.Model+"-color.svg"), model.SVGColor)
+	if err != nil {
+		return err
+	}
+
+
+	// create dir for white model icons
+	iconsDir = filepath.Join(modelDir, "icons", "white")
+	err = os.MkdirAll(iconsDir, 0777)
+	if err != nil {
+		return err
+	}
+
+	err = WriteSVG(filepath.Join(iconsDir, model.Model+"-white.svg"), model.SVGWhite)
+	if err != nil {
+		return err
+	}
+
+	// generate components metadata and create svg files
+
+	componentMetadata, err := CreateComponentsMetadataAndCreateSVGs(components, modelDir, "components") 
+	if err != nil {
+		return err
+	}
+
+
+	// generate markdown file
+	md := model.CreateMarkDownForLayer5(componentMetadata)
+		_ = WriteToFile(filepath.Join(modelDir, "index.mdx"), md)
+
+	return nil
+}
+
+func GenerateMesheryioDocs(model ModelCSV, path, mesheryioDocsJSON string) (string, error) {
+	// generate data.js file
+	jsonItem := model.CreateJSONItem()
+	mesheryioDocsJSON += jsonItem + ","
+
+
+	// create svg dir
+	err := os.MkdirAll(filepath.Join(path, "../", "images"), 0777)
+		if err != nil {
+			return "", err
+		}
+
+		// write color svg
+		err = WriteSVG(filepath.Join(path, "../", "images", model.Model+"-color.svg"), model.SVGColor)
+		if err != nil {
+			return "", err
+		}
+
+		// write white svg
+		err = WriteSVG(filepath.Join(path, "../", "images", model.Model+"-white.svg"), model.SVGWhite)
+		if err != nil {
+				return "", err
+		}
+
+
+	return mesheryioDocsJSON, nil
+}
+
+func GenerateMesheryDocs(model ModelCSV, components []ComponentCSV, path string) error {
+
+	modelName := FormatName(model.Model)
+	
+	// dir for markdown
+	mdDir := filepath.Join(path, "pages", "integrations")
+
+	// dir for icons
+	iconsDir := filepath.Join(mdDir, "assets", "img", "integrations", modelName)
+	err := os.MkdirAll(iconsDir, 0777)
+	if err != nil {
+		return err
+	}
+
+	// for color icons
+	colorIconsDir := filepath.Join(iconsDir, "icons", "color")
+	err = os.MkdirAll(colorIconsDir, 0777)
+	if err != nil {
+		return err
+	}
+
+	err = WriteSVG(filepath.Join(colorIconsDir, modelName+"-color.svg"), model.SVGColor)
+	if err != nil {
+		return err
+	}
+
+	// for white icons
+	whiteIconsDir := filepath.Join(iconsDir, "icons", "white")
+	err = os.MkdirAll(whiteIconsDir, 0777)
+	if err != nil {
+		return err
+	}
+
+	err = WriteSVG(filepath.Join(whiteIconsDir, modelName+"-white.svg"), model.SVGWhite)
+	if err != nil {
+		return err
+	}
+
+	// generate components metadata and create svg files
+	componentMetadata, err := CreateComponentsMetadataAndCreateSVGs(components, iconsDir, "components")
+	if err != nil {
+		return err
+	}
+
+	// generate markdown file
+	md := model.CreateMarkDownForMesheryDocs(componentMetadata)
+	_ = WriteToFile(filepath.Join(mdDir, modelName+".mdx"), md)
+
+
+	return nil
+}
