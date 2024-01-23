d58c2f2765d9d295d36f33593c18aad1724501ab
yash sharma <yashsharma2572@gmail.com>
2024-01-23 19:17:30 +0530

    Added get help button in troubleshoot component
    Signed-off-by: yash sharma <yashsharma2572@gmail.com>

--- ui/components/TroubleshootingComponent.js
+++ ui/components/TroubleshootingComponent.js
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import Button from '@material-ui/core/Button';
 import TroubleshootingModal from './TroubleshootingModalComponent';
+import SupportModal from './Modals/SupportModal';
 
 const Troubleshoot = (props) => {
   const [open, setOpen] = React.useState(true);
+  const [openSupportModal, setOpenSupportModal] = React.useState(false);
   const handleOpen = () => {
     setOpen(true);
+  };
+  const handleOpenSupportModal = () => {
+    setOpenSupportModal(true);
+  };
+
+  const handleCloseSupportModal = () => {
+    setOpenSupportModal(false);
   };
 
   return (
@@ -13,6 +22,10 @@ const Troubleshoot = (props) => {
       <Button variant="contained" color="primary" size="large" onClick={handleOpen}>
         Troubleshooting Guide
       </Button>
+      <Button variant="contained" color="primary" size="large" onClick={handleOpenSupportModal}>
+        Get Help
+      </Button>
+      <SupportModal open={openSupportModal} handleClose={handleCloseSupportModal} />
       <TroubleshootingModal
         viewDataErrorMessage={props?.viewDataErrorMessage}
         viewHeaderErrorMessage={props?.viewHeaderErrorMessage}
