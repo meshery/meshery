208bfd7fb6880a939278422f08cfc8a08d6b69e1
sudhanshu dasgupta <dasguptashivam23@gmail.com>
2024-02-05 12:03:56 +0530

    fix lint
    Signed-off-by: sudhanshu dasgupta <dasguptashivam23@gmail.com>

--- ui/assets/icons/InfoOutlined.js
+++ ui/assets/icons/InfoOutlined.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const InfoOutlinedIcon = ({ height, width, style = {}, className={}, fill }) => {
+const InfoOutlinedIcon = ({ height, width, style = {}, className={} }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
