c7f9477493118418be02ebcf410d0dfcb10ee1a7
l5io <l5io@users.noreply.github.com>
2024-02-22 21:11:50 +0000

    [Error Codes]: Update Meshery server and mehseryctl error codes and reference
    Signed-off-by: l5io <ci@layer5.io>

--- mesheryctl/pkg/utils/error.go
+++ mesheryctl/pkg/utils/error.go
@@ -36,8 +36,8 @@ var (
 	ErrParseGithubFileCode    = "1185"
 	ErrReadTokenCode          = "1186"
 	ErrRequestResponseCode    = "1187"
-	ErrMarshalStructToCSVCode = "mesheryctl-1309"
-	ErrAppendToSheetCode      = "mesheryctl-1310"
+	ErrMarshalStructToCSVCode = "mesheryctl-1311"
+	ErrAppendToSheetCode      = "mesheryctl-1312"
 )
 
 // RootError returns a formatted error message with a link to 'root' command usage page at
