f0e52ae6ace1fb760fd2307dbe34898d3848cc21
Aseem <Aseempratap2207@gmail.com>
2024-03-26 13:48:35 +0530

    remove makeStyle import
    Signed-off-by: Aseem <Aseempratap2207@gmail.com>

--- ui/components/MesheryChartDialog.js
+++ ui/components/MesheryChartDialog.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
