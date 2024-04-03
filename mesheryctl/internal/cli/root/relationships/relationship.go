65440ab5b0c4c8b6071bfc95b842fae5fdc2c0e9
Ripul Handoo <ripulhandoo1234@gmail.com>
2024-04-03 16:23:19 +0530

    feat: generate relationship docs
    Signed-off-by: Ripul Handoo <ripulhandoo1234@gmail.com>

---
+++ mesheryctl/internal/cli/root/relationships/relationship.go
@@ -0,0 +1,91 @@
+// Copyright 2024 Layer5, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package relationships
+
+import (
+	"fmt"
+
+	"github.com/layer5io/meshery/mesheryctl/internal/cli/root/config"
+	"github.com/layer5io/meshery/mesheryctl/internal/cli/root/system"
+	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	availableSubcommands = []*cobra.Command{GenerateRelationshipDocsCmd}
+)
+
+var RelationshipCmd = &cobra.Command{
+	Use:   "relationship",
+	Short: "View list of relationships and details of relationship",
+	Long:  "View list of relationships and detailed information of a specific relationship",
+	Example: `
+// To view list of relationships
+mesheryctl exp relationships list
+
+// To view a specific relationship
+mesheryctl exp relationships view [model-name]
+	`,
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		//Check prerequisite
+
+		mctlCfg, err := config.GetMesheryCtl(viper.GetViper())
+		if err != nil {
+			return utils.ErrLoadConfig(err)
+		}
+		err = utils.IsServerRunning(mctlCfg.GetBaseMesheryURL())
+		if err != nil {
+			return err
+		}
+		ctx, err := mctlCfg.GetCurrentContext()
+		if err != nil {
+			return system.ErrGetCurrentContext(err)
+		}
+		err = ctx.ValidateVersion()
+		if err != nil {
+			return err
+		}
+		return nil
+	},
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			errMsg := "Usage: mesheryctl exp relationships [subcommand]\nRun 'mesheryctl exp relationships --help' to see detailed help message"
+			return utils.ErrInvalidArgument(errors.New("missing required argument: [model-name]. " + errMsg))
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if ok := utils.IsValidSubcommand(availableSubcommands, args[0]); !ok {
+			return errors.New(utils.RelationshipsError(fmt.Sprintf("'%s' is an invalid subcommand. Please provide required options from [view]. Use 'mesheryctl exp relationships --help' to display usage guide.\n", args[0]), "relationship"))
+		}
+		_, err := config.GetMesheryCtl(viper.GetViper())
+		if err != nil {
+			return utils.ErrLoadConfig(err)
+		}
+
+		err = cmd.Usage()
+		if err != nil {
+			return err
+		}
+		return nil
+	},
+}
+
+func init() {
+	RelationshipCmd.AddCommand(availableSubcommands...)
+}
