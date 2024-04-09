50cccb23262d66ec8fc1aede5260026ac2475f59
Adarsh jaiswal <its.adarshjaiss@gmail.com>
2024-04-09 19:22:52 +0530

    changed default size to 10
    Co-authored-by: Yash sharma <71271069+Yashsharma1911@users.noreply.github.com>
    Signed-off-by: Adarsh jaiswal <its.adarshjaiss@gmail.com>

--- ui/components/MesheryPatterns.js
+++ ui/components/MesheryPatterns.js
@@ -336,7 +336,7 @@ function MesheryPatterns({
   const [search, setSearch] = useState('');
   const [sortOrder, setSortOrder] = useState('');
   const [count, setCount] = useState(0);
-  const [pageSize, setPageSize] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
   const modalRef = useRef();
   const [patterns, setPatterns] = useState([]);
   const [selectedRowData, setSelectedRowData] = useState(null);
