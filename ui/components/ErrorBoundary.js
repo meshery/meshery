b2b73b0c615e4f6156d0b6711faac49f12f2ec38
Himanshu Gupta <114623040+devhimanshuu@users.noreply.github.com>
2024-04-09 15:25:43 +0530

    Update ErrorBoundary.js
    Signed-off-by: Himanshu Gupta <114623040+devhimanshuu@users.noreply.github.com>

--- ui/components/ErrorBoundary.js
+++ ui/components/ErrorBoundary.js
@@ -1,30 +1,34 @@
 import Button from '@mui/material/Button';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-/**
- * ErrorBoundary is a React component that catches JavaScript errors in its child components and renders a fallback UI when an error occurs.
- * It should be used as a wrapper around components that might throw errors.
- */
 const ErrorBoundary = ({ children }) => {
   const [hasError, setHasError] = useState(false);
   const [error, setError] = useState(null);
 
-  /** Update state so the next render will show the fallback UI. */
-  const getDerivedStateFromError = (error) => {
-    setHasError(true);
-    setError(error);
-  };
+  useEffect(() => {
+    const handleErrors = (error) => {
+      setHasError(true);
+      setError(error);
+    };
+
+    const resetErrorBoundary = () => {
+      setHasError(false);
+      setError(null);
+    };
+
+    window.addEventListener('error', handleErrors);
+    return () => window.removeEventListener('error', handleErrors);
+  }, []);
 
   const resetErrorBoundary = () => {
     setHasError(false);
     setError(null);
   };
 
-  /** You can render any custom fallback UI */
   if (hasError) {
     return (
       <div className="alert alert-danger">
-        <p>Couldn&apos;t open form. Encountered the following error:</p>
+        <p>Couldn't open form. Encountered the following error:</p>
         <pre>{error.message}</pre>
         <Button color="primary" variant="contained" onClick={resetErrorBoundary}>
           Refresh Form
