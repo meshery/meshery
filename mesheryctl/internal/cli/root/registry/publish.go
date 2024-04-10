d0624ef98eadcbe500c17ded4b24e4e3193e3111
Yash Sharma <yashsharma2572@gmail.com>
2024-04-10 22:20:49 +0000

    change var name
    Signed-off-by: Yash Sharma <yashsharma2572@gmail.com>

--- mesheryctl/internal/cli/root/registry/publish.go
+++ mesheryctl/internal/cli/root/registry/publish.go
@@ -114,11 +114,12 @@ mesheryctl registry publish website $CRED 1DZHnzxYWOlJ69Oguz4LkRVTFM79kC2tuvdwiz
 					utils.Log.Error(err)
 					return nil
 				}
-				publishAllModels := false
+				// If system is remote-provider publishing of models should not consider publishedToSite spreadsheet column value.
+				publishAllModel := false
 				if system == "remote-provider" {
-					publishAllModels = true
+					publishAllModel = true
 				}
-				err := modelCSVHelper.ParseModelsSheet(publishAllModels)
+				err := modelCSVHelper.ParseModelsSheet(!publishAllModel)
 				if err != nil {
 					utils.Log.Error(err)
 					return nil
