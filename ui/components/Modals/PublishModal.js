fc999e4aa8ecfac2f1b092008fff8188b08556e0
11happy <soni5happy@gmail.com>
2024-04-17 17:53:45 +0530

    update mesh models for publish schema
    Signed-off-by: 11happy <soni5happy@gmail.com>

--- ui/components/Modals/PublishModal.js
+++ ui/components/Modals/PublishModal.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from '../Modal';
 import PublicIcon from '@material-ui/icons/Public';
+import { getMeshModels } from '../../api/meshmodel';
+import { modifyRJSFSchema } from '../../utils/utils';
 import { publishSchema, publishUiSchema } from '@layer5/sistent';
 
 // This modal is used in MeshMap also
 export default function PublishModal(props) {
   const { open, title, handleClose, handleSubmit } = props;
+  const [setPublishSchema] = useState({});
+
+  useEffect(() => {
+    async function fetchMeshModels() {
+      try {
+        const { models } = await getMeshModels();
+        const modelNames = models?.map((model) => model.displayName) || [];
+        modelNames.sort(); // Sort model names
+
+        // Modify the schema to include mesh models
+        const modifiedSchema = modifyRJSFSchema(
+          publishSchema, // Use publishSchema as the base schema
+          'properties.compatibility.items.enum',
+          modelNames,
+        );
+
+        // Set the modified schema and UI schema
+        setPublishSchema({ rjsfSchema: modifiedSchema, uiSchema: publishUiSchema });
+      } catch (error) {
+        console.error('Error fetching mesh models:', error);
+      }
+    }
+
+    fetchMeshModels();
+  }, []);
+
   return (
     <Modal
       open={open}
