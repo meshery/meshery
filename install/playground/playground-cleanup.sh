a5885fa83d42e3651c72c62a072658d1b8c942dd
Jerod Culpepper <cpepper96@gmail.com>
2024-04-27 14:10:17 -0700

    updated to match other loop syntax
    Signed-off-by: Jerod Culpepper <cpepper96@gmail.com>

--- install/playground/playground-cleanup.sh
+++ install/playground/playground-cleanup.sh
@@ -4,7 +4,8 @@
 
 valid_namespaces=("kube-system" "default" "monitoring" "kube-flannel" "kube-node-lease" "kube-public" "meshery" "metallb-system" "projectcontour" "ingress-nginx" "layer5-cloud" "postgres")
 
-for ns in $(kubectl get ns -o jsonpath="{.items[*].metadata.name}"); do
+for ns in $(kubectl get ns -o jsonpath="{.items[*].metadata.name}"); 
+do
     if [[ ! " ${valid_namespaces[*]} " =~ [[:space:]]${ns}[[:space:]] ]]; then
         echo "Deleting all resources in namespace $ns"
         kubectl delete all --all -n $ns
