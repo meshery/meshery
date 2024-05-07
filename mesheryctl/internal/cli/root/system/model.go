5b1692b9d79de5896b649043d5911013eb431e13
Saurabh Kumar Singh <singh1203.ss@gmail.com>
2024-05-07 10:44:47 +0530

    Refactor: Remove unused code and update golden files for tests
    Signed-off-by: Saurabh Kumar Singh <singh1203.ss@gmail.com>

--- mesheryctl/internal/cli/root/system/model.go
+++ mesheryctl/internal/cli/root/system/model.go
@@ -224,10 +224,6 @@ mesheryctl system model view [model-name]
 		if err != nil {
 			return err
 		}
-		/* err = utils.IsServerRunning(mctlCfg.GetBaseMesheryURL())
-		if err != nil {
-			return err
-		} */
 		ctx, err := mctlCfg.GetCurrentContext()
 		if err != nil {
 			return err
