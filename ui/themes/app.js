468cb01193aa6987a76967caa5120372a879cc5a
captain-Akshay <akshay.sharma0503@gmail.com>
2024-05-14 21:16:18 +0530

    without css baseline
    Signed-off-by: captain-Akshay <akshay.sharma0503@gmail.com>

--- ui/themes/app.js
+++ ui/themes/app.js
@@ -280,7 +280,7 @@ darkTheme = {
     MuiCssBaseline: {
       '@global': {
         body: {
-          backgroundColor: '#303030 !important',
+          backgroundColor: '#303030',
           // scrollbarColor : "#6b6b6b #263238",
           '&::-webkit-scrollbar, & *::-webkit-scrollbar': {
             backgroundColor: '#1A1A1A',
@@ -536,7 +536,7 @@ theme = {
     MuiCssBaseline: {
       '@global': {
         body: {
-          backgroundColor: '#e9eff1 !important',
+          backgroundColor: '#e9eff1',
           // scrollbarColor : "#6b6b6b #263238",
           '&::-webkit-scrollbar, & *::-webkit-scrollbar': {
             backgroundColor: '#d7d7d7',
