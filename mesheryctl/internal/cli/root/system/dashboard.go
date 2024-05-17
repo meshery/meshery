8821aabfa915b3558842349ec91c3b7c4ccec17d
Jougan-0 <prasantmishra2018@gmail.com>
2024-05-17 22:36:40 +0530

    new warning for mesheryctl start w/signoff
    Signed-off-by: Jougan-0 <prasantmishra2018@gmail.com>

--- mesheryctl/internal/cli/root/system/dashboard.go
+++ mesheryctl/internal/cli/root/system/dashboard.go
@@ -227,6 +227,7 @@ mesheryctl system dashboard --skip-browser
 		}
 
 		if !skipBrowserFlag {
+			log.Warn("This flag is intended for use when Meshery is deployed on a Kubernetes platform.")
 			log.Info("Opening Meshery (" + currCtx.GetEndpoint() + ") in browser.")
 			err = utils.NavigateToBrowser(currCtx.GetEndpoint())
 			if err != nil {
