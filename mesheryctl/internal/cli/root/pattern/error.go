65d683b65c5ebe0a06ad22523d2a923c40c66185
Mohd Uzair <muzair.shaikh810@gmail.com>
2024-05-24 16:36:42 +0530

    Apply suggestions from code review
    Signed-off-by: Mohd Uzair <muzair.shaikh810@gmail.com>

--- mesheryctl/internal/cli/root/pattern/error.go
+++ mesheryctl/internal/cli/root/pattern/error.go
@@ -89,8 +89,8 @@ func ErrPatternManifest() error {
 func ErrPatternSourceType() error {
 	return errors.New(ErrPatternSourceTypeCode, errors.Alert,
 		[]string{"Source type for the design to import not specified"},
-		[]string{"Empty  source type
-		[]string{"Manifest source-type not provided"},
+		[]string{"Empty  source type detected
+		[]string{"Design source type not provided"},
 		[]string{"Provide one of the supported source type for the design to import. \n\n%v", errPatternMsg})
 }
 
