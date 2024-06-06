442fbb99fa93ce964102b5126c3d28b0163567d4
MUzairS15 <muzair.shaikh810@gmail.com>
2024-06-07 00:15:05 +0530

    enahnce provisioning operation event
    Signed-off-by: MUzairS15 <muzair.shaikh810@gmail.com>
    Signed-off-by: Christopher Kalule <ckalule7@gmail.com>

--- server/handlers/design_engine_handler.go
+++ server/handlers/design_engine_handler.go
@@ -169,17 +169,6 @@ func (h *Handler) PatternFileHandler(
 	ec := json.NewEncoder(rw)
 	_ = ec.Encode(response)
 }
-func mergeMsgs(msgs []string) string {
-	var finalMsgs []string
-
-	for _, msg := range msgs {
-		if msg != "" {
-			finalMsgs = append(finalMsgs, msg)
-		}
-	}
-
-	return strings.Join(finalMsgs, "\n")
-}
 
 func _processPattern(
 	ctx context.Context,
@@ -277,17 +266,14 @@ func _processPattern(
 			Add(func(data *stages.Data, err error, next stages.ChainStageNextFunction) {
 				data.Lock.Lock()
 				for k, v := range data.Other {
+					var key string
 					if strings.HasSuffix(k, stages.ProvisionSuffixKey) {
-						msg, ok := v.(string)
-						if ok {
-							sap.accumulatedMsgs = append(sap.accumulatedMsgs, msg)
-						}
+						key, _ = strings.CutSuffix(k, stages.ProvisionSuffixKey)
 					}
 					if k == stages.DryRunResponseKey {
-						if v != nil {
-							resp["dryRunResponse"] = v
-						}
+						key, _ = strings.CutSuffix(k, stages.DryRunResponseKey)
 					}
+					resp[key] = v
 				}
 				data.Lock.Unlock()
 				sap.err = err
@@ -296,7 +282,6 @@ func _processPattern(
 				Pattern: &pattern,
 				Other:   map[string]interface{}{},
 			})
-		resp["messages"] = mergeMsgs(sap.accumulatedMsgs)
 		return resp, sap.err
 	}
 	return internal(k8scontexts)
@@ -493,16 +478,16 @@ func getComponentFieldPathFromK8sFieldPath(path string) (newpath string) {
 	return fmt.Sprintf("%s.%s", "settings", path)
 }
 
-func (sap *serviceActionProvider) Provision(ccp stages.CompConfigPair) (string, error) { // Marshal the component
+func (sap *serviceActionProvider) Provision(ccp stages.CompConfigPair) ([]patterns.DeploymentMessagePerContext, error) { // Marshal the component
 	jsonComp, err := json.Marshal(ccp.Component)
 	if err != nil {
-		return "", fmt.Errorf("failed to serialize the data: %s", err)
+		return nil, fmt.Errorf("failed to serialize the data: %s", err)
 	}
 
 	// Marshal the configuration
 	jsonConfig, err := json.Marshal(ccp.Configuration)
 	if err != nil {
-		return "", fmt.Errorf("failed to serialize the data: %s", err)
+		return nil, fmt.Errorf("failed to serialize the data: %s", err)
 	}
 
 	for host := range ccp.Hosts {
@@ -544,7 +529,7 @@ func (sap *serviceActionProvider) Provision(ccp stages.CompConfigPair) (string, 
 			addr,
 		)
 		if err != nil {
-			return "", fmt.Errorf("error creating a mesh client: %v", err)
+			return nil, fmt.Errorf("error creating a mesh client: %v", err)
 		}
 		defer func() {
 			_ = mClient.Close()
@@ -576,11 +561,26 @@ func (sap *serviceActionProvider) Provision(ccp stages.CompConfigPair) (string, 
 			OamConfig:   string(jsonConfig),
 			KubeConfigs: kconfigs,
 		})
-
-		return resp.GetMessage(), err
+		sucess := err == nil
+		return []patterns.DeploymentMessagePerContext{
+			{
+				ServerName: host.Hostname,
+				Location:   fmt.Sprintf("%s:%s", host.Hostname, strconv.Itoa(host.Port)),
+				Summary: []patterns.DeploymentMessagePerComp{
+					{
+						Kind:       ccp.Component.Kind,
+						CompName:   ccp.Component.Name,
+						DesignName: sap.patternName,
+						Success:    sucess,
+						Message:    resp.GetMessage(),
+					},
+				},
+			},
+		}, err
 	}
 
-	return "", nil
+	// send error for no hosts found for the component
+	return nil, nil
 }
 
 func (sap *serviceActionProvider) Persist(name string, svc core.Service, isUpdate bool) error {
--- server/models/pattern/patterns/patterns.go
+++ server/models/pattern/patterns/patterns.go
@@ -6,15 +6,11 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/gofrs/uuid"
-
 	"github.com/layer5io/meshery/server/models"
 	"github.com/layer5io/meshery/server/models/pattern/patterns/k8s"
-	"github.com/layer5io/meshkit/models/events"
 	"github.com/layer5io/meshkit/models/meshmodel/core/v1beta1"
 	"github.com/layer5io/meshkit/models/oam/core/v1alpha1"
 	"github.com/layer5io/meshkit/utils/kubernetes"
-	"github.com/spf13/viper"
 )
 
 type DeploymentMessagePerComp struct {
@@ -23,6 +19,8 @@ type DeploymentMessagePerComp struct {
 	Success    bool
 	DesignName string
 	Message    string
+	Metadata   map[string]interface{}
+	Error      error
 }
 
 type DeploymentMessagePerContext struct {
@@ -34,8 +32,6 @@ type DeploymentMessagePerContext struct {
 func ProcessOAM(kconfigs []string, oamComps []string, oamConfig string, isDel bool, patternName string, ec *models.Broadcast, userID string, provider models.Provider, hostname v1beta1.IHost, skipCrdAndOperator, upgradeExistingRelease bool) ([]DeploymentMessagePerContext, error) {
 	var comps []v1beta1.Component
 	var config v1alpha1.Configuration
-	mesheryInstanceID, _ := viper.Get("INSTANCE_ID").(*uuid.UUID)
-	userUUID, _ := uuid.FromString(userID)
 	action := "deploy"
 	if isDel {
 		action = "undeploy"
@@ -77,75 +73,42 @@ func ProcessOAM(kconfigs []string, oamComps []string, oamConfig string, isDel bo
 			for _, comp := range comps {
 
 				if !skipCrdAndOperator && hostname != nil && comp.Spec.Model != (v1beta1.Kubernetes{}).String() {
-					var description string
-					severity := events.Informational
-					if !isDel {
-						description = fmt.Sprintf("Detected dependency for %s/%s, deploying dependent model %s.", patternName, comp.Name, comp.Spec.Model)
-						msgs = append(msgs, fmt.Sprintf("Deployed %s: %s", comp.Spec.Type, comp.Name))
-					} else {
-						description = fmt.Sprintf("Detected dependency for %s/%s, undeploying dependent model %s.", patternName, comp.Name, comp.Spec.Model)
-						msgs = append(msgs, fmt.Sprintf("Deleted %s: %s", comp.Spec.Type, comp.Name))
-					}
+					var deploymentMsg DeploymentMessagePerComp
+
+					// if !isDel {
+					// 	description = fmt.Sprintf("Detected dependency for %s/%s, deploying dependent model %s.", patternName, comp.Name, comp.Spec.Model)
+					// } else {
+					// 	description = fmt.Sprintf("Detected dependency for %s/%s, undeploying dependent model %s.", patternName, comp.Name, comp.Spec.Model)
+					// }
+
 					fmt.Println("host: ", hostname, " comp: ", comp.Name, "type: ", comp.Spec.Type, comp)
 					// Deploys resources that are required inside cluster for successful deployment of the design.
 					result, err := hostname.HandleDependents(comp, kcli, !isDel, upgradeExistingRelease)
 					// If dependencies were not resolved fail forward, there can be case that dependency already exist in the cluster.
-
-					eventMetadata := map[string]interface{}{
-						"summary": result,
-					}
-
+					deploymentMsg.Message = result
 					if err != nil {
-						eventMetadata["error"] = err
-						severity = events.Error
+						deploymentMsg.Success = false
 						errs = append(errs, err)
+						deploymentMsg.Error = err
 					}
-
-					event := events.NewEvent().FromSystem(*mesheryInstanceID).WithSeverity(severity).WithCategory("pattern").WithAction(action).WithDescription(description).FromUser(userUUID).WithMetadata(eventMetadata).Build()
-					err = provider.PersistEvent(event)
-					if err != nil {
-						evt := events.NewEvent().FromSystem(*mesheryInstanceID).WithSeverity(events.Alert).WithCategory("event").WithAction("persist").WithDescription("Failed persisting events").FromUser(userUUID).Build()
-						go ec.Publish(userUUID, evt)
-					}
-					go ec.Publish(userUUID, event)
 				}
 				//All other components will be handled directly by Kubernetes
 				//TODO: Add a Mapper utility function which carries the logic for X hosts can handle Y components under Z circumstances.
 
-				severity := events.Informational
-				eventMetadata := make(map[string]interface{})
-				if err := k8s.Deploy(kcli, comp, config, isDel); err != nil {
-					errs = append(errs, err)
-					severity = events.Error
-					eventMetadata["error"] = err
-					eventMetadata["summary"] = DeploymentMessagePerComp{
-						Kind:       comp.Spec.Type,
-						CompName:   comp.Name,
-						Success:    false,
-						DesignName: patternName,
-					}
-					var description string
-
-					description = fmt.Sprintf("Error %sing %s/%s", action, patternName, comp.Name)
-
-					event := events.NewEvent().FromSystem(*mesheryInstanceID).WithSeverity(severity).WithCategory("pattern").WithAction(action).WithDescription(description).FromUser(userUUID).WithMetadata(eventMetadata).Build()
-					err := provider.PersistEvent(event)
-					if err != nil {
-						evt := events.NewEvent().FromSystem(*mesheryInstanceID).WithSeverity(severity).WithCategory("event").WithAction("persist").WithDescription("Failed persisting events").FromUser(userUUID).Build()
-						go ec.Publish(userUUID, evt)
-					}
-
-					go ec.Publish(userUUID, event)
-					continue
-				}
-
-				msgsPerComp = append(msgsPerComp, DeploymentMessagePerComp{
+				_msg := DeploymentMessagePerComp{
 					Kind:       comp.Spec.Type,
 					CompName:   comp.Name,
 					Success:    true,
 					DesignName: patternName,
-				})
+					Message:    fmt.Sprintf("%sed %s/%s", action, patternName, comp.Name),
+				}
 
+				if err := k8s.Deploy(kcli, comp, config, isDel); err != nil {
+					_msg.Message = fmt.Sprintf("Error %sing %s/%s", action, patternName, comp.Name)
+					_msg.Error = err
+
+				}
+				msgsPerComp = append(msgsPerComp, _msg)
 			}
 
 			msgsMx.Lock()
