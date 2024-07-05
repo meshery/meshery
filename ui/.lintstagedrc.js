5669cdf70bfecd9d8f1dbbc81105b839271535ee
Tharun T <tharun242424@gmail.com>
2024-07-05 22:16:19 +0530

    fix: lint-staged and use eslint
    Signed-off-by: Tharun T <tharun242424@gmail.com>

--- ui/.lintstagedrc.js
+++ ui/.lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Lint & Prettify TS and JS files
-  '**/*.(ts|tsx|js|jsx)': (filenames) => [`npm run lint:fix`, `npm run format`],
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
 
   // Prettify only Markdown and JSON files
   '**/*.(md|json)': (filenames) => `npx prettier --write ${filenames.join(' ')}`,
