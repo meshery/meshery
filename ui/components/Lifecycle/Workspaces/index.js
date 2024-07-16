f01b79b7e45c2c55dfca5b6a244f7be175285273
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2024-07-16 19:11:07 +0530

    add maxwidth property to transferlist
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>

--- ui/components/Lifecycle/Workspaces/index.js
+++ ui/components/Lifecycle/Workspaces/index.js
@@ -714,6 +714,7 @@ const Workspaces = ({ organization, classes }) => {
               closeModal={handleAssignEnvironmentModalClose}
               title={`Assign Environments to ${environmentAssignWorkspace.name}`}
               headerIcon={<EnvironmentIcon height="2rem" width="2rem" fill="white" />}
+              maxWidth="md"
             >
               <ModalBody>
                 <TransferList
@@ -773,6 +774,7 @@ const Workspaces = ({ organization, classes }) => {
               closeModal={handleAssignDesignModalClose}
               title={`Assign Designs to ${designAssignWorkspace.name}`}
               headerIcon={<DesignsIcon height="2rem" width="2rem" fill="#ffffff" />}
+              maxWidth="md"
             >
               <ModalBody>
                 <TransferList
