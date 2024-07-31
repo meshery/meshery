3ffa57fe4d94b063a6c5bb8e831e1ce70fe1021d
Nithish Karthik <nithishkarthik01@gmail.com>
2024-08-01 00:40:47 +0530

    refactor: add more information into errors
    Signed-off-by: Nithish Karthik <nithishkarthik01@gmail.com>

--- server/models/registration/error.go
+++ server/models/registration/error.go
@@ -10,36 +10,73 @@ const (
 	ErrDirPkgUnitParseFailCode = "replace_me"
 	ErrGetEntityCode = "replace_me"
 	ErrRegisterEntityCode = "replace_me"
-	ErrInvalidModelDefinitionCode = "replace_me"
 	ErrImportFailureCode = "replace_me"
 	ErrMissingRegistrantCode = "replace_me"
 	ErrSeedingComponentsCode = "replace-me"
 )
 
 func ErrSeedingComponents(err error) error {
-	return errors.New(ErrSeedingComponentsCode, errors.Alert, []string{"Failed to seed meshery components."}, []string{err.Error()}, []string{}, []string{"Make sure the models being seeded are valid."})
+	return errors.New(
+		ErrSeedingComponentsCode,
+		errors.Alert,
+		[]string{"Failed to register the given models into meshery's registry"},
+		[]string{err.Error()},
+		[]string{"Given models may not be in accordance with Meshery's schema", "Internal(OS level) error while reading files" },
+		[]string{"Make sure the models being seeded are valid in accordance with Meshery's schema", "If it is an internal error, please try again after some time"},
+	)
 }
 
 func ErrMissingRegistrant() error {
-	return errors.New(ErrMissingRegistrantCode, errors.Alert, []string{"Cannot register model without a registrant."}, []string{"Model does not contain registrant information. Models are always registered in the context of registrants."}, []string{}, []string{})
+	return errors.New(
+		ErrMissingRegistrantCode,
+		errors.Alert,
+		[]string{"Meshery models are always registered in context of a registrant"},
+		[]string{""},
+		[]string{"Given model might not contain information about the registrant"},
+		[]string{"Make sure that the registrant information is present in the model definition"},
+	)
 }
 
 func ErrRegisterEntity(err error, name, entity string) error {
-	return errors.New(ErrRegisterEntityCode, errors.Alert, []string{fmt.Sprintf("failed to register %s %s", name, entity)}, []string{err.Error()}, []string{fmt.Sprintf("%s definition violates the definition schema", entity), fmt.Sprintf("%s might be missing model details", entity)}, []string{fmt.Sprintf("ensure the %s definition follows the correct schema", entity), fmt.Sprintf("ensure %s definition belongs to correct model", entity)})
+	return errors.New(
+		ErrRegisterEntityCode,
+		errors.Alert,
+		[]string{fmt.Sprintf("Failed to register an entity of name: %s and type: %s into Meshery's registry", name, entity)},
+		[]string{err.Error()},
+		[]string{fmt.Sprintf("%s definition might be violating the definition schema", entity), fmt.Sprintf("%s might be missing model details", entity)},
+		[]string{fmt.Sprintf("ensure the %s definition follows the correct schema", entity), fmt.Sprintf("ensure %s definition belongs to correct model", entity)},
+	)
 }
 
 func ErrGetEntity(err error) error {
-	return errors.New(ErrGetEntityCode, errors.Alert, []string{err.Error()}, []string{err.Error()}, []string{}, []string{})
+	return errors.New(
+		ErrGetEntityCode,
+		errors.Alert,
+		[]string{"Could not parse the given data into any Meshery entity"},
+		[]string{err.Error()},
+		[]string{"Entity definition might not be in accordance with it's corresponding schema", "Might be an invalid/unsupported schemaVersion"},
+		[]string{"Ensure that the definition given is in accordance with it's schema", "Ensure that the schemaVersion used is valid and is supported by Meshery"},
+	)
 }
 
 func ErrDirPkgUnitParseFail(dirpath string, err error) error {
-	return errors.New(ErrDirPkgUnitParseFailCode, errors.Alert, []string{fmt.Sprintf("Directory %s is not a valid packaging unit for registration", dirpath)}, []string{fmt.Sprintf("Could not parse the given directory %s into a packaging unit:  %s", dirpath, err.Error())}, []string{}, []string{} )
-}
-
-func ErrInvalidModelDefinition(path string, err error) error {
-	return errors.New(ErrInvalidModelDefinitionCode, errors.Alert, []string{fmt.Sprintf("The file at path %s is not a valid Model definition", path)}, []string{fmt.Sprintf("Could not parse Model:  %s", err.Error())}, []string{}, []string{} )
+	return errors.New(
+		ErrDirPkgUnitParseFailCode,
+		errors.Alert,
+		[]string{fmt.Sprintf("Directory at path: %s cannot be registered into Meshery", dirpath)},
+		[]string{fmt.Sprintf(err.Error())},
+		[]string{"The directory might not have a valid model definition", "Might be some internal issues while walking the file tree"},
+		[]string{"Make sure that there is a valid model definition present in the directory. Meshery's registration pipeline currently does not support nested models, therefore the behaviour might be unexpected if it contains nested models.", "If there is an internal error, please try again after some time"},
+	)
 }
 
 func ErrImportFailure(hostname string, failedMsg string) error {
-	return errors.New(ErrImportFailureCode, errors.Alert, []string{}, []string{fmt.Sprintf("The import process for a registrant %s encountered difficulties,due to which %s. Specific issues during the import process resulted in certain entities not being successfully registered in the table.", hostname, failedMsg)}, []string{}, []string{fmt.Sprintf("Visit docs with the error code %s", "https://docs.meshery.io/reference/error-codes")})
+	return errors.New(
+		ErrImportFailureCode,
+		errors.Alert,
+		[]string{},
+		[]string{fmt.Sprintf("The import process for a registrant %s encountered difficulties,due to which %s. Specific issues during the import process resulted in certain entities not being successfully registered in the table.", hostname, failedMsg)},
+		[]string{},
+		[]string{fmt.Sprintf("Visit docs with the error code %s", "https://docs.meshery.io/reference/error-codes"),
+	})
 }
