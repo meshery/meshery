3b52197dcf425cec2e449484843fd69cb297241f
Senthil Athiban <senthilathiban0@gmail.com>
2024-08-07 13:32:21 +0530

    (feat): create metricsApi RTK query
    Signed-off-by: Senthil Athiban <senthilathiban0@gmail.com>

---
+++ ui/rtk-query/metrics.js
@@ -0,0 +1,49 @@
+import { api } from './index';
+
+const TAGS = {
+  METRICS: 'metrics',
+};
+
+export const metricsApi = api
+  .enhanceEndpoints({
+    addTagTypes: [TAGS.METRICS],
+  })
+  .injectEndpoints({
+    endpoints: (builder) => ({
+      pingGrafana: builder.query({
+        query: () => 'telemetry/metrics/grafana/ping',
+        providesTags: () => [{ type: TAGS.METRICS, id: 'GRAFANA' }],
+      }),
+      pingPrometheus: builder.query({
+        query: () => 'telemetry/metrics/pin',
+        providesTags: () => [{ type: TAGS.METRICS, id: 'PROMETHEUS' }],
+      }),
+      configureGrafana: builder.mutation({
+        query: (queryArg) => ({
+          url: 'telemetry/metrics/grafana/config',
+          method: 'POST',
+          credentials: 'include',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
+          body: queryArg.params,
+        }),
+        invalidatesTags: [{ type: TAGS.METRICS, id: 'GRAFANA' }],
+      }),
+      configurePrometheus: builder.mutation({
+        query: (queryArg) => ({
+          url: '/api/telemetry/metrics/config',
+          method: 'POST',
+          credentials: 'include',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' },
+          body: queryArg.params,
+        }),
+        invalidatesTags: [{ type: TAGS.METRICS, id: 'PROMETHEUS' }],
+      }),
+    }),
+  });
+
+export const {
+  usePingGrafanaQuery,
+  usePingPrometheusQuery,
+  useConfigureGrafanaMutation,
+  useConfigurePrometheusMutation,
+} = metricsApi;
