948276b41dd4e11967ebb896c27a1a6e0b74ea25
Jerens Soerjadi Lensun <jerensslensun@gmail.com>
2024-08-09 17:03:45 +0800

    test: add automatic comment for e2e test
    Signed-off-by: Jerens Soerjadi Lensun <jerensslensun@gmail.com>

--- ui/playwright.config.js
+++ ui/playwright.config.js
@@ -30,6 +30,8 @@ module.exports = defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: './tests/reporter.js',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
