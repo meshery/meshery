09321db6a0ee4e2443a1c553d795ca8e13380b96
Senthil Athiban <senthilathiban0@gmail.com>
2024-08-13 12:34:46 +0530

    (feat): migrate RTK query for user pref
    Signed-off-by: Senthil Athiban <senthilathiban0@gmail.com>

--- ui/pages/extensions.js
+++ ui/pages/extensions.js
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { toggleCatalogContent } from '../lib/store';
 import Head from 'next/head';
-import dataFetch from '../lib/data-fetch';
 import { EXTENSIONS } from '../utils/Enum';
 import { extensionStyles as styles } from '../css/icons.styles';
 import { Adapters } from '../components/extensions';
@@ -17,6 +16,7 @@ import CAN from '@/utils/can';
 import { keys } from '@/utils/permission_constants';
 import { UsesSistent } from '@/components/SistentWrapper';
 import { CatalogIcon } from '@layer5/sistent';
+import { useGetUserPrefQuery, useUpdateUserPrefMutation } from '@/rtk-query/user';
 
 const INITIAL_GRID_SIZE = { lg: 6, md: 12, xs: 12 };
 
@@ -169,6 +169,14 @@ const Extensions = ({ classes, toggleCatalogContent, capabilitiesRegistry }) => 
   const [extensionPreferences, setExtensionPreferences] = useState({});
   const [hasAccessToMeshMap, setHasAccessToMeshMap] = useState(false);
   const { notify } = useNotification();
+  const [updateUserPref] = useUpdateUserPrefMutation();
+
+  const {
+    data: userData,
+    isSuccess: userDataFetched,
+    isError: isUserError,
+    error: userError,
+  } = useGetUserPrefQuery();
 
   const handleToggle = () => {
     toggleCatalogContent({ catalogVisibility: !catalogContent });
@@ -176,42 +184,32 @@ const Extensions = ({ classes, toggleCatalogContent, capabilitiesRegistry }) => 
     handleCatalogPreference(!catalogContent);
   };
 
+  const fetchUser = () => {
+    if (userDataFetched && userData) {
+      setExtensionPreferences(userData?.usersExtensionPreferences);
+      setCatalogContent(userData?.usersExtensionPreferences?.catalogContent);
+    } else if (isUserError) {
+      console.log(userError);
+    }
+  };
   useEffect(() => {
-    dataFetch(
-      '/api/user/prefs',
-      {
-        method: 'GET',
-        credentials: 'include',
-      },
-      (result) => {
-        if (result) {
-          setExtensionPreferences(result?.usersExtensionPreferences);
-          setCatalogContent(result?.usersExtensionPreferences?.catalogContent);
-        }
-      },
-      (err) => console.error(err),
-    );
-  }, []);
+    fetchUser();
+  }, [userData]);
 
   const handleCatalogPreference = (catalogPref) => {
     let body = Object.assign({}, extensionPreferences);
     body['catalogContent'] = catalogPref;
-
-    dataFetch(
-      '/api/user/prefs',
-      {
-        method: 'POST',
-        credentials: 'include',
-        body: JSON.stringify({ usersExtensionPreferences: body }),
-      },
-      () => {
+    updateUserPref({ body })
+      .unwrap()
+      .then(() => {
         notify({
           message: `Catalog Content was ${catalogPref ? 'enab' : 'disab'}led`,
           event_type: EVENT_TYPES.SUCCESS,
         });
-      },
-      (err) => console.error(err),
-    );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   useEffect(() => {
