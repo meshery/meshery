42aad4c3684c5ff55e4690d3ae3a7238b20e3aaf
Senthil Athiban <senthilathiban0@gmail.com>
2024-08-19 17:56:08 +0530

    (fix): typo
    Signed-off-by: Senthil Athiban <senthilathiban0@gmail.com>

--- ui/tests/e2e/mesherySettingsPerformance.spec.js
+++ ui/tests/e2e/mesherySettingsPerformance.spec.js
@@ -4,13 +4,12 @@ import { ENV } from './env';
 test.describe('meshery settings performance', () => {
   test('Performance tab', async ({ page }) => {
     // Navigate to "User Preferences" page
-
     await page.goto(`${ENV.MESHERY_SERVER_URL}/user/preferences`);
 
     // Navigate to "Performance" tab
     await page.getByRole('tab', { name: 'Performance' }).click();
 
-    // Fill concurrent request, Queris per secons, duration, and Generator
+    // Fill concurrent requests, Queris per seconds, Time duration, and Generator
     await page.getByLabel('Concurrent requests *').click();
     await page.getByLabel('Concurrent requests *').fill('3');
     await page.getByLabel('Queries per second *').click();
