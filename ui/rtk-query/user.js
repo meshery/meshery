659e2f535f3c48f99b61118677b7f0b7291c116c
Senthil Athiban <senthilathiban0@gmail.com>
2024-08-21 16:38:57 +0530

    (fix): use queryArg.body in RTK query
    Signed-off-by: Senthil Athiban <senthilathiban0@gmail.com>

--- ui/rtk-query/user.js
+++ ui/rtk-query/user.js
@@ -23,7 +23,7 @@ const userApi = api.injectEndpoints({
       query: (queryArg) => ({
         url: 'user/prefs',
         method: 'POST',
-        body: queryArg,
+        body: queryArg.body,
         credentials: 'include',
       }),
     }),
