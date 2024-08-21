8b4ffb5af58f95e69880f3462191a897137c109e
Senthil Athiban <senthilathiban0@gmail.com>
2024-08-21 13:00:24 +0530

    (test): add e2e test for settings
    Signed-off-by: Senthil Athiban <senthilathiban0@gmail.com>

--- ui/tests/e2e/settings.spec.js
+++ ui/tests/e2e/settings.spec.js
@@ -50,6 +50,44 @@ test.describe('Settings Page Tests', () => {
     verifyAdapterResBody(body);
   });
 
+  test('Adapters, Metrics, Registry, Reset tabs are displayed', async ({ page }) => {
+    // Navigate to "Adapters" tab
+    await page.getByRole('tab', { name: 'Adapters' }).click();
+
+    // Mesh Adapter URL and Available Adapters input elements are displayed
+    await expect(page.locator('label').filter({ hasText: 'Mesh Adapter URL' })).toBeVisible();
+    await expect(page.locator('label').filter({ hasText: 'Available Adapters' })).toBeVisible();
+
+    // Navigate to "Metrics" tab
+    await page.getByRole('tab', { name: 'Metrics' }).click();
+
+    // Navigate to "Grafana" sub-tab
+    await page.getByRole('tab', { name: 'Grafana' }).click();
+
+    // Grafana Base URL input elements are displayed
+    await expect(page.locator('label').filter({ hasText: 'Grafana Base URL' })).toBeVisible();
+
+    // Navigate to "Prometheus" sub-tab
+    await page.getByRole('tab', { name: 'Prometheus' }).click();
+
+    // Prometheus Base URL subt-tab are displayed
+    await expect(page.locator('label').filter({ hasText: 'Prometheus Base URL' })).toBeVisible();
+
+    // Navigate to "Registry" tab
+    await page.getByRole('tab', { name: 'Registry' }).click();
+
+    // "Mesh Model" component is displayed
+    await expect(page.locator('[data-test="workloads"]')).toBeVisible();
+
+    /// Navigate to "DataBase summary" tab
+    await page.getByRole('tab', { name: 'Reset' }).click();
+
+    // "DataBase summary table" is displayed
+    await expect(page.getByRole('button', { name: 'RESET DATABASE' })).toBeVisible();
+    await expect(page.getByRole('columnheader', { name: 'Name' })).toBeVisible();
+    await expect(page.getByRole('columnheader', { name: 'Count' })).toBeVisible();
+  });
+
   test('Connect to Meshery Istio Adapter and configure it', async ({ page }) => {
     // Navigate to 'Adapters' tab
     await page.getByRole('tab', { name: 'Adapters', exact: true }).click({ force: true });
