8fd10be438f2eb5e0a68f353ca4de20e4072dc84
MUzairS15 <muzair.shaikh810@gmail.com>
2024-08-28 15:17:04 +0530

    fix design drawer not updating with latest design
    Signed-off-by: MUzairS15 <muzair.shaikh810@gmail.com>

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -259,10 +259,9 @@ func main() {
 		PrometheusClient:         models.NewPrometheusClient(&log),
 		PrometheusClientForQuery: models.NewPrometheusClientWithHTTPClient(&http.Client{Timeout: time.Second}, &log),
 
-		ApplicationChannel:        models.NewBroadcaster(),
-		PatternChannel:            models.NewBroadcaster(),
-		FilterChannel:             models.NewBroadcaster(),
-		EventBroadcaster:          models.NewBroadcaster(),
+		PatternChannel:            models.NewBroadcaster("Patterns"),
+		FilterChannel:             models.NewBroadcaster("Filters"),
+		EventBroadcaster:          models.NewBroadcaster("Events"),
 		DashboardK8sResourcesChan: models.NewDashboardK8sResourcesHelper(),
 		MeshModelSummaryChannel:   mesherymeshmodel.NewSummaryHelper(),
 
