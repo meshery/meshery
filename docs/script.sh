ce6d1c59fbc15d79ef9fa74dd12dc6f56272b80b
miacycle <184569369+miacycle@users.noreply.github.com>
2024-09-03 14:28:34 -0500

    docs: update archive script
    Signed-off-by: miacycle <184569369+miacycle@users.noreply.github.com>

--- docs/script.sh
+++ docs/script.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # This is a version archive script, executed when a new minor Meshery release is made.
+# A "minor" release like v0.7.0 or v0.8.0
 
 if [ -f site-dir.zip ]; then
     rm -rf site-dir.zip
