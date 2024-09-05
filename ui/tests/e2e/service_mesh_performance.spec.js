04d57b04f6e4896e09204418889ff48c70dbe079
Yash Sharma <68152307+yash37158@users.noreply.github.com>
2024-09-06 03:24:21 +0530

    Fixing es-lint issue
    Signed-off-by: Yash Sharma  <68152307+yash37158@users.noreply.github.com>

--- ui/tests/e2e/service_mesh_performance.spec.js
+++ ui/tests/e2e/service_mesh_performance.spec.js
@@ -1,6 +1,5 @@
 import { test as base, expect } from '@playwright/test';
 import { PerformancePage } from './fixtures/performancePage';
-import { DYNAMIC_TIMEOUTS } from './delays';
 import { ENV } from './env';
 
 export const test = base.extend({
