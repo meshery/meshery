2152796e8d7d83940832cdcd6dda40c7078d4ec7
Ajay Singh <37625112+Ajay-singh1@users.noreply.github.com>
2024-09-05 18:41:39 +0530

    Update app.js
    Signed-off-by: Ajay Singh <37625112+Ajay-singh1@users.noreply.github.com>

--- ui/themes/app.js
+++ ui/themes/app.js
@@ -1,4 +1,4 @@
-import { createTheme } from '@material-ui/core/styles';
+import { createTheme } from '@mui/material/styles';
 import { blueGrey } from '@material-ui/core/colors';
 import { iconMedium } from '../css/icons.styles';
 
