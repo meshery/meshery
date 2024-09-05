796fbbbc11e66a89e77d0472fe6ba6ede3e45518
yash37158 <yash37158@gmail.com>
2024-09-05 17:29:39 +0530

    Injecting the datatest-id attribute
    Signed-off-by: yash37158 <yash37158@gmail.com>

--- ui/tests/e2e/fixtures/performancePage.js
+++ ui/tests/e2e/fixtures/performancePage.js
@@ -17,6 +17,7 @@ export class PerformancePage {
 
   async goToProfiles() {
     await this.page.goto(`${ENV.MESHERY_SERVER_URL}/performance/profiles`);
+    await expect(this.page).toHaveURL(`${ENV.MESHERY_SERVER_URL}/performance/profiles`);
   }
 
   async createPerformanceProfile(profileName) {
@@ -32,6 +33,7 @@ export class PerformancePage {
       await this.fillInput(this.page.getByRole('textbox', { name: 'Duration' }), '15s');
       await expect(this.page.getByRole('button', { name: 'Run Test', exact: true })).toBeVisible();
       await this.page.getByRole('button', { name: 'Run Test', exact: true }).click();
+      // Check for notification visibility
       const notification = await this.page.locator('text=Initiating load test . . .').first();
       await expect(notification).toBeVisible();
     } else {
@@ -50,21 +52,18 @@ export class PerformancePage {
   async viewPerformanceProfileResult(profileName) {
     await this.page.waitForSelector(`text=${profileName}`, { state: 'visible' });
     await this.page.click(`button:has-text("View Results")`);
-    const graphVisible = await this.page.getByTestId('sentinelStart').isVisible();
+    await this.page.waitForSelector('button[aria-label="more"]', { state: 'visible' });
+    await this.page.click('button[aria-label="more"]');
+
+    await this.page.evaluate(() => {
+      const sentinelStartDiv = document.querySelector('div[data-testid="sentinelStart"]');
+      if (sentinelStartDiv) {
+        sentinelStartDiv.setAttribute('data-testid', 'sentinel-graph');
+      }
+    });
+
+    const graphVisible = await this.page.getByTestId('sentinel-graph').isVisible();
     expect(graphVisible);
-  }
-
-  async viewPerformanceProfileConfiguration(profileName) {
-    await this.page.click(`div:has-text("${profileName}")`);
-    await this.page
-      .locator(
-        'svg path[d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 00-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"]',
-      )
-      .click();
-  }
-
-  async fillInput(locator, value) {
-    await locator.fill(value);
   }
 
   async runPerformanceTest(profileName) {
@@ -79,7 +78,25 @@ export class PerformancePage {
     await this.fillInput(this.page.getByRole('textbox', { name: 'Duration' }), '15s');
     await expect(this.page.getByRole('button', { name: 'Run Test', exact: true })).toBeVisible();
     await this.page.getByRole('button', { name: 'Run Test', exact: true }).click();
+    // Check for notification visibility
     const notification = await this.page.locator('text=Initiating load test . . .').first();
     await expect(notification).toBeVisible();
+  }
+
+  async viewPerformanceProfileConfiguration(profileName) {
+    await this.page.click(`div:has-text("${profileName}")`);
+    await this.page.evaluate(() => {
+      const editIcon = document.querySelector(
+        'svg path[d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 00-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"]',
+      );
+      if (editIcon) {
+        editIcon.closest('svg').setAttribute('data-testid', 'edit-icon');
+      }
+    });
+    await this.page.getByTestId('edit-icon').click();
+  }
+
+  async fillInput(locator, value) {
+    await locator.fill(value);
   }
 }
