33fcb8bc90f95103c41b6f4f4ac8ba35e1522239
lokesh-katari <lokeshkatari921@outlook.com>
2024-09-24 22:31:32 +0530

    fixed the id not passing correctly form the header
    Signed-off-by: lokesh-katari <lokeshkatari921@outlook.com>

--- ui/components/Header.js
+++ ui/components/Header.js
@@ -248,8 +248,8 @@ const K8sContextConnectionChip_ = ({
           )}
           <_ConnectionChip
             title={ctx?.name}
-            onDelete={onDelete ? () => onDelete(ctx.name, ctx.connection_id) : null}
-            handlePing={() => ping(ctx.name, ctx.server, ctx.connection_id)}
+            onDelete={onDelete ? () => onDelete(ctx.name, ctx.id) : null}
+            handlePing={() => ping(ctx.name, ctx.server, ctx.id)}
             iconSrc={
               connectionMetadataState && connectionMetadataState[CONNECTION_KINDS.KUBERNETES]?.icon
                 ? `/${connectionMetadataState[CONNECTION_KINDS.KUBERNETES]?.icon}`
