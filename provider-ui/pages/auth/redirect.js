b701cc373628ff5f2d9be6c0be38be425ace4883
MUzairS15 <muzair.shaikh810@gmail.com>
2024-10-05 00:31:33 +0530

    support issuing of session from remote provider
    Signed-off-by: MUzairS15 <muzair.shaikh810@gmail.com>

--- provider-ui/pages/auth/redirect.js
+++ provider-ui/pages/auth/redirect.js
@@ -1,23 +1,25 @@
 
 import { useEffect } from "react";
+import Router from 'next/router';
+
 
 const Redirect = () => {
-    useEffect(() => {
-        const params = new URLSearchParams(window.location.search);
-        const returnTo = params.get("return_to");
-        let redirectURL;
-        if (returnTo) {
-            redirectURL = new URL(`${returnTo}?${params.toString()}`)
-        } else {
-            redirectURL = new URL(`https://meshery.layer5.io/?${params.toString()}`)
-        }
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const returnTo = params.get("return_to");
+    let redirectURL;
+    if (returnTo) {
+      redirectURL = new URL(`${returnTo}?${params.toString()}`)
+    } else {
+      redirectURL = new URL(`https://meshery.layer5.io/?${params.toString()}`)
+    }
 
-        window.location.href = redirectURL.toString();
+    window.location.href = redirectURL.toString();
 
-    }, [])
-    return (
-        <></>
-    )
+  }, [])
+  return (
+    <></>
+  )
 }
 
 export default Redirect;
