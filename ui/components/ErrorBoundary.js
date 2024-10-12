54b4eeb96f43306f93d47e94a53ddbe1660355a9
THEBOSS0369 <anujkumsharma9876@gmail.com>
2024-10-12 06:01:04 +0000

    migrated error boundary to functional coomponent
    Signed-off-by: THEBOSS0369 <anujkumsharma9876@gmail.com>

--- ui/components/ErrorBoundary.js
+++ ui/components/ErrorBoundary.js
@@ -1,41 +1,51 @@
-import { Button } from '@material-ui/core';
-import React from 'react';
+import { Button } from '@mui/material';
+import React, { useState, useEffect } from 'react';
 
 /**
  * ErrorBoundary is a React component that catches JavaScript errors in its child components and renders a fallback UI when an error occurs.
  * It should be used as a wrapper around components that might throw errors.
  * @deprecated use error boundary from sistent instead
  */
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false, error: null };
-  }
+const ErrorBoundary = ({ children }) => {
+  const [hasError, setHasError] = useState(false);
+  const [error, setError] = useState(null);
 
-  /** Update state so the next render will show the fallback UI. */
-  static getDerivedStateFromError(error) {
-    return { hasError: true, error: error };
-  }
-
-  resetErrorBoundary = () => {
-    this.setState({ hasError: false, error: null });
+  const resetErrorBoundary = () => {
+    setHasError(false);
+    setError(null);
   };
 
-  /** You can render any custom fallback UI */
-  render() {
-    if (this.state.hasError) {
-      return (
-        <div className="alert alert-danger">
-          <p>Couldn&apos;t open form. Encountered the following error:</p>
-          <pre>{this.state.error.message}</pre>
-          <Button color="primary" variant="contained" onClick={this.resetErrorBoundary}>
-            Refresh Form
-          </Button>
-        </div>
-      );
+  useEffect(() => {
+    if (hasError && error) {
+      // Log the error, send to analytics, etc.
+      console.error(error);
     }
-    return this.props.children;
+  }, [hasError, error]);
+
+  // React Error boundary behavior
+  const ErrorCatcher = ({ children }) => {
+    try {
+      return children;
+    } catch (err) {
+      setHasError(true);
+      setError(err);
+      return null;
+    }
+  };
+
+  if (hasError && error) {
+    return (
+      <div className="alert alert-danger">
+        <p>Couldn&apos;t open form. Encountered the following error:</p>
+        <pre>{error.message}</pre>
+        <Button color="primary" variant="contained" onClick={resetErrorBoundary}>
+          Refresh Form
+        </Button>
+      </div>
+    );
   }
-}
+
+  return <ErrorCatcher>{children}</ErrorCatcher>;
+};
 
 export default ErrorBoundary;
