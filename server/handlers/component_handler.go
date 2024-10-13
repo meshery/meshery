8721400cc5693630cd9df282a4468f4320374f28
Yash Sharma <yashsharma2572@gmail.com>
2024-10-13 13:14:12 +0530

    Add query param for GetAllMeshmodelComponents
    Signed-off-by: Yash Sharma <yashsharma2572@gmail.com>
    Signed-off-by: THEBOSS0369 <anujkumsharma9876@gmail.com>

--- server/handlers/component_handler.go
+++ server/handlers/component_handler.go
@@ -988,6 +988,7 @@ func (h *Handler) GetAllMeshmodelComponents(rw http.ResponseWriter, r *http.Requ
 	v := queryParams.Get("version")
 	returnAnnotationComp := queryParams.Get("annotations")
 	filter := &regv1beta1.ComponentFilter{
+		Id:          queryParams.Get("id"),
 		Version:     v,
 		Trim:        queryParams.Get("trim") == "true",
 		APIVersion:  queryParams.Get("apiVersion"),
