dd2e7fba6eea651a97a473c99a0390abab9dcbcc
THEBOSS0369 <anujkumsharma9876@gmail.com>
2024-10-19 12:36:01 +0000

    fixed tha changes requested
    Signed-off-by: THEBOSS0369 <anujkumsharma9876@gmail.com>

--- ui/components/ErrorBoundary.js
+++ ui/components/ErrorBoundary.js
@@ -1,51 +1,28 @@
 import { Button } from '@mui/material';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { ErrorBoundary as SistentErrorBoundary } from '@layer5/sistent';
 
 /**
  * ErrorBoundary is a React component that catches JavaScript errors in its child components and renders a fallback UI when an error occurs.
  * It should be used as a wrapper around components that might throw errors.
- * @deprecated use error boundary from sistent instead
  */
-const ErrorBoundary = ({ children }) => {
-  const [hasError, setHasError] = useState(false);
-  const [error, setError] = useState(null);
-
-  const resetErrorBoundary = () => {
-    setHasError(false);
-    setError(null);
-  };
-
-  useEffect(() => {
-    if (hasError && error) {
-      // Log the error, send to analytics, etc.
-      console.error(error);
-    }
-  }, [hasError, error]);
-
-  // React Error boundary behavior
-  const ErrorCatcher = ({ children }) => {
-    try {
-      return children;
-    } catch (err) {
-      setHasError(true);
-      setError(err);
-      return null;
-    }
-  };
-
-  if (hasError && error) {
-    return (
-      <div className="alert alert-danger">
-        <p>Couldn&apos;t open form. Encountered the following error:</p>
-        <pre>{error.message}</pre>
-        <Button color="primary" variant="contained" onClick={resetErrorBoundary}>
-          Refresh Form
-        </Button>
-      </div>
-    );
-  }
-
-  return <ErrorCatcher>{children}</ErrorCatcher>;
-};
+function ErrorBoundary({ children }) {
+  return (
+    <SistentErrorBoundary
+      FallbackComponent={({ error, resetErrorBoundary }) => (
+        <div className="alert alert-danger">
+          <p>Couldn&apos;t open form. Encountered the following error:</p>
+          <pre>{error.message}</pre>
+          <Button color="primary" variant="contained" onClick={resetErrorBoundary}>
+            Refresh Form
+          </Button>
+        </div>
+      )}
+      onError={(error) => console.error('Error in Spaces Preferences Component', error)}
+    >
+      {children}
+    </SistentErrorBoundary>
+  );
+}
 
 export default ErrorBoundary;
