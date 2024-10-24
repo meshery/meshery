1ca319a97101e7cb2e93836ef0c6794b69a6e734
THEBOSS0369 <anujkumsharma9876@gmail.com>
2024-10-24 03:07:54 +0000

    Using error boundary directly from sistent
    Signed-off-by: THEBOSS0369 <anujkumsharma9876@gmail.com>

--- ui/components/ErrorBoundary.js
+++ ui/components/ErrorBoundary.js
@@ -1,22 +1,5 @@
 import React from 'react';
-import { Button, ErrorBoundary as SistentErrorBoundary } from '@layer5/sistent';
-
-/**
- * ErrorBoundary is a React component that catches JavaScript errors in its child components and renders a fallback UI when an error occurs.
- * It should be used as a wrapper around components that might throw errors.
- */
-
-function FallbackComponent({ error, resetErrorBoundary }) {
-  return (
-    <div className="alert alert-danger">
-      <p>Couldn&apos;t open form. Encountered the following error:</p>
-      <pre>{error.message}</pre>
-      <Button color="primary" variant="contained" onClick={resetErrorBoundary}>
-        Refresh Form
-      </Button>
-    </div>
-  );
-}
+import { ErrorBoundary as SistentErrorBoundary } from '@layer5/sistent';
 
 /**
  * Error handler function that logs the error.
@@ -26,11 +9,7 @@ function handleError(error) {
 }
 
 function ErrorBoundary({ children }) {
-  return (
-    <SistentErrorBoundary FallbackComponent={FallbackComponent} onError={handleError}>
-      {children}
-    </SistentErrorBoundary>
-  );
+  return <SistentErrorBoundary onError={handleError}>{children}</SistentErrorBoundary>;
 }
 
 export default ErrorBoundary;
