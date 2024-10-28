c196fc1411e6d1238de813f306b6799b3041f70a
Eti Ijeoma <ijayeti@gmail.com>
2024-10-29 00:34:33 +0100

    add docs to generate models from local csv
    Signed-off-by: Eti Ijeoma <ijayeti@gmail.com>

--- mesheryctl/internal/cli/root/registry/generate.go
+++ mesheryctl/internal/cli/root/registry/generate.go
@@ -71,14 +71,16 @@ var generateCmd = &cobra.Command{
 	Long:  "Prerequisite: Excecute this command from the root of a meshery/meshery repo fork.\n\nGiven a Google Sheet with a list of model names and source locations, generate models and components any Registrant (e.g. GitHub, Artifact Hub) repositories.\n\nGenerated Model files are written to local filesystem under `/server/models/<model-name>`.",
 	Example: `
 // Generate Meshery Models from a Google Spreadsheet (i.e. "Meshery Integrations" spreadsheet).
-mesheryctl registry generate --spreadsheet-id "1DZHnzxYWOlJ69Oguz4LkRVTFM79kC2tuvdwizOJmeMw" --spreadsheet-cred 
+mesheryctl registry generate --spreadsheet-id "1DZHnzxYWOlJ69Oguz4LkRVTFM79kC2tuvdwizOJmeMw" --spreadsheet-cred
 // Directly generate models from one of the supported registrants by using Registrant Connection Definition and (optional) Registrant Credential Definition
 mesheryctl registry generate --registrant-def [path to connection definition] --registrant-cred [path to credential definition]
 // Generate a specific Model from a Google Spreadsheet (i.e. "Meshery Integrations" spreadsheet).
 mesheryctl registry generate --spreadsheet-id "1DZHnzxYWOlJ69Oguz4LkRVTFM79kC2tuvdwizOJmeMw" --spreadsheet-cred --model "[model-name]"
-// Generate Meshery Models and Component from csv files in a local directory.
-mesheryctl registry generate --directory <DIRECTORY_PATH>
+// Easily generate Meshery models and components from CSV files in a local directory using the following command
+mesheryctl registry generate --directory https://github.com/meshery/meshery/tree/master/docs/assets/model-and-component-csv-templates
     `,
+	Annotations: linkDocPatternView,
+
 	PreRunE: func(cmd *cobra.Command, args []string) error {
 		// Prerequisite check is needed - https://github.com/meshery/meshery/issues/10369
 		// TODO: Include a prerequisite check to confirm that this command IS being the executED from within a fork of the Meshery repo, and is being executed at the root of that fork.
@@ -179,6 +181,10 @@ type compGenerateTracker struct {
 }
 
 var modelToCompGenerateTracker = store.NewGenericThreadSafeStore[compGenerateTracker]()
+var linkDocPatternView = map[string]string{
+	"link":    "![generate-model-directory-usage](https://github.com/meshery/meshery/tree/master/docs/assets/model-and-component-csv-templates)",
+	"caption": "Usage of registry generate --directory",
+}
 
 func InvokeGenerationFromSheet(wg *sync.WaitGroup) error {
 	weightedSem := semaphore.NewWeighted(20)
