00286da67aca72ade35431775eddb5497ea1302f
Anuj Kumar Sharma <anujkumsharma9876@gmail.com>
2024-10-31 11:56:52 +0530

    Update ErrorBoundary.js
    Signed-off-by: Anuj Kumar Sharma <anujkumsharma9876@gmail.com>

--- ui/components/ErrorBoundary.js
+++ ui/components/ErrorBoundary.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { ErrorBoundary as SistentErrorBoundary } from '@layer5/sistent';
 
-  useEffect(() => {
-    if (hasError && error) {
-      // Log the error, send to analytics, etc.
-      console.error(error);
-    }
-  }, [hasError, error]);
+function handleError(error) {
+  console.error('Error in Spaces Preferences Component', error);
+}
 
 function ErrorBoundary({ children }) {
-  return <SistentErrorBoundary onError={handleError}>{children}</SistentErrorBoundary>;
+  return (
+    <SistentErrorBoundary onError={handleError}>
+      {children}
+    </SistentErrorBoundary>
+  );
 }
 
 export default ErrorBoundary;
