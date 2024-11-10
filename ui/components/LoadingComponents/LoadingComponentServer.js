5dcd6a81b7686b6acd19a0c45bc87e0a0bd61ba2
aabidsofi19 <mailtoaabid01@gmail.com>
2024-11-11 00:43:25 +0530

    fix styling mess due global sistent
    Signed-off-by: aabidsofi19 <mailtoaabid01@gmail.com>

--- ui/components/LoadingComponents/LoadingComponentServer.js
+++ ui/components/LoadingComponents/LoadingComponentServer.js
@@ -2,51 +2,52 @@ import React from 'react';
 // import AnimatedMeshery from './Animations/AnimatedMesheryCSS';
 import PropTypes from 'prop-types';
 import uiConfig from '../../ui.config';
-import { Typography, styled } from '@layer5/sistent';
-import { UsesSistent } from '../SistentWrapper';
+// import { Typography } from '@layer5/sistent';
+// import { UsesSistent } from '../SistentWrapper';
 
-const StyledAnimatedLogoDark = styled(uiConfig.AnimatedLogoDark)(({ theme }) => ({
-  height: '100px',
-  margin: '4px 0px 8px',
-  fill: theme.palette.mode === 'dark' ? '#fff' : '',
-}));
+// const StyledAnimatedLogoDark = styled(uiConfig.AnimatedLogoDark)(({ theme }) => ({
+//   height: '100px',
+//   margin: '4px 0px 8px',
+//   fill: theme.palette.mode === 'dark' ? '#fff' : '',
+// }));
 
 function LoadingScreen(props) {
   const { message, children, isLoading, ...other } = props;
+  const AnimatedLogoDark = uiConfig.AnimatedLogoDark;
 
   if (isLoading) {
     return (
-      <UsesSistent>
-        <div
-          {...other}
-          style={{
-            display: 'grid',
-            placeItems: 'center',
-            minHeight: '100vh',
-            textAlign: 'center',
-            backgroundColor: '#263238', // color of navigation menu
-            color: '#dedede', // soften the subtitle / message
-          }}
-        >
-          <div>
-            <StyledAnimatedLogoDark />
-            <Typography
-              sx={{
-                '@font-face': {
-                  fontFamily: 'Qanelas Soft',
-                  src: 'url("/static/fonts/qanelas-soft/QanelasSoftRegular.otf") format("opentype")',
-                  fontWeight: 'normal',
-                  fontSize: '.9rem',
-                },
-                fontFamily: 'Qanelas Soft, Roboto',
-                marginTop: '1rem',
-              }}
-            >
-              {message}
-            </Typography>
-          </div>
+      <div
+        {...other}
+        style={{
+          '@font-face': {
+            fontFamily: 'Qanelas Soft',
+            src: 'url("/static/fonts/qanelas-soft/QanelasSoftBlack.otf") format("opentype")',
+            fontWeight: 'normal',
+          },
+          fontFamily: 'QanelasSoftRegular, Qanelas Soft Regular, sans-serif',
+          display: 'grid',
+          placeItems: 'center',
+          minHeight: '100vh',
+          textAlign: 'center',
+          backgroundColor: '#263238', // color of navigation menu
+          color: '#dedede', // soften the subtitle / message
+        }}
+      >
+        <div>
+          <AnimatedLogoDark />
+          <h1
+            style={{
+              fontFamily: 'QanelasSoftRegular',
+              fontSize: '.9rem',
+              fontWeight: 'normal',
+              marginTop: '1rem',
+            }}
+          >
+            {message}
+          </h1>
         </div>
-      </UsesSistent>
+      </div>
     );
   }
 
