5bd78b9f7afc3d9a7182cf9db3f5c75e2c8a5e90
Tanishq <tanishq162005@gmail.com>
2024-11-10 06:19:36 +0530

    refractor scripts.go
    Signed-off-by: Tanishq <tanishq162005@gmail.com>

--- mesheryctl/pkg/utils/scripts.go
+++ mesheryctl/pkg/utils/scripts.go
@@ -6,13 +6,46 @@ import (
 	"os/exec"
 )
 
-// TODO@Aisuko this function is so hard to read and maintain, please refactor it
+// GKEConfig holds configuration for GKE script generation
+
+type GKEConfig struct {
+	ConfigPath string
+	SAName     string
+	Namespace  string
+}
+
+// GenerateConfigGKE generates kubernetes config for GKE
 func GenerateConfigGKE(configPath, SAName, namespc string) error {
-	if configPath == "" || SAName == "" || namespc == "" {
-		return fmt.Errorf("configPath, SAName, and namespc are required")
+	cfg := &GKEConfig{
+		ConfigPath: configPath,
+		SAName:     SAName,
+		Namespace:  namespc,
 	}
 
-	script := fmt.Sprintf(`
+	if err := cfg.validate(); err != nil {
+		return fmt.Errorf("validation failed: %w", err)
+	}
+
+	return cfg.executeScript()
+}
+
+func (c *GKEConfig) validate() error {
+	if c.ConfigPath == "" || c.SAName == "" || c.Namespace == "" {
+		return fmt.Errorf("configPath, SAName, and namespc are required")
+	}
+	return nil
+}
+
+func (c *GKEConfig) executeScript() error {
+	script := c.generateScript()
+	cmd := exec.Command("sh", "-c", script)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func (c *GKEConfig) generateScript() string {
+	return fmt.Sprintf(`
 set -e
 set -o pipefail
 
@@ -21,12 +54,25 @@ SERVICE_ACCOUNT_NAME=%s
 NAMESPACE="%s"
 TARGET_FOLDER=$(dirname ${KUBECFG_FILE_NAME})
 
-create_service_account() {
+%s
+%s
+%s
+%s
+
+main
+`, c.ConfigPath, c.SAName, c.Namespace,
+		serviceAccountTemplate(),
+		secretOpsTemplate(),
+		kubeConfigTemplate(),
+		mainOpsTemplate())
+}
+
+func serviceAccountTemplate() string {
+	return `create_service_account() {
     echo -e "\nCreating a service account in ${NAMESPACE} namespace: ${SERVICE_ACCOUNT_NAME}"
     kubectl create sa "${SERVICE_ACCOUNT_NAME}" --namespace "${NAMESPACE}"
     kubectl create clusterrolebinding "${SERVICE_ACCOUNT_NAME}" --clusterrole=cluster-admin --serviceaccount=default:"${SERVICE_ACCOUNT_NAME}" --namespace "${NAMESPACE}"
     
-    # Create token secret for service account
     echo -e "\nCreating token secret for service account..."
     cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -39,14 +85,14 @@ metadata:
 type: kubernetes.io/service-account-token
 EOF
     
-    # Add delay to allow secret creation
-    echo "Waiting for service account secret to be created..."
     sleep 10
+}`
 }
 
+func secretOpsTemplate() string {
+	return `
 get_secret_name_from_service_account() {
     echo -e "\nGetting secret of service account ${SERVICE_ACCOUNT_NAME} on ${NAMESPACE}"
-    # Get the secret name
     SECRET_NAME="${SERVICE_ACCOUNT_NAME}-token"
     echo "Secret name: ${SECRET_NAME}"
 }
@@ -65,8 +111,11 @@ get_user_token_from_secret() {
         exit 1
     fi
     printf "done"
+}`
 }
 
+func kubeConfigTemplate() string {
+	return `
 set_kube_config_values() {
     context=$(kubectl config current-context)
     echo -e "\nSetting current context to: $context"
@@ -82,22 +131,18 @@ set_kube_config_values() {
         exit 1
     fi
 
-    # Set up the config
     echo -e "\nPreparing k8s-${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-conf"
-    echo -n "Setting a cluster entry in kubeconfig..."
     kubectl config set-cluster "${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}" \
     --server="${ENDPOINT}" \
     --certificate-authority="${TARGET_FOLDER}/ca.crt" \
     --embed-certs=true
 
-    echo -n "Setting token credentials entry in kubeconfig..."
     kubectl config set-credentials \
     "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}" \
     --token="${USER_TOKEN}"
 
-    echo -n "Setting a context entry in kubeconfig..."
     kubectl config set-context \
     "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}" \
@@ -105,11 +150,13 @@ set_kube_config_values() {
     --user="${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
     --namespace="${NAMESPACE}"
 
-    echo -n "Setting the current-context in the kubeconfig file..."
     kubectl config use-context "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}"
+}`
 }
 
+func mainOpsTemplate() string {
+	return `
 main() {
     mkdir -p "${TARGET_FOLDER}"
     create_service_account
@@ -122,14 +169,5 @@ main() {
     echo "KUBECONFIG=${KUBECFG_FILE_NAME} kubectl get pods"
     echo "you should not have any permissions by default - you have just created the authentication part"
     echo "You will need to create RBAC permissions"
-}
-
-main
-`, configPath, SAName, namespc)
-
-	generateCFG := exec.Command("sh", "-c", script)
-	generateCFG.Stdout = os.Stdout
-	generateCFG.Stderr = os.Stderr
-
-	return generateCFG.Run()
+}`
 }
