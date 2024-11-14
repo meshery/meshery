257e3076209acee3f7be06c2192699e59093be0e
Yaten Dhingra <yaten598@gmail.com>
2024-11-14 23:53:36 +0530

    init: rewrite tests for 'exp relationship'
    Signed-off-by: Yaten Dhingra <yaten598@gmail.com>

--- mesheryctl/internal/cli/root/experimental/experimental_test.go
+++ mesheryctl/internal/cli/root/experimental/experimental_test.go
@@ -1,37 +1,61 @@
 package experimental
 
 import (
+	"path/filepath"
+	"runtime"
 	"testing"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
 )
 
 func TestExperimentalList(t *testing.T) {
-	// test scenrios
+	// setup current context
+	utils.SetupContextEnv(t)
+
+	//initialize mock server for handling requests
+	utils.StartMockery(t)
+
+	// create a test helper
+	testContext := utils.NewTestHelper(t)
+
+	// get current directory
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		t.Fatal("Not able to get current working directory")
+	}
+	currDir := filepath.Dir(filename)
+	fixturesDir := filepath.Join(currDir, "fixtures")
+
+	// test scenarios for fetching data
 	tests := []struct {
 		Name             string
 		Args             []string
+		URL              string
+		Fixture          string
+		Token            string
 		ExpectedResponse string
-		ExpecteError     bool
+		ExpectedError    bool
 	}{
 		{
 			Name:             "List registered relationships",
 			Args:             []string{"relationship", "list"},
-			ExpectedResponse: "",
-			ExpecteError:     false,
+			URL:              testContext.BaseURL + "/api/meshmodels/relationships",
+			Fixture:          "list.exp.api.response.golden",
+			ExpectedResponse: "list.exp.output.golden",
+			Token:            filepath.Join(fixturesDir, "token.golden"),
+			ExpectedError:    false,
 		},
 	}
 
-	// Run tests
-	for _, tt := range tests {
+	// run tests
+	/* for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) {
-			ExpCmd.SetArgs(tt.Args)
-			err := ExpCmd.Execute()
-			if err != nil {
-				// if we're supposed to get an error
-				if tt.ExpecteError {
-					// if the error was expected, we've to check that if the generated error is equal to the "ExpectedResponse"
-					return
-				}
-			}
+			apiResponse := utils.NewGoldenFile(t, tt.Fixture, fixturesDir).Load()
+
+			utils.TokenFlag = tt.Token
+
+			httpmock.RegisterResponder("GET", tt.URL, httpmock.NewStringResponder(200, apiResponse))
 		})
-	}
+	} */
 }
