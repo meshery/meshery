b03367d753f788f03d568882f41be6b213888edc
Yaten Dhingra <yaten598@gmail.com>
2024-11-17 16:13:18 +0530

    updated experimental_test.go
    Signed-off-by: Yaten Dhingra <yaten598@gmail.com>

--- mesheryctl/internal/cli/root/experimental/experimental_test.go
+++ mesheryctl/internal/cli/root/experimental/experimental_test.go
@@ -1,13 +1,19 @@
 package experimental
 
 import (
+	"flag"
+	"io"
+	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/jarcoal/httpmock"
 	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
 )
+
+var update = flag.Bool("update", true, "update golden files")
 
 func TestExperimentalList(t *testing.T) {
 	// setup current context
@@ -35,27 +41,82 @@ func TestExperimentalList(t *testing.T) {
 		Fixture          string
 		Token            string
 		ExpectedResponse string
-		ExpectedError    bool
+		ExpectError      bool
 	}{
 		{
 			Name:             "List registered relationships",
 			Args:             []string{"relationship", "list"},
 			URL:              testContext.BaseURL + "/api/meshmodels/relationships",
-			Fixture:          "list.exp.api.response.golden",
-			ExpectedResponse: "list.exp.output.golden",
+			Fixture:          "list.exp.relationship.api.response.golden",
+			ExpectedResponse: "list.exp.relationship.output.golden",
 			Token:            filepath.Join(fixturesDir, "token.golden"),
-			ExpectedError:    false,
+			ExpectError:      true,
 		},
 	}
 
 	// run tests
-	/* for _, tt := range tests {
+	for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) {
 			apiResponse := utils.NewGoldenFile(t, tt.Fixture, fixturesDir).Load()
 
 			utils.TokenFlag = tt.Token
 
-			httpmock.RegisterResponder("GET", tt.URL, httpmock.NewStringResponder(200, apiResponse))
+			httpmock.RegisterResponder("GET", tt.URL,
+				httpmock.NewStringResponder(200, apiResponse))
+
+			testdataDir := filepath.Join(currDir, "testdata")
+			golden := utils.NewGoldenFile(t, tt.ExpectedResponse, testdataDir)
+
+			// Grab console prints
+			rescueStdout := os.Stdout
+			r, w, _ := os.Pipe()
+			os.Stdout = w
+			_ = utils.SetupMeshkitLoggerTesting(t, false)
+			ExpCmd.SetArgs(tt.Args)
+			ExpCmd.SetOutput(rescueStdout)
+			err := ExpCmd.Execute()
+			if err != nil {
+				// if we're supposed to get an error
+				if tt.ExpectError {
+					// write it in file
+					if *update {
+						golden.Write(err.Error())
+					}
+					expectedResponse := golden.Load()
+
+					utils.Equals(t, expectedResponse, err.Error())
+					return
+				}
+				t.Fatal(err)
+			}
+
+			w.Close()
+			out, _ := io.ReadAll(r)
+			os.Stdout = rescueStdout
+
+			actualResponse := string(out)
+
+			if *update {
+				golden.Write(actualResponse)
+			}
+			expectedResponse := golden.Load()
+			expectedResponse = trimLastNLines(expectedResponse, 2)
+
+			cleanedActualResponse := utils.CleanStringFromHandlePagination(actualResponse)
+			cleanedExceptedResponse := utils.CleanStringFromHandlePagination(expectedResponse)
+
+			utils.Equals(t, cleanedExceptedResponse, cleanedActualResponse)
 		})
-	} */
+		t.Log("List Exp test Passed")
+	}
+
+	utils.StopMockery(t)
+}
+
+func trimLastNLines(s string, n int) string {
+	lines := strings.Split(s, "\n")
+	if len(lines) <= n {
+		return ""
+	}
+	return strings.Join(lines[:len(lines)-n], "\n")
 }
