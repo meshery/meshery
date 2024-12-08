4ea9248e6f6326a078d74f04475d83e086e8e74d
Ajay Singh <37625112+Ajay-singh1@users.noreply.github.com>
2024-12-08 03:54:40 +0000

    Updated import to the latest version
    Signed-off-by: Ajay Singh <37625112+Ajay-singh1@users.noreply.github.com>

--- ui/components/load-test-timer-dialog.js
+++ ui/components/load-test-timer-dialog.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NoSsr } from '@material-ui/core';
+import { NoSsr } from '@mui/material/NoSsr';
 
 let ReactCountdownClock;
 if (typeof window !== 'undefined') {
