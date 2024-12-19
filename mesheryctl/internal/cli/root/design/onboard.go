634de5730e3b56f5d9438ace0ec6ba72a583e20e
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-12-19 18:33:18 +0530

    change quotes
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- mesheryctl/internal/cli/root/design/onboard.go
+++ mesheryctl/internal/cli/root/design/onboard.go
@@ -86,7 +86,7 @@ mesheryctl design onboard -f ./pattern.yml -s "Kubernetes Manifest"
 			// search and fetch patterns with pattern-name
 			utils.Log.Debug("Fetching patterns")
 
-			req, err = utils.NewRequest("GET", patternURL+`?populate=pattern_file&search=`+patternName, nil)
+			req, err = utils.NewRequest("GET", patternURL+"?populate=pattern_file&search="+patternName, nil)
 			if err != nil {
 				utils.Log.Error(err)
 				return nil
