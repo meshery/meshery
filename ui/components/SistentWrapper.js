0658272527923957e097c2b51843b2240ce165cf
Ajay Singh <37625112+Ajay-singh1@users.noreply.github.com>
2024-12-21 06:52:07 +0000

    Update the SistentWrapper to Use MUI v5
    Signed-off-by: Ajay Singh <37625112+Ajay-singh1@users.noreply.github.com>

--- ui/components/SistentWrapper.js
+++ ui/components/SistentWrapper.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { useTheme } from '@material-ui/core/styles';
+import { useTheme } from '@mui/material/styles';
 import { SistentThemeProviderWithoutBaseLine } from '@layer5/sistent';
 
 export const UsesSistent = ({ children }) => {
   const theme = useTheme();
-  const mode = theme.palette.type;
+  const mode = theme.palette.mode;
   if (mode === 'dark') {
     return (
       <SistentThemeProviderWithoutBaseLine initialMode={mode}>
