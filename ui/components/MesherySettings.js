cad9c2b8576493859c828c018386f74d0ebea0b6
Amit Amrutiya <amitamrutiya2210@gmail.com>
2025-01-06 18:50:30 +0530

    feat: integrate ErrorBoundary and enhance MesherySettings with new charts
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- ui/components/MesherySettings.js
+++ ui/components/MesherySettings.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'next/router';
-import { connect } from 'react-redux';
+import { connect, Provider } from 'react-redux';
+import { NoSsr } from '@material-ui/core';
 import { bindActionCreators } from 'redux';
+import { UsesSistent } from './SistentWrapper';
 import {
   CustomTooltip,
   Tab,
@@ -12,7 +14,9 @@ import {
   Typography,
   styled,
   useTheme,
+  Grid,
 } from '@layer5/sistent';
+import DashboardMeshModelGraph from './DashboardComponent/charts/DashboardMeshModelGraph';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faPoll, faDatabase, faFileInvoice } from '@fortawesome/free-solid-svg-icons';
 import { faMendeley } from '@fortawesome/free-brands-svg-icons';
@@ -45,7 +49,9 @@ import {
 } from '@/constants/navigator';
 import { removeDuplicateVersions } from './MeshModelRegistry/helper';
 import DefaultError from './General/error-404';
-import Overview from './DashboardComponent/overview';
+import { store } from '../store';
+import MesheryConfigurationChart from './DashboardComponent/charts/MesheryConfigurationCharts';
+import ConnectionStatsChart from './DashboardComponent/charts/ConnectionCharts';
 
 const StyledPaper = styled(Paper)(() => ({
   flexGrow: 1,
@@ -86,6 +92,11 @@ const StyledIcon = styled('img')(({ theme }) => ({
   verticalAlign: 'text-top',
   width: theme.spacing(1.75),
   marginLeft: theme.spacing(0.5),
+}));
+
+const RootClass = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#303030' : '#eaeff1',
+  marginTop: '1rem',
 }));
 
 function TabContainer(props) {
@@ -250,7 +261,7 @@ const MesherySettings = (props) => {
     );
   }
   return (
-    <>
+    <UsesSistent>
       {CAN(keys.VIEW_SETTINGS.action, keys.VIEW_SETTINGS.subject) ? (
         <div sx={{ flexGrow: 1, maxWidth: '100%', height: 'auto' }}>
           <StyledPaper square>
@@ -325,7 +336,28 @@ const MesherySettings = (props) => {
           </StyledPaper>
           {tabVal === OVERVIEW && (
             <TabContainer>
-              <Overview />
+              <NoSsr>
+                <Provider store={store}>
+                  <RootClass>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12} md={12}>
+                        <DashboardMeshModelGraph />
+                      </Grid>
+
+                      <Grid item xs={12}>
+                        <Grid container spacing={2}>
+                          <Grid item xs={12} md={6}>
+                            <ConnectionStatsChart />
+                          </Grid>
+                          <Grid item xs={12} md={6}>
+                            <MesheryConfigurationChart />
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </RootClass>
+                </Provider>
+              </NoSsr>
             </TabContainer>
           )}
           {tabVal === ADAPTERS &&
@@ -412,7 +444,7 @@ const MesherySettings = (props) => {
       ) : (
         <DefaultError />
       )}
-    </>
+    </UsesSistent>
   );
 };
 
