f0b370b5817f82cdc34202d631404e467a7bf11e
Amit Amrutiya <amitamrutiya2210@gmail.com>
2025-01-10 12:57:22 +0000

    add tables for the custom resource
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

---
+++ ui/components/DashboardComponent/resources/crds/config.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import { timeAgo } from '../../../../utils/k8s-utils';
+import {  getK8sContextFromClusterId } from '@/utils/multi-ctx';
+import {  SINGLE_VIEW } from '../config';
+import { Title } from '../../view';
+import { TootltipWrappedConnectionChip } from '../../../connections/ConnectionChip';
+import useKubernetesHook from '../../../hooks/useKubernetesHook';
+import { DefaultTableCell, SortableTableCell } from '../sortable-table-cell';
+import { CONNECTION_KINDS } from '../../../../utils/Enum';
+import { FormatId } from '@/components/DataFormatter';
+
+export const CustomResourceConfig = (
+  switchView,
+  meshSyncResources,
+  k8sConfig,
+  connectionMetadataState,
+) => {
+  const ping = useKubernetesHook();
+  const customResources = [];
+  const customResourceConfigs = {};
+
+  customResources?.forEach((resource) => {
+    customResourceConfigs[resource] = {
+      name: resource,
+      colViews: [
+        ['id', 'na'],
+        ['metadata.name', 'xs'],
+        ['apiVersion', 's'],
+        ['metadata.namespace', 'm'],
+        ['cluster_id', 'xs'],
+        ['metadata.creationTimestamp', 'l'],
+      ],
+      columns: [
+        {
+          name: 'id',
+          label: 'ID',
+          options: {
+            display: false,
+            customHeadRender: function CustomHead({ ...column }) {
+              return <DefaultTableCell columnData={column} />;
+            },
+            customBodyRender: (value) => <FormatId id={value} />,
+          },
+        },
+        {
+          name: 'metadata.name',
+          label: 'Name',
+          options: {
+            sort: false,
+            customHeadRender: function CustomHead({ ...column }) {
+              return <DefaultTableCell columnData={column} />;
+            },
+            customBodyRender: function CustomBody(value, tableMeta) {
+              return (
+                <Title
+                  onClick={() => switchView(SINGLE_VIEW, meshSyncResources[tableMeta.rowIndex])}
+                  data={
+                    meshSyncResources[tableMeta.rowIndex]
+                      ? meshSyncResources[tableMeta.rowIndex]?.component_metadata
+                      : {}
+                  }
+                  value={value}
+                />
+              );
+            },
+          },
+        },
+        {
+          name: 'apiVersion',
+          label: 'API version',
+          options: {
+            sort: true,
+            sortThirdClickReset: true,
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+          },
+        },
+        {
+          name: 'metadata.namespace',
+          label: 'Namespace',
+          options: {
+            sort: true,
+            sortThirdClickReset: true,
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+            customBodyRender: function CustomBody(value) {
+              return <>{value}</>;
+            },
+          },
+        },
+        {
+          name: 'cluster_id',
+          label: 'Cluster',
+          options: {
+            sort: true,
+            sortThirdClickReset: true,
+            customHeadRender: function CustomHead({ index, ...column }, sortColumn, columnMeta) {
+              return (
+                <SortableTableCell
+                  index={index}
+                  columnData={column}
+                  columnMeta={columnMeta}
+                  onSort={() => sortColumn(index)}
+                />
+              );
+            },
+            customBodyRender: function CustomBody(val) {
+              let context = getK8sContextFromClusterId(val, k8sConfig);
+
+              return (
+                <>
+                  <TootltipWrappedConnectionChip
+                    title={context.name}
+                    iconSrc={
+                      connectionMetadataState
+                        ? connectionMetadataState[CONNECTION_KINDS.KUBERNETES]?.icon
+                        : ''
+                    }
+                    handlePing={() => ping(context.name, context.server, context.connection_id)}
+                  />
+                </>
+              );
+            },
+          },
+        },
+        {
+          name: 'metadata.creationTimestamp',
+          label: 'Age',
+          options: {
+            sort: false,
+            customHeadRender: function CustomHead({ ...column }) {
+              return <DefaultTableCell columnData={column} />;
+            },
+            customBodyRender: function CustomBody(value) {
+              let time = timeAgo(value);
+              return <>{time}</>;
+            },
+          },
+        },
+      ],
+    };
+  });
+  console.log('amit customResourceConfigs', customResourceConfigs);
+  return customResourceConfigs;
+};
