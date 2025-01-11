6c9634d65b28d01a104a82a0a890ec2b8f2b6ac6
Riya Garg <riyag1452003@gmail.com>
2025-01-11 23:42:15 +0530

    Modify generate relationship test - TestExperimentalGenerate_CreateJsonFile
    Modify google sheet response to fetch data from golden file
    
    Signed-off-by: Riya Garg <riyag1452003@gmail.com>

--- mesheryctl/internal/cli/root/relationships/generate_test.go
+++ mesheryctl/internal/cli/root/relationships/generate_test.go
@@ -12,25 +12,33 @@ import (
 
 func TestExperimentalGenerate_CreateJsonFile(t *testing.T) {
 
+	sheetData, err := os.ReadFile("./fixtures/generate.relationship.sheet.data.golden")
+	if err != nil {
+		t.Fatal("Error in reading file 'generate.relationship.sheet.data.golden': ", err)
+	}
+
+	var sheetDataParsed map[string]interface{}
+	err = json.Unmarshal(sheetData, &sheetDataParsed)
+	if err != nil {
+		t.Fatal("Error in parsing file 'generate.relationship.sheet.data.golden': ", err)
+	}
+
 	resp := &sheets.ValueRange{
 		MajorDimension: "ROWS",
 		Range:          "Relationships!A1:O1000",
 		Values: [][]interface{}{
 			{},
 			{},
-			{"kubernetes", "v1.25.2", "Hierarchical", "", "Inventory", "A hierarchical inventory relationship in which the configuration of (parent) component is patched with the configuration of other (child) component. Eg: The configuration of the EnvoyFilter (parent) component is patched with the configuration as received from WASMFilter (child) component.\"", "", "", "hierarchical_inventory_relationship", "", "", "{\"kind\": \"PersistentVolume\",\r\n            \"model\": \"kubernetes\",\r\n            \"patch\": {\r\n              \"patchStrategy\": \"replace\",\r\n              \"mutatorRef\": [\r\n                [\"name\"]\r\n              ],\r\n              \"description\": \"PersistentVolume in Kubernetes provides durable storage for Pods, ensuring data persistence across Pod restarts and rescheduling.\"\r\n            }\r\n          }",
-				"{\"kind\": \"VolumeAttachment\",\r\n            \"model\": \"kubernetes\",\r\n            \"patch\": {\r\n              \"patchStrategy\": \"replace\",\r\n              \"mutatedRef\": [\r\n                [\"settings\", \"spec\", \"source\", \"persistentVolumeName\"]\r\n              ],\r\n              \"description\": \"VolumeAttachment in Kubernetes facilitates the attachment of a PersistentVolume to a node, enabling its utilization by Pods on that node.\"\r\n            }\r\n          }",
-				"{\"apiVersion\": \"core.meshery.io/v1alpha1\",\r\"kind\":\"Hierarchical\",\r\"metadata\": {\r  \"description\": \"A hierarchical inventory relationship in which the configuration of (parent) component is patched with the configuration of other (child) component. Eg: The configuration of the EnvoyFilter (parent) component is patched with the configuration as received from WASMFilter (child) component.\"\",\r  \"styles\": { \"\",}\r  },\r  \"model\": {\r    \"name\": \"kubernetes\",\r    \"version\": \"v1.25.2\",\r    \"category\": {\r      \"name\": \"Orchestration \\u0026 Management\",\r      \"metadata\": null\r    },\r    \"metadata\": {}\r},\r\"subType\": \"Inventory\",\r\"rego_query\": \"hierarchical_inventory_relationship\",\r\"selectors\": {\r  \"allow\": {\r    \"from\": [{\"kind\": \"PersistentVolume\",\r\r            \"model\": \"kubernetes\",\r\r            \"patch\": {\r\r              \"patchStrategy\": \"replace\",\r\r              \"mutatorRef\": [\r\r                [\"name\"]\r\r              ],\r\r              \"description\": \"PersistentVolume in Kubernetes provides durable storage for Pods, ensuring data persistence across Pod restarts and rescheduling.\"\r\r            }\r\r          }],\r    \"to\": [{\"kind\": \"VolumeAttachment\",\r\r            \"model\": \"kubernetes\",\r\r            \"patch\": {\r\r              \"patchStrategy\": \"replace\",\r\r              \"mutatedRef\": [\r\r                [\"settings\", \"spec\", \"source\", \"persistentVolumeName\"]\r\r              ],\r\r              \"description\": \"VolumeAttachment in Kubernetes facilitates the attachment of a PersistentVolume to a node, enabling its utilization by Pods on that node.\"\r\r            }\r\r          }]\r    },\r  \"deny\": {\r    \"from\": [],\r    \"to\": []\r    }\r  }\r}",
-				""},
+			sheetDataParsed["ROW3"].([]interface{}),
 		},
 	}
 
 	// for testing, relative path is required in createJsonFile function
 	jsonFilePath := "../../../../../docs/_data/RelationshipsDataTest.json"
 
-	err := createJsonFile(resp, jsonFilePath)
+	err = createJsonFile(resp, jsonFilePath)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("Error in createJsonFile function: ", err)
 	}
 
 	// comparing the generated JSON file with the expected JSON file
@@ -38,32 +46,33 @@ func TestExperimentalGenerate_CreateJsonFile(t *testing.T) {
 
 	relationshipData, err := os.ReadFile(jsonFilePath)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("Error in reading file 'RelationshipsDataTest.json': ", err)
 	}
 
 	expectedRelationshipData, err := os.ReadFile(goldenFilePath)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("Error in reading file 'generate.relationship.json.output.golden': ", err)
 	}
 
 	var relationshipDataJson, expectedRelationshipDataJson []map[string]interface{}
 	err = json.Unmarshal([]byte(relationshipData), &relationshipDataJson)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("Error in parsing file 'RelationshipsDataTest.json': ", err)
 	}
 
 	err = json.Unmarshal([]byte(expectedRelationshipData), &expectedRelationshipDataJson)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("Error in parsing file 'RelationshipsDataTest.json': ", err)
 	}
 
 	assert.JSONEqf(t, string(expectedRelationshipData), string(relationshipData), "Generated JSON data does not match expected data.\n Difference: %s", cmp.Diff(relationshipData, expectedRelationshipDataJson))
 
 	t.Log("Create JSON file test passed")
 
+	// remove the test file after running the test
 	err = os.Remove(jsonFilePath)
 	if err != nil {
-		t.Fatal("Error deleting file:", err)
+		t.Fatal("Error deleting file 'RelationshipDataTest.json': ", err)
 		return
 	}
 }
