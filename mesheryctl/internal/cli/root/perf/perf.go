ca4bf39b07bb41140b365b8964913c5091a402f3
weilirs <weilirs@umich.edu>
2025-01-22 15:33:14 -0500

    feat: Lifecycle management for meshery-nighthawk without integration test
    Signed-off-by: weilirs <weilirs@umich.edu>

--- mesheryctl/internal/cli/root/perf/perf.go
+++ mesheryctl/internal/cli/root/perf/perf.go
@@ -56,7 +56,7 @@ mesheryctl perf result -o yaml
 			return cmd.Help()
 		}
 		if ok := utils.IsValidSubcommand(availableSubcommands, args[0]); !ok {
-			availableSubCmds := []string{"apply", "profile", "result"}
+			availableSubCmds := []string{"apply", "profile", "result", "start", "stop"}
 
 			suggestedCmd := utils.FindClosestArg(args[0], availableSubCmds)
 			if suggestedCmd != "" && suggestedCmd[0] == args[0][0] {
@@ -73,6 +73,6 @@ func init() {
 	PerfCmd.PersistentFlags().StringVarP(&outputFormatFlag, "output-format", "o", "", "(optional) format to display in [json|yaml]")
 	PerfCmd.PersistentFlags().BoolVarP(&utils.SilentFlag, "yes", "y", false, "(optional) assume yes for user interactive prompts.")
 
-	availableSubcommands = []*cobra.Command{profileCmd, resultCmd, applyCmd}
+	availableSubcommands = []*cobra.Command{profileCmd, resultCmd, applyCmd, startCmd, stopCmd}
 	PerfCmd.AddCommand(availableSubcommands...)
 }
