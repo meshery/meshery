f450c0d7dc622a732c2aafddf51ec3bdfe9b3121
TheFaheem <faheemmushtaq89@gmail.com>
2025-01-27 18:13:30 +0530

    Migrate components in TypingFilter for MUI v5
    Signed-off-by: TheFaheem <faheemmushtaq89@gmail.com>

--- ui/components/TypingFilter/style.js
+++ ui/components/TypingFilter/style.js
@@ -1,54 +1,51 @@
-import { makeStyles, alpha } from '@material-ui/core';
-
-export const useStyles = makeStyles((theme) => ({
-  root: {
-    position: 'relative',
-    backgroundColor: theme.palette.secondary.elevatedComponents,
-  },
-  input: {
-    width: '100%',
-    marginBottom: '.1rem',
-    '& .MuiOutlinedInput-root': {
-      borderRadius: '6px',
-      backgroundColor: theme.palette.secondary.searchBackground,
-      '& fieldset': {
-        borderRadius: '6px',
-        border: `2px solid ${theme.palette.secondary.searchBorder}`,
-      },
-    },
-  },
-
-  dropDown: {
-    backgroundColor: theme.palette.secondary.searchBackground,
-    borderRadius: '6px',
-    boxShadow:
-      '0px 2px 4px 0px rgba(0, 0, 0, 0.20), 0px 1px 10px 0px rgba(0, 0, 0, 0.12), 0px 4px 5px 0px rgba(0, 0, 0, 0.14)',
-    border: `2px solid ${theme.palette.secondary.searchBorder}`,
-    marginTop: '0.2rem',
-  },
+import { styled, alpha } from '@mui/system';
+import { TextField, Typography } from '@layer5/sistent';
+export const Root = styled('div')(({ theme }) => ({
+  position: 'relative',
+  backgroundColor: theme.palette.background?.default,
 }));
 
-export const useFilterStyles = makeStyles((theme) => ({
-  item: {
-    fontFamily: 'Qanelas Soft, sans-serif',
-    display: 'flex',
-    gap: '0.3rem',
-    margin: '0.3rem',
-    padding: '0.3rem',
-    paddingInline: '3rem',
+export const InputField = styled(TextField)(({ theme }) => ({
+  width: '100%',
+  marginBottom: '.1rem',
+  '& .MuiOutlinedInput-root': {
     borderRadius: '6px',
-    cursor: 'pointer',
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.secondary.link2, 0.25),
+    backgroundColor: theme.palette.mode === 'dark' ? '#294957' : '#fff',
+    '& fieldset': {
+      borderRadius: '6px',
+      border: `2px solid ${theme.palette.mode === 'dark' ? '#396679' : '#CCCCCC'}`,
     },
   },
+}));
+export const DropDown = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#294957' : '#fff',
+  borderRadius: '6px',
+  boxShadow:
+    '0px 2px 4px 0px rgba(0, 0, 0, 0.20), 0px 1px 10px 0px rgba(0, 0, 0, 0.12), 0px 4px 5px 0px rgba(0, 0, 0, 0.14)',
+  border: `2px solid ${theme.palette.background.brand.secondary}`,
+  marginTop: '0.2rem',
+  maxHeight: '20rem',
+  overflowY: 'auto',
+}));
+export const Item = styled('div')(({ theme }) => ({
+  fontFamily: 'Qanelas Soft, sans-serif',
+  display: 'flex',
+  gap: '0.3rem',
+  margin: '0.3rem',
+  padding: '0.3rem',
+  paddingInline: '3rem',
+  borderRadius: '6px',
+  cursor: 'pointer',
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.text.brand, 0.25),
+  },
+}));
+export const Label = styled(Typography)(({ theme }) => ({
+  fontWeight: 500,
+  color: theme.palette.secondary.icon,
+}));
 
-  label: {
-    fontWeight: 500,
-    color: theme.palette.secondary.icon,
-  },
-  description: {
-    fontWeight: 400,
-    color: theme.palette.secondary.number,
-  },
+export const Description = styled(Typography)(({ theme }) => ({
+  fontWeight: 400,
+  color: theme.palette.secondary.number,
 }));
