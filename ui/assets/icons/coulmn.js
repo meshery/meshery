865ddb53527c6c7e5af9c69987adbb1c93f228b5
Amit Amrutiya <amitamrutiya2210@gmail.com>
2025-01-27 09:40:13 +0530

    fix: linting
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- ui/assets/icons/coulmn.js
+++ ui/assets/icons/coulmn.js
@@ -5,7 +5,6 @@ const ColumnIcon = (props) => {
   const theme = useTheme();
   return (
     <svg
-      className={classes.icon}
       xmlns="http://www.w3.org/2000/svg"
       height="24"
       viewBox="0 -960 960 960"
