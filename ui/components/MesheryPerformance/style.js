c810612fad8f705ebc7296be5081675095e9f9ed
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2025-01-28 18:29:51 +0530

    migrate performace card section to sistent
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>

---
+++ ui/components/MesheryPerformance/style.js
@@ -0,0 +1,21 @@
+import { styled } from '@layer5/sistent';
+
+export const CardButton = styled('div')(() => ({
+  display: 'flex',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+}));
+
+export const BottomPart = styled('div')(() => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+}));
+
+export const ResultContainer = styled('div')(() => ({
+  margin: '0 0 1rem',
+  '& div': {
+    display: 'flex',
+    alignItems: 'center',
+  },
+}));
