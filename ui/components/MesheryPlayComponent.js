a0d686fff202d517188f2491e27df5607dc6c04c
Vidit Kushwaha <viditkushwaha530@gmail.com>
2025-01-29 18:44:30 +0000

    fix: styling issue in adaptor dropdown
    Signed-off-by: Vidit Kushwaha <viditkushwaha530@gmail.com>

--- ui/components/MesheryPlayComponent.js
+++ ui/components/MesheryPlayComponent.js
@@ -191,6 +191,9 @@ const MesheryPlayComponent = (props) => {
                   value={adapter && adapter.adapter_location ? adapter.adapter_location : ''}
                   margin="normal"
                   variant="outlined"
+                  sx={{
+                    display: 'flex',
+                  }}
                   onChange={handleAdapterChange()}
                   SelectProps={{
                     MenuProps: {
