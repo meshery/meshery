f55f9a294e447773ba50f9d9d03f59615811c838
ashish111333 <ashishkumar0265aaa@gmail.com>
2025-01-30 20:51:17 +0530

    added count flag and getAllorgs func in org cmd
    Signed-off-by: ashish111333 <ashishkumar0265aaa@gmail.com>

--- mesheryctl/internal/cli/root/orgs/org.go
+++ mesheryctl/internal/cli/root/orgs/org.go
@@ -2,26 +2,43 @@ package orgs
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 
+	"github.com/layer5io/meshery/mesheryctl/internal/cli/root/config"
 	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
 	availableSubcommands = []*cobra.Command{ListOrgCmd}
+	count                bool
 )
 var OrgCmd = &cobra.Command{
 
 	Use:   "org",
 	Short: "view list of registered orgs",
 	Long:  "view list of registered orgs with their name,id and date of creation",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) == 0 {
-			if err := cmd.Help(); err != nil {
-				utils.Log.Error(err)
-				return nil
-			}
+	Example: `
+	// Number of  registered orgs
+	mesheryctl org --count 
+
+	// List registerd orgs
+	mesheryctl org list
+	
+	`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 && !count {
+			cmd.Usage()
+			utils.ErrInvalidArgument(fmt.Errorf("No arguments passed, provide a subcommand"))
 			return nil
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if count {
+
 		}
 		if ok := utils.IsValidSubcommand(availableSubcommands, args[0]); !ok {
 			utils.Log.Error(fmt.Errorf("invalid subcommand"))
@@ -32,6 +49,30 @@ var OrgCmd = &cobra.Command{
 }
 
 func init() {
+	OrgCmd.Flags().BoolVarP(&count, "count", "", false, "get the number of registered orgs, --count")
 	OrgCmd.AddCommand(availableSubcommands...)
+
+}
+
+func getAllOrgs(countOnly bool) (int, error) {
+	mctlCfg, err := config.GetMesheryCtl(viper.GetViper())
+	if err != nil {
+		return 0, utils.ErrLoadConfig(err)
+	}
+	baseUrl := mctlCfg.GetBaseMesheryURL()
+	url := fmt.Sprintf("%s/api/identity/orgs", baseUrl)
+
+	req, err := utils.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return 0, err
+	}
+	res, err := utils.MakeRequest(req)
+	if err != nil {
+		return 0, utils.ErrFailRequest(err)
+	}
+	jsonBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return 0, err
+	}
 
 }
