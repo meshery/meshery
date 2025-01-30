fd3e7e75f76e77e9c25b7384c0f8daea0489828f
ashish111333 <ashishkumar0265aaa@gmail.com>
2025-01-31 00:43:05 +0530

    updated org cmd
    Signed-off-by: ashish111333 <ashishkumar0265aaa@gmail.com>

--- mesheryctl/internal/cli/root/orgs/org.go
+++ mesheryctl/internal/cli/root/orgs/org.go
@@ -1,12 +1,14 @@
 package orgs
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/layer5io/meshery/mesheryctl/internal/cli/root/config"
 	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
+	"github.com/layer5io/meshery/server/models"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -31,13 +33,20 @@ var OrgCmd = &cobra.Command{
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) == 0 && !count {
 			cmd.Usage()
-			utils.ErrInvalidArgument(fmt.Errorf("No arguments passed, provide a subcommand"))
-			return nil
+
+			return utils.ErrInvalidArgument(fmt.Errorf("No arguments passed, provide a subcommand"))
 		}
 		return nil
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if count {
+			_, c, err := getAllOrgs(true)
+			if err != nil {
+				utils.Log.Error(err)
+				return nil
+			}
+			fmt.Println(fmt.Sprintf("Total registered orgs : %v", c))
+			return nil
 
 		}
 		if ok := utils.IsValidSubcommand(availableSubcommands, args[0]); !ok {
@@ -49,30 +58,41 @@ var OrgCmd = &cobra.Command{
 }
 
 func init() {
-	OrgCmd.Flags().BoolVarP(&count, "count", "", false, "get the number of registered orgs, --count")
+	OrgCmd.Flags().BoolVarP(&count, "count", "", false, "total number of registered orgs")
 	OrgCmd.AddCommand(availableSubcommands...)
 
 }
 
-func getAllOrgs(countOnly bool) (int, error) {
+func getAllOrgs(countOnly bool) (*models.OrganizationsPage, int, error) {
+	var orgPage models.OrganizationsPage
 	mctlCfg, err := config.GetMesheryCtl(viper.GetViper())
 	if err != nil {
-		return 0, utils.ErrLoadConfig(err)
+		return nil, 0, utils.ErrLoadConfig(err)
 	}
 	baseUrl := mctlCfg.GetBaseMesheryURL()
-	url := fmt.Sprintf("%s/api/identity/orgs", baseUrl)
+	url := fmt.Sprintf("%s/api/identity/orgs?all=%v", baseUrl, countOnly)
 
 	req, err := utils.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		return 0, err
+		return nil, 0, err
 	}
 	res, err := utils.MakeRequest(req)
 	if err != nil {
-		return 0, utils.ErrFailRequest(err)
+		return nil, 0, utils.ErrFailRequest(err)
 	}
+
 	jsonBytes, err := io.ReadAll(res.Body)
 	if err != nil {
-		return 0, err
+		return nil, 0, err
 	}
+	err = json.Unmarshal(jsonBytes, &orgPage)
+	if err != nil {
+		return nil, 0, err
+	}
+	if countOnly {
+		return nil, orgPage.TotalCount, nil
+	}
+
+	return &orgPage, 0, nil
 
 }
