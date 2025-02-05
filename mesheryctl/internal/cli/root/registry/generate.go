07f1fd1b1e3fe1348d6c7b404855803fee2db08f
Souvik Kar Mahapatra <souvikat001@gmail.com>
2025-02-05 18:58:58 +0530

    updated cli docs
    Signed-off-by: Souvik Kar Mahapatra <souvikat001@gmail.com>

--- mesheryctl/internal/cli/root/registry/generate.go
+++ mesheryctl/internal/cli/root/registry/generate.go
@@ -56,7 +56,7 @@ mesheryctl registry generate --spreadsheet-id "1DZHnzxYWOlJ69Oguz4LkRVTFM79kC2tu
 // Directly generate models from one of the supported registrants by using Registrant Connection Definition and (optional) Registrant Credential Definition
 mesheryctl registry generate --registrant-def [path to connection definition] --registrant-cred [path to credential definition]
 // Generate a specific Model from a Google Spreadsheet (i.e. "Meshery Integrations" spreadsheet).
-mesheryctl registry generate --spreadsheet-id "1DZHnzxYWOlJ69Oguz4LkRVTFM79kC2tuvdwizOJmeMw" --spreadsheet-cred --model "[model-name]"
+mesheryctl registry generate --spreadsheet-id "1DZHnzxYWOlJ69Oguz4LkRVTFM79kC2tuvdwizOJmeMw" --spreadsheet-cred $CRED --model "[model-name]"
 // Generate Meshery Models and Component from csv files in a local directory.
 mesheryctl registry generate --directory <DIRECTORY_PATH>
     `,
