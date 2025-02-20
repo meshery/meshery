245374f40d0243bfd586219cc520dc9fd6c9e32a
lekaf974 <matthieu.evrin@gmail.com>
2025-02-19 23:47:02 -0500

    chore: uncomment required running steps
    Signed-off-by: lekaf974 <matthieu.evrin@gmail.com>

--- mesheryctl/test/e2e/run_tests.bash
+++ mesheryctl/test/e2e/run_tests.bash
@@ -9,15 +9,15 @@ source ./setup_suite.bash
 # Run the tests
 
 # Uncomment the following line to enable junit format output
-FORMATTER="--formatter junit"
+# FORMATTER="--formatter junit"
 
 # Run the prerequisites first
 ## cli is available
 ## server is started
-#bats $FORMATTER prerequisites/*.bats
+bats $FORMATTER prerequisites/*.bats
 
 # make server available for the following tests
-#kubectl port-forward -n meshery service/meshery 9081:9081 &
+kubectl port-forward -n meshery service/meshery 9081:9081 &
 
 # Run the tests
 bats $FORMATTER *-*/*.bats
