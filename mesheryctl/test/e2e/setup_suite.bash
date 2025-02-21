63aa01c37618abd564865bef1e65e77f8aa84497
lekaf974 <matthieu.evrin@gmail.com>
2025-02-20 23:10:29 -0500

    test: restore mesheryctl install prior to tests
    Signed-off-by: lekaf974 <matthieu.evrin@gmail.com>

--- mesheryctl/test/e2e/setup_suite.bash
+++ mesheryctl/test/e2e/setup_suite.bash
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "Setting up test environment"
+echo "Setting up test environment\n"
 
+echo "Install mesheryctl"
+curl -L https://meshery.io/install -s | DEPLOY_MESHERY=false bash -
 
 # Expose binary path to the following tests
 export MESHERYCTL_BIN="mesheryctl"
 
-echo -e "\ncreate temp directory for test data"
+echo -e "\nCreate temp directory for test data"
 TEMP_DATA_DIR=`mktemp -d`
 # Expose the temp directory to the following tests
 export TEMP_TEST_DATA_DIR=$TEMP_DATA_DIR
