ce9e1880a41879f5d22b8f00320b2fe325117f91
Lee Calcote <lee.calcote@layer5.io>
2025-02-21 19:00:26 -0600

    chore: partial fix of provider UI styling
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- provider-ui/components/Provider.style.js
+++ provider-ui/components/Provider.style.js
@@ -1,64 +1,72 @@
 import { styled, MenuItem, Dialog, DialogActions, Typography, Popover } from "@layer5/sistent"
 export const CustomDiv = styled("div")(({ theme }) => ({
-  width : "60%",
-  marginLeft : "auto",
-  marginRight : "auto",
-  marginTop : theme.spacing(3),
+  width: "60%",
+  marginLeft: "auto",
+  marginRight: "auto",
+  marginTop: theme.spacing(3),
 }));
 
 export const CustomTypography = styled(Typography)(({ theme }) => ({
-  "@font-face" : {
-    fontFamily : "Qanelas Soft",
-    src : "url('/provider/static/fonts/qanelas-soft/QanelasSoftRegular.otf') format('opentype')",
+  "@font-face": {
+    fontFamily: "Qanelas Soft",
+    src: "url('/provider/static/fonts/qanelas-soft/QanelasSoftRegular.otf') format('opentype')",
   },
-  fontWeight : 400,
-  fontStyle : "normal",
-  fontSize : "1.5rem",
-  lineHeight : "2rem",
-  letterSpacing : "0.01em",
-  color : theme.palette.text.default,
+  fontWeight: 400,
+  fontStyle: "normal",
+  fontSize: "1.5rem",
+  lineHeight: "2rem",
+  letterSpacing: "0.01em",
+  color: theme.palette.text.default,
 }));
 
 export const MesheryLogo = styled("img")(({ theme }) => ({
-  width : theme.spacing(50),
-  maxWidth : "100%",
-  height : "auto",
+  width: theme.spacing(50),
+  maxWidth: "100%",
+  height: "auto",
 }));
 
-export const MenuProviderDisabled = styled(MenuItem)(() => ({
-  display : "flex",
-  justifyContent : "space-between",
+export const MenuProviderDisabled = styled(MenuItem)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  backgroundColor: theme.palette.text.default,
+  "> span": {
+    fontStyle: "italic"
+  },
 }));
 
 export const CustomDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogContent-root" : {
-    padding : theme.spacing(2),
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
   },
-  "& .MuiDialogActions-root" : {
-    padding : theme.spacing(1),
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
   },
-  "& .MuiDialogContentText-root > a" : {
-    color : "#222",
+  "& .MuiDialogContentText-root > a": {
+    color: "#222",
   },
 }));
 export const CustomDialogActions = styled(DialogActions)(({ theme }) => ({
-  display : "flex",
-  justifyContent : "space-between",
-  background : theme.palette.background.tabs,
-  padding : theme.spacing(2),
-  "& div > a" : {
-    color : theme.palette.text.default,
+  display: "flex",
+  justifyContent: "space-between",
+  background: theme.palette.background.tabs,
+  padding: theme.spacing(2),
+  "& div > a": {
+    color: theme.palette.text.default,
   },
 }));
 export const StyledPopover = styled(Popover)(({ theme }) => ({
-  ".MuiPaper-root" : {
-    backgroundColor : theme.palette.background.brand.disabled,
-    color : theme.palette.text.default,
+  ".MuiPaper-root": {
+    backgroundColor: theme.palette.background.brand.disabled,
+    color: theme.palette.text.default,
+    
   },
 }));
 export const LearnMore = styled("div")(() => ({
-  width : "60%",
-  marginLeft : "auto",
-  marginRight : "auto",
-  marginTop : "3rem",
+  width: "60%",
+  marginLeft: "auto",
+  marginRight: "auto",
+  marginTop: "3rem",
+  "& :hover": {
+    cursor: "pointer"
+  }
 }));
