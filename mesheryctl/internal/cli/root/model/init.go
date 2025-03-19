1b50da7531e2a70a3bac31a0bbd910b687287b4a
Nikita B <n2h9z4@gmail.com>
2025-03-19 21:18:56 +0100

    feature: 13849: model init command: add a general code to create folders, files and put content
    Signed-off-by: Nikita B <n2h9z4@gmail.com>

--- mesheryctl/internal/cli/root/model/init.go
+++ mesheryctl/internal/cli/root/model/init.go
@@ -1,7 +1,10 @@
 package model
 
 import (
+	"errors"
+	"fmt"
 	"os"
+	"slices"
 	"strings"
 
 	"github.com/layer5io/meshery/mesheryctl/internal/cli/root/config"
@@ -32,6 +35,20 @@ mesheryctl model init [model-name] --path [path-to-location] (default is current
 mesheryctl model init [model-name] --output-format [json|yaml|csv] (default is json)
     `,
 	Args: cobra.ExactArgs(1),
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		format, _ := cmd.Flags().GetString("output-format")
+		getValidOutputFormatSlices := getValidOutputFormat()
+		if !slices.Contains(getValidOutputFormatSlices, format) {
+			validFormatsString := strings.Join(getValidOutputFormat(), ", ")
+			return ErrModelUnsupportedOutputFormat(
+				fmt.Sprintf(
+					"[ %s ] are the only format supported\n\nUsage: mesheryctl model view --output-format [yaml|json]\nRun 'mesheryctl model view --help' to see detailed help message",
+					validFormatsString,
+				),
+			)
+		}
+		return nil
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		_, err := config.GetMesheryCtl(viper.GetViper())
 		if err != nil {
@@ -70,44 +87,59 @@ mesheryctl model init [model-name] --output-format [json|yaml|csv] (default is j
 			//utils.Log.Debug(string(modelJSONContent))
 		}
 
-		folderPath := strings.Join([]string{path, modelName, version}, string(os.PathSeparator))
-		err = os.MkdirAll(folderPath, 0755)
+		const DirPerm = 0755
+		mainFolderPath := strings.Join([]string{path, modelName, version}, string(os.PathSeparator))
+		err = os.MkdirAll(mainFolderPath, DirPerm)
 		if err != nil {
 			// TODO use meshkit error format
 			utils.Log.Error(err)
 			return err
 		}
 
-		modelJSONContent, err := initModelReadTemplate(initModelTemplatePathModelJSON)
-		if err != nil {
-			utils.Log.Error(err)
-			// TODO use meshkit error format instead during implementation phase
-			return err
-		}
+		for _, item := range initModelData {
+			if item.folderPath != "" {
+				itemFolderPath := strings.Join([]string{mainFolderPath, item.folderPath}, string(os.PathSeparator))
+				err = os.MkdirAll(itemFolderPath, DirPerm)
+				if err != nil {
+					// TODO use meshkit error format
+					utils.Log.Error(err)
+					return err
+				}
+			}
+			for name, templatePath := range item.files {
+				content, err := getTemplateInOutputFormat(templatePath, outputFormat)
+				if err != nil {
+					utils.Log.Error(err)
+					// TODO use meshkit error format instead during implementation phase
+					return err
+				}
+				filePath := strings.Join(
+					[]string{
+						mainFolderPath,
+						strings.Join(
+							[]string{name, outputFormat},
+							".",
+						),
+					},
+					string(os.PathSeparator),
+				)
+				file, err := os.Create(filePath)
+				if err != nil {
+					utils.Log.Error(err)
+					// TODO use meshkit error format instead during implementation phase
+					return err
+				}
+				defer file.Close() // Ensure the file is closed when the function exits
 
-		modelFilePath := strings.Join(
-			[]string{
-				folderPath,
-				strings.Join(
-					[]string{"model", outputFormat},
-					".",
-				),
-			},
-			string(os.PathSeparator),
-		)
-		modelFile, err := os.Create(modelFilePath)
-		if err != nil {
-			utils.Log.Error(err)
-			// TODO use meshkit error format instead during implementation phase
-			return err
-		}
-		defer modelFile.Close() // Ensure the file is closed when the function exits
+				_, err = file.Write(content)
+				if err != nil {
+					utils.Log.Error(err)
+					// TODO use meshkit error format instead during implementation phase
+					return err
+				}
 
-		_, err = modelFile.Write(modelJSONContent)
-		if err != nil {
-			utils.Log.Error(err)
-			// TODO use meshkit error format instead during implementation phase
-			return err
+			}
+
 		}
 
 		// TODO maybe clean partial data (if error occurs in the middle of execution)
@@ -121,17 +153,57 @@ func init() {
 	initModelCmd.Flags().StringP("output-format", "o", "json", "(optional) format to display in [json|yaml]")
 }
 
+func initModelGetValidOutputFormat() []string {
+	return []string{"json"}
+	// TODO implement yamls, csv
+	// return []string{"json", "yaml", "csv"}
+}
+
 const initModelTemplatePathModelJSON = "json_models/constructs/v1beta1/model.json"
 const initModelTemplatePathDesignJSON = "json_models/constructs/v1beta1/design.json"
 const initModelTemplatePathComponentJSON = "json_models/constructs/v1beta1/component.json"
 const initModelTemplatePathConnectionJSON = "json_models/constructs/v1beta1/connection.json"
 const initModelTemplatePathRelathionshipJSON = "json_models/constructs/v1alpha3/relationship.json"
 
+var initModelData = []struct {
+	folderPath string
+	files      map[string]string
+}{
+	{
+		folderPath: "",
+		// map file name to template key
+		files: map[string]string{
+			"model": initModelTemplatePathModelJSON,
+		},
+	},
+}
+
 func initModelReadTemplate(templatePath string) ([]byte, error) {
 	return schemas.Schemas.ReadFile(templatePath)
 }
 
-func initModelGetValidOutputFormat() []string {
-	return []string{"json"}
-	// return []string{"yaml", "json", "csv"}
+func getTemplateInOutputFormat(templatePath string, outputFormat string) ([]byte, error) {
+	// outputFormat was already validated, it is one of the initModelGetValidOutputFormat()
+	content, err := initModelReadTemplate(templatePath)
+	if err != nil {
+		return nil, err
+	}
+
+	if outputFormat == "json" {
+		return content, err
+	}
+
+	if outputFormat == "yaml" {
+		// TODO process yaml
+		return nil, errors.New("implement yaml")
+	}
+
+	if outputFormat == "csv" {
+		// TODO process csv
+		return nil, errors.New("implement csv")
+	}
+
+	// should impossible to get here
+	// TODO use meshkit error format instead during implementation phase
+	return nil, errors.New("invalid format")
 }
