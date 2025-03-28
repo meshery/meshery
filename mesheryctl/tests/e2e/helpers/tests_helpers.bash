6f3cb122966e9b3d301664428c0125b007ef1bfa
lekaf974 <matthieu.evrin@gmail.com>
2025-03-27 21:43:57 -0400

    chore: update bats local setup
    Signed-off-by: lekaf974 <matthieu.evrin@gmail.com>

--- mesheryctl/tests/e2e/helpers/tests_helpers.bash
+++ mesheryctl/tests/e2e/helpers/tests_helpers.bash
@@ -2,8 +2,7 @@
 
 _tests_helper() {
     export BATS_LIB_PATH=${BATS_LIB_PATH:-"/usr/lib"}
-    echo $BATS_LIB_PATH
     bats_load_library bats-support
     bats_load_library bats-assert
-    bats_load_library bats-detik/detik.bash
+    bats_load_library bats-detik/lib/detik.bash
 }
