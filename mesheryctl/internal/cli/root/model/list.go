2d3821abd80c31b568cb274a203ad6931b82b634
lekaf974 <matthieu.evrin@gmail.com>
2025-04-03 21:07:37 -0400

    chore(msheryctl-workspace): update workspace command using reusable fetch and list function
    chore(mesheryctl): enhance common Fetch managing base meshery api url locally
    
    Signed-off-by: lekaf974 <matthieu.evrin@gmail.com>

--- mesheryctl/internal/cli/root/model/list.go
+++ mesheryctl/internal/cli/root/model/list.go
@@ -4,13 +4,10 @@ import (
 	"fmt"
 
 	"github.com/layer5io/meshery/mesheryctl/internal/cli/pkg/api"
-	"github.com/layer5io/meshery/mesheryctl/internal/cli/root/config"
 	"github.com/layer5io/meshery/mesheryctl/pkg/utils"
 	"github.com/layer5io/meshery/server/models"
 	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var listModelCmd = &cobra.Command{
@@ -28,20 +25,16 @@ mesheryctl model list --page [page-number]
 // Display number of available models in Meshery
 mesheryctl model list --count
     `,
-	RunE: func(cmd *cobra.Command, args []string) error {
+	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) != 0 {
 			return errors.New(utils.SystemModelSubError("this command takes no arguments\n", "list"))
 		}
-		mctlCfg, err := config.GetMesheryCtl(viper.GetViper())
-		if err != nil {
-			log.Fatalln(err, "error processing config")
-		}
-
-		baseUrl := mctlCfg.GetBaseMesheryURL()
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
 		page, _ := cmd.Flags().GetInt("page")
-		url := fmt.Sprintf("%s/%s?%s", baseUrl, modelsApiPath, utils.GetPageQueryParameter(cmd, page))
 
-		modelsResponse, err := api.Fetch[models.MeshmodelsAPIResponse](url)
+		modelsResponse, err := api.Fetch[models.MeshmodelsAPIResponse](fmt.Sprintf("%s?%s", modelsApiPath, utils.GetPageQueryParameter(cmd, page)))
 
 		if err != nil {
 			return err
