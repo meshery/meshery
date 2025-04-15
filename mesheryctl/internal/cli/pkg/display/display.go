2325951d9d7485d2fda503ae9ba69b414cfe867c
lekaf974 <matthieu.evrin@gmail.com>
2025-04-14 22:25:03 -0400

    chore(mesheryctl): fix failing tests + fix empty output result displaying header if no data found
    Signed-off-by: lekaf974 <matthieu.evrin@gmail.com>

--- mesheryctl/internal/cli/pkg/display/display.go
+++ mesheryctl/internal/cli/pkg/display/display.go
@@ -20,7 +20,8 @@ type DisplayedData struct {
 func List(data DisplayedData) error {
 	utils.DisplayCount(data.DataType, data.Count)
 
-	if data.DisplayCountOnly {
+	// flag --count is set or no data available
+	if data.DisplayCountOnly || data.Count == 0 {
 		return nil
 	}
 
