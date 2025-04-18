47c55a4be804b88e2f5f82a9411df8f5b00075b8
ianwhitney <ian.r.whitney@gmail.com>
2025-04-17 21:42:36 -0600

    feat: fixed pef playwright flainess
    Signed-off-by: ianwhitney <ian.r.whitney@gmail.com>

--- ui/tests/e2e/service-mesh-performance.spec.js
+++ ui/tests/e2e/service-mesh-performance.spec.js
@@ -1,8 +1,7 @@
-import { ENV } from './env';
 import { v4 as uuidv4 } from 'uuid';
 import { expect, test } from './fixtures/project';
 
-test.describe('Service Mesh Performance Management Tests', { tag: '@unstable' }, () => {
+test.describe.serial('Service Mesh Performance Management Tests', () => {
   const peformanceProfiles = [
     {
       profileWithUUID: `Sample-Perf-Test-${uuidv4()}`,
@@ -16,61 +15,64 @@ test.describe('Service Mesh Performance Management Tests', { tag: '@unstable' },
     test(`Add performance profile with load generator "${loadGenerator}" and service mesh "${serviceMesh}"`, async ({
       page,
     }) => {
-      await page.goto(`${ENV.MESHERY_SERVER_URL}/performance/profiles`);
+      await page.goto('/performance/profiles');
       await page.getByLabel('Add Performance Profile').click();
       await page.getByLabel('Profile Name').fill(profileWithUUID);
-      await page.getByLabel('Service Mesh').click();
+      await page.getByLabel('Technology').click();
       await page.getByRole('option', { name: serviceMesh }).click();
       await page.getByLabel('URL to test').fill(url);
       await page.getByLabel('Concurrent requests').fill('2');
       await page.getByLabel('Queries per second').fill('2');
       await page.getByLabel('Duration').fill('15s');
       await page.getByLabel(loadGenerator).check();
+      await page.getByTestId('run-performance-test').click();
+      const successNofications = page.getByTestId('SnackbarContent-success');
+      const infoNofications = page.getByTestId('SnackbarContent-info');
 
-      const runPerformanceTest = await page.getByTestId('run-performance-test');
-      await expect(runPerformanceTest).toBeVisible();
-      await runPerformanceTest.click();
-      await expect(page.getByText('Load test has been submitted')).toBeVisible();
+      await expect(successNofications.filter({ hasText: /submitted/i })).toHaveCount(1);
+      await expect(infoNofications.filter({ hasText: /completed/i })).toHaveCount(1);
+
     });
 
     test(`View detailed result of a performance profile (Graph Visualiser) with load generator "${loadGenerator}" and service mesh "${serviceMesh}"`, async ({
       page,
     }) => {
-      await page.goto(`${ENV.MESHERY_SERVER_URL}/performance/profiles`);
+      await page.goto('/performance/profiles');
       await expect(await page.getByText(`${profileWithUUID}`)).toBeVisible();
       await page.getByRole('button', { name: 'View Results', exact: true }).first().click();
-      await page.getByTestId('open-performance-result-bar-chart').first().click();
-      await expect(page.getByTestId('sentinelStart')).toBeHidden();
-      await expect(await page.getByText(`URL: ${url}`, { exact: true })).toBeVisible();
+      await page.getByTestId('TableChartIcon').first().click();
+      await expect(page.getByText(`${url}`, { exact: true })).toBeTruthy();
     });
 
     test(`Edit the configuration of a performance profile with load generator "${loadGenerator}" and service mesh "${serviceMesh}"`, async ({
       page,
     }) => {
-      await page.goto(`${ENV.MESHERY_SERVER_URL}/performance/profiles`);
+      await page.goto('/performance/profiles');
       await page.getByText(profileWithUUID, { exact: true }).first().click();
 
       await page.getByTestId('performanceProfileCard-edit').click();
-      await page.getByLabel('Service Mesh').click();
+      await page.getByLabel('Technology').click();
       await page.getByRole('option', { name: serviceMesh }).click();
       await page.getByLabel('Concurrent requests').fill('3');
       await page.getByLabel(loadGenerator).check();
 
-      const runPerformanceTest = await page.getByTestId('run-performance-test');
-      await expect(runPerformanceTest).toBeEnabled();
-      await runPerformanceTest.click();
-      await expect(page.getByText('Load test has been submitted')).toBeVisible();
+     await page.getByTestId('run-performance-test').click();
+     const successNofications = page.getByTestId('SnackbarContent-success');
+     const infoNofications = page.getByTestId('SnackbarContent-info');
+
+     await expect(successNofications.filter({ hasText: /submitted/i })).toHaveCount(1);
+     await expect(infoNofications.filter({ hasText: /completed/i })).toHaveCount(1);
     });
 
     test(`Compare test of a performance profile with load generator "${loadGenerator}" and service mesh "${serviceMesh}"`, async ({
       page,
     }) => {
-      await page.goto(`${ENV.MESHERY_SERVER_URL}/performance/profiles`);
+      await page.goto('/performance/profiles');
       await page.getByText(profileWithUUID, { exact: true }).first().click();
       await page.getByRole('button', { name: 'View Results', exact: true }).first().click();
       await page.getByTestId('MUIDataTableBodyRow-0').locator('input[type="checkbox"]').check();
 
-      await page.getByTestId('compare-selected').click();
+      await page.getByTestId('CompareArrowsIcon').first().click();
       await expect(page.getByTestId('chart-dialog-title')).toBeVisible();
       await expect(page.getByRole('heading', { name: 'Comparison' })).toBeVisible();
     });
@@ -78,11 +80,11 @@ test.describe('Service Mesh Performance Management Tests', { tag: '@unstable' },
     test(`Delete a performance profile with load generator "${loadGenerator}" and service mesh "${serviceMesh}"`, async ({
       page,
     }) => {
-      await page.goto(`${ENV.MESHERY_SERVER_URL}/performance/profiles`);
+      await page.goto('performance/profiles');
       await page.getByText(profileWithUUID, { exact: true }).first().click();
       await page.getByTestId('performanceProfileCard-delete').first().click();
 
-      await expect(await page.getByText('Performance Profile Deleted!')).toBeVisible();
+      await expect(page.getByTestId('SnackbarContent-success').filter(/deleted/i)).toHaveCount(1);
     });
   });
 });
