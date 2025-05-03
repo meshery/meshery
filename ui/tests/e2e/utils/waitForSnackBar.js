20f4817aa7e99cec467fd956b6287389ad61577b
ianwhitney <ian.r.whitney@gmail.com>
2025-05-02 19:05:29 -0600

    fix: eslint
    Signed-off-by: ianwhitney <ian.r.whitney@gmail.com>

--- ui/tests/e2e/utils/waitForSnackBar.js
+++ ui/tests/e2e/utils/waitForSnackBar.js
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 
 export const waitForSnackBar = async (page, message) => {
-    const snackbar = page.locator(`text=${message}`).first();
-    await expect(snackbar).toBeVisible();
-    await snackbar.waitFor({ state: 'detached', timeout: 8000 });
+  const snackbar = page.locator(`text=${message}`).first();
+  await expect(snackbar).toBeVisible();
+  await snackbar.waitFor({ state: 'detached', timeout: 8000 });
 };
