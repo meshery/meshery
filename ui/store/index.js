bb8f31379888fc10cba0b41a069fe21e4ad58422
Amit Amrutiya <amitamrutiya2210@gmail.com>
2025-05-09 08:01:13 +0000

    feat: add core reducer to the store configuration

--- ui/store/index.js
+++ ui/store/index.js
@@ -2,11 +2,13 @@ import eventsReducer from './slices/events';
 import globalEnvironmentContextReducer from './slices/globalEnvironmentContext';
 import { configureStore } from '@reduxjs/toolkit';
 import { api } from '../rtk-query/index';
+import coreRedcer from './slices/core';
 
 export const store = configureStore({
   reducer: {
     events: eventsReducer,
     globalEnvironmentContext: globalEnvironmentContextReducer,
+    core: coreRedcer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
