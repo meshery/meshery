b8b0670d2df96ba5a6d54d71854677eea3098a7a
Nikita B <n2h9z4@gmail.com>
2025-05-11 14:08:41 +0200

    feature: meshsync integration test: introduce database content based test case
    Signed-off-by: Nikita B <n2h9z4@gmail.com>

--- server/integration-tests/meshsync/integration_test.go
+++ server/integration-tests/meshsync/integration_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/layer5io/meshkit/database"
-	meshsyncmodel "github.com/layer5io/meshsync/pkg/model"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -17,51 +16,26 @@ func init() {
 	pathToSQLFile = os.Getenv("PATH_TO_SQL_FILE")
 }
 
-func TestMeshSyncIntegration(t *testing.T) {
+func TestBasedOnDatabaseContentMeshSyncIntegration(t *testing.T) {
 	if !runIntegrationTest {
 		t.Skip("skipping integration test")
 	}
 
-	t.Run(
-		"test case will be here soon...",
-		func(t *testing.T) {
-			dboptions := database.Options{
-				Engine:   database.SQLITE,
-				Filename: pathToSQLFile,
-			}
-			handler, errDatabaseNew := database.New(dboptions)
-			if errDatabaseNew != nil {
-				assert.NoError(t, errDatabaseNew, "expect no error on  database connection")
-				t.Fatal(errDatabaseNew)
-			}
-			assert.NotNil(t, handler)
+	dboptions := database.Options{
+		Engine:   database.SQLITE,
+		Filename: pathToSQLFile,
+	}
+	handler, errDatabaseNew := database.New(dboptions)
+	if errDatabaseNew != nil {
+		assert.NoError(t, errDatabaseNew, "expect no error on  database connection")
+		t.Fatal(errDatabaseNew)
+	}
+	// NOTE : didn't find how to close the handler
 
-			handler.Lock()
-			defer handler.Unlock()
-
-			k8sResources := make([]*meshsyncmodel.KubernetesResource, 0, 8)
-
-			// Query the database for the complete component definition
-			dbresult := handler.Model(meshsyncmodel.KubernetesResource{}).Find(&k8sResources)
-
-			if dbresult == nil {
-				t.Fatal("db result is nil")
-			}
-			if dbresult.Error != nil {
-				t.Fatalf("db result ended with an error %v", dbresult.Error)
-			}
-
-			// TODO add more meaningfull check
-			assert.True(t, len(k8sResources) > 0, "table must contains resources")
-			t.Logf("found %d k8s resources", len(k8sResources))
-
-			for _, resource := range k8sResources {
-				t.Logf(
-					"found k8s resource with api = %s, kind = %s",
-					resource.APIVersion,
-					resource.Kind,
-				)
-			}
-		},
-	)
+	for _, tc := range testCaseBasedOnDatabaseContentData {
+		t.Run(
+			tc.name,
+			tc.run(handler),
+		)
+	}
 }
