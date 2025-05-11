6f5834f768f2a8adebed6ed52fa2a152813dfb5a
Nikita B <n2h9z4@gmail.com>
2025-05-11 16:19:57 +0200

    feature: meshsync integration test: database content based test cases: assert custom resources: replica set
    Signed-off-by: Nikita B <n2h9z4@gmail.com>
    Signed-off-by: Sai-Thirumal <Sai-Thirumal@users.noreply.github.com>

--- server/integration-tests/meshsync/database_content_assertion_testcases_test.go
+++ server/integration-tests/meshsync/database_content_assertion_testcases_test.go
@@ -143,7 +143,31 @@ var testCaseBasedOnDatabaseContentData []testCaseBasedOnDatabaseContentStruct = 
 						assert.Equal(t, CUSTOM_APP_NAME, k8sResources[0].Name, "deployment must have correct name")
 					}
 				})
-				t0.Run("at least 1 replica set", func(t *testing.T) {})
+				t0.Run("at least 1 replica set", func(t *testing.T) {
+					handler.Lock()
+					defer handler.Unlock()
+
+					k8sResources := make([]*meshsyncmodel.KubernetesResourceObjectMeta, 0, 8)
+
+					dbresult := handler.
+						Model(&meshsyncmodel.KubernetesResourceObjectMeta{}).
+						Joins("JOIN kubernetes_resources ON kubernetes_resource_object_meta.id = kubernetes_resources.id").
+						Where("namespace = ?", CUSTOM_K8S_NAMESPACE).
+						Where("kubernetes_resources.kind = ?", "ReplicaSet").
+						Find(&k8sResources)
+
+					if dbresult == nil {
+						t.Fatal("db result is nil")
+					}
+					if dbresult.Error != nil {
+						t.Fatalf("db result ended with an error %v", dbresult.Error)
+					}
+
+					assert.GreaterOrEqual(t, len(k8sResources), 1, "must contains at least one replica set")
+					for _, rs := range k8sResources {
+						assert.Contains(t, rs.Name, CUSTOM_APP_NAME, "replica set must have correct name")
+					}
+				})
 				t0.Run(fmt.Sprintf("at least %d pods", CUSTOM_APP_REPLICAS_NUM), func(t *testing.T) {})
 			}
 		},
