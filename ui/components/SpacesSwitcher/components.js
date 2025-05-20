41ad9bc7e32aa5c9724b4faa6ed6008e95efd323
Amit Amrutiya <amitamrutiya2210@gmail.com>
2025-05-20 09:29:04 +0530

    rename sort by
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- ui/components/SpacesSwitcher/components.js
+++ ui/components/SpacesSwitcher/components.js
@@ -158,10 +158,10 @@ export const SortBySelect = ({ sortBy, handleSortByChange }) => {
   const sortOptions = [
     { value: 'name asc', label: 'Name (A-Z)' },
     { value: 'name desc', label: 'Name (Z-A)' },
-    { value: 'updated_at desc', label: 'Last Updated (Newest First)' },
-    { value: 'updated_at asc', label: 'Last Updated (Oldest First)' },
-    { value: 'created_at desc', label: 'Created At (Newest First)' },
-    { value: 'created_at asc', label: 'Created At (Oldest First)' },
+    { value: 'updated_at desc', label: 'Last Updated' },
+    { value: 'updated_at asc', label: 'Last Updated' },
+    { value: 'created_at desc', label: 'Created At' },
+    { value: 'created_at asc', label: 'Created At' },
   ];
 
   return (
