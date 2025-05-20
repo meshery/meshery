21c3ccf4f4d7e4220be952952e2a826e30004349
Amit Amrutiya <amitamrutiya2210@gmail.com>
2025-05-20 10:40:45 +0530

    refactor: update layout to use Grid for better responsiveness in MyDesignsContent, MyViewsContent, and RecentContent
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- ui/components/SpacesSwitcher/MyDesignsContent.js
+++ ui/components/SpacesSwitcher/MyDesignsContent.js
@@ -3,7 +3,7 @@ import { useGetLoggedInUserQuery } from '@/rtk-query/user';
 import React, { useCallback, useState } from 'react';
 import MainDesignsContent from './MainDesignsContent';
 import { VISIBILITY } from '@/utils/Enum';
-import { Box, useTheme } from '@layer5/sistent';
+import { Box, Grid, useTheme } from '@layer5/sistent';
 import { StyledSearchBar } from '@layer5/sistent';
 import { ImportButton, SortBySelect, TableListHeader, VisibilitySelect } from './components';
 
@@ -74,31 +74,40 @@ const MyDesignsContent = () => {
   const theme = useTheme();
   return (
     <Box display={'flex'} flexDirection="column" gap="1rem">
-      <Box display={'flex'} alignItems="center" marginBottom="1rem" gap={'1rem'}>
-        <StyledSearchBar
-          sx={{
-            backgroundColor: 'transparent',
-          }}
-          width="auto"
-          placeholder={'Search Designs'}
-          value={filters.searchQuery}
-          onChange={onSearchChange}
-          endAdornment={
-            <p style={{ color: theme.palette.text.default }}>Total Designs: {total_count}</p>
-          }
-        />
-        <Box sx={{ minWidth: 200 }}>
+      <Grid container spacing={2} alignItems="center" marginBottom="1rem">
+        {/* Search Bar */}
+        <Grid item xs={12} md={6}>
+          <StyledSearchBar
+            sx={{ backgroundColor: 'transparent' }}
+            placeholder="Search Designs"
+            value={filters.searchQuery}
+            onChange={onSearchChange}
+            endAdornment={
+              <p style={{ color: theme.palette.text.default }}>Total Designs: {total_count}</p>
+            }
+          />
+        </Grid>
+
+        {/* Sort By Select */}
+        <Grid item xs={4} md={2}>
           <SortBySelect sortBy={filters.sortBy} handleSortByChange={handleSortByChange} />
-        </Box>
-        <Box sx={{ minWidth: 150 }}>
+        </Grid>
+
+        {/* Visibility Select */}
+        <Grid item xs={4} md={2}>
           <VisibilitySelect
             visibility={filters.visibility}
             handleVisibilityChange={handleVisibilityChange}
             visibilityItems={visibilityItems}
           />
-        </Box>
-        <ImportButton />
-      </Box>
+        </Grid>
+
+        {/* Import Button */}
+        <Grid item xs={4} md={2}>
+          <ImportButton />
+        </Grid>
+      </Grid>
+
       <TableListHeader />
       <MainDesignsContent
         key={'my-designs-content'}
