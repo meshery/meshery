6c3db9d4f0d5c216d923f191b226466d9cc5c1a3
Amit Amrutiya <amitamrutiya2210@gmail.com>
2025-05-23 14:55:10 +0530

    create empty state in workspace move modal
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- ui/components/SpacesSwitcher/WorkspaceContentMoveModal.js
+++ ui/components/SpacesSwitcher/WorkspaceContentMoveModal.js
@@ -8,6 +8,8 @@ import {
 import { WorkspaceModalContext } from '@/utils/context/WorkspaceModalContextProvider';
 import { RESOURCE_TYPE } from '@/utils/Enum';
 import {
+  AddCircleIcon,
+  Button,
   CircularProgress,
   Modal,
   ModalBody,
@@ -15,10 +17,14 @@ import {
   PrimaryActionButtons,
   styled,
   Typography,
+  useTheme,
 } from '@layer5/sistent';
 import React, { useContext, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useGetIconBasedOnMode } from './hooks';
+import { useRouter } from 'next/router';
+import CAN from '@/utils/can';
+import { keys } from '@/utils/permission_constants';
 
 const WorkspaceItem = styled('div')(({ theme, selected }) => ({
   padding: '12px 16px',
@@ -43,6 +49,20 @@ const CurrentWorkspaceSection = styled('div')(({ theme }) => ({
   border: `1px solid ${theme.palette.border.strong}`,
   fontWeight: 'bolder',
 }));
+
+const NoWorkspacesContainer = styled('div')(({ theme }) => ({
+  padding: '1rem',
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  border: `1px dashed ${theme.palette.border.strong}`,
+  borderRadius: '8px',
+  margin: '1rem 0',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '1rem',
+}));
+
 const getModalTitle = (type, selectedContent, multiSelectedContent) => {
   const itemCount = multiSelectedContent?.length || 1;
   const resourceName = selectedContent?.name || itemCount;
@@ -58,7 +78,9 @@ const WorkspaceContentMoveModal = ({
   type,
   selectedContent,
 }) => {
-  const { setMultiSelectedContent, multiSelectedContent } = useContext(WorkspaceModalContext);
+  const router = useRouter();
+  const { setMultiSelectedContent, multiSelectedContent, closeModal } =
+    useContext(WorkspaceModalContext);
   const { organization: currentOrg } = useSelector((state) => state.ui);
   const { id: orgId } = currentOrg;
   const { data: workspaceData, isLoading } = useGetWorkspacesQuery(
@@ -69,7 +91,7 @@ const WorkspaceContentMoveModal = ({
       orgId: orgId,
     },
     {
-      skip: !orgId ? true : false,
+      skip: !orgId,
     },
   );
 
@@ -81,14 +103,61 @@ const WorkspaceContentMoveModal = ({
   const [unAssignDesignFromWorkspace] = useUnassignDesignFromWorkspaceMutation();
   const [assignViewToWorkspace] = useAssignViewToWorkspaceMutation();
   const [unAssignViewFromWorkspace] = useUnassignViewFromWorkspaceMutation();
+  const handleMove = async () => {
+    setWorkspaceContentMoveModal(false);
 
+    const moveDesign = async (designId) => {
+      await assignDesignToWorkspace({
+        workspaceId: selectedWorkspaceForMove.id,
+        designId,
+      }).unwrap();
+      await unAssignDesignFromWorkspace({
+        workspaceId: currentWorkspace.id,
+        designId,
+      });
+    };
+
+    const moveView = async (viewId) => {
+      await assignViewToWorkspace({
+        workspaceId: selectedWorkspaceForMove.id,
+        viewId,
+      }).unwrap();
+      await unAssignViewFromWorkspace({
+        workspaceId: currentWorkspace.id,
+        viewId,
+      });
+    };
+
+    if (RESOURCE_TYPE.DESIGN === type) {
+      if (multiSelectedContent.length > 0) {
+        await Promise.all(multiSelectedContent.map((design) => moveDesign(design.id)));
+        setMultiSelectedContent([]);
+      }
+      if (selectedContent) {
+        await moveDesign(selectedContent.id);
+      }
+    } else {
+      if (multiSelectedContent.length > 0) {
+        await Promise.all(multiSelectedContent.map((view) => moveView(view.id)));
+        setMultiSelectedContent([]);
+      }
+      if (selectedContent) {
+        await moveView(selectedContent.id);
+      }
+    }
+  };
+
+  const handleCreateWorkspace = () => {
+    closeModal();
+    setWorkspaceContentMoveModal(false);
+    router.push('/management/workspaces');
+  };
+  const theme = useTheme();
   return (
     <Modal
       open={workspaceContentMoveModal}
       headerIcon={useGetIconBasedOnMode({ mode: type })}
-      closeModal={() => {
-        setWorkspaceContentMoveModal(false);
-      }}
+      closeModal={() => setWorkspaceContentMoveModal(false)}
       title={getModalTitle(type, selectedContent, multiSelectedContent)}
     >
       <ModalBody>
@@ -98,10 +167,22 @@ const WorkspaceContentMoveModal = ({
 
         {isLoading ? (
           <CircularProgress size={24} />
+        ) : !filteredWorkspaces?.length ? (
+          <NoWorkspacesContainer>
+            <Typography>No other workspaces available to move content to.</Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleCreateWorkspace}
+              disabled={!CAN(keys.CREATE_WORKSPACE.action, keys.CREATE_WORKSPACE.subject)}
+            >
+              Create Workspace
+            </Button>
+          </NoWorkspacesContainer>
         ) : (
           <>
             <Typography style={{ marginBottom: '0.5rem' }}>Select destination workspace</Typography>
-            {filteredWorkspaces?.map((workspace) => (
+            {filteredWorkspaces.map((workspace) => (
               <WorkspaceItem
                 key={workspace.id}
                 selected={selectedWorkspaceForMove?.id === workspace.id}
@@ -118,80 +199,11 @@ const WorkspaceContentMoveModal = ({
           primaryText={'Move'}
           secondaryText="Cancel"
           primaryButtonProps={{
-            onClick: () => {
-              setWorkspaceContentMoveModal(false);
-              {
-                if (RESOURCE_TYPE.DESIGN === type) {
-                  multiSelectedContent.length > 0 &&
-                    multiSelectedContent.map((design) => {
-                      return assignDesignToWorkspace({
-                        workspaceId: selectedWorkspaceForMove.id,
-                        designId: design.id,
-                      })
-                        .unwrap()
-                        .then(() => {
-                          unAssignDesignFromWorkspace({
-                            workspaceId: currentWorkspace.id,
-                            designId: design.id,
-                          });
-                        })
-                        .then(() => {
-                          setMultiSelectedContent([]);
-                        });
-                    });
-                  if (selectedContent) {
-                    assignDesignToWorkspace({
-                      workspaceId: selectedWorkspaceForMove.id,
-                      designId: selectedContent.id,
-                    })
-                      .unwrap()
-                      .then(() => {
-                        unAssignDesignFromWorkspace({
-                          workspaceId: currentWorkspace.id,
-                          designId: selectedContent.id,
-                        });
-                      });
-                  }
-                } else {
-                  multiSelectedContent.length > 0 &&
-                    multiSelectedContent.map((view) => {
-                      return assignViewToWorkspace({
-                        workspaceId: selectedWorkspaceForMove.id,
-                        viewId: view.id,
-                      })
-                        .unwrap()
-                        .then(() => {
-                          unAssignViewFromWorkspace({
-                            workspaceId: currentWorkspace.id,
-                            viewId: view.id,
-                          });
-                        })
-                        .then(() => {
-                          setMultiSelectedContent([]);
-                        });
-                    });
-                  if (selectedContent) {
-                    assignViewToWorkspace({
-                      workspaceId: selectedWorkspaceForMove.id,
-                      viewId: selectedContent.id,
-                    })
-                      .unwrap()
-                      .then(() => {
-                        unAssignViewFromWorkspace({
-                          workspaceId: currentWorkspace.id,
-                          viewId: selectedContent.id,
-                        });
-                      });
-                  }
-                }
-              }
-            },
-            disabled: isLoading || !selectedWorkspaceForMove,
+            onClick: handleMove,
+            disabled: isLoading || !selectedWorkspaceForMove || !filteredWorkspaces?.length,
           }}
           secondaryButtonProps={{
-            onClick: () => {
-              setWorkspaceContentMoveModal(false);
-            },
+            onClick: () => setWorkspaceContentMoveModal(false),
           }}
         />
       </ModalFooter>
