2bde008f5cc2fc5cc5c368008f3a3cad41c85803
winkletinkle <182693777+winkletinkle@users.noreply.github.com>
2025-06-02 16:22:20 -0500

    Update Makefile.core.mk: Add remote provider for Exoscale
    Signed-off-by: winkletinkle <182693777+winkletinkle@users.noreply.github.com>

--- install/Makefile.core.mk
+++ install/Makefile.core.mk
@@ -36,12 +36,15 @@ SHELL := /usr/bin/env bash -o pipefail
 ADAPTER_URLS := "localhost:10000 localhost:10001 localhost:10012 localhost:10013"
 
 #-----------------------------------------------------------------------------
-# Providers
+# Providers (Add your provider here. See https://docs.meshery.io/extensibility/providers)
 #-----------------------------------------------------------------------------
 REMOTE_PROVIDER_LOCAL="http://localhost:9876"
 MESHERY_CLOUD_DEV="http://localhost:9876"
 MESHERY_CLOUD_PROD="https://cloud.layer5.io"
 MESHERY_CLOUD_STAGING="https://staging-cloud.layer5.io"
+EXOSCALE_STG="https://stg-sks.exoscale.com"
+EXOSCALE_PROD="https://dev-sks.exoscale.com"
+EXOSCALE_PROD="https://dev-sks.exoscale.com"
 
 #-----------------------------------------------------------------------------
 # Server
