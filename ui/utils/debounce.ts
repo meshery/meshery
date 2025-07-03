f19b9879e8cd45383cd28d0b4ada9502eb8ee300
Ranit Mukherjee <littleranit@gmail.com>
2025-07-03 21:41:54 +0530

    fix: update variable declarations from var to let for improved scoping
    Signed-off-by: Ranit Mukherjee <littleranit@gmail.com>

--- ui/utils/debounce.ts
+++ ui/utils/debounce.ts
@@ -8,7 +8,7 @@
  */
 export default function debounce<T extends (...args: any[]) => void>(
   func: T,
-  timeout = 500
+  timeout = 500,
 ): (...args: Parameters<T>) => void {
   let timer: ReturnType<typeof setTimeout>;
   return (...args: Parameters<T>) => {
