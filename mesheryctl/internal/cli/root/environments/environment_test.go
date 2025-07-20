dfd39e329a5ad92675f895156bcfa11bbc9f7b55
Wyllie Fang <wylliefang@gmail.com>
2025-07-19 18:47:52 -0700

    fix uuid in orgID

--- mesheryctl/internal/cli/root/environments/environment_test.go
+++ mesheryctl/internal/cli/root/environments/environment_test.go
@@ -5,6 +5,6 @@ import "flag"
 var update = flag.Bool("update", false, "update golden files")
 
 var testConstants = map[string]string{
-	"orgID":           "3f8319e0-33a9-4736-b248-12nm3kiuh3yu",
+	"orgID":           "3f8319e0-33a9-4736-b248-12bc3def4a56",
 	"environmentName": "test-environment",
 }
