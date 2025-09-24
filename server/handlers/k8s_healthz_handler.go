72caafe29654030c0cb01bda3052221553f4ef0b
Babbili <babbili.dxb@gmail.com>
2025-09-24 10:33:25 +0400

    modify to status header
    Signed-off-by: Babbili <babbili.dxb@gmail.com>

--- server/handlers/k8s_healthz_handler.go
+++ server/handlers/k8s_healthz_handler.go
@@ -3,8 +3,5 @@ package handlers
 import "net/http"
 
 func (h *Handler) K8sHealthzHandler(w http.ResponseWriter, r *http.Request) {
-
-	if _, err := w.Write([]byte("ok")); err != nil {
-		http.Error(w, "K8s Health Probe Error", http.StatusInternalServerError)
-	}
+	w.WriteHeader(http.StatusOK)
 }
