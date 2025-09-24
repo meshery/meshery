9e1733faca88802ee05fcfd1cc9cd0b3c6801887
ianwhitney <ian.r.whitney@gmail.com>
2025-09-24 07:04:11 -0600

    feat: css fix
    Signed-off-by: ianwhitney <ian.r.whitney@gmail.com>

--- ui/components/Settings/MesherySettings.js
+++ ui/components/Settings/MesherySettings.js
@@ -247,7 +247,7 @@ const MesherySettings = () => {
               <Tabs
                 value={tabVal}
                 onChange={handleChange('tabVal')}
-                variant='fullWidth'
+                variant="fullWidth"
                 scrollButtons="on"
                 indicatorColor="primary"
                 textColor="primary"
