71c0351bac735f433b400b61a9ff3e8949cd1fa3
Babbili <babbili.dxb@gmail.com>
2025-09-30 12:11:51 +0400

    update to /healthz/[live|ready] endpoints
    Signed-off-by: Babbili <babbili.dxb@gmail.com>

--- server/router/server.go
+++ server/router/server.go
@@ -424,7 +424,8 @@ func NewRouter(_ context.Context, h models.HandlerInterface, port int, g http.Ha
 		Methods("GET")
 
 	// Kubernetes Health Probes
-	gMux.HandleFunc("/healthz", h.K8sHealthzHandler).Methods("GET")
+	gMux.HandleFunc("/healthz/live", h.K8sHealthzHandler).Methods("GET")
+	gMux.HandleFunc("/healthz/ready", h.K8sHealthzHandler).Methods("GET")
 
 	// Swagger Interactive Playground
 	swaggerOpts := middleware.SwaggerUIOpts{SpecURL: "./swagger.yaml"}
