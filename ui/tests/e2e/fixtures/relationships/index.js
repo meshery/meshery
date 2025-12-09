8fed12f07ff7258e1feb5c54a20b352c3a5f6788
Aabid <mailtoaabid01@gmail.com>
2025-12-09 18:13:48 +0530

    add more relationship tests
    Signed-off-by: Aabid <mailtoaabid01@gmail.com>

--- ui/tests/e2e/fixtures/relationships/index.js
+++ ui/tests/e2e/fixtures/relationships/index.js
@@ -3,6 +3,9 @@ import configmapPodReferenceFixture from './configmap-pod-edge-non-binding-refer
 import namespaceHierarchicalParentInventoryFixture from './namespace-hierarchical-parent-inventory-fixture.json';
 import containerHierarchicalParentAliasFixture from './container-hierarchical-parent-alias-fixture.json';
 import mesheryDesignFixture from './meshery-design-fixture.json';
+import pvPvcReferenceFixture from './pv-pvc-edge-non-binding-reference-fixture.json';
+import secretDeploymentReferenceFixture from './secret-deployment-edge-non-binding-reference-fixture.json';
+import roleRolebindingPermissionFixture from './role-rolebinding-serviceaccount-edge-binding-permission-fixture.json';
 
 
 export const RelationshipTestFixtures = [
@@ -10,5 +13,8 @@ export const RelationshipTestFixtures = [
     namespaceHierarchicalParentInventoryFixture,
     configmapPodReferenceFixture,
     containerHierarchicalParentAliasFixture,
-    mesheryDesignFixture
+    mesheryDesignFixture,
+    pvPvcReferenceFixture,
+    secretDeploymentReferenceFixture,
+    roleRolebindingPermissionFixture
 ];
