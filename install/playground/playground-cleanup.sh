d52ce81f8253662024b81eb0ab58d1e806b0b5e3
Sangram Rath <sangram.rath@gmail.com>
2025-12-10 19:08:28 +0530

    Add new valid namespaces to cleanup script
    Signed-off-by: Sangram Rath <sangram.rath@gmail.com>
    Signed-off-by: ItsYash1421 <yashamanmeena2@gmail.com>
    Signed-off-by: YashKumarMeena001 <yashkumar.meena@infyair.com>

--- install/playground/playground-cleanup.sh
+++ install/playground/playground-cleanup.sh
@@ -2,7 +2,7 @@
 #TODO: Make the cleanup smarter than basic namespace deletion
 #Script is placed in /root directory in VM.
 
-valid_namespaces=("kube-system" "default" "monitoring" "kube-flannel" "kube-node-lease" "kube-public" "meshery" "metallb-system" "projectcontour" "ingress-nginx" "layer5-cloud" "postgres" "staging-fullstack")
+valid_namespaces=("kube-system" "default" "monitoring" "kube-flannel" "kube-node-lease" "kube-public" "meshery" "metallb-system" "projectcontour" "ingress-nginx" "layer5-cloud" "postgres" "staging-fullstack" "cnpg" "cnpg-system" "cnpg-postgres" "cloud" "layer5" "local-path-storage")
 
 for ns in $(kubectl get ns -o jsonpath="{.items[*].metadata.name}"); 
 do
