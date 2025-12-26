cc0175a876a63f07004257e837c60fa0390c6524
Aabid Sofi <mailtoaabid01@gmail.com>
2025-12-26 16:35:46 +0530

    instrument mux
    Signed-off-by: Aabid Sofi <mailtoaabid01@gmail.com>

--- server/router/server.go
+++ server/router/server.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/go-openapi/runtime/middleware"
-
+"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux"
 	"github.com/gorilla/mux"
 	"github.com/meshery/meshery/server/models"
 )
@@ -21,6 +21,7 @@ type Router struct {
 // NewRouter returns a new ServeMux with app routes.
 func NewRouter(_ context.Context, h models.HandlerInterface, port int, g http.Handler, gp http.Handler) *Router {
 	gMux := mux.NewRouter()
+  gMux.Use(otelmux.Middleware("meshery-server"))
 
 	gMux.Handle("/api/system/graphql/query", h.ProviderMiddleware(h.AuthMiddleware(h.SessionInjectorMiddleware(h.GraphqlMiddleware(g)), models.ProviderAuth))).Methods("GET", "POST")
 	gMux.Handle("/api/system/graphql/playground", h.ProviderMiddleware(h.AuthMiddleware(h.SessionInjectorMiddleware(h.GraphqlMiddleware(gp)), models.ProviderAuth))).Methods("GET", "POST")
