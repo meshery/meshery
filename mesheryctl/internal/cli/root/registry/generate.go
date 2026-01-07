a72020526641b13d5fa95dd0302f7485783a7616
Lee Calcote <lee.calcote@layer5.io>
2026-01-06 22:18:09 -0600

    remove use of non-existant function
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- mesheryctl/internal/cli/root/registry/generate.go
+++ mesheryctl/internal/cli/root/registry/generate.go
@@ -132,9 +132,6 @@ mesheryctl registry generate --spreadsheet-id "1DZHnzxYWOlJ69Oguz4LkRVTFM79kC2tu
 	},
 
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// Ensure log files are closed when function returns
-		defer meshkitRegistryUtils.CloseLogger()
-
 		var wg sync.WaitGroup
 		cwd, _ = os.Getwd()
 		registryLocation = filepath.Join(cwd, outputLocation)
