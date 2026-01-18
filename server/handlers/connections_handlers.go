cd77ca5b6d9accde812a3e1688cfde11b3e7e79c
Ady0333 <adityashinde1525@gmail.com>
2026-01-19 03:47:41 +0530

    fix(server): handle error from GetProviderToken in UpdateConnectionById
    Signed-off-by: Ady0333 <adityashinde1525@gmail.com>

--- server/handlers/connections_handlers.go
+++ server/handlers/connections_handlers.go
@@ -441,6 +441,11 @@ func (h *Handler) UpdateConnectionById(w http.ResponseWriter, req *http.Request,
 	}
 
 	token, err := provider.GetProviderToken(req)
+	if err != nil {
+		h.log.Error(ErrGetResult(err))
+		http.Error(w, ErrGetResult(err).Error(), http.StatusInternalServerError)
+		return
+	}
 	updatedConnection, err := provider.UpdateConnectionById(token, connection, mux.Vars(req)["connectionId"])
 	if err != nil {
 		_err := ErrFailToSave(err, obj)
