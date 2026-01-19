40c2e5ee49d2a80a1039950f199621ba3855db1e
marblom007 <158522975+marblom007@users.noreply.github.com>
2026-01-19 15:42:07 -0600

    [UI] Nextjs script warning addressed. Interactive strategy used on script load.
    Signed-off-by: marblom007 <158522975+marblom007@users.noreply.github.com>

--- ui/pages/_app.js
+++ ui/pages/_app.js
@@ -5,6 +5,7 @@ import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import 'billboard.js/dist/theme/dark.min.css';
 import _ from 'lodash';
 import Head from 'next/head';
+import Script from 'next/script';
 import { SnackbarProvider } from 'notistack';
 import React, { useEffect, useMemo, useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -712,6 +713,8 @@ const MesheryAppWrapper = (props) => {
         <link rel="shortcut icon" href="/static/img/meshery-logo/meshery-logo.svg" />
         <title>Meshery</title>
       </Head>
+      {/* Load loading messages before interactive */}
+      <Script src="/loadingMessages.js" strategy="beforeInteractive" />
       <LocalizationProvider dateAdapter={AdapterMoment}>
         <MesheryApp {...props} />
       </LocalizationProvider>
