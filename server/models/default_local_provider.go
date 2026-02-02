a86f901f633fb057255f578622d5599d13acf197
KhaledSayed04 <KhaledSA15Me@gmail.com>
2026-02-02 07:07:13 +0200

    fix(server): explicitly ignore fmt return values (staticcheck)
    Signed-off-by: KhaledSayed04 <KhaledSA15Me@gmail.com>

--- server/models/default_local_provider.go
+++ server/models/default_local_provider.go
@@ -1762,7 +1762,7 @@ func downloadContent(comp string, downloadpath string) error {
 			if err != nil {
 				return err
 			}
-			fmt.Fprintf(file, "%s", content)
+			_, _ = fmt.Fprintf(file, "%s", content)
 			return nil
 		}).Walk()
 	case "Filter":
