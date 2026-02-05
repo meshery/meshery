27bbf88d2894dc1970d441ed25f1aa325c6f57cb
Suryansh Garg <suryanshgarg04@gmail.com>
2026-02-05 13:51:09 +0000

    test: restore global config state between initConfig subtests
    Signed-off-by: Suryansh Garg <suryanshgarg04@gmail.com>

--- mesheryctl/internal/cli/root/root_initconfig_test.go
+++ mesheryctl/internal/cli/root/root_initconfig_test.go
@@ -79,6 +79,18 @@ func TestInitConfigUseCases(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			viper.Reset()
 
+			// Save global state
+			origMesheryFolder := utils.MesheryFolder
+			origDefaultConfigPath := utils.DefaultConfigPath
+			origCfgFile := cfgFile
+
+			// Restore after each test
+			t.Cleanup(func() {
+				utils.MesheryFolder = origMesheryFolder
+				utils.DefaultConfigPath = origDefaultConfigPath
+				cfgFile = origCfgFile
+			})
+
 			tt.setup(t)
 
 			// ensure default config path branch is exercised
