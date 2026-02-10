906046cb04fffa967c40790040bf0cdaa67c1a5d
Pragalva Sapkota <pragalva.sapkota@deerwalk.edu.np>
2026-02-10 18:48:43 +0545

    cli message enhancement
    Signed-off-by: Pragalva Sapkota <pragalva.sapkota@deerwalk.edu.np>

--- mesheryctl/internal/cli/root/system/provider.go
+++ mesheryctl/internal/cli/root/system/provider.go
@@ -360,7 +360,7 @@ mesheryctl system provider reset
 			return nil
 		}
 
-		log.Info("Provider has been reset. Users may need to log out or clear existing browser sessions to see the provider selection UI on the next Meshery start.")
+		log.Info("Provider has been reset. Users will be prompted to select a provider on next Meshery start. If not, log out or clear existing browser sessions.")
 		return nil
 	},
 }
