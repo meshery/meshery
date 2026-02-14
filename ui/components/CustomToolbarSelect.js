745baeb06793f11852dbc715298ed75b195103a1
Bhumika Garg <bhumikatech2024@gmail.com>
2026-02-14 22:11:07 +0530

    change mui icon
    Signed-off-by: Bhumika Garg <bhumikatech2024@gmail.com>

--- ui/components/CustomToolbarSelect.js
+++ ui/components/CustomToolbarSelect.js
@@ -1,9 +1,13 @@
+import {
+  IndeterminateCheckBox,
+  CompareArrows,
+  GetApp,
+  IconButton,
+  Tooltip, 
+  styled, 
+  NoSsr
+} from '@sistent/sistent';
 import React, { useState } from 'react';
-import { IconButton, Tooltip, styled } from '@sistent/sistent';
-import IndeterminateCheckBoxIcon from '@mui/icons-material/IndeterminateCheckBox';
-import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
-import { NoSsr } from '@sistent/sistent';
-import GetAppIcon from '@mui/icons-material/GetApp';
 import MesheryChartDialog from './MesheryChartDialog';
 import MesheryChart from './MesheryChart';
 import { useDispatch, useSelector } from 'react-redux';
@@ -96,7 +100,7 @@ function CustomToolbarSelect({ setSelectedRows }) {
           <Tooltip title="Deselect ALL">
             <StyledIconButton onClick={handleClickDeselectAll}>
               <StyledIcon>
-                <IndeterminateCheckBoxIcon />
+                <IndeterminateCheckBox />
               </StyledIcon>
             </StyledIconButton>
           </Tooltip>
@@ -110,7 +114,7 @@ function CustomToolbarSelect({ setSelectedRows }) {
                 download={`${fullData[0].name}_test_result.json`}
               >
                 <StyledIcon>
-                  <GetAppIcon />
+                  <GetApp />
                 </StyledIcon>
               </StyledIconButton>
             </Tooltip>
@@ -118,7 +122,7 @@ function CustomToolbarSelect({ setSelectedRows }) {
           <Tooltip title="Compare selected">
             <StyledIconButton onClick={handleCompareSelected}>
               <StyledIcon>
-                <CompareArrowsIcon />
+                <CompareArrows />
               </StyledIcon>
             </StyledIconButton>
           </Tooltip>
