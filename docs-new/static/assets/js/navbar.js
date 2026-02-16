38c05b86e3185a20483c564e733e049f5b2002fb
Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>
2026-02-16 22:54:51 +0530

    Enabled dark-light mode for new docs
    Signed-off-by: Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>

---
+++ docs-new/static/assets/js/navbar.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const toggleBtn = document.getElementById("mode-toggle-btn");
+toggleBtn.addEventListener("click", setMode);
+
+function setMode() {
+    document.body.classList.toggle("dark-mode");
+    updateLogos();
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("mode", "dark-mode");
+    } else {
+        localStorage.setItem("mode", "light-mode");
+    }
+}
+
+function updateLogos() {
+    const allLogos = document.querySelectorAll("#logo-dark-light");
+    allLogos.forEach(logo => {
+        if (document.body.classList.contains("dark-mode")) {
+            logo.src = logo.dataset.logoForDark;
+        } else {
+            logo.src = logo.dataset.logoForLight;
+        }
+    });
+}
