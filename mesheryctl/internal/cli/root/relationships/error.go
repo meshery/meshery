1a6faad490657285bbc9d885a8f2c94446910275
DharunMR <maddharun56@gmail.com>
2026-02-21 13:46:29 +0530

    changed test  cases
    Signed-off-by: DharunMR <maddharun56@gmail.com>

--- mesheryctl/internal/cli/root/relationships/error.go
+++ mesheryctl/internal/cli/root/relationships/error.go
@@ -6,4 +6,5 @@ var (
 	viewUsageMsg           = "\n\nUsage: mesheryctl exp relationship view [model-name]\nRun 'mesheryctl exp relationship view --help' to see detailed help message"
 	errNoModelNameProvided = fmt.Errorf("[model-name] isn't specified%s", viewUsageMsg)
 	errTooManyArgs         = fmt.Errorf("too many arguments, only [model-name] is expected%s", viewUsageMsg)
+	errMsg                 = "[ Spreadsheet ID | Spreadsheet Credentials ] aren't specified\n\nUsage: mesheryctl exp relationship generate --spreadsheet-id [Spreadsheet ID] --spreadsheet-cred $CRED\nRun 'mesheryctl exp relationship generate --help' to see detailed help message"
 )
