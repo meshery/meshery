c5a9cf2f16ab9f3f100069c64c210c193590d9c5
DharunMR <maddharun56@gmail.com>
2026-02-22 20:13:04 +0530

    changed msg
    Signed-off-by: DharunMR <maddharun56@gmail.com>

--- mesheryctl/internal/cli/root/components/error.go
+++ mesheryctl/internal/cli/root/components/error.go
@@ -2,7 +2,7 @@ package components
 
 const (
 	errViewCmdMsg      = "Usage: mesheryctl component view [component-name]\nRun 'mesheryctl component view --help' to see detailed help message"
-	errNoArg           = "no arguments specified"
-	errMultiArg        = "too many arguments specified"
+	errNoArg           = "no component name specified as an arugment"
+	errMultiArg        = "too many argument specified, only one component name is accepted"
 	searchUsageMessage = "Usage: mesheryctl component search [component-name]\nRun 'mesheryctl component search --help' to see detailed help message"
 )
