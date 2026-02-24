48175eb28c2c80e64972aaba19ce18f2487bc141
Bhumika Garg <bhumikatech2024@gmail.com>
2026-02-24 22:01:20 +0530

    fix: suggestion given by gemini
    Signed-off-by: Bhumika Garg <bhumikatech2024@gmail.com>

--- docs-new/static/js/main.js
+++ docs-new/static/js/main.js
@@ -1,20 +1,15 @@
 /*clipboard*/
 
-var getcodeelement = $('.clipboardjs'); /*create custom id*/
+const codeBlockElements = $('.clipboardjs'); /*create custom id*/
 
-getcodeelement.each(function(i) {  
+codeBlockElements.each(function(i) {  
     /*target*/
     var currentId = 'codeblock' + (i + 1);
     $(this).attr('id', currentId);
 
     /*trigger*/
     var text = $(this).text();
-    text = text.replace(/\$ /gi, '')
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/'/g, "&#39;")
-    .replace(/"/g, "&quot;");
+    text = text.replace(/\$ /gi, '').replace(/"/g, "&quot;");
     var clipButton = '<div class="btn-copy-wrap"><button class="clipbtn" data-clipboard-text="' + text + '" data-clipboard-target="#' + currentId + '"><i class="far fa-copy"></i></button></div>';
        $(this).after(clipButton);
 });
@@ -22,19 +17,17 @@ getcodeelement.each(function(i) {
 var clipboard = new Clipboard('.clipbtn');
 
 /* Change copy icon to text when successfully copied*/
-clipboard.on("success", (e)=>{
-    console.info(e.trigger);
-    console.info(e.trigger.childNodes[0]);
-    let originalIcon = e.trigger.childNodes[0];
+clipboard.on("success", (e) => {
+    const originalIcon = e.trigger.querySelector('i');
+    if (!originalIcon) return;
 
-    var icon = e.trigger.childNodes[0];
-    var text = document.createElement('span');
-    text.textContent = "Copied!";
-    text.style.color = "white";
+    const feedbackText = document.createElement('span');
+    feedbackText.textContent = "Copied!";
+    feedbackText.classList.add('clipboard-success');
 
-    e.trigger.replaceChild(text, icon);
+    e.trigger.replaceChild(feedbackText, originalIcon);
 
-    setTimeout(()=>{
-        e.trigger.replaceChild(originalIcon, text);
-    },2000)
-})
+    setTimeout(() => {
+        e.trigger.replaceChild(originalIcon, feedbackText);
+    }, 2000);
+});
