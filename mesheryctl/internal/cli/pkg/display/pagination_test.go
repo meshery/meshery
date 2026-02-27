d3b6d20ab4329b36c7b4e86a7e87191a77f548f6
Shrikant Shingare <shrikantshingare77@gmail.com>
2026-02-27 23:17:25 +0530

    Update test to match generic no results error message
    Signed-off-by: Shrikant Shingare <shrikantshingare77@gmail.com>

--- mesheryctl/internal/cli/pkg/display/pagination_test.go
+++ mesheryctl/internal/cli/pkg/display/pagination_test.go
@@ -251,7 +251,7 @@ func TestPromptAsyncPagination(t *testing.T) {
 			apiStatusCode: 200,
 			hasToken:      true,
 			expectError:   true,
-			errContains:   "no results for nonexistent",
+			errContains:   "no results found",
 		},
 		{
 			name:       "Given_Single_Result_When_PromptAsyncPagination_Then_AutoSelect",
