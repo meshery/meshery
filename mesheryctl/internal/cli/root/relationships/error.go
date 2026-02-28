e252cf0ea9b0c1ee9f634a1985458a267aef0aa0
Shrikant Shingare <shrikantshingare77@gmail.com>
2026-02-28 11:11:45 +0530

    refactor(mesheryctl): use shared PromptAsyncPagination
    Signed-off-by: Shrikant Shingare <shrikantshingare77@gmail.com>

--- mesheryctl/internal/cli/root/relationships/error.go
+++ mesheryctl/internal/cli/root/relationships/error.go
@@ -3,7 +3,8 @@ package relationships
 import "fmt"
 
 var (
-	viewUsageMsg           = "\n\nUsage: mesheryctl exp relationship view [model-name]\nRun 'mesheryctl exp relationship view --help' to see detailed help message"
-	errNoModelNameProvided = fmt.Errorf("[model-name] isn't specified%s", viewUsageMsg)
-	errTooManyArgs         = fmt.Errorf("too many arguments, only [model-name] is expected%s", viewUsageMsg)
+	viewUsageMsg               = "\n\nUsage: mesheryctl exp relationship view [model-name]\nRun 'mesheryctl exp relationship view --help' to see detailed help message"
+	errNoModelNameProvided     = fmt.Errorf("[model-name] isn't specified%s", viewUsageMsg)
+	errTooManyArgs             = fmt.Errorf("too many arguments, only [model-name] is expected%s", viewUsageMsg)
+	errRelationshipNotFoundMsg = "No relationship(s) found for the model with name: "
 )
