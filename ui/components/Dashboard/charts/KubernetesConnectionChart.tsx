9e77418b8302bebda796b598b67af55c08805cb7
Oluwatunmise-olat <oluwatunmiseolatunbosun2001@gmail.com>
2026-03-07 13:22:14 +0100

    [UI] Refine Kubernetes cluster status tooltip copy
    Signed-off-by: Oluwatunmise-olat <oluwatunmiseolatunbosun2001@gmail.com>

--- ui/components/Dashboard/charts/KubernetesConnectionChart.tsx
+++ ui/components/Dashboard/charts/KubernetesConnectionChart.tsx
@@ -95,7 +95,7 @@ export default function KubernetesConnectionStatsChart() {
             </Typography>
           </div>
           <div onClick={(e) => e.stopPropagation()}>
-            <CustomTextTooltip title="This chart shows the status of your connections to Kubernetes clusters.">
+            <CustomTextTooltip title="This chart shows the status of connections to your Kubernetes clusters.">
               <div>
                 <InfoOutlined
                   color={theme.palette.icon.default}
