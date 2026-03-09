be40a48447b9e54b77ffff31ed813b3838b29d32
DharunMR <maddharun56@gmail.com>
2026-03-09 07:13:12 +0530

    Added reference for relationship csv in meshkit err
    Signed-off-by: DharunMR <maddharun56@gmail.com>

--- mesheryctl/internal/cli/root/relationships/error.go
+++ mesheryctl/internal/cli/root/relationships/error.go
@@ -16,9 +16,9 @@ func ErrEmptySheetData(err error) error {
 	return errors.New(
 		ErrEmptySheetDataCode,
 		errors.Alert,
-		[]string{"Invalid spreadsheet"},
+		[]string{"Invalid or empty spreadsheet provided"},
 		[]string{err.Error()},
-		[]string{"Ensure the spreadsheet contains valid relationship entries before importing"},
-		[]string{"Spreadsheet must contain two headers and values for it's headers"},
+		[]string{"The spreadsheet must contain valid relationship entries with the required headers."},
+		[]string{"Ensure the spreadsheet contains at least two headers and corresponding values for it's headers. Refer to the Meshery relationship csv for the expected spreadsheet format \"https://github.com/meshery/meshery/blob/master/mesheryctl/templates/template-csvs/Relationships.csv\""},
 	)
 }
