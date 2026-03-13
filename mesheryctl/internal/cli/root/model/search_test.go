df47ba9e1614c196eaa2ad8992e17a2a89158eb5
lekaf974 <matthieu.evrin@gmail.com>
2026-03-13 18:46:24 -0400

    refactor(mesheryctl): enhance model search command with flag validation and initialization
    Signed-off-by: lekaf974 <matthieu.evrin@gmail.com>

--- mesheryctl/internal/cli/root/model/search_test.go
+++ mesheryctl/internal/cli/root/model/search_test.go
@@ -8,6 +8,7 @@ import (
 	"runtime"
 	"testing"
 
+	mesheryctlflags "github.com/meshery/meshery/mesheryctl/internal/cli/pkg/flags"
 	"github.com/meshery/meshery/mesheryctl/pkg/utils"
 )
 
@@ -42,5 +43,6 @@ func TestSearchModel(t *testing.T) {
 		},
 	}
 
+	mesheryctlflags.InitValidators(ModelCmd)
 	utils.InvokeMesheryctlTestListCommand(t, update, ModelCmd, tests, currDir, "model")
 }
