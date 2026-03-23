f06921310c2c9fc8137026cc47c8f30f9d6aaac0
Pragalva <sapkotapragalva@gmail.com>
2026-03-23 17:34:58 +0545

    update example for better representation
    Signed-off-by: Pragalva <sapkotapragalva@gmail.com>

--- mesheryctl/internal/cli/root/model/generate.go
+++ mesheryctl/internal/cli/root/model/generate.go
@@ -43,7 +43,7 @@ var generateModelCmd = &cobra.Command{
 	Long: `Generate models by specifying the directory, file, or URL. You can also provide a template JSON file and registrant name
 Find more information at: https://docs.meshery.io/reference/mesheryctl/model/generate`,
 	Example: ` 
-// Generate a model from a CSV file(s)
+// Generate a model from a CSV directory
 mesheryctl model generate --f [path-to-csv-directory]
 
 // Generate a model from a URL based on a JSON template
