91206e0f5d3a7e5956c168d62d1906dc13195049
miacycle <184569369+miacycle@users.noreply.github.com>
2026-04-01 23:56:20 -0500

    dataFetch migration: add system sync RTK endpoint, remove dead helpers
    - Add getSystemSync and getKubernetesContexts RTK Query endpoints
    - Migrate Header.tsx from promisifiedDataFetch to useLazyGetSystemSyncQuery
    - Migrate adapters.tsx from promisifiedDataFetch to useLazyGetSystemSyncQuery
    - Remove dead code: serviceMeshes.ts and metrics.ts (zero external imports)
    
    Signed-off-by: l <l@meshery.io>
    Signed-off-by: Arjun Mehta <231106746+arjunmehta-git@users.noreply.github.com>

--- ui/components/Header.tsx
+++ ui/components/Header.tsx
@@ -6,7 +6,7 @@ import { Search } from '@mui/icons-material';
 import { deleteKubernetesConfig } from '../utils/helpers/kubernetesHelpers';
 import { successHandlerGenerator, errorHandlerGenerator } from '../utils/helpers/common';
 import { ConnectionChip } from './connections/ConnectionChip';
-import { promisifiedDataFetch } from '../lib/data-fetch';
+import { useLazyGetSystemSyncQuery } from '../rtk-query/system';
 import _PromptComponent from './PromptComponent';
 import { iconMedium, iconSmall } from '../css/icons.styles';
 import { createPathForRemoteComponent } from './ExtensionSandbox';
@@ -65,19 +65,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { updateK8SConfig } from '@/store/slices/mesheryUi';
 import { ErrorBoundary } from '@sistent/sistent';
 import { WorkspaceModalContext } from '../utils/context/WorkspaceModalContextProvider';
-
-async function loadActiveK8sContexts() {
-  try {
-    const res = await promisifiedDataFetch('/api/system/sync');
-    if (res?.k8sConfig) {
-      return res.k8sConfig;
-    } else {
-      throw new Error('No kubernetes configurations found');
-    }
-  } catch (e) {
-    console.error('An error occurred while loading k8sconfig', e);
-  }
-}
 
 const K8sContextConnectionChip_ = ({
   ctx,
@@ -156,6 +143,7 @@ function K8sContextMenu({
   const [transformProperty, setTransformProperty] = useState(100);
   const deleteCtxtRef = React.createRef();
   const { notify } = useNotification();
+  const [fetchSystemSync] = useLazyGetSystemSyncQuery();
   const { controllerState: meshsyncControllerState } = useSelector((state) => state.ui);
   const dispatch = useDispatch();
   const { connectionMetadataState } = useSelector((state) => state.ui);
@@ -226,9 +214,13 @@ function K8sContextMenu({
     });
     if (responseOfDeleteK8sCtx === 'CONFIRM') {
       const successCallback = async () => {
-        const updatedConfig = await loadActiveK8sContexts();
-        if (Array.isArray(updatedConfig)) {
-          dispatch(updateK8SConfig({ k8sConfig: updatedConfig }));
+        try {
+          const res = await fetchSystemSync().unwrap();
+          if (Array.isArray(res?.k8sConfig)) {
+            dispatch(updateK8SConfig({ k8sConfig: res.k8sConfig }));
+          }
+        } catch (e) {
+          console.error('An error occurred while loading k8sconfig', e);
         }
       };
       deleteKubernetesConfig(
--- ui/rtk-query/system.ts
+++ ui/rtk-query/system.ts
@@ -3,6 +3,7 @@ import { api } from './index';
 const TAGS = {
   SYSTEM: 'system',
   ADAPTERS: 'adapters',
+  SYNC: 'sync',
 };
 
 const systemApi = api.injectEndpoints({
@@ -46,6 +47,27 @@ const systemApi = api.injectEndpoints({
       }),
       providesTags: (result, error, adapterLoc) => [{ type: TAGS.ADAPTERS, id: adapterLoc }],
     }),
+    getSystemSync: builder.query({
+      query: () => ({
+        url: 'system/sync',
+        method: 'GET',
+        credentials: 'include',
+      }),
+      providesTags: [TAGS.SYNC],
+    }),
+
+    getKubernetesContexts: builder.query({
+      query: (queryArg) => ({
+        url: 'system/kubernetes/contexts',
+        params: {
+          pagesize: queryArg?.pagesize || 10,
+          search: queryArg?.search || '',
+        },
+        method: 'GET',
+      }),
+      providesTags: [TAGS.SYSTEM],
+    }),
+
     manageAdapter: builder.mutation({
       query: (queryArg) => {
         if (queryArg.method === 'DELETE') {
@@ -76,4 +98,8 @@ export const {
   useGetAvailableAdaptersQuery,
   useLazyPingAdapterQuery,
   useManageAdapterMutation,
+  useGetSystemSyncQuery,
+  useLazyGetSystemSyncQuery,
+  useGetKubernetesContextsQuery,
+  useLazyGetKubernetesContextsQuery,
 } = systemApi;
