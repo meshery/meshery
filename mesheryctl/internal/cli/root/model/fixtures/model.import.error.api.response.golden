34250a2551b5c29eb3fb082c5ce2b4b103243f56
Umesh <tiruvalluruumesh@gmail.com>
2026-04-02 19:22:47 +0530

    [mesheryctl] return proper errors in model import
    Signed-off-by: Umesh <tiruvalluruumesh@gmail.com>

---
+++ mesheryctl/internal/cli/root/model/fixtures/model.import.error.api.response.golden
@@ -0,0 +1,1 @@
+internal server error
--- mesheryctl/internal/cli/root/model/import_test.go
+++ mesheryctl/internal/cli/root/model/import_test.go
@@ -1,8 +1,13 @@
 package model
 
 import (
+	"errors"
+	"path/filepath"
+	"runtime"
 	"testing"
 
+	mesheryctlflags "github.com/meshery/meshery/mesheryctl/internal/cli/pkg/flags"
+	"github.com/meshery/meshery/mesheryctl/pkg/utils"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -36,4 +41,30 @@ func TestHasCSVs(t *testing.T) {
 			assert.Equal(t, tc.expectedResult, res)
 		})
 	}
+}
+
+func TestImportModelReturnsErrorForURLImportFailure(t *testing.T) {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		t.Fatal("Not able to get current working directory")
+	}
+
+	currDir := filepath.Dir(filename)
+
+	tests := []utils.MesheryCommandTest{
+		{
+			Name:           "given a URL import failure when importing model then return the error",
+			Args:           []string{"import", "https://example.com/model"},
+			URL:            "/api/meshmodels/register",
+			HttpMethod:     "POST",
+			HttpStatusCode: 500,
+			Fixture:        "model.import.error.api.response.golden",
+			ExpectError:    true,
+			ExpectedError:  utils.ErrMesheryServerInternalError(errors.New("internal server error\n")),
+			IsOutputGolden: false,
+		},
+	}
+
+	mesheryctlflags.InitValidators(ModelCmd)
+	utils.InvokeMesheryctlTestCommand(t, update, ModelCmd, tests, currDir, "model")
 }
