fc9d2e7e469d868cf270d2db49b42b96b3bb931d
Aditya Upasani <adityaupasani29@gmail.com>
2026-04-04 20:00:12 +0100

    fix(mesheryctl): return errors instead of nil in restart command
    Signed-off-by: Aditya Upasani <adityaupasani29@gmail.com>

--- mesheryctl/internal/cli/root/system/restart.go
+++ mesheryctl/internal/cli/root/system/restart.go
@@ -75,7 +75,7 @@ func restart() error {
 	mctlCfg, err := config.GetMesheryCtl(viper.GetViper())
 	if err != nil {
 		utils.Log.Error(err)
-		return nil
+		return err
 	}
 	// get the platform, channel and the version of the current context
 	// if a temp context is set using the -c flag, use it as the current context
@@ -96,7 +96,7 @@ func restart() error {
 	running, err := utils.AreMesheryComponentsRunning(currPlatform)
 	if err != nil {
 		utils.Log.Error(err)
-		return nil
+		return err
 	}
 	if !running { // Meshery is not running
 		if err := start(); err != nil {
