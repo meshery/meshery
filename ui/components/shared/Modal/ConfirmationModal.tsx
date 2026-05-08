7879e0b9fed9ebce6c8daf35c6e1e2e2aec0b163
Utkarsh Maurya <deepraj5484@gmail.com>
2026-05-08 23:51:55 +0530

    fixed some more import paths
    Signed-off-by: Utkarsh Maurya <deepraj5484@gmail.com>

--- ui/components/shared/Modal/ConfirmationModal.tsx
+++ ui/components/shared/Modal/ConfirmationModal.tsx
@@ -18,24 +18,24 @@ import {
   useTheme,
   Search,
 } from '@sistent/sistent';
-import { errorHandlerGenerator, successHandlerGenerator } from '../utils/helpers/common';
+import { errorHandlerGenerator, successHandlerGenerator } from '../../../utils/helpers/common';
 import { useLazyPingKubernetesQuery } from '@/rtk-query/connection';
-import { getK8sConfigIdsFromK8sConfig } from '../utils/multi-ctx';
+import { getK8sConfigIdsFromK8sConfig } from '../../../utils/multi-ctx';
 import { useEffect, useState } from 'react';
-import { iconMedium, iconSmall } from '../css/icons.styles';
-import { RoundedTriangleShape } from './shapes/RoundedTriangle';
-import { notificationColors } from '../themes/app';
-import RedOctagonSvg from './shapes/Octagon';
-import PatternIcon from '../assets/icons/Pattern';
-import { useNotification } from '../utils/hooks/useNotification';
-import { EVENT_TYPES } from '../lib/event-types';
-import { K8sEmptyState } from './EmptyState/K8sContextEmptyState';
-import { ACTIONS } from '../utils/Enum';
-import CAN from '../utils/can';
+import { iconMedium, iconSmall } from '../../../css/icons.styles';
+import { RoundedTriangleShape } from '../../shapes/RoundedTriangle';
+import { notificationColors } from '../../../themes/app';
+import RedOctagonSvg from '../../shapes/Octagon';
+import PatternIcon from '../../../assets/icons/Pattern';
+import { useNotification } from '../../../utils/hooks/useNotification';
+import { EVENT_TYPES } from '../../../lib/event-types';
+import { K8sEmptyState } from '../../EmptyState/K8sContextEmptyState';
+import { ACTIONS } from '../../../utils/Enum';
+import CAN from '../../../utils/can';
 import { keys } from '@/utils/permission_constants';
-import { K8sContextConnectionChip } from './Header';
-import { useFilterK8sContexts } from './hooks/useKubernetesHook';
-import { TooltipWrappedConnectionChip } from './connections/ConnectionChip';
+import { K8sContextConnectionChip } from '../../Header';
+import { useFilterK8sContexts } from '../../hooks/useKubernetesHook';
+import { TooltipWrappedConnectionChip } from '../../connections/ConnectionChip';
 import { setK8sContexts, updateProgress } from '@/store/slices/mesheryUi';
 import { useDispatch, useSelector } from 'react-redux';
 
