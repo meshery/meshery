4e318d33992b26ee5b9436afbd068228767ee74d
Rishi Raj <rishiraj438gt@gmail.com>
2026-05-16 20:17:43 +0530

    [UI] Fix InfoOutlinedIcon import in MesheryTreeView
    Signed-off-by: Rishi Raj <rishiraj438gt@gmail.com>

--- ui/components/registry/MesheryTreeView.tsx
+++ ui/components/registry/MesheryTreeView.tsx
@@ -5,7 +5,7 @@ import {
   Switch,
   CircularProgress,
   Typography,
-  InfoOutlined as InfoOutlinedIcon,
+  InfoOutlined,
 } from '@sistent/sistent';
 import { MODELS, COMPONENTS, RELATIONSHIPS, REGISTRANTS } from '../../constants/navigator';
 import SearchBar from '@/utils/custom-search';
@@ -281,7 +281,7 @@ const MesheryTreeView = React.memo(
                     sx={{ margin: '0rem', padding: '0rem' }}
                   >
                     <IconButton>
-                      <InfoOutlinedIcon height={20} width={20} />
+                      <InfoOutlined height={20} width={20} />
                     </IconButton>
                   </CustomTextTooltip>
                 </>
