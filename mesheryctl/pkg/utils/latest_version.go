f7b94e6bfdeb4682fe6374ff03bc26ee99225b55
creo04 <creo572005@gmail.com>
2026-06-17 20:59:37 +0530

    "[mesheryctl] Add HTTP timeout to version check (#19223)"

--- mesheryctl/pkg/utils/latest_version.go
+++ mesheryctl/pkg/utils/latest_version.go
@@ -5,22 +5,23 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func GetLatestVersionForMesheryctl() (string, error) {
 	req, err := http.NewRequest(http.MethodGet, "https://docs.meshery.io/project/releases/latest", nil)
-
 	if err != nil {
 		return "", err
 	}
 
-	client := http.Client{}
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
 
 	resp, err := client.Do(req)
 	if err != nil {
 		return "", err
 	}
-
 	defer func() { _ = resp.Body.Close() }()
 
 	data, err := io.ReadAll(resp.Body)
@@ -34,7 +35,6 @@ func GetLatestVersionForMesheryctl() (string, error) {
 func CheckMesheryctlClientVersion(build string) {
 	Log.Info("Checking for latest version of mesheryctl...\n")
 
-	// Inform user of the latest release version
 	latestVersion, err := GetLatestVersionForMesheryctl()
 	if err != nil {
 		Log.Warn(fmt.Errorf("unable to check for latest version of mesheryctl. %s", err))
@@ -44,12 +44,11 @@ func CheckMesheryctlClientVersion(build string) {
 		Log.Warn(fmt.Errorf("unable to check for latest version of mesheryctl. %s", fmt.Errorf("no version found")))
 		return
 	}
-	// If user is running an outdated release, let them know.
 	if latestVersion != build {
-		Log.Infof("A new release of mesheryctl is available: %s → %s", build, latestVersion)
+		Log.Infof("A new release of mesheryctl is available: %s \u2192 %s", build, latestVersion)
 		Log.Info("https://docs.meshery.io/project/releases/latest")
 		Log.Info("Check https://docs.meshery.io/installation/upgrades#upgrading-meshery-cli for instructions on how to update mesheryctl\n")
-	} else { // If user is running the latest release, let them know.
+	} else {
 		Log.Info(latestVersion, " is the latest release.")
 	}
 }
