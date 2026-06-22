81f48b8d4ffb1eb902445768443b12af3aa8b0d7
KhushamBansal <kbkhushambansal@gmail.com>
2026-06-22 14:20:44 +0530

    fix: registry mobile view
    Signed-off-by: KhushamBansal <kbkhushambansal@gmail.com>

---
+++ ui/components/registry/MeshModelMobileDetails.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  IconButton,
+  CloseIcon,
+  alpha,
+  styled,
+} from '@sistent/sistent';
+import MeshModelDetails from './MeshModelDetails';
+
+const TitleSpan = styled('span')(() => ({
+  fontWeight: 600,
+  fontSize: '1rem',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+}));
+
+const ContentDiv = styled('div')(() => ({
+  width: '100%',
+}));
+
+const MeshModelMobileDetails = ({ showDetailsData, setShowDetailsData, view }) => {
+  return (
+    <Dialog
+      open={Object.keys(showDetailsData.data).length > 0 && showDetailsData.type !== 'none'}
+      onClose={() => setShowDetailsData({ type: '', data: {} })}
+      fullWidth
+      maxWidth="sm"
+      sx={{
+        zIndex: 1600,
+        '& .MuiDialog-container': {
+          alignItems: 'flex-end',
+        },
+        '& .MuiDialog-paper': {
+          margin: 0,
+          width: '100%',
+          maxWidth: '100%',
+          borderRadius: '12px 12px 0 0',
+          maxHeight: '80vh',
+        },
+      }}
+    >
+      <DialogTitle
+        sx={(theme) => ({
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: '0.75rem 1rem',
+          borderBottom: `1px solid ${alpha(theme.palette.common.white, 0.1)}`,
+        })}
+      >
+        <TitleSpan>{showDetailsData.data?.displayName || showDetailsData.type}</TitleSpan>
+        <IconButton
+          onClick={() => setShowDetailsData({ type: '', data: {} })}
+          size="small"
+          sx={{ flexShrink: 0 }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent sx={{ padding: '1rem', overflowY: 'auto' }}>
+        <ContentDiv>
+          <MeshModelDetails
+            view={view}
+            setShowDetailsData={setShowDetailsData}
+            showDetailsData={showDetailsData}
+          />
+        </ContentDiv>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default MeshModelMobileDetails;
