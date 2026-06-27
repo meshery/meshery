05dbb5eaf8b6e01466e6e4c72fd56b22f0f991f0
Ayush-kr-giga <kumarayushsharma2@gmail.com>
2026-06-27 23:38:23 +0530

    fix: use optional chaining for currentOrg.id (#20324)
    Signed-off-by: Ayush-kr-giga <kumarayushsharma2@gmail.com>

--- ui/components/dashboard/widgets/getting-started/index.tsx
+++ ui/components/dashboard/widgets/getting-started/index.tsx
@@ -24,7 +24,7 @@ const GetStarted = (props: { iconsProps?: object }) => {
     skip: !currentUser?.id,
   });
   const { organization: currentOrg } = useSelector((state) => state.ui);
-  const { id: org_id } = currentOrg ?? {};
+  const org_id = currentOrg?.id;
   return (
     <>
       <ActionButtonCard
