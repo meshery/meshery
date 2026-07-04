6fc67f2b7467986b619c48b112fe6a6401537b11
Priyanshu6968 <priyanshunigam469@gmail.com>
2026-07-04 21:20:50 +0530

    [Server] Fix ProviderTracker.Publish send on closed channel panic + snapshot ordering
    Signed-off-by: Priyanshu6968 <priyanshunigam469@gmail.com>

--- server/handlers/provider_handler.go
+++ server/handlers/provider_handler.go
@@ -104,7 +104,7 @@ func (h *Handler) ProvidersStreamHandler(w http.ResponseWriter, r *http.Request)
 	flusher.Flush()
 
 	ctx := r.Context()
-	events, unsubscribe := h.config.ProviderTracker.Subscribe(ctx)
+	events, unsubscribe := h.config.ProviderTracker.Subscribe()
 	defer unsubscribe()
 
 	// Refresh status on subscribe so a freshly-opened chooser shows live
