24c9f0ad6e0b3fbc3617130878769f0a92506024
Lavjeet Kumar Rai <railavjeet897@gmail.com>
2026-07-23 10:22:35 +0530

    test: fail fast on error type assertion mismatch
    Signed-off-by: Lavjeet Kumar Rai <railavjeet897@gmail.com>

--- server/helpers/error_test.go
+++ server/helpers/error_test.go
@@ -23,26 +23,26 @@ func TestErrInvalidK8SConfig(t *testing.T) {
 	expectedSuggestedRemediation := "Upload your kubernetes config via the settings dashboard. If uploaded, wait for a minute for it to get initialized"
 
 	var e *errors.Error
-	if stderrs.As(err, &e) {
-		if e.Code != expectedCode {
-			t.Errorf("Expected code %s, got %s", expectedCode, e.Code)
-		}
-		if len(e.ShortDescription) != 1 || e.ShortDescription[0] != expectedShortDesc {
-			t.Errorf("Expected ShortDescription to be '%s', got '%v'", expectedShortDesc, e.ShortDescription)
-		}
-		if len(e.LongDescription) != 1 || e.LongDescription[0] != expectedLongDesc {
-			t.Errorf("Expected LongDescription to be '%s', got '%v'", expectedLongDesc, e.LongDescription)
-		}
-		if len(e.ProbableCause) != 1 || e.ProbableCause[0] != expectedProbableCause {
-			t.Errorf("Expected ProbableCause to be '%s', got '%v'", expectedProbableCause, e.ProbableCause)
-		}
-		if len(e.SuggestedRemediation) != 1 || e.SuggestedRemediation[0] != expectedSuggestedRemediation {
-			t.Errorf("Expected SuggestedRemediation to be '%s', got '%v'", expectedSuggestedRemediation, e.SuggestedRemediation)
-		}
-		if e.Severity != errors.Alert {
-			t.Errorf("Expected Severity to be %v, got %v", errors.Alert, e.Severity)
-		}
-	} else {
-		t.Errorf("Expected error to be of type *errors.Error, got %T", err)
+	if !stderrs.As(err, &e) {
+		t.Fatalf("Expected error to be of type *errors.Error, got %T", err)
+	}
+
+	if e.Code != expectedCode {
+		t.Errorf("Expected code %s, got %s", expectedCode, e.Code)
+	}
+	if len(e.ShortDescription) != 1 || e.ShortDescription[0] != expectedShortDesc {
+		t.Errorf("Expected ShortDescription to be '%s', got '%v'", expectedShortDesc, e.ShortDescription)
+	}
+	if len(e.LongDescription) != 1 || e.LongDescription[0] != expectedLongDesc {
+		t.Errorf("Expected LongDescription to be '%s', got '%v'", expectedLongDesc, e.LongDescription)
+	}
+	if len(e.ProbableCause) != 1 || e.ProbableCause[0] != expectedProbableCause {
+		t.Errorf("Expected ProbableCause to be '%s', got '%v'", expectedProbableCause, e.ProbableCause)
+	}
+	if len(e.SuggestedRemediation) != 1 || e.SuggestedRemediation[0] != expectedSuggestedRemediation {
+		t.Errorf("Expected SuggestedRemediation to be '%s', got '%v'", expectedSuggestedRemediation, e.SuggestedRemediation)
+	}
+	if e.Severity != errors.Alert {
+		t.Errorf("Expected Severity to be %v, got %v", errors.Alert, e.Severity)
 	}
 }
