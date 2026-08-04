4361bea981ab39199cfda3df62c7c6a917885053
Piyush Tripathi <piyushtripathi@Mac.lan>
2026-08-05 00:15:38 +0530

    fix: improve dry run error label formatting
    Signed-off-by: Piyush Tripathi <piyushtripathi@Mac.lan>

--- ui/components/data-formatter/index.tsx
+++ ui/components/data-formatter/index.tsx
@@ -185,10 +185,19 @@ export const TextWithLinks = ({ text, ...typographyProps }) => {
   return <Typography {...typographyProps}> {elements}</Typography>;
 };
 
+export const formatLabel = (value) => {
+  if (typeof value !== 'string') return value;
+  return value
+    .replace(/([a-z0-9])([A-Z])/g, '$1 $2')
+    .replace(/([A-Z])([A-Z][a-z])/g, '$1 $2')
+    .replace(/([a-zA-Z])(\d)/g, '$1 $2')
+    .replace(/(\d)([a-zA-Z])/g, '$1 $2')
+    .replaceAll('_', ' ');
+};
+
 export const KeyValue = ({ Key, Value, style }) => {
   const theme = useTheme();
-  const formattedKey =
-    typeof Key === 'string' ? Key.replace(/([a-z])([A-Z])/g, '$1 $2').replaceAll('_', ' ') : Key;
+  const formattedKey = formatLabel(Key);
   return (
     <div
       style={{
@@ -230,10 +239,7 @@ export const SectionHeading = ({ children, isLevel, ...props }) => {
   const level = useContext(LevelContext);
   const fontSize = isLevel ? Math.max(0.9, 1.3 - 0.1 * level) + 'rem' : '1rem';
   const margin = isLevel ? Math.max(0.25, 0.55 - 0.15 * level) + 'rem' : 'inherit';
-  const formattedText =
-    typeof children === 'string'
-      ? children.replace(/([a-z])([A-Z])/g, '$1 $2').replaceAll('_', ' ')
-      : children;
+  const formattedText = formatLabel(children);
 
   return (
     <div style={{ marginBlock: margin }}>
