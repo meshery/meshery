f2be0d986d046082564dc4d4d15c01628e0f87d1
a-simptoad <71639383+a-simptoad@users.noreply.github.com>
2026-08-16 04:35:39 +0530

    refactor: move academy under experimental
    Signed-off-by: a-simptoad <71639383+a-simptoad@users.noreply.github.com>

--- mesheryctl/internal/cli/root/root.go
+++ mesheryctl/internal/cli/root/root.go
@@ -22,7 +22,6 @@ import (
 
 	mesheryctlflags "github.com/meshery/meshery/mesheryctl/internal/cli/pkg/flags"
 	mesheryctllogger "github.com/meshery/meshery/mesheryctl/internal/cli/pkg/logger"
-	"github.com/meshery/meshery/mesheryctl/internal/cli/root/academy"
 	"github.com/meshery/meshery/mesheryctl/internal/cli/root/adapter"
 	"github.com/meshery/meshery/mesheryctl/internal/cli/root/components"
 	"github.com/meshery/meshery/mesheryctl/internal/cli/root/config"
@@ -123,7 +122,6 @@ func init() {
 		system.SystemCmd,
 		design.DesignCmd,
 		perf.PerfCmd,
-		academy.AcademyCmd,
 		adapter.AdapterCmd,
 		experimental.ExpCmd,
 		filter.FilterCmd,
