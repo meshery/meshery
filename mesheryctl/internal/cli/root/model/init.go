0e794f715c64402075a0be1ad2d3c79694de1628
Gurkaran18 <gurkaranbirsinghsidhu@gmail.com>
2026-08-18 19:25:59 +0530

    Restore working documentation URL per maintainer review
    The /reference/mesheryctl/model/init path returns 404. Reverted to /reference/references/mesheryctl/model/init which correctly resolves to 200. This is the actual working URL for this command's reference page.
    
    Signed-off-by: Gurkaran18 <gurkaranbirsinghsidhu@gmail.com>

--- mesheryctl/internal/cli/root/model/init.go
+++ mesheryctl/internal/cli/root/model/init.go
@@ -276,7 +276,7 @@ $ mesheryctl model import {modelFolder}
 To export this model as OCI image:
 $ mesheryctl model build {modelName}/{modelVersion} --path {path}
 
-Detailed guide: https://docs.meshery.io/reference/mesheryctl/model/init`
+Detailed guide: https://docs.meshery.io/reference/references/mesheryctl/model/init`
 
 // TODO
 // initModelData fits well for json and yaml format
